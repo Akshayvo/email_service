5c1e182bd62298ad676300203940f89820acf78e
neha <neha.a@codeparva.in>
2018-11-26 14:02:49 +0530

    case-#16649-Automation Script for cloning New Project

--- scripts/createProject.sh
+++ scripts/createProject.sh
@@ -13,33 +13,42 @@ then
     path="$(pwd)"
 fi
 
-
 if [ -w $path ]
 
 then
     cd $path
     
-    git clone "https://syrasoft.kilnhg.com/Code/Apps/SMSv11/Websites.git"
+    git clone https://syrasoft.kilnhg.com/Code/Apps/SMSv11/Websites.git --branch develop --single-branch
 
     if [ $? -eq 0 ]
     then
 	 echo "Login successful"
 
-	 mv Websites $pName
+	 cd Websites
+
+	 mkdir $pName
+
+	 cp -a Universal\ Template/. $pName/
 
 	 ls
-	 
-	 cd $path/$pName
 
+	 cd $path/Websites/$pName
+	 
 	 npm install
 	 
-	 sed -i 's,storage-space-template,ng-starter-pack,g' package.json
+	 sed -i 's,ngStarterPack,'$pName',g' package.json
 
-	 sed -i  's,StorageSpaceTemplate,ng-starter-pack,g' angular.json
+	  sed -i 's,ng-starter-pack,'$pName',g' package.json
 
-	 sed -i  's,storage-space-template,ng-starter-pack,g' package-lock.json
+	 sed -i  's,ngStarterPack,'$pName',g' angular.json
 
-	 ng serve --port 3200 --open
+	 sed -i  's,ng-starter-pack,'$pName',g' package-lock.json
+
+	 npm run build:ssr
+
+	 npm run serve:ssr
+
+	 open localhost:5000
 
     else
 	echo "Login Fail"
