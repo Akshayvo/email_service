056f19e98d123bf2c26f73ef0f94703a84905694
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2018-12-17 14:13:06 +0530

    WIP - changed the requested data for view rates and home page

--- Drayton-Harbor/src/app/data/view-rates.ts
+++ Drayton-Harbor/src/app/data/view-rates.ts
@@ -5,8 +5,9 @@ export const dataViewRates = [
             tape, wrap and other supplies to make your move that much easier!`,
     },
     {
-        p: `We are an excellent solution for the business in need of that little extra space.
-            Our units and parking spaces can be accessed from 6:00AM to 11:00PM so you are free
-            to work around your schedule not just the standard business hours!`,
+        p: `We are an excellent solution for the business in need of that little extra space. Our units and parking spaces can
+            be accessed from 6:00AM to 11:00PM so you are free to work around your schedule,
+            not just standard business hours! Drayton Harbor Mini Storage is proud to serve Blaine, Bellingham, Custer,
+            and Fraser Valley areas!`,
     },
 ];
