7d0feec7a0b23f37216ca06f592a432ebbb40487
Sagar G <sagar.g@codeparva.in>
2018-12-20 03:54:52 -0800

    Renaming project

--- sample.sh
+++ sample.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo "Enter The Project Name:"
 read project_name
+output=$(echo "$project_name" | sed -r 's/([a-z0-9])([A-Z])/\1_\L\2/g')
+echo $output
+if [ -z ${output} ];then
+	echo 'empty'
+else
+	echo 'not empty'
+fi
+
 echo "enter the path to create the Project or press 'ENTER' to to select by defaultdirectory: "
 read dest_path
 
@@ -23,13 +31,10 @@ else
 	mkdir $dest_path/$project_name
       	echo "project created successfully"
 fi
-
 #source=/home/sagar/web_scripts/Victor/
 source=/home/sagar/web_scripts/Universal_Template/
 #source="/home/sagar/web_scripts/Universal Template"
 
-
-
 rsync -av --progress $source $dest_path/$project_name --exclude dist --exclude node_modules
-echo completed
-
+echo sed -i "s,ng-starter-pack,$output,g;s,ngStarterPack,$project_name,g" $dest_path/$project_name/angular.json
+sed -i "s,ng-starter-pack,${output},g;s,ngStarterPack,$project_name,g" $dest_path/$project_name/package.json
