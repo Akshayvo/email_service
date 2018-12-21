0d1f0cc1b4673c48c41fb71bb9a1648a23bb5aa7
neha <neha.a@codeparva.in>
2018-12-21 14:12:12 +0530

    Gallery images tsline 120 char rule is disabled.

--- security-storage/src/app/data/galleryImage.ts
+++ security-storage/src/app/data/galleryImage.ts
@@ -2,6 +2,7 @@ export const gallery = [
     {
         class: `active`,
         id: `1`,
+        // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Images/Security-Storage/security-storage-cuero-climate-control-building.jpg`,
     },
     {
@@ -10,22 +11,27 @@ export const gallery = [
     },
     {
         id: `3`,
+        // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Images/Security-Storage/security-storage-cuero-climate-controlled.jpg`,
     },
     {
         id: `4`,
+        // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Images/Security-Storage/security-storage-cuero-drive-up-indoor-units.jpg`,
     },
     {
         id: `5`,
+        // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Images/Security-Storage/security-storage-cuero-indoor-climate-controlled.jpg`,
     },
     {
         id: `6`,
+        // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Images/Security-Storage/security-storage-cuero-sign.jpg`,
     },
     {
         id: `7`,
+        // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Images/Security-Storage/security-storage-cuero-units.jpg`,
     },
 ];
