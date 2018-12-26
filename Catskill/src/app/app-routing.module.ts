d1ba059ef5fd699e523e772b0feca9d984884587
unknown <ref>
2018-12-26 12:26:45 +0530

    Merging Catskill

---
+++ Catskill/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReserveComponent } from './reserve/reserve.component';
+import { ContactComponent } from './contact/contact.component';
+import { ViewRatesComponent } from './view-rates/view-rates.component';
+import { PaymentComponent } from './payment/payment.component';
+import { StorageTipsComponent } from './storage-tips/storage-tips.component';
+import { ErrorComponent } from './error/error.component';
+
+const routes: Routes = [
+  // Fallback when no prior route is matched
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'reserve', component: ReserveComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'view-rates', component: ViewRatesComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'storage-tips', component: StorageTipsComponent },
+  { path: '**', component: ErrorComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
