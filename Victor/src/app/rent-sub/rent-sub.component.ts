0d3e36f9d752df5b74909f5d1e5596a0c42a8aab
neha <neha.a@codeparva.in>
2018-12-31 12:00:47 +0530

    Victor routing issue resolved

--- Victor/src/app/rent-sub/rent-sub.component.ts
+++ Victor/src/app/rent-sub/rent-sub.component.ts
@@ -8,13 +8,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RentSubComponent implements OnInit {
   tabId: any;
+  name: any;
+  currentTab; any;
   private sub: any;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
       this.tabId = params['id'];
+      this.currentTab = params['currentTab'];
     });
   }
 }
