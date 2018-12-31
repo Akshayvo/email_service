517ea3ff51b9bc09c9949ebb7273d53e92cbbc8b
Vinay G <vinay.g@codeparva.in>
2018-12-31 14:21:02 +0530

    Added Lockwood new

---
+++ Lockwood/src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
+import { contact, hours } from '../data/contact';
+import { featuresList, aboutUs, gettingStarted, feature, blurbHeading, blurbText } from '../data/home';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  contactDetails: any;
+  hours: any;
+  featuresHead: any;
+  featuresList: any;
+  aboutUs: any;
+  serviceOffered: any;
+  gettingStarted: any;
+  feature: any;
+  blurbHeading: string;
+  blurbText: string;
+
+  constructor(
+    private router: Router,
+    private titleService: Title,
+    private meta: Meta
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: `We offer high quality self storage units, vehicle storage,
+                and a tenant-use truck to Oviedo and nearby communities!`
+    });
+    this.titleService.setTitle('Storage Units Near Oviedo, FL | Lockwood Self Storage');
+  }
+
+  public navigate(location: any) {
+    this.router.navigate([location]);
+  }
+
+  ngOnInit() {
+    this.fetchContactDetails();
+    this.fetchHours();
+    this.fetchFeatures();
+    this.fetchStaticContent();
+    this.fetchFeature();
+    this.fetchblurbData();
+    window.scrollTo(0, 0);
+  }
+
+  public fetchblurbData() {
+    this.blurbText = blurbText;
+    this.blurbHeading = blurbHeading;
+
+  }
+
+  public fetchContactDetails() {
+    this.contactDetails = contact;
+  }
+
+  public fetchHours() {
+    this.hours = hours;
+  }
+
+  public fetchFeatures() {
+    this.featuresList = featuresList;
+  }
+
+  public fetchStaticContent() {
+    this.aboutUs = aboutUs;
+    this.gettingStarted = gettingStarted;
+  }
+  public fetchFeature () {
+    this.feature = feature;
+  }
+
+}
---
+++ Lockwood/src/app/view-rates/view-rates.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { viewrates } from '../data/view-rates';
+@Component({
+  selector: 'app-view-rates',
+  templateUrl: './view-rates.component.html',
+  styleUrls: ['./view-rates.component.scss']
+})
+export class ViewRatesComponent implements OnInit {
+  viewRates: any;
+
+  constructor(
+    private titleService: Title,
+    private meta: Meta
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: `Find prices for Oviedo's only privately-owned self storage!
+                Be sure to ask about our Community Champion's discount!`
+    });
+    this.titleService.setTitle('Storage Units Near Oviedo, FL | Lockwood Self Storage');
+  }
+
+  ngOnInit() {
+    this.fetchViewRates();
+    window.scrollTo(0, 0);
+  }
+
+  public fetchViewRates() {
+    this.viewRates = viewrates;
+  }
+}
