f201e6776223ec426dab7b957c1d7e623a4b8674
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-01-01 17:15:50 +0530

    WIP - drayton completed

--- Drayton-Harbor/src/app/pay-rent/pay-rent.component.spec.ts
+++ Drayton-Harbor/src/app/pay-rent/pay-rent.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PaymentComponent } from './payment.component';
+import { PayRentComponent } from './pay-rent.component';
 
-describe('PaymentComponent', () => {
-  let component: PaymentComponent;
-  let fixture: ComponentFixture<PaymentComponent>;
+describe('PayRentComponent', () => {
+  let component: PayRentComponent;
+  let fixture: ComponentFixture<PayRentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PaymentComponent ]
+      declarations: [ PayRentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PaymentComponent);
+    fixture = TestBed.createComponent(PayRentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
