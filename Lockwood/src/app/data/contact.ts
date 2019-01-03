9d98ec750c671e0526b97887e6f3c1a6ef666ce2
Vinay G <vinay.g@codeparva.in>
2019-01-03 15:49:22 +0530

    Merged changes of Lockwood fixes

--- Lockwood/src/app/data/contact.ts
+++ Lockwood/src/app/data/contact.ts
@@ -9,7 +9,7 @@ export const contact = [
         id: 1,
         redirect: 'mailto:mmanager@lockwood-storage.com',
         label: 'Email:',
-        data: 'mmanager@lockwood-storage.com',
+        data: 'manager@lockwood-storage.com',
         class: 'fas fa-envelope mx-2 iconColor',
     },
     {
