85108914caedae84b89b05a2f8a9cd8f99aedb01
Vinay G <vinay.g@codeparva.in>
2019-01-03 16:32:20 +0530

    Added Dark THeme

---
+++ Dark-Theme/src/app/contact/contact.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { WINDOW } from '@ng-toolkit/universal';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { contact, hours } from '../data/contact';
+import { EmailService } from '../services/email.service';
+import { contactUs } from '../data/blurb';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  currentActive: any = 'CONTACT';
+  contactDetails: any;
+  hours: any;
+  name: string;
+  email: any;
+  phone: any;
+  subject: string;
+  message: string;
+  receiveremail: string;
+  completeMessage: string;
+  contactForm: FormGroup;
+  submitted = false;
+  isSubmitted = false;
+  contactUs: any;
+
+  constructor(
+    @Inject(WINDOW) private window: Window,
+    private emailService: EmailService,
+    private titleService: Title,
+    private meta: Meta,
+    private formBuilder: FormBuilder,
+  ) {
+    this.meta.addTag({
+      name: 'contact-meta-name',
+      content: `contact-meta-content`
+    });
+    this.titleService.setTitle('contact-PageTitle');
+  }
+
+  ngOnInit() {
+    this.fetchContactDetails();
+    this.fetchContactUs();
+    this.fetchHours();
+    this.window.scrollTo(0, 0);
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      phone: ['', [Validators.required,
+              Validators.pattern('^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,5}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      subject: [''],
+  });
+  }
+
+  get f() { return this.contactForm.controls; }
+
+  public fetchContactDetails() {
+    this.contactDetails = contact;
+  }
+
+  public fetchHours() {
+    this.hours = hours;
+  }
+
+  public fetchContactUs() {
+    this.contactUs = contactUs;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+   // stop here if form is
+   if (this.contactForm.invalid) {
+       return;
+   } else {
+     this.isSubmitted = true;
+     this.receiveremail = this.contactDetails[1].data;
+
+         this.completeMessage = `<strong>Phone: </strong> ${this.contactForm.value.phone}, <br/>
+                                <strong>Message: </strong> ${this.contactForm.value.message}`;
+
+         if (!this.contactForm.value.subject) {
+          this.contactForm.value.subject = 'Website Contact Form';
+         }
+
+         const body = {
+           name: this.contactForm.value.name,
+           email: this.contactForm.value.email,
+           receiveremail: this.receiveremail,
+           message: this.completeMessage,
+           subject: this.contactForm.value.subject
+         };
+         this.emailService.sendEmail(body)
+           .subscribe((response: any) => {
+             if (response.result != null) {
+
+             } else {
+
+             }
+           }, (err) => {
+
+           });
+         this.submitted = false;
+         this.contactForm.reset();
+   }
+ }
+}
