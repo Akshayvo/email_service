1cc18ffabd8e96b780d51ae4ef0344920ae1d8fd
neha <neha.a@codeparva.in>
2019-01-09 14:23:48 +0530

    Cuba Website added

---
+++ Cuba-Website/src/app/data/contact.ts
@@ -0,0 +1,49 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Address:',
+        data: '711 S. Franklin St.  Cuba  MO   65453',
+        class: 'fas fa-map-marker-alt mr-2 iconColor',
+    },
+    {
+        id: 1,
+        redirect: 'mailto:cubaselfstorage@gmail.com',
+        label: 'Email:',
+        data: 'cubaselfstorage@gmail.com',
+        class: 'fas fa-envelope mr-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:5738852539',
+        label: 'Phone:',
+        data: '(573)885 2539',
+        class: 'fas fa-phone mr-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday - Friday: ',
+        data: '9:00AM - 5:00PM'
+    },
+    {
+        label: 'Saturday: ',
+        data: '9:00AM - 1:00PM'
+    },
+    {
+        label: 'Sunday: ',
+        data: 'Closed'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '24/7'
+    },
+];
+
+export const socialLinks = [
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/pages/Cuba-Self-Storage/1628393900708444',
+        class: 'fab fa-facebook-square',
+    },
+];
---
+++ Cuba-Website/src/app/data/unitSizer.ts
@@ -0,0 +1,55 @@
+export const unitSizer = [
+    {
+        size: '5\' x 10\' General Storage',
+        img: '../../assets/images/UnitSizer/5x10.png',
+        data: `This unit is approximately 50 sq feet of space that can hold
+                the contents of one small room or large closet, such as: `,
+        amenities: [
+            { content: 'chairs', },
+            { content: 'small desks', },
+            { content: 'small bookcases', },
+            { content: 'suitcases', },
+            { content: 'lamps', },
+        ],
+    },
+
+    {
+        size: '8\' x 10\' General Storage',
+        img: '../../assets/images/UnitSizer/10x10.png',
+        data: `This unit is approximately 80 sq feet of space that can hold
+                the contents of one small room or large closet, such as: `,
+        amenities: [
+                    { content: 'mattress sets', },
+                    { content: 'sofas', },
+                    { content: 'bookcases', },
+                    { content: 'dining room sets', },
+                    { content: 'lamps', },
+        ],
+    },
+    {
+        size: '8\' x 15\' General Storage',
+        img: '../../assets/images/UnitSizer/10x15.png',
+        data: `This unit is approximately 120 sq feet of space that can hold
+                the contents of one small room or large closet, such as: `,
+        amenities: [
+            { content: 'mattress sets', },
+            { content: 'armchairs', },
+            { content: 'bookcases', },
+            { content: 'sofas', },
+            { content: 'dining room sets', },
+        ],
+    },
+    {
+        size: '8\' x 20\' General Storage',
+        img: '../../assets/images/UnitSizer/10x15.png',
+        data: `This unit is approximately 160 sq feet of space that can hold
+                the contents of one small room or large closet, such as: `,
+        amenities: [
+            { content: 'mattress sets', },
+            { content: 'armchairs', },
+            { content: 'bookcases', },
+            { content: 'sofas', },
+            { content: 'dining room sets', },
+        ],
+    },
+];
