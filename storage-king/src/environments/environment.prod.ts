1f3bbf4c0eaed54be59a6c3e22b747b9eac3cd3b
neha <neha.a@codeparva.in>
2019-01-14 16:20:10 +0530

    case: 17130 data updated

--- storage-king/src/environments/environment.prod.ts
+++ storage-king/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  s3_base_url: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites',
-  s3_powered_by_url: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby',
+    s3_base_url: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Storage_King_Images',
+    s3_powered_by_url: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby',
 };
