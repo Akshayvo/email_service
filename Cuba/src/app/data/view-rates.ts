b79c0b8a95f23028edfdf11d3932dcc6bab752c1
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-01-15 15:41:40 +0530

    fixed tslint

--- Cuba/src/app/data/view-rates.ts
+++ Cuba/src/app/data/view-rates.ts
@@ -1,13 +1,12 @@
-// tslint:disable:indent
 export const viewRates = [
-	{
-		p: `Cuba Self Storage serves both residential and commercial storage needs in Cuba,
+  {
+    p: `Cuba Self Storage serves both residential and commercial storage needs in Cuba,
 		Steelville and surrounding communities! Whether you're a business that just needs
 		a little extra room for equipment and supplies or a homeowner preparing for a move we've got you covered! `,
-	},
-	{
-		p: `Our manager is on-site daily and ready to answer questions or help you move in to your well maintained,
+  },
+  {
+    p: `Our manager is on-site daily and ready to answer questions or help you move in to your well maintained,
 		clean storage unit. Our facility is also fenced, well lit and has security cameras to help protect
 		you and your belongings! Call or reserve your unit online today!`,
-	}
+  }
 ];
