f72cde3ecf4b9d1f75739a289a084f726076be8c
neha <neha.a@codeparva.in>
2019-02-01 11:32:32 +0530

    Case 17168 Lockwood review redirect link created and home url updated

--- Lockwood/src/app/app-routing.module.ts
+++ Lockwood/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ViewRatesComponent } from './view-rates/view-rates.component';
@@ -9,12 +9,19 @@ import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   // Fallback when no prior route is matched
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'view-rates', component: ViewRatesComponent },
   { path: 'payment', component: PaymentComponent },
   { path: 'storage-tips', component: StorageTipsComponent },
+  { path: 'review', component: HomeComponent,
+    resolve: {
+        url: 'externalUrlRedirectResolver'
+    },
+    data: {
+        externalUrl: 'https://search.google.com/local/writereview?placeid=ChIJx7PpHPRp54gR4TqqcKFi2Io'
+    }
+  },
   { path: '**', component: ErrorComponent }
 ];
 
