983d433ce1b2d3db77fc0cacbe433a290a2bfc29
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-02-08 16:58:46 +0530

    deleted unnecessary files

--- stoner-trail/src/app/data/view-rates.ts
+++
@@ -1,7 +0,0 @@
-export const viewRates = [
-  {
-    p: `Our friendly team of self storage experts are available to help you find a unit that suits your needs!
-		Simply call (518) 762-7867 or reserve your unit today and one of our helpful staff will reach out and answer
-		any questions you may have!`,
-  }
-];
