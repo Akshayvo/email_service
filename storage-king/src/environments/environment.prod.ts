a06569c4e76d730dedaa96db0ab50d43f193376f
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-03-18 11:12:39 +0530

    updated environments

--- storage-king/src/environments/environment.prod.ts
+++ storage-king/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
     s3_base_url: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Storage_King_Images',
     s3_powered_by_url: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby',
-    errorsMails: ['sanghmitra.v@codeparva.in'],
+    errorsMails: ['vinay.g@codeparva.in', 'preetha.ts@codeparva.in'],
     senderEmail: 'errors@codeparva.in',
     appId: 'StorageKing'
 };
