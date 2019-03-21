580e06a92ca5a3c7df532f36e6951387ff79ace7
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-03-21 17:28:34 +0530

    initial commit

---
+++ platinum/src/app/data/view-rates.ts
@@ -0,0 +1,12 @@
+export const dataViewRates = [
+    {
+        p: `Platinum Storage was founded in 2019 and offer brand new, well-lit and fully-fenced in self storage
+        units with paved parking and security camera coverage! Our on-site manager is available to assist
+        tenants with questions and transactions while providing an extra layer of security for your belongings!
+        `,
+    },
+    {
+        p: `We look forward to meeting you and providing you with high quality self storage and stellar
+        customer service!`,
+    },
+];
---
+++ platinum/src/app/view-rates/view-rates.component.ts
@@ -0,0 +1,37 @@
+import { WINDOW } from '@ng-toolkit/universal';
+import { Component, OnInit , Inject} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { dataViewRates } from '../data/view-rates';
+
+@Component({
+  selector: 'app-view-rates',
+  templateUrl: './view-rates.component.html',
+  styleUrls: ['./view-rates.component.scss']
+})
+export class ViewRatesComponent implements OnInit {
+
+  currentActive: any = 'VIEW RATES';
+  dataViewRates: any;
+
+  constructor(
+    @Inject(WINDOW) private window: Window,
+    private titleService: Title,
+    private meta: Meta
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: `View our available inventory and rates then make your reservation with
+      our easy online reservation form!`
+    });
+    this.titleService.setTitle('Storage Units in Summerville, SC | Platinum Storage');
+  }
+
+  ngOnInit() {
+    this.window.scrollTo(0, 0);
+    this.viewRates();
+  }
+
+  public viewRates() {
+    this.dataViewRates = dataViewRates;
+  }
+}
