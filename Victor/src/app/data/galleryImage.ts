108f9817e975f38e70dfdebac10390bfb8719ae2
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-03-25 11:26:07 +0530

    updated data for gallery and home page

--- Victor/src/app/data/galleryImage.ts
+++ Victor/src/app/data/galleryImage.ts
@@ -47,16 +47,14 @@ export const galleryLocation2 = [
 ];
 
 export const galleryDataLocation1 = [
-    { p: `Our indoor units are accessible via oversized elevator for a fast, and easy moving experience!
-          Our wide selection of convenient unit sizes ensures every storage need under the sun is covered!
-          Need boxes, tape or other moving supplies? Stop into our full retail store during office hours!
-          Our goal is to provide the highest quality customer service and self storage experience!` }
+    { p: `Our indoor units are accessible via an oversized elevator for a fast, and easy moving experience! Our wide selection
+    of convenient unit sizes ensures every storage need under the sun is covered! Need boxes, tape, or other moving supplies? Stop
+    into our full retail store during office hours! Our goal is to provide the highest quality customer service and self storage experience!` }
 ];
 
 export const galleryDataLocation2 = [
-    { p: `We offer 24/7 closed circuit video recording, and
-          our resident manager performs regular lot checks to ensure your units are undisturbed.
-          You’ll feel safe, and secure with motion activated lighting, and brightly lit interior hallways.
-          We even offer clean, well maintained private restrooms, for the comfort and convenience of our customers.
-          Our first goal is your complete satisfaction during your stay! Call Victor Self Storage today!`, }
+    { p: `We offer 24/7 closed-circuit video recording, and our resident manager performs regular lot checks to ensure your units are undisturbed.
+    You’ll feel safe, and secure with motion activated lighting, and brightly lit interior hallways. We even offer clean,
+    well maintained private restrooms for the comfort and convenience of our customers. Our first goal is your complete
+    satisfaction during your stay! Call Victor Self Storage today!`, }
 ];
--- Victor/src/app/data/home.ts
+++ Victor/src/app/data/home.ts
@@ -20,17 +20,14 @@ export const featuresHead = [
 
 export const serviceOffered = [
     {
-        p: `Victor Self Storage has served Victor, Mendon, Farmington,
-            and the surrounding communities with high quality, affordable units in a wide variety of convenient sizes.
-            Our full perimeter fence, 24/7 recording video cameras, unique access codes, and theft-proof cylinder locks,
-            are just a few of the many features we have in place to protect your belongings.`,
+        p: `Victor Self Storage has served Victor, Mendon, Farmington, and the surrounding communities with high quality,
+        affordable units in a wide variety of convenient sizes. Our full perimeter fence, 24/7 recording video cameras,
+        unique access codes, and theft-proof cylinder locks, are just a few of the many features we have in place to protect your belongings.`,
     },
     {
-        p: `Both the Village, and Mall locations offer 24/7 access, motion activated interior lights,
-            a well stocked retail store with all of the tape, boxes,
-            and you will need to make your moving experience a successful one!
-            Our full time, on-site staff maintain the properties to provide both a clean experience,
-            and an extra layer of security for your benefit.`,
+        p: `Both the Village, and Mall locations offer 24/7 access, motion activated interior lights, a well-stocked retail store
+        with all of the tape, boxes, and supplies you will need to make your moving experience a successful one! Our full time,
+        on-site staff maintain the properties to provide both a clean experience and an extra layer of security for your benefit.`,
     },
 ];
 
