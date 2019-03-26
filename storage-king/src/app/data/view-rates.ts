fda7e52c4c41b9436d68cb45012442de29cf767a
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-03-26 10:32:33 +0530

    updated chnages

--- storage-king/src/app/data/view-rates.ts
+++ storage-king/src/app/data/view-rates.ts
@@ -1,11 +1,13 @@
 export const viewrates = [
     {
+        id: 1,
         p: `Offering a variety of units! Choices include: climate, non-climate,
         dehumidified, drive up access, Complete storage solutions to meet your needs.
         You choose which one is right for you.
         `,
     },
     {
+        id: 2,
         p: `Still not sure of what size or type you would like or would best fit your needs?
         Visit or call our local facility then decide which unit would be the best solution for you.`,
     }
