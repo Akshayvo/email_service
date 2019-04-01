6da8dd8c08ef6a51c0e999ab53f0558655a3875b
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-04-01 14:37:04 +0530

    created new brnach

--- Catskill/src/app/data/view-rates.ts
+++
@@ -1,8 +0,0 @@
-export const viewRates = [
-	{
-		p: `Catskill Self Storage offers many amenities to make your storage experience
-		a fast, easy, and hassle-free one! Located conveniently off of Route 87 at Exit 21 just west of downtown Leeds,
-		we are situated in a perfect spot for both residential and commercial storage.
-		Ask about out online payments and EZ Auto-Pay option!`,
-	  }
-];
