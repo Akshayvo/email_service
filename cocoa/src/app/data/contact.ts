bb2c9563425273ccedb760850c0753d332ed748d
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-04-10 12:06:26 +0530

    space between label and data in contact.ts file

--- cocoa/src/app/data/contact.ts
+++ cocoa/src/app/data/contact.ts
@@ -1,21 +1,21 @@
 export const contact = [
     {
         id: 0,
-        label: 'Facility Address:',
+        label: 'Facility Address: ',
         data: '4909 N US 1 Cocoa, FL  32927',
         class: 'fas fa-map-marker-alt mr-2 iconColor',
     },
     {
         id: 1,
         redirect: 'mailto:allspacestorage@aol.com',
-        label: 'Email:',
+        label: 'Email: ',
         data: 'allspacestorage@aol.com',
         class: 'fas fa-envelope mr-2 iconColor',
     },
     {
         id: 2,
         redirect: 'tel:3216398576',
-        label: 'Phone:',
+        label: 'Phone: ',
         data: '(321) 639-8576',
         class: 'fas fa-phone mr-2 iconColor',
     }
@@ -27,7 +27,7 @@ export const hours = [
         data: '8:00AM to 5:00PM'
     },
     {
-        label: 'Sunday',
+        label: 'Sunday: ',
         data: 'CLOSED'
     },
     {
