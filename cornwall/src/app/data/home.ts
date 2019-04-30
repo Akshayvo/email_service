017ebda9d054067f2b3dcd4c28e11b0243c54789
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-04-30 16:44:02 +0530

    about us and header chnaged

--- cornwall/src/app/data/home.ts
+++ cornwall/src/app/data/home.ts
@@ -50,24 +50,27 @@ export const feature = [
 
 export const aboutUs = [
     {
-        p: `Cornwall Storage Box is Cornwall's newest storage facility. Our brand new, well-list indoor storage facility
-        is conveniently located right in downtown Cornwall and our units are heated to defend your belongings from the
-        elements! Our facility is just a stone's throw away from Water Street Condos, The Cotton Mill Condos, and the
-        Knox City Center.`,
+        p: `Cornwall Storage Box is Cornwall's newest storage facility. Our brand new, well-lit indoor storage
+        facility is conveniently located right in Downtown Cornwall and our units are heated to defend your
+        belongings from the elements! Our facility is just a stone's throw away from Water Street Condos, The
+        Cotton Mill Condos, and the Knox City Center.`,
     },
     {
-        p: `Whether you're planning a move or just need to make some elbow room at home, if you live or work near
-        downtown Cornwall, our wide selection of unit sizes is guaranteed to have the perfect unit for you!.`,
+        p: `Whether you're planning a move or just need to make some elbow room at home, if you live or work
+        near downtown Cornwall, our wide selection of unit sizes is guaranteed to have the perfect unit for you!.`,
     },
     {
-        p: `Our heated, indoor units are the ideal solution for medical, legal, or accounting records that require
-        stable temperatures for storage. We cater to a number of legal and accounting firms seeking long term
-        document storage.`,
+        p: `Our heated, indoor units are the ideal solution for medical, legal, or accounting records that
+        require stable temperatures for storage. We cater to a number of legal and accounting firms seeking
+        long term document storage.`,
     },
     {
         p: `At Cornwall Storage Box, we take pride in our customer service, which is why we offer convenient office
         hours, 24-hour access to your units, and an easy online tenant portal. You can reserve a unit, check your
         balance, or pay your rent online, 24 hours a day!`,
     },
+    {
+        p: `Our second and third phases of construction are in the works now! `
+    }
 ];
 
