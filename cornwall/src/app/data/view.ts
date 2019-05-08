54ff12a56afe1587453f70eb208a684d2d834a0b
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-05-08 09:58:13 +0530

    view rates paragraph change

--- cornwall/src/app/data/view.ts
+++ cornwall/src/app/data/view.ts
@@ -2,13 +2,14 @@ export const viewRate = [
     {
         p: `We proudly serve residential and business customers from Cornwall, Lancaster, Martintown, and the
         surrounding communities with affordable, heated self storage services and excellent customer service.
-        Our convenient location in downtown Cornwall makes us an especially great choice for residents living in
-        Ultra Vista Towers, Adolphus Court Apartments, and Sunset Tower Apartments.`,
+        Our convenient location in Downtown Cornwall makes us an especially great choice for residents living
+        in Ultra Vista Towers, Adolphus Court Apartments, and Sunset Tower Apartments. We are also just around
+        the corner from Water Street Condos, The Cotton Mill Condos, and the Knox City Center.`,
     },
     {
-        p: `Our business customers enjoy the convenience of 24/7 access to their equipment, supplies, and document
-        storage. It doesn't matter what your operating hours are, unlike many self storage facilities with limited
-        access hours, your unit is always available!`,
+        p: `Our business customers enjoy the convenience of 24/7 access to their equipment, supplies, and
+        document storage. It doesn't matter what your operating hours are, unlike many self storage facilities
+        with limited access hours, your unit is always available!`,
     },
 ];
 
