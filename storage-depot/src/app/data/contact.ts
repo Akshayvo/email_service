e1dcd7458892164dc359cded64f8e9d07f42e2d8
Sagar <sagar.g@codeparva.in>
2019-05-10 16:47:24 +0530

    storage-depot website

---
+++ storage-depot/src/app/data/contact.ts
@@ -0,0 +1,46 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Facility Address:',
+        data: '275 Manchester Rd, Poughkeepsie, NY, 12603',
+        class: 'fas fa-map-marker-alt mr-2 iconColor',
+    },
+    
+    {
+        id: 1,
+        redirect: 'mailto:storagedepotny@verizon.net',
+        label: 'Email:',
+        data: 'storagedepotny@verizon.net',
+        class: 'fas fa-envelope mr-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:845473-2848',
+        label: 'Phone:',
+        data: '(845) 473-2848',
+        class: 'fas fa-phone mr-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday-Friday: ',
+        data: '9:00AM to 5:00PM'
+    },
+    {
+        label: 'Saturday-Sunday: ',
+        data: 'CLOSED'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '7 Days a Week 6:00AM-11:00PM'
+    },
+];
+
+export const socialLinks = [
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/Storage-Depot-141280683061330/',
+        class: 'fab fa-facebook-square',
+    },  
+];
---
+++ storage-depot/src/app/data/home.ts
@@ -0,0 +1,61 @@
+export const featuresHead = 'The Only Self Storage in Greene County With All of The Following';
+
+
+
+export const feature = [
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Storage_Depot/stellar.svg',
+        label: 'Stellar Customer Service'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Storage_Depot/thermometer.svg',
+        label: 'Climate Controlled Units Available'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Storage_Depot/cctv.svg',
+        label: 'Security Cameras on Premises'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Storage_Depot/hand.svg',
+        label: 'Affordable Self Storage Rates'
+    },
+];
+
+
+export const aboutUs = [
+    {
+        p: `The Storage Depot has been serving Poughkeepsie, Arlington, Red Oaks Mill, and other communities in Dutchess
+            and Ulster Counties with quality, affordable self storage since 2005. `,
+    },
+    {
+        p: `We offer regular self storage units and dedicated single level climate controlled units to protect your belongings
+            from the elements while making loading and unloading easier! `,
+    },
+    {
+        p: `Do you have extra bulky items or are you looking for convenient unloading? Use our drive-thru loading area, or ask about
+            the available loading docks! Businesses may also sign up to have our staff accept packages for a small fee.`,
+    },
+    {
+        p: `We are also only a few miles from Vassar College, Marist College, Dutchess Community College and the Culinary Institute of America.
+            College students love our reasonable rates and convenient access hours!`
+    }
+];
+
+
+
+
+
+export const gettingStarted = `Are you ready to start de-cluttering your home or workplace? Make that call or fill out a reservation form today!`;
+
+export const blurb = [
+    {
+        h1: `Affordable & Convenient Self Storage Units`,
+        p: `The Storage Depot has been serving Dutchess and Ulster Counties with high-quality, well-maintained
+            self storage services, and stellar customer service since 2005! Whether you're a residential user 
+            looking for that extra room around the home or the owner of a growing business, we've got the perfect unit for you!`,
+    }
+];
