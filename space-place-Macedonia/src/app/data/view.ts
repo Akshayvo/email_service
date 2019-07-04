82296def98732682f22274df4db4cbb199ce13e5
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-07-04 11:43:10 +0530

    case 18237 - changes updated

--- space-place-Macedonia/src/app/data/view.ts
+++ space-place-Macedonia/src/app/data/view.ts
@@ -1,12 +1,12 @@
 export const viewRate = [
     {
-        p: `Space Place Macedonia has been serving Macedonia, Twinsburg, Sagamore Hills, and the Oakland Village
-        communities for over thirty five years. That success is driven by our commitment to quality self storage
+        p: `Space Place Macedonia has been serving Macedonia, Twinsburg, Sagamore Hills, and the Oakwood Village
+        communities for over thirty-five years. That success is driven by our commitment to quality self storage
         and excellent customer service!`,
     },
     {
-        p: `We offer both short and long-term leasing terms for our valued customers, so whether you're in need
-        of some extra space for a month or seasonal storage, we've got a deal for you! Contact one of our friendly
+        p: `We offer both short and long-term leasing terms for our valued customers, so whether you're in need of
+        some extra space for a month or seasonal storage, we've got a deal for you! Contact one of our friendly
         self storage specialists today!`,
     },
 ];
