370302ca12a6bf5825946f174643bd43a31984ba
Sagar <sagar.g@codeparva.in>
2019-07-10 17:12:27 +0530

    api call

--- login/login/src/main/login.component.js
+++ login/login/src/main/login.component.js
@@ -3,13 +3,41 @@ import './login.style.scss';
 import { withRouter } from 'react-router-dom';
 
 class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            items:[],
+            // isLoaded: false,
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                // isLoaded: true,
+                items: json,
+            })
+        })
+    }
 handle = () => {
+var { isLoaded,items } = this.state;
   this.props.history.push('/domain');
+    {items.map(item =>(
+    <li key={item.id}>
+        {item.name}
+    </li>
+))}
 {/* <Redirect to="/domain" /> */}
 };
 
 
 render(){
+    var { isLoaded,items } = this.state;
+    // if (!isLoaded) {
+    //     return <div> Loading...</div>
+    // }
+    // else {
     return(
         <div className="container">
             <div className="row">
@@ -26,11 +54,19 @@ render(){
                             <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                         </div>  
                         <button  onClick={this.handle}>Submit</button>
+                        <ul>
+                            {items.map(item =>(
+                                <li key={item.id}>
+                                    {item.name}
+                                </li>
+                            ))}
+                        </ul>
                     </form>
                 </div>
             </div>     
         </div>
     )
+    // }
 }
 }
 
