185a85980848596b8f5e3355ef3dfe9c94689fa5
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-07-15 12:39:14 +0530

    updated  version

--- storage-world-mobile/src/app/data/galleryImage.ts
+++ storage-world-mobile/src/app/data/galleryImage.ts
@@ -1,15 +1,15 @@
 export const galleryImages = [
   {
-      url: '../../assets/Storage_World_Mobile/storage-world-portable-storage-units-delivery.jpg',
+      url: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Storage_World_Mobile/storage-world-portable-storage-units-delivery.jpg',
       alt: 'A photograph of a pickup truck delivering a portable storage unit to a residential yard',
   },
   {
-      url: '../../assets/Storage_World_Mobile/storage-world-portable-units-truck.jpg',
+      url: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Storage_World_Mobile/storage-world-portable-units-truck.jpg',
       alt: `A photograph of a pickup truck pulling away from its delivery of a self storage unit to a residential yard`,
 
   },
   {
-      url: '../../assets/Storage_World_Mobile/storage-world-portable-storage-unit.jpg',
+      url: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Storage_World_Mobile/storage-world-portable-storage-unit.jpg',
       alt: `A photograph of a portable self storage unit on a blacktop surface`,
   },
 ];
