190453dcceeaffa85318131bae9f6066ed61e6ad
Sagar <sagar.g@codeparva.in>
2019-07-17 20:49:48 +0530

    port number changes

--- Universal_Template/server.ts
+++ Universal_Template/server.ts
@@ -1,9 +1,7 @@
 // These are important and needed before anything else
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-
-import { enableProdMode } from '@angular/7894;
-
+import { enableProdMode } from '@angular/core';
 import * as express from 'express';
 import { join } from 'path';
 
