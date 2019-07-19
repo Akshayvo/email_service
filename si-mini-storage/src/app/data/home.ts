6bb7407c8db76ff3692933483e3d392c480cfef6
Sagar <sagar.g@codeparva.in>
2019-07-19 15:24:05 +0530

    need to update image url

---
+++ si-mini-storage/src/app/data/home.ts
@@ -0,0 +1,78 @@
+
+export const featuresList = [
+    {
+        td1: 'Well-Lit',
+        td2: 'Fully-Fenced',
+    },
+    {
+        td1: 'Convenient Location',
+        td2: 'Security Cameras',
+    },
+    {
+        td1: 'Online Reservations',
+        td2: '24 Hour Access'
+    },
+    {
+        td1: 'Online Payments',
+        td2: 'Full-Time Manager'
+    }
+];
+
+export const feature = [
+    {
+        // tslint:disable-next-line:max-line-length
+        path: '../../assets/location.svg',
+        label: 'Conveniently Located',
+        alt: 'Icon art displaying a location pin'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: '../../assets/24-hours.svg',
+        label: '24 Hour Facility Access',
+        alt: 'Icon art displaying clock hands and the number 24'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: '../../assets/hand.svg',
+        label: 'Affordable Self Storage Rates',
+        alt: 'Icon of a hand holding money'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: '../../assets/light-bulb.svg',
+        label: 'Well-Lit Facility',
+        alt: 'Icon of an illuminated light'
+    },
+];
+
+
+export const aboutUs = [
+    {
+        p: `SI Mini Storage has been serving Barberton, Akron, Norton and the surrounding communities with high-quality, affordable self storage units 
+            since 1992. We are conveniently located about a mile south of State Route 76 between the Barber Rd and South Cleveland Massillon Rd.`,
+    },
+    {
+        p: `We offer a wide variety of unit types, from our 5 foot by 5 foot units that are perfect for stowing furniture while renovating a room, 
+            to our 11 foot by 30 foot units that can accommodate a large multi-bedroom home.  It doesn't matter if you're a residential user looking 
+            for some seasonal storage to make extra room at home or a business searching for long term storage of equipment or supplies, we've got you 
+            covered!`,
+    },
+    {
+        p: `Our facility is well-lit, includes wide drive aisles, a full time manager and security cameras to ensure our customers always feel safe. 
+            You'll even have access to your units 24 hours a day, 7 days a week thanks to our pin-code activated electronic gate system.`,
+    },
+    {
+        p: `Call today and learn how our friendly managers can help you find the perfect self storage unit!`,
+    },
+];
+
+
+export const gettingStarted = `Are you ready to start de-cluttering your home or get some elbow room back around the office? Reach out today, 
+                               speak with our friendly self storage management staff, and begin your stay with us today!`;
+
+export const blurb = [
+    {
+        h1: `Affordable Self Storage in Barberton`,
+        p: `Since 1992, SI Mini Storage has been serving Barberton and the surrounding communities`,
+    }
+];
---
+++ si-mini-storage/src/app/storage-tips/storage-tips.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { storagePoints, storageTips } from '../data/storage-tips';
+import { Title, Meta } from '@angular/platform-browser';
+import { WINDOW } from '@ng-toolkit/universal';
+
+@Component({
+  selector: 'app-storage-tips',
+  templateUrl: './storage-tips.component.html',
+  styleUrls: ['./storage-tips.component.scss']
+})
+
+
+export class StorageTipsComponent implements OnInit {
+  storagePoints: any;
+  storageTips: any;
+  currentActive: any = 'STORAGE TIPS';
+
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+    @Inject(WINDOW) private window: Window,
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: `We've had nearly three decades of self storage experience. Take a few lessons from us and use these helpful storage tips to make your 
+                stay with us easier!`
+    });
+    this.titleService.setTitle('Storage Tips | SI Mini Storage');
+  }
+
+  ngOnInit() {
+    this.fetchstoragePoints();
+    this.fetchstorageTips();
+    window.scrollTo(0, 0);
+
+  }
+
+
+  /**
+   * fetchstoragePoints
+   */
+  public fetchstoragePoints() {
+    this.storagePoints = storagePoints;
+  }
+
+  public fetchstorageTips() {
+    this.storageTips = storageTips;
+  }
+}
