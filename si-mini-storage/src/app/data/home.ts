89115a2593e9a8f707f62814b38f03e327f92c6a
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-07-23 10:17:56 +0530

    home page jumbotron, view rate and about us para changes, replaced about us image

--- si-mini-storage/src/app/data/home.ts
+++ si-mini-storage/src/app/data/home.ts
@@ -48,31 +48,35 @@ export const feature = [
 
 export const aboutUs = [
     {
-        p: `SI Mini Storage has been serving Barberton, Akron, Norton and the surrounding communities with high-quality, affordable self storage units 
-            since 1992. We are conveniently located about a mile south of State Route 76 between the Barber Rd and South Cleveland Massillon Rd.`,
+        p: `SI Mini Storage has been serving Barberton, Akron, Norton, and the surrounding communities with
+        high-quality, affordable self storage units since 1992. We are conveniently located about a mile south
+        of State Route 76 between the Barber Rd and South Cleveland Massillon Rd.`,
     },
     {
-        p: `We offer a wide variety of unit types, from our 5 foot by 5 foot units that are perfect for stowing furniture while renovating a room, 
-            to our 11 foot by 30 foot units that can accommodate a large multi-bedroom home.  It doesn't matter if you're a residential user looking 
-            for some seasonal storage to make extra room at home or a business searching for long term storage of equipment or supplies, we've got you 
-            covered!`,
+        p: `We offer a wide variety of unit types, from our 5 foot by 5 foot units, that are perfect for
+        stowing furniture while renovating a room, to our 11 foot by 30 foot units that can accommodate a
+        large multi-bedroom home. It doesn't matter if you're a residential user looking for some seasonal storage
+        to make extra room at home, or a business, searching for long term storage of equipment or supplies,
+        we've got you covered!`,
     },
     {
-        p: `Our facility is well-lit, includes wide drive aisles, a full time manager and security cameras to ensure our customers always feel safe. 
-            You'll even have access to your units 24 hours a day, 7 days a week thanks to our pin-code activated electronic gate system.`,
+        p: `Our facility is well-lit, wide drive aisles, a full-time manager, and security cameras, to
+        ensure our customers always feel safe. You'll even have access to your units 24 hours a day, 7 days a week.`,
     },
     {
-        p: `Call today and learn how our friendly managers can help you find the perfect self storage unit!`,
+        p: `Call today to learn how our friendly managers can help you find the perfect self storage unit!`,
     },
 ];
 
 
-export const gettingStarted = `Are you ready to start de-cluttering your home or get some elbow room back around the office? Reach out today, 
-                               speak with our friendly self storage management staff, and begin your stay with us today!`;
+export const gettingStarted = `Are you ready to start de-cluttering your home or get some elbow
+room back around the office? Reach out today, speak with our friendly self storage management
+staff, and begin your stay with us today!`;
 
 export const blurb = [
     {
         h1: `Affordable Self Storage in Barberton`,
-        p: `Since 1992, SI Mini Storage has been serving Barberton and the surrounding communities`,
+        p: `Since 1992, SI Mini Storage has been serving Barberton and the surrounding communities with high-quality,
+        affordable self storage, and superb customer service!`,
     }
 ];
--- si-mini-storage/src/app/data/view-rates.ts
+++ si-mini-storage/src/app/data/view-rates.ts
@@ -1,14 +1,15 @@
 export const dataViewRates = [
     {
-        p: `We have been proudly serving our neighbors in Barberton, Akron, Norton and the surrounding communities with high quality self 
-            storage services for nearly three decades! `,
+        p: `We have been proudly serving our neighbors in Barberton, Akron, Norton and the surrounding
+        communities with high quality self storage services for nearly three decades! `,
     },
     {
-        p: `SI Mini Storage is well-lit, and well-maintained with both convenience and security in mind! You have access to your belongings 
-            24 hours a day, 7 days a week with your custom gate pin-code, and you can pay your bill online at your convenience!`,
+        p: `SI Mini Storage is well-lit and well-maintained with both convenience and security in mind! You have access
+        to your belongings 24 hours a day, 7 days a week, and you can pay your bill online at your convenience!`,
     },
     {
-        p: `On top of the convenience factor, SI Mini Storage has an on-site manager who performs regular walk arounds, we maintain a well-lit facility, 
-            and are covered by security cameras. We want you to feel safe at all times!`,
+        p: `On top of the convenience factor, SI Mini Storage has an on-site manager who performs regular
+        walk arounds, we maintain a well-lit facility, and are covered by security cameras.
+        We want you to feel safe at all times!`,
     },
 ];
