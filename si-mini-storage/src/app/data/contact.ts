1d86b558078e8b1f530eca1cdec55450bcb3c9c8
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-07-24 10:38:40 +0530

    ready for the prod

---
+++ si-mini-storage/src/app/data/contact.ts
@@ -0,0 +1,47 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Address: ',
+        data: '436 15th St NW, Barberton, OH 44203',
+        class: 'fas fa-map-marker-alt mr-2 iconColor',
+    },
+    {
+        id: 1,
+        redirect: 'mailto:siministorage@outlook.com',
+        label: 'Email: ',
+        // data: 'siministorage@outlook.com',
+        data: 'sanghmitra.v@codeparva.in',
+        class: 'fas fa-envelope mr-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:3308486966',
+        label: 'Phone: ',
+        data: '(330) 848-6966',
+        class: 'fas fa-phone mr-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday - Friday: ',
+        data: '9:00AM to 5:00PM'
+    },
+    {
+        label: 'Saturday - Sunday: ',
+        data: 'CLOSED'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '24/7/365'
+    }
+];
+export const socialLinks = [
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/SIministorage',
+        class: 'fab fa-facebook-square',
+    },
+    
+];
+
