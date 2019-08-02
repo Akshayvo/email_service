017f148cde885601cbfada50b6ad08e6fa4cd1e0
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-08-02 16:55:00 +0530

    changed errorsMails

--- stoner-trail/src/environments/environment.ts
+++ stoner-trail/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
+  errorsMails: [ 'website.dev@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   appId: 'stoner-trail',
   VERSION: require('../../package.json').version
