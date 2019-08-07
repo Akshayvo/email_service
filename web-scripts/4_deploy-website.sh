67b2f03a5d295def1cb849ecbc8f742d26fb3772
Sagar <sagar.g@codeparva.in>
2019-08-07 17:33:24 +0530

    updated on 07/08/2019

--- web-scripts/4_deploy-website.sh
+++ web-scripts/4_deploy-website.sh
@@ -9,19 +9,18 @@ echo "Enter The Domain Name (Avoid using WWW.): "
 read  dom_name
 echo "Enter the port number"
 read port_num
-if [ $port_number -gt 10000 ]; then
+if [ $port_num -gt 10000 ]; then
         echo "Enter Valid Port Number"
         exit 1
 fi
 
 
-
-ssh PORT="${port_num}" /home/ubuntu/.nvm/versions/node/v8.11.4/bin/pm2 start "${proj_path}"/"${proj_name}"_dist/dist/server.js --name=$app_name --max-memory-restart 175M
+PORT="${port_num}" /home/ubuntu/.nvm/versions/node/v8.11.4/bin/pm2 start "${proj_path}"/"${proj_name}"_dist/dist/server.js --name=$app_name --max-memory-restart 175M
 # ssh -i Codeparva-dev.pem ubuntu@13.232.104.125 ./5_redirect-domain.sh
 cd /etc/nginx/sites-available
 # sites_avail_path="$(pwd)"
-# sudo cp www_example_com $dom_name
-# sudo sed -i "s,example.com,$dom_name,g" "${sites_avail_path}"/$dom_name
+sudo cp www_example_com $dom_name
+sudo sed -i "s,example.com,$dom_name,g" "${sites_avail_path}"/$dom_name
 sudo nginx -t 2>&1 |sudo tee somefile
 grep_out_put="$(sudo grep "test is successful" somefile)"
 if [[ -z $grep_out_put ]];then
