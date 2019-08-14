7e8384da3841c01a1491a04c80e27ba7d60a545b
Sagar <sagar.g@codeparva.in>
2019-08-14 09:16:48 +0530

    updated

---
+++ gatsby-learn/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'gatsby'
+import Header from '../components/header'
+
+export default() =>(
+    <div style={{color:'teal'}}>
+        <Header headerText="About Gallery" arbitaryPhase="is arbitary" />
+        <Header headerText="It's a pretty cool" />
+        <Link to="/">home</Link>
+        <h1>About Gatsby</h1>
+        <p>Such wow. Very React</p>
+    </div>
+)
