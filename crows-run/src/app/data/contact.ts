69581fbd56eb46d2c8a459ea4b0ae5b00b7dc952
Sagar <sagar.g@codeparva.in>
2019-09-03 17:44:44 +0530

    Crows Run Self Storage V 0.0.0

---
+++ crows-run/src/app/data/contact.ts
@@ -0,0 +1,49 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Address: ',
+        data: '1901 Crows Run Rd  Conway, PA 15027 ',
+        class: 'fas fa-map-marker-alt mr-2 iconColor',
+    },
+    {
+        id: 1,
+        redirect: 'mailto:selsman@crowsrunstorage.com',
+        label: 'Email: ',
+        data: 'selsman@crowsrunstorage.com',
+        class: 'fas fa-envelope mr-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:7242425600',
+        label: 'Phone: ',
+        data: '(724) 242-5600',
+        class: 'fas fa-phone mr-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'SUN: ',
+        data: 'Closed '
+    },
+    {
+        label: 'MON-FRI: ',
+        data: '9:00 AM - 5:00 PM'
+    },
+    {
+        label: 'SAT: ',
+        data: 'By Appointment Only'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '24 Hours a day 7 Days a Week'
+    }
+];
+
+export const socialLinks = [
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/Crows-Run-Self-Storage-563126960369408/',
+        class: 'fab fa-facebook-square',
+    }
+];
