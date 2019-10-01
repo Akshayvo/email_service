4e81d8bbc0d381074ff36c76634ac934d8668691
Sagar <sagar.g@codeparva.in>
2019-10-01 12:35:10 +0530

    view rates content update

--- stor-it-associates/src/app/data/view-rates.ts
+++ stor-it-associates/src/app/data/view-rates.ts
@@ -7,4 +7,20 @@ export const dataViewRates = [
         p: `It doesn't matter if you're renovating your home, downsizing to smaller accommodations, or running a growing business; 
             Store-It Associates has the ideal self storage, or parking solution for you!  Call to reserve your unit, today!`,
     }
+    
 ];
+
+export const currentDiscounts = [
+    {
+        data:`20% off military active duty.`
+    },
+    {
+        data:`15% off 1st month, just tell us you found us on the internet.`  
+    },
+    {
+        data:`5% off prepaid 6 months.`
+    },
+    {
+        data:`1 month free with prepaid 1 year.`
+    }
+]
