8ba3402882e92d6ec4ab5201d65dad50289bd192
Sagar <sagar.g@codeparva.in>
2019-10-21 14:44:28 +0530

    updated location

--- all-portable-easy-storage/src/app/data/nav.ts
+++ all-portable-easy-storage/src/app/data/nav.ts
@@ -2,21 +2,43 @@ export const navLinks = [
     {
         label: 'HOME',
         path: '/',
+        type: 'link'
+    },
+    {
+        label: 'LOCATIONS',
+        type: 'dropdown',
+        path: '/location',
+        sublinks: [
+            {
+                id: '1',
+                label: 'Stor More Self Storage',
+                path: 'https://www.stormoreministorage.com/',
+              },
+              {
+                  id: '2',
+                  label: 'All Space Self Storage',
+                  path: 'https://www.allspaceministorage.com/',
+              },
+        ]
     },
     {
         label: 'VIEW RATES',
         path: '/view-rates',
+        type: 'link'
     },
     {
         label: 'PAY RENT',
-        path: 'pay-rent'
+        path: 'pay-rent',
+        type: 'link'
     },
     {
         label: 'STORAGE TIPS',
         path: 'storage-tips',
+        type: 'link'
     },
     {
         label: 'CONTACT US',
-        path: 'contact-us'
+        path: 'contact-us',
+        type: 'link'
     },
 ];
