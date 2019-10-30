60cab8a2c801a5c4132bea67c5b91146b43870fe
unknown <ref>
2019-10-30 15:10:05 +0530

    Changed API Key

--- Catskill/src/environments/environment.ts
+++ Catskill/src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   appId: 'Catskill',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
+  APIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // AnotherLocationAPIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // baseUrl: `http://localhost:60444/api/`,
