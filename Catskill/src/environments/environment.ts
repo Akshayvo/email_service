4b3b04e4f1153ac8462318eee0e7d4c959095c29
unknown <ref>
2019-10-30 15:12:27 +0530

    Added another key

--- Catskill/src/environments/environment.ts
+++ Catskill/src/environments/environment.ts
@@ -11,9 +11,10 @@ export const environment = {
   appId: 'Catskill',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  APIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // AnotherLocationAPIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
+  // DemoNorthAPIKey: 'Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O',
   // baseUrl: `http://localhost:60444/api/`,
 };
 
