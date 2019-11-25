740de74f487b314c272efb6b4e50587107c2347b
Sagar <sagar.g@codeparva.in>
2019-11-25 15:33:21 +0530

    macedon storage staging update

---
+++ macedon-storage/src/app/data/view-rates.ts
@@ -0,0 +1,16 @@
+export const dataViewRates = [
+    {
+        p: `Macedon Storage offers a number of self storage unit sizes in both regular and climate-controlled varieties. 
+            We also offer limited recreational vehicle and boat storage for seasonal vehicle storage!`,
+    },
+    {
+        p: `We are proud to serve our friends and neighbors in Wayne County as an independently owned, customer service-centric 
+            storage facility. Our three locations and friendly staff are always available to answer your questions and help you get settled in. `,
+    },
+    {
+        p: `Our 24-hour access is a favorite feature for our business customers. Whether you're storing documents, inventory, supplies, or 
+            equipment you have access whenever your business requires it! Stop paying commercial real estate prices for storage when you've 
+            got as much space as you need right down the road at Macedon Storage!`
+    }
+    
+];
