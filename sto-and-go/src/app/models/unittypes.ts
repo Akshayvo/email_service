d664edb86931e6ed21fa033828a17443824242e6
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2019-11-25 16:42:09 +0530

    deleted ua-parse and added emailbaseurl in env

---
+++ sto-and-go/src/app/models/unittypes.ts
@@ -0,0 +1,52 @@
+export class UnitTypes {
+    lstUnitTypes: LstUnitTypes[];
+
+    constructor(lstUnitTypes: LstUnitTypes[]) {
+        this.lstUnitTypes = lstUnitTypes;
+    }
+}
+
+export class LstUnitTypes {
+    Description: string;
+    MonthlyRate: number;
+    ReservationFee: number;
+    ReservationFeeTax: number;
+    UnitTypeID: number;
+    IsUnitsAvailable: boolean;
+    IsClimateControlled: boolean
+
+    constructor(
+        Description: string,
+        MonthlyRate: number,
+        ReservationFeeTax: number,
+        ReservationFee: number,
+        UnitTypeID: number,
+        IsUnitsAvailable: boolean,
+        IsClimateControlled: boolean,
+        ) {
+        this.Description = Description,
+        this.MonthlyRate = MonthlyRate;
+        this.ReservationFeeTax  = ReservationFeeTax;
+        this.ReservationFee = ReservationFee;
+        this.UnitTypeID = UnitTypeID;
+        this.IsUnitsAvailable = IsUnitsAvailable;
+        this.IsClimateControlled = IsClimateControlled;
+    }
+}
+
+
+export class RentalPeriod {
+    lstRentalPeriods: LstRentalPeriods[];
+
+    constructor(lstRentalPeriods: LstRentalPeriods[]) {
+        this.lstRentalPeriods = lstRentalPeriods;
+    }
+}
+
+export class LstRentalPeriods {
+    PeriodDescription: string;
+
+    constructor(PeriodDescription: string) {
+        this.PeriodDescription = PeriodDescription;
+    }
+}
--- sto-and-go/src/environments/environment.ts
+++ sto-and-go/src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   senderEmail: 'errors@codeparva.in',
   appId: 'Sto and Go',
   VERSION: require('../../package.json').version,
+  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey:`dzJ0OFc4SjQqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` //29229
   APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, //demo north
