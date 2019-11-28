dad74d12462ac7a01f1dc9bb015d2d6f698bbf3f
Sagar <sagar.g@codeparva.in>
2019-11-28 11:52:05 +0530

    staging changes

---
+++ 390-self-storage/src/app/data/home.ts
@@ -0,0 +1,87 @@
+
+export const featuresList = [
+    {
+        td1: 'Convenient Location',
+        td2: 'Brand New Facility',
+    },
+    {
+        td1: 'Climate-Controlled Units Available',
+        td2: 'Security Cameras',
+    },
+    {
+        td1: 'Online Reservations',
+        td2: 'Pin-Code Activated Gate'
+    },
+    {
+        td1: 'Paved Parking',
+        td2: 'Fully-Fenced Facility'
+    },
+    {
+        td1: 'Excellent Customer Service',
+        td2: 'Drive-Up Units'
+    },
+    {
+        td1: 'Well-Lit Facility',
+        td2: 'Online Payment'
+    }
+];
+
+export const feature = [
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/location.svg',
+        label: 'Conveniently Located',
+        alt: 'Icon art displaying a location pin'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/thermometer.svg',
+        label: 'Climate Controlled Units Available',
+        alt: 'Icon of thermometer indicating climate controlled units'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/24-hours.svg',
+        label: '24 Hour Facility Access',
+        alt: 'Icon art displaying clock hands and the number 24'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/team.svg',
+        label: 'Locally Owned and Operated',
+        alt: 'Icon of people standing together '
+    },
+];
+
+
+export const aboutUs = [
+    {
+        p: `390 Self Storage is Rochester's newest self storage facility. We are locally owned & operated with 3 self storage 
+            facilities in the greater Rochester area. When it comes to self storage, we're your hometown experts! We work hard 
+            every day to provide the right mix of convenience, security, and affordability our customers seek.`,
+    },
+    {
+        p: `We are conveniently located on Buell Road right off of Chili Ave at the 390 Chili Ave exit right next to the Greater 
+            Rochester Airport. We offer convenient 24-hour access 7 days a week, and 24-hour account management and bill-pay through 
+            our tenant portal online!  We are a fully-fenced, and well-lit self storage facility with ample security camera coverage 
+            to ensure a sense of safety at all times!`,
+    },
+    {
+        p: `Our facility is the ideal solution to help you recover some of that elbow room around the house, or to use during a move 
+            or remodeling project. Our residential users love the affordable rates and easy drive-up access, plus our climate-controlled 
+            units are ideal for the more delicate belongings like furniture or documents. Speaking of documents, businesses love our 
+            climate-controlled units for records storage! Local businesses routinely store their supplies and inventory with us to 
+            save money over expanding with commercial real estate prices!`,
+    },
+];
+
+
+export const gettingStarted = `If you're ready to reclaim that space around your home, or expand your business without breaking the 
+                               bank on commercial real estate just to store supplies, give 390 Self Storage a call!`;
+
+export const blurb = [
+    {
+        h1: ``,
+        p: ``,
+    }
+];
---
+++ 390-self-storage/src/app/pay-rent/pay-rent.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { WINDOW } from '@ng-toolkit/universal';
+import { contact } from '../data/contact';
+
+@Component({
+  selector: 'app-pay-rent',
+  templateUrl: './pay-rent.component.html',
+  styleUrls: ['./pay-rent.component.scss']
+})
+export class PayRentComponent implements OnInit {
+
+  currentActive: any = 'PAY RENT';
+  contact: any;
+
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+    @Inject(WINDOW) private window: Window,
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: `Pay your rent online quickly and easily with our online payment portal. Follow the instructions on the page and pay your rent 24/7!`
+    });
+    this.titleService.setTitle('Pay Rent | 390 Self Storage');
+  }
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    this.fetchContact();
+  }
+
+  public fetchContact() {
+    this.contact = contact;
+  }
+
+}
