6eed5aec8c2d5db5cae84005934603be221ebaa2
Sagar <sagar.g@codeparva.in>
2019-12-06 10:54:58 +0530

    fort locks starting

---
+++ fort-locks/src/app/data/contact.ts
@@ -0,0 +1,45 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Address: ',
+        data: '1395 Danby Rd, Ithaca, NY 14850 ',
+        class: 'fas fa-map-marker-alt mr-2 iconColor',
+    },
+    {
+        id: 1,
+        redirect: 'mailto:manager@fortlocks.biz',
+        label: 'Email: ',
+        data: 'manager@fortlocks.biz',
+        class: 'fas fa-envelope mr-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:6072773700',
+        label: 'Phone: ',
+        data: '(607) 277-3700',
+        class: 'fas fa-phone mr-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday to Friday: ',
+        data: '9:00 AM - 5:00 PM'
+    },
+    // {
+    //     label: 'SAT-SUN: ',
+    //     data: 'By Appointment Only'
+    // },
+    // {
+    //     label: 'Access Hours: ',
+    //     data: '24 Hours a day 7 Days a Week'
+    // }
+];
+
+export const socialLinks = [
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/Crows-Run-Self-Storage-563126960369408/',
+        class: 'fab fa-facebook-square',
+    }
+];
---
+++ fort-locks/src/app/data/home.ts
@@ -0,0 +1,88 @@
+
+export const featuresList = [
+    {
+        td1: 'Conveniently Located',
+        td2: 'Well-Lit Facility',
+    },
+    {
+        td1: ' Security Cameras',
+        td2: 'Paved Parking',
+    },
+    {
+        td1: ' Drive-Up Units',
+        td2: ' Online Bill Pay'
+    },
+    {
+        td1: '  Climate-Controlled Units With Humidity Control ',
+        td2: 'Online Reservations'
+    },
+    {
+        td1: 'Single-Level Facility',
+    }
+];
+
+export const feature = [
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Crows_Run/location.svg',
+        label: 'Conveniently Located',
+        alt: 'Icon art displaying a location pin'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Crows_Run/24-hours.svg',
+        label: '24 Hour Facility Access',
+        alt: 'Icon art displaying clock hands and the number 24'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Crows_Run/operator.svg',
+        label: 'Stellar Customer Service',
+        alt: 'Customer Service Representative Icon'
+    },
+    // {
+    //     // tslint:disable-next-line:max-line-length
+    //     path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Crows_Run/team.svg',
+    //     label: 'Family Owned & Operated',
+    //     alt: 'Icon of people standing together '
+    // },
+];
+
+
+export const aboutUs = [
+    {
+        p: `Crows Run Self Storage is Conway, Pennsylvania's best self storage facility, with indoor units
+            ranging from 5'x10' to 10'x'30'. Our indoor units are the ideal solution for residential storage
+            users in the middle of a hectic move, renovation, or those preparing to downsize their accommodations.
+            Business customers take advantage of our 24-hour access to pick up and drop off their inventory, supplies,
+            and equipment. Why pay commercial real estate prices when you can use Crows Run Self Storage
+            at a fraction of the cost?`,
+    },
+    {
+        p: `The facility is conveniently located right off of Route 65, between Conway and Freedom. We are a
+            fully-fenced facility, well-lit facility with a password-controlled entrance gate and video cameras.
+            All of our units are drive-up at ground-level, making loading and unloading a breeze.`,
+    },
+    {
+        p: `The facility is conveniently located right off of Route 65, between Conway and Freedom. We are a
+            fully-fenced facility, well-lit facility with a keypad-controlled entrance gate and video cameras.
+            All of our units are drive-up at ground-level, making loading and unloading a breeze.`,
+    },
+];
+
+
+export const gettingStarted = `Are you ready to begin de-cluttering your life, and regaining
+                               some of that elbow room around the house?
+                               Click here to get started with Crows Run Self Storage!`;
+
+export const blurb = [
+    {
+        h1: `Affordable Storage Units in Conway`,
+        p: `Crows Run Self Storage serves Conway, Pennsylvania, and the surrounding communities with
+            quality indoor self storage units, and outdoor RV, camper, boat and vehicle storage. Our
+            indoor storage units range from 5'x10' to 10'x30', and our outdoor spaces range from 10'x20'
+            to 13'x40'. The facility is a fully-fenced, well-lit self storage facility and is covered by
+            video cameras. Access is secured by the keypad-activated gate. Our friendly management team
+            looks forward to meeting you! Call today!`,
+    }
+];
