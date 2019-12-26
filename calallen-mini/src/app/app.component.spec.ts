0a783ef3836355c10227d5855282ca7c01b933b0
Sagar <sagar.g@codeparva.in>
2019-12-26 18:50:58 +0530

    ready to deploy

--- calallen-mini/src/app/app.component.spec.ts
+++ calallen-mini/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Catskill'`, async(() => {
+  it(`should have as title 'calallen'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Catskill');
+    expect(app.title).toEqual('calallen');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Catskill!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to calallen!');
   }));
 });
--- calallen-mini/src/environments/environment.prod.ts
+++ calallen-mini/src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
   errorsMails: ['sanghmitra.v@codeparva.in'],
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   senderEmail: 'errors@codeparva.in',
-  appId: 'Catskill',
+  appId: 'calallen',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  APIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
+  APIKey: `dzJ0OVcySjMqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
-  websiteUrl: 'https://catskillselfstorage.com'
+  websiteUrl: ''
 };
