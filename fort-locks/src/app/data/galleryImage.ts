f90beb6cc97ef4c2543d36a0bd2729e220e37ada
Sagar <sagar.g@codeparva.in>
2019-12-27 14:59:49 +0530

    Image thumbnails

--- fort-locks/src/app/data/galleryImage.ts
+++ fort-locks/src/app/data/galleryImage.ts
@@ -1,19 +1,23 @@
 export const galleryHome = [
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-3.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-3-thumb.jpg`,
         alt: `photograph of the inside of a climate and humidity-controlled facility with two rows
               of blue self storage doors on either side of a concrete floor`,
     },
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-5.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-5-thumb.jpg`,
         alt: `photograph of two rows of self storage units on either side of a paved driveway under a blue sky`,
     },
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-2.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-2-thumb.jpg`,
         alt: `photograph of outdoor self storage buildings on either side of a paved parking area`,
     },
     {
-     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-jumbotron.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-jumbotron.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Fort_Locks/fort-locks-self-storage-jumbotron-thumb.jpg`,
         alt: `photograph of self storage units inside a climate-controlled building`,
     },
 ];
