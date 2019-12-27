8b32fd8a6ed78dba21b4671d0d5c9516a5a6830b
Sagar <sagar.g@codeparva.in>
2019-12-27 15:25:56 +0530

    image thumbnails

--- 390-self-storage/src/app/data/galleryImage.ts
+++ 390-self-storage/src/app/data/galleryImage.ts
@@ -1,22 +1,27 @@
 export const galleryHome = [
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-front-office.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-front-office-thumb.jpg`,
         alt: `photograph of a two story front office building with a gate protecting the self storage facility behind it`
     },
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-security-cameras.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-security-cameras-thumb.jpg`,
         alt: `photograph of a screen displaying many security camera feeds around a self storage facility`
     },
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-climate-controlled.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-climate-controlled-thumb.jpg`,
         alt: `photograph of the inside of a climate-controlled self storage building with bright lights and clean floors`
     },
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-outside-climate-control.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-outside-climate-control-thumb.jpg`,
         alt: `photograph of the exterior of a self storage climate-controlled unit building with metal walls and deep-set doorways`
     },
     {
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-security-cameras-front-desk.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-security-cameras-front-desk-thumb.jpg`,
         alt: `photograph of a front counter in an office with a tv displaying security camera feeds behind it and copier in the background`
     },
 
