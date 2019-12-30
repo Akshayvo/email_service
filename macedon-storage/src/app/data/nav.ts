2f3a25142a38cb2fa2e76c39eaadd598f7ef71ed
Sagar <sagar.g@codeparva.in>
2019-12-30 17:28:35 +0530

    location menu added

--- macedon-storage/src/app/data/nav.ts
+++ macedon-storage/src/app/data/nav.ts
@@ -2,25 +2,48 @@ export const navLinks = [
     {
         label: 'HOME',
         path: '/',
+        type: 'link',
+    },
+    {
+        label: 'LOCATIONS',
+        type: 'dropdown',
+        path: '/location',
+        sublinks: [
+            {
+                id: '1',
+                label: 'Linden Self Storage',
+                path: 'https://www.lindenstorage.com/',
+              },
+              {
+                  id: '2',
+                  label: '390 Self Storage',
+                  path: 'https://www.390selfstorage.com/',
+              },
+        ]
     },
     {
         label: 'VIEW RATES',
         path: '/view-rates',
+        type: 'link',
     },
     {
         label: 'PAY RENT',
-        path: 'pay-rent'
+        path: 'pay-rent',
+        type: 'link',
     },
     {
         label: 'UNIT SIZER',
-        path: 'unit-sizer'
+        path: 'unit-sizer',
+        type: 'link',
     },
     {
         label: 'STORAGE TIPS',
         path: 'storage-tips',
+        type: 'link',
     },
     {
         label: 'CONTACT US',
-        path: 'contact-us'
+        path: 'contact-us',
+        type: 'link',
     },
 ];
