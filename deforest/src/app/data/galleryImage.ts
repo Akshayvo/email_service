d7d3f5df5d5b94e86b65ffbb1a954d5ebb62541b
Sagar <sagar.g@codeparva.in>
2019-12-30 11:22:45 +0530

    jumbotron image update

--- deforest/src/app/data/galleryImage.ts
+++ deforest/src/app/data/galleryImage.ts
@@ -4,18 +4,21 @@ export const gallery = [
         id: `1`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-8.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-8-thumb.jpg`,
         alt: `photograph of a riding lawnmower on a trailer being backed into an open self storage unit`
     },
     {
         id: `2`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-1.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-1-thumb.jpg`,
         alt: `aerial photograph of a large self storage facility surrounded by a fence with concrete parking`,
     },
     {
         id: `3`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-3.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-3-thumb.jpg`,
         alt: `photograph of a concrete self storage front office with electronic gate guarding
               the self storage facility in the background`,
     },
@@ -23,12 +26,14 @@ export const gallery = [
         id: `4`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-4.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-4-thumb.jpg`,
         alt: `photograph of a hallway of indoor climate controlled units`,
     },
     {
         id: `5`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-6.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-6-thumb.jpg`,
         alt: `photograph of a television screen mounted on a wall displaying security camera feeds
               from around the self storage facility`,
     },
@@ -36,6 +41,7 @@ export const gallery = [
         id: `6`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-7.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-7-thumb.jpg`,
         alt: `photograph of a concrete block single story office in front of an electronic gate
               guarding a self storage facility`,
     },
@@ -43,6 +49,7 @@ export const gallery = [
         id: `7`,
         // tslint:disable-next-line:max-line-length
         url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-9.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Deforest/deforest-self-storage-9-thumb.jpg`,
         alt: `photograph of several self storage units in a row`,
     },
 
