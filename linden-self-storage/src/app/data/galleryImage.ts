bd4339617eea3b4c5daafd4f8a5ef8ca0a09705f
Sagar <sagar.g@codeparva.in>
2019-12-30 19:55:41 +0530

    added location menu and updated image thumbnails

--- linden-self-storage/src/app/data/galleryImage.ts
+++ linden-self-storage/src/app/data/galleryImage.ts
@@ -1,27 +1,33 @@
 export const galleryHome = [
   {
     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/climate-controlled-self-storage-units.jpg`,
+    thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/climate-controlled-self-storage-units-thumb.jpg`,
     alt: `photograph of indoor climate controlled self storage units under bright lights`
   },
   
   {
     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-building.jpg`,
+    thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-building-thumb.jpg`,
     alt: `photograph of a row of metal self storage unit buildings`
   },
   {
     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-buildings.jpg`,
+    thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-buildings-thumb.jpg`,
     alt: `photograph of two self storage buildings on either side of a parking lot`
   },
   {
     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-security-camera.jpg`,
+    thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-security-camera-thumb.jpg`,
     alt: `photograph of a camera mounted on the side of a metal self storage building`
   },
   {
     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-climate-controlled.jpg`,
+    thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-climate-controlled-thumb.jpg`,
     alt: `photograph of a hallway with climate-controlled units on either side of the hallway`
   },
   {
     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-security-pin.jpg`,
+    thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Linden_Self_Storage/linden-self-storage-security-pin-thumb.jpg`,
     alt: ` photograph of a pin-pad at the front gate mounted on a secure pole`
   },
 ];
