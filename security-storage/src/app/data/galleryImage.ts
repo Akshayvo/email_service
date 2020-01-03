0f1128c1ebffcf12ed80233611713dc710c913cc
Sagar <sagar.g@codeparva.in>
2020-01-03 17:08:45 +0530

    image scaling and view rates page update

--- security-storage/src/app/data/galleryImage.ts
+++ security-storage/src/app/data/galleryImage.ts
@@ -4,48 +4,64 @@ export const gallery = [
         id: `1`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-front-sign.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-front-sign-thumb.jpg`,
         alt: `Security Storage front sign`
     },
     {
         id: `2`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-indoor-units.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-indoor-units-thumb.jpg`,
         alt: `Indoor self storage units`,
     },
     {
         id: `3`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-open-indoor-units.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-open-indoor-units-thumb.jpg`,
         alt: `Indoor self storage units with one door open`,
     },
     {
         id: `4`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-outdoor-units.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-outdoor-units-thumb.jpg`,
         alt: `Outdoor self storage units facing the road`,
     },
     {
         id: `5`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-tree-and-truck.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-tree-and-truck-thumb.jpg`,
         alt: `Front of Security Storage main building with a tree and red truck.`,
     },
     {
         id: `6`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-well-lit-indoor-units.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-well-lit-indoor-units-thumb.jpg`,
         alt: `Indoor climate controlled units.`,
     },
     {
         id: `7`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-front-office.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-front-office-thumb.jpg`,
         alt: `Front of Security Storage main office`,
     },
     {
         id: `8`,
         // tslint:disable-next-line:max-line-length
         url: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-exterior-photo.jpg`,
+        // tslint:disable-next-line:max-line-length
+        thumb: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Security_Cuero_Images/gallery/security-storage-exterior-photo-thumb.jpg`,
         alt: `Exterior of Security Storage indoor storage building`,
     },
 
