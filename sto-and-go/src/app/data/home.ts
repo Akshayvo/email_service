809f6d0ae246b2e2b382b5aa0e04c5973649e554
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-02-28 10:59:05 +0530

    removed security text

--- sto-and-go/src/app/data/home.ts
+++ sto-and-go/src/app/data/home.ts
@@ -17,16 +17,13 @@ export const featuresList = [
         td2: 'No Deposit'
     },
     {
-        td1: 'Security Cameras',
+        td1: 'Dump Station Coming Soon',
         td2: 'No Insurance Required'
     },
     {
         td1: 'Facility Access at Cloverleaf Dr and Hwy 9E Stoplight ',
         td2: 'Climate Controlled Units Available'
     },
-    {
-        td1: 'Dump Station Coming Soon'
-    }
 ];
 
 export const feature = [
@@ -48,43 +45,40 @@ export const feature = [
         label: 'Climate Controlled Units Available',
         alt: 'Icon of thermometer indicating climate controlled units'
     },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Sto_And_Go/cctv.svg',
-        label: 'Security Cameras on Premises',
-        alt: 'Icon of a security camera'
-    },
 ];
 
 
 export const aboutUs = [
     {
-        p: `Sto and Go has been serving Longs, Little River, North Myrtle & Myrtle Beach with excellent customer service, great storage unit, 
-            and parking options since 2000. We are conveniently located right off of Highway 9, behind the Parkway Plaza.`,
+        p: `Sto and Go has been serving Longs, Little River, North Myrtle & Myrtle Beach with
+        excellent customer service, great storage unit, and parking options since 2000. We are
+        conveniently located right off of Highway 9, behind the Parkway Plaza.`,
     },
     {
-        p: `Our facility has a wide variety of storage options, from regular self storage units to our climate-controlled variety. 
-            So, whether you are a residential self storage user just looking for a little elbow room during a renovation, or are a 
-            business in need of inventory, or document storage, we've got you covered! We also have plenty of parking spaces for boats, 
-            campers, or other recreational vehicles.`,
+        p: `Our facility has a wide variety of storage options, from regular self storage units
+        to our climate-controlled variety. So, whether you are a residential self storage user
+        just looking for a little elbow room during a renovation, or are a business in need of
+        inventory, or document storage, we've got you covered! We also have plenty of parking spaces for boats,
+        campers, or other recreational vehicles.`,
     },
     {
-        p: `Rest easy, knowing our facility has on-site management, security cameras, and pin-code activated gates for added security. 
-            Need access to your unit outside of regular business hours? Inquire about our 24 hour access, and your belongings, or vehicle 
-            will always be accessible!`,
+        p: `Rest easy, knowing our facility has on-site management, and pin-code activated gates for added
+        security. Need access to your unit outside of regular business hours? Inquire about our 24 hour access,
+        and your belongings, or vehicle will always be accessible!`,
     },
 ];
 
 
-export const gettingStarted = `Whether you're a residential user looking for a little elbow-room around the house, or a business seeking storage
-                               space that's cheaper than commercial real estate, Sto and Go has a solution for you! Get that boat, or camper out of 
-                               your yard, and start decluttering with Sto and Go today!`;
+export const gettingStarted = `Whether you're a residential user looking for a little elbow-room around the house,
+or a business seeking storage space that's cheaper than commercial real estate, Sto and Go has a solution for you!
+Get that boat, or camper out of your yard, and start decluttering with Sto and Go today!`;
 
 export const blurb = [
     {
         h1: `Affordable Storage Units & Parking in Longs`,
-        p: `Sto and Go has served Longs, SC since 2000 with high-quality, well-lit, and affordable self storage units, and vehicle parking. 
-            Our facility has a wide variety of self storage unit sizes in both regular, and climate-controlled varieties. We also have ample 
-            parking for your boat, and camper! Check out our prices, and reserve your space, today!`,
+        p: `Sto and Go has served Longs, SC since 2000 with high-quality, well-lit, and affordable self storage units,
+        and vehicle parking. Our facility has a wide variety of self storage unit sizes in both regular,
+        and climate-controlled varieties. We also have ample parking for your boat, and camper! Check out our prices,
+        and reserve your space, today!`,
     }
 ];
