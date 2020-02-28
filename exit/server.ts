d2608668018ec35db0ae4a4057c3e285b02cd0c0
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-02-28 15:31:30 +0530

    ready for production

--- exit/server.ts
+++ exit/server.ts
@@ -13,7 +13,7 @@ enableProdMode();
 // Express server
 const app = express();
 
-const PORT = process.env.PORT || 10007;
+const PORT = process.env.PORT || 6020;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
