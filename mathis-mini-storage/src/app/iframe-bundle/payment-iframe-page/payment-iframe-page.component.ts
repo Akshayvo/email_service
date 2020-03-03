794bf5fe34f3c2413d47570a7540b2e9d420224b
Sagar <sagar.g@codeparva.in>
2020-03-03 19:14:56 +0530

    staging changes

---
+++ mathis-mini-storage/src/app/iframe-bundle/payment-iframe-page/payment-iframe-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { contact } from '../../data/contact';
+import { WINDOW } from '@ng-toolkit/universal';
+@Component({
+  selector: 'app-payment-iframe-page',
+  templateUrl: './payment-iframe-page.component.html',
+  styleUrls: ['./payment-iframe-page.component.scss']
+})
+export class PaymentIframePageComponent implements OnInit {
+
+  contact: any;
+
+  constructor(
+    private titleService: Title,
+    @Inject(WINDOW) private window: Window,
+    private meta: Meta
+  ) {
+    this.meta.addTag({
+      name: 'description',
+      content: ``
+    });
+    this.titleService.setTitle('');
+  }
+
+  ngOnInit() {
+    this.fetchContactDetails();
+    window.scrollTo(0, 0);
+  }
+
+  public fetchContactDetails() {
+    this.contact = contact;
+  }
+}
