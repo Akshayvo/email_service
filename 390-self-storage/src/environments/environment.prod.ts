186151ab04e6e0cb7f94a78be0926826fe5d7d1f
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-03-11 11:10:55 +0530

    removed unnecessary files

--- 390-self-storage/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  errorsMails: ['website.dev@codeparva.in'],
-  senderEmail: 'errors@codeparva.in',
-  appId: '390 Self Storage',
-  VERSION: require('../../package.json').version
-
-};
