416125546a0796c7d6427424ec15f6b930bc1622
Sagar <sagar.g@codeparva.in>
2020-03-11 19:51:21 +0530

    initial

---
+++ advantage-climate-control/src/app/data/contact.ts
@@ -0,0 +1,54 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Facility Address:',
+        data: '1008 Jordan Ln NW, Huntsville, AL 35816',
+        class: 'fas fa-map-marker-alt mx-2 iconColor',
+    },
+    {
+        id: 1,
+        redirect: 'mailto:advantage.storage@yahoo.com',
+        data: 'advantage.storage@yahoo.com',
+        label: 'Email:',
+        class: 'fas fa-envelope mx-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:2567157781',
+        label: 'Phone:',
+        data: '(256) 715-7781',
+        class: 'fas fa-phone mx-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday - Friday: ',
+        data: '9:00AM to 6:00PM'
+    },
+    {
+        label: 'Saturday: ',
+        data: '8:30AM to 2:30PM'
+    },
+    {
+        label: 'Sunday: ',
+        data: 'CLOSED'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '7 Days a Week 6:00AM to 11:00PM'
+    }
+];
+
+export const socialLinks = [
+    // {
+    //     label: 'twitter',
+    //     path: '',
+    //     class: 'fab fa-twitter-square',
+    // },
+    // {
+    //     label: 'facebook',
+    //     path: '',
+    //     class: 'fab fa-facebook-square',
+    // },
+];
---
+++ advantage-climate-control/src/app/data/home.ts
@@ -0,0 +1,87 @@
+export const featuresList = [
+    {
+        td1: 'Convenient Location',
+        td2: 'Online Bill Pay'
+    },
+    {
+        td1: 'Security Cameras',
+        td2: 'Well-Lit Facility'
+    },
+    {
+        td1: 'Brand New Units',
+        td2: 'Truck Rentals'
+    },
+    {
+        td1: 'Auto-Pay Available',
+        td2: 'Friendly Customer Service'
+    },
+    {
+        td1: 'Affordable Rates',
+        td2: 'Retail Store On-Premises'
+    },
+    {
+        td1: 'Packing Supplies Available',
+        td2: 'Climate Controlled Units'
+    },
+    {
+        td1: `Tenant's Lounge`,
+        td2: 'Insurance'
+    }
+];
+
+export const aboutUs = [
+    {
+        p: `We understand the stress that typically accompanies a storage experience, that's why we've designed our facility and services from the ground up to make the entire storage process a hassle-free one.`,
+    },
+    {
+        p: `Begin your stay with an easy online reservation, or a quick call to one of our friendly managers! Grab some packing supplies in our retail store, and rent a U-Haul truck for a day! Pull up to our loading dock for easy access to your climate controlled unit! You can even pay your rent online, 24/7 with our handy. It doesn't matter if you're a residential storage user in the middle of a remodel, or if you're downsizing and need space for seasonal items that isn't your garage; we even service growing local businesses who need room for document, or equipment storage!`,
+    },
+    {
+        p: `Rest easy knowing that your belongings are stored in an entirely enclosed environment, apart from the elements. Access to the well-lit interior is granted by unique keypad code, and our entire facility is covered by security cameras to ensure a constant sense of safety. `,
+    },
+    {
+        p: `Security is a critical aspect of every self storage facility. You can rest easy knowing your belongings, or vehicles,
+            are in a fully-fenced facility with ample lighting and are covered by security cameras. The Mathis Mini Storage staff
+            perform regular walkarounds to monitor the units and recreational vehicles or trailers parked in our facility. `
+    }
+];
+
+export const gettingStarted = `Are you ready to reclaim some of that elbow room around the home, or free up space at the job-site or in the office? Click below to start the reservation process today!`;
+
+export const feature = [
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/location.svg',
+        label: 'Conveniently Located',
+        alt: 'Icon art displaying a location pin'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/thermometer.svg',
+        label: 'Climate Controlled',
+        alt: 'Icon of thermometer indicating climate controlled units'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/light-bulb.svg',
+        label: 'Well-Lit Facility',
+        alt: 'Icon of an illuminated light'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/cctv.svg',
+        label: 'Security Cameras on Premises',
+        alt: ' Icon of a security camera'
+    },
+];
+
+export const jumbotron = [
+    {
+        h1: `Climate-Controlled Storage Units in Huntsville`,
+        blurb: `Advantage Climate Control Self Storage offers over 300 climate controlled storage units that
+                range from closet-sized through an extra large garage, perfect for the belongings that need a
+                little extra protection from the elements. Our state-of-the-art facility has a full retail store
+                with packing supplies, keypad-secured doors, and we even rent U-Haul trucks to make your self
+                storage or moving experience a breeze!`,
+    }
+];
