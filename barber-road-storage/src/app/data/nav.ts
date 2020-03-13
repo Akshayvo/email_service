449faf43cb67e8a0058dd1d43dd62cc8a7bd5965
Sagar <sagar.g@codeparva.in>
2020-03-13 15:09:30 +0530

    logo update

---
+++ barber-road-storage/src/app/data/nav.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+    {
+        label: 'HOME',
+        path: '/',
+    },
+    {
+        label: 'VIEW RATES',
+        path: '/view-rates',
+    },
+    {
+        label: 'PAY RENT',
+        path: '/pay-rent',
+    },
+    {
+        label: 'UNIT SIZER',
+        path: '/unit-sizer',
+    },
+    {
+        label: 'STORAGE TIPS',
+        path: '/storage-tips'
+    },
+    {
+        label: 'CONTACT US',
+        path: '/contact-us'
+    },
+];
