f48d6eca466168583e4e3f0b3964c33802f9e72c
Sagar <sagar.g@codeparva.in>
2020-03-13 18:19:07 +0530

    initial

--- si-mini-storage/src/app/app.module.ts
+++ si-mini-storage/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -12,21 +12,44 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { ViewRatesComponent } from './view-rates/view-rates.component';
+// import { ViewRatesComponent } from './view-rates/view-rates.component';
 import { TablesComponent } from './tables/tables.component';
 import { ErrorComponent } from './error/error.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import { Angulartics2Module } from 'angulartics2';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 import { StorageTipsComponent } from './storage-tips/storage-tips.component';
-import { ContactButtonComponent } from './contact-button/contact-button.component';
-import { PayRentComponent } from './pay-rent/pay-rent.component';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { ErrorHandlerComponent } from './error-handler/error-handler.component';
-
-import { ReserveUnitComponent } from './reserve-unit/reserve-unit.component';
 import { ErrorHandlerService } from './services/error-handler.service';
-
+// import { UnitSizerComponent } from './unit-sizer/unit-sizer.component';
+import { ReserveComponent } from './api-bundle/reserve/reserve.component';
+import { ViewRatesComponent } from './api-bundle/view-rates/view-rates.component';
+import { ViewRatesIframePageComponent } from './iframe-bundle/view-rates-iframe-page/view-rates-iframe-page.component';
+import { ViewRatesPageComponent } from './api-bundle/view-rates-page/view-rates-page.component';
+import { ReserveUnitFormComponent } from './api-bundle/reserve-unit-form/reserve-unit-form.component';
+import { ReserveUnitIframePageComponent } from './iframe-bundle/reserve-unit-iframe-page/reserve-unit-iframe-page.component';
+import { ConfirmationDataComponent } from './api-bundle/confirmation-data/confirmation-data.component';
+import { PayRentFormComponent } from './api-bundle/pay-rent-form/pay-rent-form.component';
+import { PaymentComponent } from './api-bundle/payment/payment.component';
+import { PaymentIframePageComponent } from './iframe-bundle/payment-iframe-page/payment-iframe-page.component';
+import { LoginComponent } from './api-bundle/login/login.component';
+import { LoginModalComponent } from './api-bundle/login-modal/login-modal.component';
+import { ForgotPasswordComponent } from './api-bundle/forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './api-bundle/change-password/change-password.component';
+import { VerifictionCodeGuard } from './auth-guard/verificationCode.guard';
+import { VerifyCodeComponent } from './api-bundle/verify-code/verify-code.component';
+import { DatePipe } from '@angular/common';
+import { AuthGuard } from './auth-guard/auth.gurad';
+import { AuthService } from './api-bundle/services/auth.service';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { ResetPasswordComponent } from './api-bundle/reset-password/reset-password.component';
+import { RequestInterceptorService } from './api-bundle/services/request-interceptor.service';
+import { ContactButtonComponent } from './contact-button/contact-button.component';
+import { CanDeactivateGuard } from './preventRouteChange.guard';
+import { PhotosComponent } from './photos/photos.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,9 +63,25 @@ import { ErrorHandlerService } from './services/error-handler.service';
     ErrorHandlerComponent,
     StorageTipsComponent,
     AccordionComponent,
-    ContactButtonComponent,
-    PayRentComponent,
-    ReserveUnitComponent,
+    // UnitSizerComponent,
+    ReserveComponent,
+    ViewRatesComponent,
+    ViewRatesIframePageComponent,
+    ViewRatesPageComponent,
+    ReserveUnitFormComponent,
+    ReserveUnitIframePageComponent,
+    ConfirmationDataComponent,
+    PayRentFormComponent,
+    PaymentComponent,
+    PaymentIframePageComponent,
+    LoginComponent,
+    LoginModalComponent,
+    ForgotPasswordComponent,
+    ChangePasswordComponent,
+    PhotosComponent,
+    VerifyCodeComponent,
+    ResetPasswordComponent,
+    ContactButtonComponent
   ],
   imports: [
     CommonModule,
@@ -53,9 +92,17 @@ import { ErrorHandlerService } from './services/error-handler.service';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    SelectDropDownModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [
+    DatePipe,
+    AuthService,
+    AuthGuard,
+    VerifictionCodeGuard,
     Title,
+    CanDeactivateGuard,
     {
       provide: ErrorHandler,
       useClass: ErrorHandlerService
@@ -66,7 +113,12 @@ import { ErrorHandlerService } from './services/error-handler.service';
       useValue: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
           window.location.href = (route.data as any).externalUrl;
       }
-    }
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptorService,
+      multi: true
+    },
   ],
   bootstrap: [AppComponent],
 })
--- si-mini-storage/src/environments/environment.prod.ts
+++ si-mini-storage/src/environments/environment.prod.ts
@@ -1,8 +1,13 @@
 export const environment = {
   production: false,
-  errorsMails: ['website.dev@codeparva.in'],
+  type: 0,  // { API: 0, iframe: 1 }
+  errorsMails: [ 'website.dev@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
+  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   appId: 'Si Mini Storage',
-  VERSION: require('../../package.json').version
+  VERSION: require('../../package.json').version,
+  baseUrl: `https://simapi.syrasoft.com/1/api/`,
+  APIKey: `dzF0OFcwSjAqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 18009 mathis
+  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
 
 };
