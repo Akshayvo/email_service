474c3c67ec6a5c5f408d48949758129f9639a458
Sagar <sagar.g@codeparva.in>
2020-03-18 19:53:37 +0530

    staging update

---
+++ ferrisburgh-self-storage/src/app/data/contact.ts
@@ -0,0 +1,56 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Facility Address:',
+        data: '7175 US-7, North Ferrisburgh, VT 05473',
+        class: 'fas fa-map-marker-alt mx-2 iconColor',
+    },
+    {
+        id: 1,
+        label: 'Office Address:',
+        data: 'P.O. Box 743 Burlington, VT, 05402',
+        class: 'fas fa-map-marker-alt mx-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'mailto:ferrisburghselfstorage@gmail.com',
+        data: 'ferrisburghselfstorage@gmail.com',
+        label: 'Email:',
+        class: 'fas fa-envelope mx-2 iconColor',
+    },
+    {
+        id: 3,
+        redirect: 'tel:8023773775',
+        label: 'Phone:',
+        data: '(802) 377-3775',
+        class: 'fas fa-phone mx-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday - Friday: ',
+        data: '8:00AM to 6:00PM'
+    },
+    {
+        label: 'Saturday - Sunday: ',
+        data: 'CLOSED'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '24 Hours a Day 7 Days a Week'
+    }
+];
+
+export const socialLinks = [
+    // {
+    //     label: 'twitter',
+    //     path: '',
+    //     class: 'fab fa-twitter-square',
+    // },
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/FerrisburghSelfStorage',
+        class: 'fab fa-facebook-square',
+    },
+];
---
+++ ferrisburgh-self-storage/src/app/data/title.ts
@@ -0,0 +1,28 @@
+export const  homePageTitle = `24 Hour Access Self Storage Units | Ferrisburgh Self Storage`;
+export const homePageContent = `Ferrisburgh Self Storage offers convenient 24/7 access to your belongings, online payments and
+                                a variety of unit sizes for all of your self storage needs.`;
+
+
+export const viewRatesPageTitle = `View Rates  | Ferrisburgh Self Storage`;
+export const viewRatesPageContent = `Take a moment to view our affordable self storage unit rates, then make your reservation
+                                     by filling out our form or calling our office today!`;
+
+
+export const reservePageTitle = `Reserve Your Unit  | Ferrisburgh Self Storage`;
+export const reservePageContent = `Are you ready to start de-cluttering your life and reclaiming that space around the house or
+                                   work-site? Make your reservation here and store with us today!`;
+
+export const payRentPageTitle = `Pay Rent  | Ferrisburgh Self Storage`;
+export const payRentPageContent = `Follow the instructions on this page and pay your rent online 24 hours a day 7 days a week!`;
+
+
+export const storageTipsTitle = `Storage Tips  | Ferrisburgh Self Storage`;
+export const storageTipsContent = `Our quick storage tips page will help you figure out how to best use our services and avoid common pitfalls in self storage!`;
+
+
+export const unitSizerPageTitle = `Unit Sizer  | Ferrisburgh Self Storage`;
+export const unitSizerPageContent = `Are you not sure what size unit you require? Use this guide to help you figure out what fits inside the units offered at Ferrisburgh Self Storage!`;
+
+
+export const contactPageTitle = `Contact Us  | Ferrisburgh Self Storage`;
+export const contactPageContent = `Do you have a question about our services, or would you like help with your account? Use our form or contact information to reach out, today!`;
