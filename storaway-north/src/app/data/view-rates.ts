b6420b189a539629f9156477841c3ef056e031ed
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-03-26 13:13:37 +0530

    added special discount

--- storaway-north/src/app/data/view-rates.ts
+++ storaway-north/src/app/data/view-rates.ts
@@ -1,28 +1,33 @@
 export const dataViewRates = [
     {
-        p: `Our fully-fenced, well-lit facility has a number of storage options to make your move or downsizing experience easier. 
-            From closet-sized units to units large enough to store an entire 5-bedroom house, Stor-A-Way North has all of your storage needs 
+        p: `Our fully-fenced, well-lit facility has a number of storage options to make your move or
+        downsizing experience easier. From closet-sized units to units large enough to store an entire
+        5-bedroom house, Stor-A-Way North has all of your storage needs
             covered at exceptionally low rates!`,
     },
     {
-        p: `Do you need access outside of our regular gate hours? Inquire with our management team and get approved for 24 hour gate access! `
+        p: `Do you need access outside of our regular gate hours? Inquire with our
+        management team and get approved for 24 hour gate access! `
     }
 ];
 
 export const currentDiscounts = [
     {
-        p:`Veterans, Police and Fireman - 10% discount`
+        p: `Veterans, Police and Fireman - 10% discount`
     },
     {
-        p:`Active duty military - 20% `
+        p: `Active duty military - 20% `
     },
     {
-        p:`Internet discount - 10% off the first month`
+        p: `Internet discount - 10% off the first month`
     },
     {
-        p:`1 year prepaid, 1 month free`
+        p: `1 year prepaid, 1 month free`
     },
     {
-        p:`Fall special October - December - 3 months prepaid, 1 month free for rental of 10x20 unit.`
+        p: `Fall special October - December - 3 months prepaid, 1 month free for rental of 10x20 unit.`
+    },
+    {
+        p: `Student Discount - 20%`
     }
-] 
+];
