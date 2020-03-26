8f89a96e271096a6d113b0d83c06d65f2dc312d4
Sagar <sagar.g@codeparva.in>
2020-03-26 10:50:13 +0530

    contact update

--- sto-and-go/src/app/data/contact.ts
+++ sto-and-go/src/app/data/contact.ts
@@ -16,7 +16,7 @@ export const contact = [
         id: 2,
         redirect: 'tel:8433900019',
         label: 'Phone: ',
-        data: '843-390-0019',
+        data: '(843) 390-0019',
         class: 'fas fa-phone mr-2 iconColor',
     }
 ];
