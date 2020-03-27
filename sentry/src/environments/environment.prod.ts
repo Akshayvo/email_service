c6502f6cfa1995ac576af86ed49fef519f8c18e9
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-03-27 15:47:54 +0530

    removed unnecessary file and increased view rates's iframe

--- sentry/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  errorsMails: ['website.dev@codeparva.in'],
-  senderEmail: 'errors@codeparva.in',
-  appId: 'Sentry Stor-ALL',
-  VERSION: require('../../package.json').version
-
-};
