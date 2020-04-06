54424ae9a2bb98a5d52dc1dfb565d6e978994b18
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-04-06 14:43:01 +0530

    updated blurb

--- yes-storage/src/app/data/blurb.ts
+++ yes-storage/src/app/data/blurb.ts
@@ -7,43 +7,50 @@ export const jumbotronHome = [
 
 export const viewRates = [
   {
-    heading: ``,
-    blurb: ``,
+    heading: `View Rates`,
+    blurb: `Whether you're looking for short term storage during a move or long term self storage for
+    a permanent downsizing, Yes Storage has the perfect unit at the perfect price! Our friendly staff and high
+    tech facility are here to make your self storage experience a breeze!`,
   },
 ];
 
 export const unitSizers = [
   {
-    heading: ``,
-    blurb: ``,
+    heading: `Unit Sizer`,
+    blurb: `Having trouble figuring out what sized unit you will need? Our handy unit sizing chart
+    will help you visualize what is likely to fit in what sized units.`,
   },
 ];
 
 export const storageTips = [
   {
-    heading: ``,
-    blurb: ``,
+    heading: `Storage Tips`,
+    blurb: `Here are some free tips for storing your belongings with Yes Storage! Have specific questions that
+    aren't covered here? Contact us today and let us make your self storage experience a breeze.`,
   },
 ];
 
 
 export const payRent = [
   {
-    heading: ``,
-    blurb: ``,
+    heading: `Pay Rent`,
+    blurb: `Just follow the instructions below, and you'll have access to your online account
+    24 hours a day, 7 days a week. If you have trouble logging in, just give out on-site manager a call!`,
   },
 ];
 
 export const reserve = [
   {
-    heading: ``,
-    blurb: ``,
+    heading: `Reserve Unit`,
+    blurb: `Reserve your unit, and start de-cluttering your home, office, or worksite by filling out
+    the form below! Our on-site manager will be in touch shortly!`,
   },
 ];
 
 export const contactUs = [
   {
-    heading: ``,
-    blurb: ``,
+    heading: `Contact Us`,
+    blurb: `Have a question about our storage units or your account? Use the convenient form
+    to email our managers directly or use the contact information below!`,
   },
 ];
