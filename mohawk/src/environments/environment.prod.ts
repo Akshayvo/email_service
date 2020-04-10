36cf5b68f3a94daa139d5f849ce5d53ebb7e6d28
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-04-10 12:39:01 +0530

    removed unnecessary files

--- mohawk/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  errorsMails: ['website.dev@codeparva.in'],
-  senderEmail: 'errors@codeparva.in',
-  appId: 'Mohawk Self Storage',
-  VERSION: require('../../package.json').version
-
-};
--- stor-it-associates/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  errorsMails: ['website.dev@codeparva.in'],
-  senderEmail: 'errors@codeparva.in',
-  appId: 'Stor-it Associates',
-  VERSION: require('../../package.json').version
-
-};
