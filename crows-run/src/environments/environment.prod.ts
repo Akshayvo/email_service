4bb7fc9b3dce32831bfd93a057d715b90b3533f8
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-04-15 11:55:13 +0530

    udated special row and removed unnecessary files

--- crows-run/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  errorsMails: ['website.dev@codeparva.in'],
-  senderEmail: 'errors@codeparva.in',
-  appId: 'Crows Run Self Storage',
-  VERSION: require('../../package.json').version
-
-};
