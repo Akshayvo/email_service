3b97e6f671e8b407a8a5555f4d59081e3d80c326
Sagar <sagar.g@codeparva.in>
2020-04-20 18:55:36 +0530

    sentry stor all Api update

--- sentry/src/environments/environment.ts
+++ sentry/src/environments/environment.ts
@@ -4,11 +4,14 @@
 
 export const environment = {
   production: false,
+  type: 0,  // { API: 0, iframe: 1 }
   errorsMails: [ 'website.dev@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
+  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   appId: 'Sentry Stor-ALL',
-  VERSION: require('../../package.json').version
-
+  VERSION: require('../../package.json').version,
+  baseUrl: `https://simapi.syrasoft.com/1/api/`,
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
 };
 
 /*
