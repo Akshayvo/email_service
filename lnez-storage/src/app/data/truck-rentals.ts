d7116501e487635defc8a8160354247a021b1ac1
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-04-30 12:06:11 +0530

    removed unnecessary files

--- lnez-storage/src/app/data/truck-rentals.ts
+++
@@ -1,8 +0,0 @@
-export const truckRentalblurb = `Need a truck to help with your move? Have business equipment
-          or inventory you need to move? Our Charleston location has
-          you covered! Call us directly for booking and tenant specials
-          or click below to begin your booking process at Penske's website.`;
-
-export const tuckRentalSizes = [
-   
-];
