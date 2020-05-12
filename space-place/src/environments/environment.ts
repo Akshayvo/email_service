023b5eeec9ba04efe542dd60ad7eb6b9aa7fd8a1
Sagar <sagar.g@codeparva.in>
2020-05-12 18:12:35 +0530

    appid update

--- space-place/src/environments/environment.ts
+++ space-place/src/environments/environment.ts
@@ -9,11 +9,11 @@ export const environment = {
   senderEmail: 'errors@codeparva.in',
   websiteUrl: 'http://www.spaceplacemacedonia.com',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Space Place Macedonia LLC ',
+  appId: 'Space Place Macedonia LLC -Staging',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
-  // APIKey: `dzJ0OFc0SjgqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`  // 28484 
+  // APIKey: `dzJ0OFc0SjgqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`  // 28484
   APIKey: `dzJ0N1c2SjkqNk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` //  Space Place Madeconia 27696
 };
 
