293c48fae3527575e784b43fb1d8059515913ea8
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-05-13 07:32:40 +0530

    removed unnecessary files

--- space-place-Macedonia/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  errorsMails: ['website.dev@codeparva.in'],
-  senderEmail: 'errors@codeparva.in',
-  appId: 'Space Place Macedonia LLC ',
-  VERSION: require('../../package.json').version
-};
