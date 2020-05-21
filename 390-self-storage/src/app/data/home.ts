27f12f019f7b2d6f4ada23a62c47d40a7dbaaed9
Sagar <sagar.g@codeparva.in>
2020-05-21 12:38:32 +0530

    production changes and home page update

--- 390-self-storage/src/app/data/home.ts
+++ 390-self-storage/src/app/data/home.ts
@@ -1,79 +1,79 @@
 // for production
 
-export const featuresList = [
-
-    {
-        td1: 'Fully Fenced with Barbed Wire',
-        td2: 'Well-Lit, Inside & Out',
-    },
-    {
-        td1: 'Climate Controlled & Standard Units',
-        td2: 'Asphalt Paved'
-    },
-    {
-        td1: 'Brand New, Clean Facility',
-        td2: 'On-Site Management'
-    },
-    {
-        td1: 'Online Reservations & Payment',
-        td2: 'Easy Access of I-390'
-    },
-    {
-        td1: 'Key-Pad Operated Gate with 24-7 Access',
-        td2: 'Security Cameras with Replay',
-    },
-    {
-        td1: 'Fire Sprinklers for Climate Controlled Units ',
-        td2: 'Units are Drive-Up Accessible'
-    },
-];
-
-
-// for staging site only
-
 // export const featuresList = [
 
+//     {
+//         td1: 'Fully Fenced with Barbed Wire',
+//         td2: 'Well-Lit, Inside & Out',
+//     },
+//     {
+//         td1: 'Climate Controlled & Standard Units',
+//         td2: 'Asphalt Paved'
+//     },
+//     {
+//         td1: 'Brand New, Clean Facility',
+//         td2: 'On-Site Management'
+//     },
+//     {
+//         td1: 'Online Reservations & Payment',
+//         td2: 'Easy Access of I-390'
+//     },
 //     {
 //         td1: 'Key-Pad Operated Gate with 24-7 Access',
 //         td2: 'Security Cameras with Replay',
 //     },
 //     {
-//         td1: 'Fully Fenced Barbed Wire',
-//         td2: 'Well-Lit, Inside & Out'
-//     },
-//     {
-//         td1: 'Fire Sprinklers for Climate Controlled Units',
-//         td2: 'Asphalt Paved'
-//     },
-//     {
-//         td1: 'Climate-Controlled & Standard Units',
+//         td1: 'Fire Sprinklers for Climate Controlled Units ',
 //         td2: 'Units are Drive-Up Accessible'
-//     },
-//     {
-//         td1: 'A Brand New, Clean Facility',
-//         td2: 'On-Site Management',
-//     },
-//     {
-//         td1: 'Online Reservations & Payment',
-//         td2: 'Easy Access Off I-390'
 //     },
 // ];
 
-export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/location.svg',
-        label: 'Conveniently Located',
-        alt: 'Icon art displaying a location pin'
-    },
 
-     // for staging purpose changed location icon to lock
+// for staging site only
+
+export const featuresList = [
+
+    {
+        td1: 'Key-Pad Operated Gate with 24-7 Access',
+        td2: 'Security Cameras with Replay',
+    },
+    {
+        td1: 'Fully Fenced Barbed Wire',
+        td2: 'Well-Lit, Inside & Out'
+    },
+    {
+        td1: 'Fire Sprinklers for Climate Controlled Units',
+        td2: 'Asphalt Paved'
+    },
+    {
+        td1: 'Climate-Controlled & Standard Units',
+        td2: 'Units are Drive-Up Accessible'
+    },
+    {
+        td1: 'A Brand New, Clean Facility',
+        td2: 'On-Site Management',
+    },
+    {
+        td1: 'Online Reservations & Payment',
+        td2: 'Easy Access Off I-390'
+    },
+];
+
+export const feature = [
     // {
     //     // tslint:disable-next-line:max-line-length
-    //     path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/lock.svg',
-    //     label: 'Secure Storage',
-    //     alt: 'Icon of a locked padlock'
+    //     path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/location.svg',
+    //     label: 'Conveniently Located',
+    //     alt: 'Icon art displaying a location pin'
     // },
+
+     // for staging purpose changed location icon to lock
+    {
+        // tslint:disable-next-line:max-line-length
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/lock.svg',
+        label: 'Secure Storage',
+        alt: 'Icon of a locked padlock'
+    },
     {
         // tslint:disable-next-line:max-line-length
         path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/thermometer.svg',
@@ -97,37 +97,78 @@ export const feature = [
 
 export const aboutUs = [
     {
-        p: `390 Self Storage is Rochester’s newest self storage facility. With three self storage
-        facilities in the greater Rochester area; when it comes to self storage, we are your home
-        town experts. We work hard every day to provide the right mix of convenience, affordability,
-        and security for our patrons.`,
-    },
-    {
-        p: `We are conveniently located just minutes from the Greater Rochester Airport.  Our address
-        is 142 Buell Road. Take Route 390 and exit at Chili Avenue, exit 19. Cross straight over Chili
-        Avenue at the light where you will see our 390 Self Storage sign at the end of the driveway.
-        Follow that straight back to our office on the left.`,
-    },
-    {
-        p: `Our facility offers convenient 24-hour access, 7 days a week. We also offer 24-hour online
-        account management with bill payment available through our tenant on-line portal.`,
-    },
-    {
-        p: `We offer a fully fenced facility along with a drive up security gate. Our gate is fully lit
-        with security cameras and accessible only with personal access codes.  Our security also includes
-        well-lit drive-up standard units as well as throughout our climate-controlled buildings. Our entire
-        facility has enhanced security with 24 hours state of the art security cameras. This ensures a true
-        sense of safety at all times.`
-    },
-    {
-        p: `Our facility is the ideal solution to help you recover some of that elbow room around the house,
-        or to use during a move or a remodeling project. Our residential users love the affordable rate and
-        easy drive-up access. Our climate-controlled units are ideal for more delicate belongings such as
-        furniture, pictures and important documents. Businesses love our climate-controlled units for records
-        storage. Local businesses routinely store their supplies and inventory with us to save money over
-        expanding with commercial real estate prices.`
+        mainHeading: `Affordable Self Storage Units in Rochester`,
+        mainParagraph: `390 Self Storage is Rochester’s newest self storage facility. With three self
+                        storage facilities in the greater Rochester area; when it comes to self storage,
+                        we are your home town experts. We work hard every day to provide the right mix of
+                        convenience, affordability, and security for our patrons.`,
+        subHeading1: {
+            heading: `Conveniently Located`,
+            p1: `Just a couple minutes from the Greater Rochester Airport.`,
+            p2: `From I-390 traveling North: Take the Chile Ave. Exit 19. Cross straight over Chile Ave. onto Buell Rd.
+                 You'll see a sign on the left for 390 Self Storage.`,
+            p3: `From I-390 traveling South: Take the Chile Ave. exit 19. Turn left onto Chile Ave. and then turn right
+                 at the traffic light onto Buell Rd. You'll see a sign on the left fro 390 Self Storage.`
+        },
+        subHeading2: {
+            heading: `Security for your Possessions`,
+            p: `Beyond the standard padlock, 390 Self Storage offers additional security for your possessions.
+            The entire facility is fenced with barbed wire where more easily accessible. The security gate is
+            controlled by your unique personal access code. It is well-lit and constantly monitored by camera.
+            The entire facility is well-lit with state of the art security cameras that record 24 hours a day.
+            The climate controlled storage requires your access code to enter and has a fire protection
+            sprinkler system.`
+        },
+        subHeading3: {
+            heading: `For Your Convenience`,
+            p: `Convenience starts with drive-up accessibility, 24-7. Our online portal offers 24-hour account
+            management where payment can be made. For personal assistance, the on-site office management is
+            always happy to help.`
+        },
+        subHeading4: {
+            heading: `Storage Solutions`,
+            p: `390 Self Storage offers a wide array of unit sizes that will fit your needs for whatever time you may
+            need it. Climate controlled units(heated and cooled) are ideal for furniture, pictures, and important
+            documents. Businesses routinely store their supplies, inventory, and records to save on expensive
+            commercial office rates. Outside Storage is available for boats, RVs, and cars.`
+        },
+
     }
-];
+]
+
+// export const aboutUs = [
+//     {
+//         p: `390 Self Storage is Rochester’s newest self storage facility. With three self storage
+//         facilities in the greater Rochester area; when it comes to self storage, we are your home
+//         town experts. We work hard every day to provide the right mix of convenience, affordability,
+//         and security for our patrons.`,
+//     },
+//     {
+//         p: `We are conveniently located just minutes from the Greater Rochester Airport.  Our address
+//         is 142 Buell Road. Take Route 390 and exit at Chili Avenue, exit 19. Cross straight over Chili
+//         Avenue at the light where you will see our 390 Self Storage sign at the end of the driveway.
+//         Follow that straight back to our office on the left.`,
+//     },
+//     {
+//         p: `Our facility offers convenient 24-hour access, 7 days a week. We also offer 24-hour online
+//         account management with bill payment available through our tenant on-line portal.`,
+//     },
+//     {
+//         p: `We offer a fully fenced facility along with a drive up security gate. Our gate is fully lit
+//         with security cameras and accessible only with personal access codes.  Our security also includes
+//         well-lit drive-up standard units as well as throughout our climate-controlled buildings. Our entire
+//         facility has enhanced security with 24 hours state of the art security cameras. This ensures a true
+//         sense of safety at all times.`
+//     },
+//     {
+//         p: `Our facility is the ideal solution to help you recover some of that elbow room around the house,
+//         or to use during a move or a remodeling project. Our residential users love the affordable rate and
+//         easy drive-up access. Our climate-controlled units are ideal for more delicate belongings such as
+//         furniture, pictures and important documents. Businesses love our climate-controlled units for records
+//         storage. Local businesses routinely store their supplies and inventory with us to save money over
+//         expanding with commercial real estate prices.`
+//     }
+// ];
 
 export const testimonials = [
     {
