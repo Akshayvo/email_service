8d244cf448513d02d0b6ecd1197adcf1f6d77b7e
Sagar <sagar.g@codeparva.in>
2020-05-22 20:49:08 +0530

    production changes

--- stor-right/src/environments/environment.ts
+++ stor-right/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   errorsMails: [ 'website.dev@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'STOR-RIGHT STORAGE - Staging',
+  appId: 'STOR-RIGHT STORAGE - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://www.stor-right.com',
