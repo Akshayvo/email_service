6df4e0cf1abcb498a0c4c45f938afd4931ecb18f
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-05-27 15:55:04 +0530

    updated env file and deployed on production

--- access-self-storage/src/environments/environment.prod.ts
+++ access-self-storage/src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  errorsMails: ['sanghmitra.v@codeparva.in'],
+  errorsMails: ['website.dev@codeparva.in'],
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   senderEmail: 'errors@codeparva.in',
   appId: 'Access Self Storage - Testing',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: ``, // 29265
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28463 demo north
-  // APIKey: `dzJ0MFcxSjQqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 20149 access self storage
+  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28463 demo north
+  APIKey: `dzJ0MFcxSjQqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 20149 access self storage
 
   websiteUrl: ''
 };
