d7734fc1778dec722eb4e897d2ae64aa009b379a
Sagar <sagar.g@codeparva.in>
2020-05-28 13:27:54 +0530

    fixed unittypes availability for iOs devices

--- calallen-mini/src/environments/environment.ts
+++ calallen-mini/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'calallen',
+  appId: 'calallen - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `dzJ0N1c3SjMqME46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 27730
