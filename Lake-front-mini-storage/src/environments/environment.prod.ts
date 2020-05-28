2ef09d2d6a899e31fab5120ff42bd21f4f15cad7
Sagar <sagar.g@codeparva.in>
2020-05-28 15:01:52 +0530

    updated navigation and error handler service

--- Lake-front-mini-storage/src/environments/environment.prod.ts
+++ Lake-front-mini-storage/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   // errorsMails: ['website.dev@codeparva.in'],
   errorsMails: ['sanghmitra.v@codeparva.in'],
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  senderEmail: 'errors@codeparva.in',
+  senderEmail: 'errors@codeparva.in -Production',
   appId: 'Lake Front Mini Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
--- Lake-front-mini-storage/src/environments/environment.ts
+++ Lake-front-mini-storage/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Lake Front Mini Storage',
+  appId: 'Lake Front Mini Storage - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
