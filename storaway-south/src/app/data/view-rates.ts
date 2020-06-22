32505bab93ade1350430f39b7f2c66031fb38d47
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-06-22 20:39:11 +0530

    updated text

--- storaway-south/src/app/data/view-rates.ts
+++ storaway-south/src/app/data/view-rates.ts
@@ -1,8 +1,8 @@
 export const dataViewRates = [
     {
         p: `Stor-A-Way South provides clean, well-maintained self storage units at the lowest rates in
-        the area. We've been proudly serving our friends and family the community for over three decades.
-        We also offer outdoor boat, RV, and vehicle storage. Free up some space on the lawn today, and store
+        the area. We've been proudly serving our friends and family in the community for over three decades.
+        We also offer outdoor vehicle and RV storage. Free up some space on the lawn today, and store
         your vehicles with Stor-A-Way South! `,
     },
     {
