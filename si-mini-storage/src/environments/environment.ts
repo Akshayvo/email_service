8a45fdf7aa6a1e05e1c03e29dffeba491eb5a74a
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-06-22 23:08:24 +0530

    deployed to production

--- si-mini-storage/src/environments/environment.ts
+++ si-mini-storage/src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   type: 0,  // { API: 0, iframe: 1 }
-  errorsMails: [ 'website.dev@codeparva.in' ],
+  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Si Mini Storage - Staging',
+  appId: 'Si Mini Storage - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: `dzF0OFcwSjAqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 18009 mathis
