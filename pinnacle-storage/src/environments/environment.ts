a947c20e4aaf62068d537a256b5371dc4f2dde45
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-07-02 21:49:11 +0530

    ready for production

--- pinnacle-storage/src/environments/environment.ts
+++ pinnacle-storage/src/environments/environment.ts
@@ -14,9 +14,9 @@ export const environment = {
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://catskillselfstorage.com',
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
+  APIKey: `XjJ7OUYxVzZHN0k6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29167
+  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
   // APIKey: `dzJ0OFc0SjYqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464 (For 0 reservetion charges)
-  // APIKey: `XjJ7OUYxVzZHN0k6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29167
 };
 
 /*
