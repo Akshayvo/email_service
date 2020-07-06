144dd1110730644839154f0dcce503741a031e74
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-07-06 11:35:37 +0530

    updated photo gallery

--- Lake-front-mini-storage/src/app/data/galleryImage.ts
+++ Lake-front-mini-storage/src/app/data/galleryImage.ts
@@ -1,9 +1,37 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: ``,
+    url: `lake-front-mini-storage-row-of-units`,
     // tslint:disable-next-line:max-line-length
-    thumb: ``,
-    alt: ``
+    thumb: `lake-front-mini-storage-row-of-units`,
+    alt: `row of red metal self storage unit doors under a blue sky with puffy clouds`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `lake-front-mini-storage-unit-building`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `lake-front-mini-storage-unit-building`,
+    alt: `a single story metal building with a row of red self storage unit doors all closed under a cloudy sky`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `lake-front-mini-storage-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `lake-front-mini-storage-units`,
+    alt: `a single story metal self storage building with large red metal roll up doors under a blue sky with clouds`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `lake-front-mini-storage-front-gate`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `lake-front-mini-storage-front-gate`,
+    alt: ` a small open structure with a self-service kiosk outside of an electronic fence at the entrance of a self storage facility`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `lake-front-mini-storage`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `lake-front-mini-storage`,
+    alt: ` red self storage unit doors and wide parking lanes under a cloudy sky`
   },
 ];
