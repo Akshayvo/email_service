6f5592ec3787c6ee9a1866d650c4e56d21779b65
Sagar <sagar.g@codeparva.in>
2020-07-06 11:55:37 +0530

    fixed null check issue and updated navigation service

--- sentry/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
+++ sentry/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
@@ -277,6 +277,7 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
     this.selectedDescription = JSON.stringify(event.target.value);
     const indexValue = event.target.value;
     const index = this.lstPayTypes.findIndex(x => x.PayTypeDescription === indexValue);
+    if (this.lstPayTypes && this.lstPayTypes.length > 0) {
     this.PayTypeIDValue = this.lstPayTypes[index].PayTypeID;
     this.surchargeService.getIdPaytype(this.PayTypeIDValue);
     this.payRentForm.patchValue({
@@ -286,6 +287,7 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
         }
       }
     });
+  }
 
 
     if ( this.showInput) {
--- sentry/src/environments/environment.ts
+++ sentry/src/environments/environment.ts
@@ -8,11 +8,11 @@ export const environment = {
   errorsMails: [ 'website.dev@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Sentry Stor-ALL - Production',
+  appId: 'Sentry Stor-ALL - Testing',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
-  APIKey: `dzJ0N1c1SjcqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 27573 sentry stor-all
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
+  // APIKey: `dzJ0N1c1SjcqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 27573 sentry stor-all
 };
 
 /*
