ada9190191b4aa8e2e35dad54e8411b503156843
Sagar <sagar.g@codeparva.in>
2020-07-07 11:21:06 +0530

    fixed null check issue of paytypeid

--- access-self-storage/src/environments/environment.ts
+++ access-self-storage/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Access Self Storage - Production',
+  appId: 'Access Self Storage - Testing',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: ``, // 29265
