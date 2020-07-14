32ad4756e246f8ca08c2c57c798c9af6940a0490
Sagar <sagar.g@codeparva.in>
2020-07-14 14:08:46 +0530

    logo and gallery update

--- sandridge/src/app/header/header.component.ts
+++ sandridge/src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { navLinks } from '../data/nav';
 import { contact, socialLinks } from '../data/contact';
+import { UaParserService } from '../services/ua-parser.service';
 
 
 @Component({
@@ -14,10 +15,16 @@ export class HeaderComponent implements OnInit {
   navLinks: any;
   socialLinks: any;
   navbarCollapsed = true;
+  imagetype: any;
+  imageBaseUrl: any;
 
   constructor(
     private router: Router,
-  ) { }
+    private uaParserService: UaParserService,
+  ) {
+    this.imagetype = this.uaParserService.typeOfImages.toLowerCase();
+    this.imageBaseUrl = this.uaParserService.baseUrl;
+   }
 
   ngOnInit() {
     this.fetchNavigationLinks();
