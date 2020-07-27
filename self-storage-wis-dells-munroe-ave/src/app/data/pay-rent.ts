b307d03f2b6e22cb824d2b356f2b6a8dcbbde61f
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-07-27 14:30:20 +0530

    updated date formation on pay rent page

--- self-storage-wis-dells-munroe-ave/src/app/data/pay-rent.ts
+++ self-storage-wis-dells-munroe-ave/src/app/data/pay-rent.ts
@@ -1,7 +1,7 @@
 export const loginDetail = [
     {
         userName: `Tenant's Email Address`,
-        password: `Date of Birth in MMDDYYY format`,
+        password: `Date of Birth in MMDDYY format`,
         // example: {
         //     code: '1234',
         //     userName: 'smith',
