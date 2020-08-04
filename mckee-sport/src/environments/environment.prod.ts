f8661a2964a35c0360863a31c55dd50ef2f02de2
Sagar <sagar.g@codeparva.in>
2020-08-04 13:41:46 +0530

    fixed payment null check issue

--- mckee-sport/src/environments/environment.prod.ts
+++ mckee-sport/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   errorsMails: ['website.dev@codeparva.in'],
   senderEmail: 'errors@codeparva.in',
-  appId: 'McKeesport Storage',
+  appId: 'McKeesport Storage - Production',
   VERSION: require('../../package.json').version,
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
--- mckee-sport/src/environments/environment.ts
+++ mckee-sport/src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   production: false,
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
-  appId: 'McKeesport Storage',
+  appId: 'McKeesport Storage - Testing',
   VERSION: require('../../package.json').version,
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  APIKey: `dzJ0OFcySjIqOE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28228
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
+  // APIKey: `dzJ0OFcySjIqOE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28228
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
 };
 
 /*
