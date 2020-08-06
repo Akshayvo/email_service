495a51af1040aa4e4688a84fa7fa2b8111b1f850
Sagar <sagar.g@codeparva.in>
2020-08-06 22:42:23 +0530

    google tag manager update and null check for paytype

--- Cuba/src/environments/environment.prod.ts
+++ Cuba/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   websiteUrl: 'https://www.cubamostorage.com',
-  appId: 'Cuba',
+  appId: 'Cuba Self Storage - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
--- Cuba/src/environments/environment.ts
+++ Cuba/src/environments/environment.ts
@@ -9,11 +9,11 @@ export const environment = {
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   websiteUrl: 'https://www.cubamostorage.com',
-  appId: 'Cuba',
+  appId: 'Cuba Self Storage - Testing',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
-  APIKey: `dzJ0OFc1SjIqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // cuba
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
+  // APIKey: `dzJ0OFc1SjIqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // cuba
 };
 
 /*
