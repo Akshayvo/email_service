7084e6526281904b4812980344573c9ee7e85bcb
Sagar <sagar.g@codeparva.in>
2020-08-10 23:11:49 +0530

    environment file update

--- rifle/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
+++ rifle/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
@@ -474,7 +474,7 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
     this.makePaymentSubscribe$ = this.paymentService.makePayment(paymentData)
       .subscribe(paymentDataResponse => {
         this.showloaderForPayment = false;
-        if (!!paymentDataResponse.payTypeForResult.PaymentAmountTotal) {
+        if (paymentDataResponse && paymentDataResponse.PayTypeForResult && paymentDataResponse.PayTypeForResult.PaymentAmountTotal) {
           this.PaymentAmount = paymentDataResponse.PayTypeForResult.PaymentAmountTotal;
         }
         this.CCApprovalCode = paymentDataResponse.PayTypeForResult.CCApprovalCode;
