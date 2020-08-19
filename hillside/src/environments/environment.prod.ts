5983904af1faead032e2d50ebca509a80fe7121e
Sagar <sagar.g@codeparva.in>
2020-08-19 15:07:33 +0530

    deployed for production

--- hillside/src/environments/environment.prod.ts
+++ hillside/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   errorsMails: ['website.dev@codeparva.in'],
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   senderEmail: 'errors@codeparva.in',
-  appId: 'Hillside Self Storage - Staging',
+  appId: 'Hillside Self Storage - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `dzJ0OVcySjMqN046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29237
