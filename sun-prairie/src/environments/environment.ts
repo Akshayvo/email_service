e09206b32f1e1c1df984211a74128e00f3322089
Sagar <sagar.g@codeparva.in>
2020-08-25 20:29:40 +0530

    reserve unit form update

--- sun-prairie/src/environments/environment.ts
+++ sun-prairie/src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Sun Prairie Self Storage - Production',
+  appId: 'Sun Prairie Self Storage - Testing',
   VERSION:  require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
