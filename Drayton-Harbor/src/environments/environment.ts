c6d74e15ecd6abddf100e2fa85126bb558012d5e
Sagar <sagar.g@codeparva.in>
2020-08-27 12:41:50 +0530

    Google tag Manager update

--- Drayton-Harbor/src/environments/environment.ts
+++ Drayton-Harbor/src/environments/environment.ts
@@ -9,12 +9,12 @@ export const environment = {
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Drayton-Harbor - Production',
+  appId: 'Drayton-Harbor - Testing',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  APIKey: `dzJ0OVcwSjgqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` // 29083 drayton
+  // APIKey: `dzJ0OVcwSjgqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` // 29083 drayton
     // APIKey: `dzJ0OFc0SjYqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464 (For 0 reservetion charges)
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo 28463
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo 28463
 };
 
 /*
