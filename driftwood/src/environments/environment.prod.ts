a1f53000fec71104933471509d0cf1247a08279b
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-08-27 20:12:42 +0530

    deployed on production

--- driftwood/src/environments/environment.prod.ts
+++ driftwood/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   errorsMails: ['website.dev@codeparva.in'],
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   senderEmail: 'errors@codeparva.in',
-  appId: 'Driftwood Village Self Storage - Staging',
+  appId: 'Driftwood Village Self Storage - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `dTJXNkQ2UDRINE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // Driftwood Village
