41b33a384085a5e8e7caab181a91a2c5f54edbf2
Sagar <sagar.g@codeparva.in>
2020-08-31 11:27:35 +0530

    google tag manager update

--- storage-town/src/environments/environment.prod.ts
+++ storage-town/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   type: 0,  // { API: 0, iframe: 1 }
   errorsMails: ['website.dev@codeparva.in'],
   senderEmail: 'errors@codeparva.in',
-  appId: 'Storage- Town - Staging',
+  appId: 'Storage- Town - Production',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
--- storage-town/src/environments/environment.ts
+++ storage-town/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   errorsMails: ['sanghmitra.v@codeparva.in'],
   senderEmail: 'errors@codeparva.in',
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Storage- Town - Staging',
+  appId: 'Storage- Town - Testing',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
 };
