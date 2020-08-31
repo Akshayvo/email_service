c70abcf25e5d4892edcb33d350caf763efd4a8e4
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-08-31 22:33:30 +0530

    Fixed window.matchMedia

--- store-and-go/src/app/app.component.ts
+++ store-and-go/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject  } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { WINDOW } from '@ng-toolkit/universal';
+
 
 @Component({
   selector: 'app-root',
@@ -15,12 +17,13 @@ export class AppComponent implements OnInit {
   x: any;
   constructor(
     private router: Router,
-    private angulatics: Angulartics2GoogleAnalytics
+    private angulatics: Angulartics2GoogleAnalytics,
+    @Inject(WINDOW) private window: Window,
   ) {
 
   }
   ngOnInit() {
-    this.x = window.matchMedia('(max-width: 600px)');
+    this.x = this.window.matchMedia('(max-width: 600px)');
     this.flag = false;
     this.angulatics.eventTrack('Dev', {category: 'App initialized'});
     const onNavigationEnd = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
