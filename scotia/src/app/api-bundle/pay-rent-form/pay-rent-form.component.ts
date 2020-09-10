a65556355b81874aff16f0a6d9ec4af176f82fa4
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-09-10 13:21:37 +0530

    null check

--- scotia/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
+++ scotia/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
@@ -244,18 +244,21 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
   autoCardType(number: any) {
    this.cardType = this.getCardType(number.target.value);
    const index = this.lstPayTypes.findIndex(x => x.PayTypeDescription === this.cardType);
-   // tslint:disable-next-line: max-line-length
-   const cardTypeId = ((index > -1 ) ? this.lstPayTypes[index].PayTypeID : this.lstPayTypes[0].PayTypeID);
-   this.paytypeid =  cardTypeId;
-   this.surchargeService.getIdPaytype(this.paytypeid);
-   this.payRentForm.patchValue({
-     objPayment: {
-       PayType: {
-         PayTypeDescription: this.cardType,
-         PayTypeID: cardTypeId,
+   if (!!index) {
+     // tslint:disable-next-line: max-line-length
+     const cardTypeId = ((index > -1 ) ? this.lstPayTypes[index].PayTypeID : this.lstPayTypes[0].PayTypeID);
+     this.paytypeid =  cardTypeId;
+     this.surchargeService.getIdPaytype(this.paytypeid);
+     this.payRentForm.patchValue({
+       objPayment: {
+         PayType: {
+           PayTypeDescription: this.cardType,
+           PayTypeID: cardTypeId,
+         }
        }
-     }
-   });
+     });
+   }
+
   }
 
    getCardType(number: any) {
@@ -293,7 +296,9 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
     this.selectedDescription = JSON.stringify(event.target.value);
     const indexValue = event.target.value;
     const index = this.lstPayTypes.findIndex(x => x.PayTypeDescription === indexValue);
-    this.PayTypeIDValue = this.lstPayTypes[index].PayTypeID;
+    if (!!index) {
+      this.PayTypeIDValue = this.lstPayTypes[index].PayTypeID;
+    }
     this.surchargeService.getIdPaytype(this.PayTypeIDValue);
     this.payRentForm.patchValue({
       objPayment: {
@@ -375,21 +380,23 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
         // tslint:disable-next-line: max-line-length
           this.defaultCardType = ((Tenant.CCNumber) ? this.getCardType(Tenant.CCNumber) : this.lstPayTypes[0].PayTypeDescription);
           const index = this.lstPayTypes.findIndex(x => x.PayTypeDescription === this.defaultCardType);
-          // tslint:disable-next-line: max-line-length
-          const defaultCardPayTypeId = ((index > -1 ) ? this.lstPayTypes[index].PayTypeID : this.lstPayTypes[0].PayTypeID);
-
-          if (localStorage.getItem('strTenantToken')) {
-            this.paytypeid =  defaultCardPayTypeId;
-            this.surchargeService.getIdPaytype(this.paytypeid);
-            this.payRentForm.patchValue({
-              objPayment: {
-                PayType: {
-                  PayTypeDescription: this.defaultCardType,
-                  PayTypeID: defaultCardPayTypeId,
+          if (!!index) {
+            // tslint:disable-next-line: max-line-length
+            const defaultCardPayTypeId = ((index > -1 ) ? this.lstPayTypes[index].PayTypeID : this.lstPayTypes[0].PayTypeID);
+            if (localStorage.getItem('strTenantToken')) {
+              this.paytypeid =  defaultCardPayTypeId;
+              this.surchargeService.getIdPaytype(this.paytypeid);
+              this.payRentForm.patchValue({
+                objPayment: {
+                  PayType: {
+                    PayTypeDescription: this.defaultCardType,
+                    PayTypeID: defaultCardPayTypeId,
+                  }
                 }
-              }
-            });
+              });
+            }
           }
+
 
           this.payRentForm.patchValue({
             objPayment: {
