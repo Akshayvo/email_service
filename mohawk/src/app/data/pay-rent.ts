02ab7903e30827c96039590c38443df4fbed6064
Sagar <sagar.g@codeparva.in>
2020-09-14 18:42:05 +0530

    with error

---
+++ mohawk/src/app/data/pay-rent.ts
@@ -0,0 +1,11 @@
+export const loginDetail = [
+    {
+        userName: `Tenant's Last Name`,
+        password: `Tenant's gate access code`,
+        example: {
+            code: 'code is 1234',
+            userName: 'smith',
+            password: '1234'
+        }
+    }
+];
--- mohawk/src/environments/environment.ts
+++ mohawk/src/environments/environment.ts
@@ -4,10 +4,20 @@
 
 export const environment = {
   production: false,
+  type: 0,  // { API: 0, iframe: 1 }
+  alternateTenant: `navigateToMoveIn`, // {0, 1, navigateToReserve, navigateToMoveIn},
+  paymentPageType: 1, // { withoutTab: 0, withTab: 1}
+  reservetionFormType: 0, // { withoutAlternateDetail:0, withAlternateDetial: 1}
+  template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
   errorsMails: [ 'website.dev@codeparva.in' ],
   senderEmail: 'errors@codeparva.in',
+  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   appId: 'Mohawk Self Storage',
-  VERSION: require('../../package.json').version
+  VERSION: require('../../package.json').version,
+  baseUrl: `https://simapi.syrasoft.com/1/api/`,
+  websiteUrl: '',
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
 
 };
 
