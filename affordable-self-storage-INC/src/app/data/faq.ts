3505bd1e2bf2299f211c4b1704c92db5ce28fc20
Sagar <sagar.g@codeparva.in>
2020-09-16 12:12:25 +0530

    initial

---
+++ affordable-self-storage-INC/src/app/data/faq.ts
@@ -0,0 +1,67 @@
+export const faq = [
+    {
+        h2: `Is there a long-term lease?`,
+        paragraph: `No, the rental agreement is month-to-month.`
+    },
+    {
+        h2: `Can I switch units?`,
+        paragraph: `Contact our manager for details – we can prorate you out of your current unit and into a new one for no fees other than the difference in rent.`
+    },
+    {
+        h2: `When is my rental fee due?`,
+        paragraph: `Your rent is due on the first (1st) day of the month. If payment is not received by the end of the day on the 8th, a late fee of $20 is assessed, your gate code deactivated, and your unit will be over locked.`
+    },
+    {
+        h2: ` Will I receive a monthly "bill"?`,
+        paragraph: `We do not send a monthly bill. We offer a paperless automatic payment system from your credit or debit charge card. In the event that the bank rejects our payment request, an alert will be emailed.`
+    },
+    {
+        h2: `What forms of payment do you accept?`,
+        paragraph: `We accept check, cash, and major credit cards. You may mail your payment, pay via our web site or give us your credit card number via telephone. For units that are delinquent, we require cash or money order payment.`
+    },
+    {
+        h2: `What may I store?`,
+        paragraph: `Flammable, explosive, or illicit materials are not allowed. Occupying the unit as living quarters is prohibited by law and is cause for immediate eviction. Storage of live animals is not allowed. Food is prohibited.`
+    },
+    {
+        h2: `How tall / wide are your doors?`,
+        paragraph: `Door sizes vary by unit size and range from 3’8” to 11’ wide. Our roll-up doors are easy to operate and coil up once opened. If you have specific door requirements, talk to our manager and he or she will find the best solution for your need and be able to provide specific door measurements.`
+    },
+    {
+        h2: `Do I need to provide my own lock?`,
+        paragraph: `We provide a new disc lock at move in. Only one lock is allowed per door latch, and you must keep your unit locked at all times. We inspect all locks periodically. When you move out you must remove your lock, which is yours to keep.`
+    },
+    {
+        h2: `Does the facility assume any liability for the goods I store?`,
+        paragraph: `No, we recommend contacting your home or renter’s insurance agent. Adding stored goods to an existing policy is usually inexpensive, or your existing insurance may already cover stored goods.  Information on the MiniCo Tenant One policy is available at our facility – brochures are near the rental kiosk.`
+    },
+    {
+        h2: `Are surveillance cameras monitored?`,
+        paragraph: `The cameras are recording video any time motion is detected but they are not constantly monitored. Management is not responsible for immediately responding to help if required.`
+    },
+    {
+        h2: `May I allow friends or relatives to access my unit?`,
+        paragraph: `You may allow anyone you choose to have access by accompanying them on the premises or giving them your key and gate code. You remain responsible for your obligations on the lease regardless. Leases may not be transferred.
+`
+    },
+    {
+        h2: `Can I share access to the space with my family and coworkers?`,
+        paragraph: `Yes, you decide who may have access to your storage unit.`
+    },
+    {
+        h2: `What if I or one of my guests damages my unit or another building or fixture on the property?`,
+        paragraph: `The tenant shall be liable for any damage to the unit or any building, fixture, or other structure on the premises caused by tenant or his agents or guests. Tenant shall indemnify and hold Management harmless from any claim or cause of action arising out of Tenant’s use of the premises or any part thereof.`
+    },
+    {
+        h2: `Can I get a refund?`,
+        paragraph: `It is a policy that our self-storage facility does not prorate rental payments for any partial months. Much like a hotel rents in one night increments and does not refund your payment if you choose to leave at 5am, we rent in one month increments and do not offer refunds for partial months.`
+    },
+    {
+        h2: `What are the office hours?`,
+        paragraph: `We do not have an on-site office, but the owner lives nearby and is available to meet clients at the site. Most issues can be resolves via phone. `
+    },
+    {
+        h2: `What happens if I lose the key to my storage unit?`,
+        paragraph: `You hold the only key to your storage unit. However, if you should misplace it, we have procedures in place to help you access your storage unit. `
+    },
+];
