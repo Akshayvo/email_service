764743bf3fe70755de395473d439b162de7dd4ec
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-09-17 20:32:55 +0530

    deployed on production

--- rock-lake/src/environments/environment.prod.ts
+++ rock-lake/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   errorsMails: ['website.dev@codeparva.in'],
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   senderEmail: 'errors@codeparva.in',
-  appId: 'Rock Lake Storage - Staging',
+  appId: 'Rock Lake Storage - Production',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `XjJ7N0Y5VzVHMkk6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 27952
