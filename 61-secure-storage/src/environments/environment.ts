85e06c0d2c94cba15381cb32ee8e8a604feaff70
Sagar <sagar.g@codeparva.in>
2020-10-07 12:55:07 +0530

    view rates page update

--- 61-secure-storage/src/environments/environment.ts
+++ 61-secure-storage/src/environments/environment.ts
@@ -17,8 +17,8 @@ export const environment = {
   // baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // AnotherLocationAPIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // DemoNorthAPIKey: 'Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O',
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
-  // APIKey: `dTJXMEQxUDJINU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`  // 61 secure storage
+  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
+  APIKey: `dTJXMEQxUDJINU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`  // 61 secure storage
   // baseUrl: `http://localhost:60444/api/`,
 };
 
