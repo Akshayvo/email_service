16c6827c679c609983dfb85567dce5cbc0ada95e
Sagar <sagar.g@codeparva.in>
2020-10-13 18:36:23 +0530

    staging deployment

--- oswego/src/environments/environment.ts
+++ oswego/src/environments/environment.ts
@@ -21,7 +21,7 @@ export const environment = {
   // AnotherLocationAPIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // DemoNorthAPIKey: 'Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O',
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
-  APIKey: `XjJ7OEY3VzVHNUk6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28755 Oswego
+  APIKey: `dzJ0N1c1SjIqME46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 27520 Oswego
   // APIKey: `dzJ0OFc0SjYqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464 (For 0 reservetion charges)
   // baseUrl: `http://localhost:60444/api/`,
 };
