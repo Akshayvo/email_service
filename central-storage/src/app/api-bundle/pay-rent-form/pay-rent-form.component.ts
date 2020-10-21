027cf3b9871e8eb4983d416377e6dc0c93ee11df
Sagar <sagar.g@codeparva.in>
2020-10-21 17:47:43 +0530

    google tag manager update

--- central-storage/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
+++ central-storage/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
@@ -272,6 +272,7 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
     this.selectedDescription = JSON.stringify(event.target.value);
     const indexValue = event.target.value;
     const index = this.lstPayTypes.findIndex(x => x.PayTypeDescription === indexValue);
+    if (this.lstPayTypes && this.lstPayTypes.length > 0) {
     this.PayTypeIDValue = this.lstPayTypes[index].PayTypeID;
     this.surchargeService.getIdPaytype(this.PayTypeIDValue);
     this.payRentForm.patchValue({
@@ -281,6 +282,7 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
         }
       }
     });
+  }
 
 
     if ( this.showInput) {
@@ -468,7 +470,9 @@ export class PayRentFormComponent implements OnInit, OnDestroy {
     this.makePaymentSubscribe$ = this.paymentService.makePayment(paymentData)
       .subscribe(paymentDataResponse => {
         this.showloaderForPayment = false;
-        this.PaymentAmount = paymentDataResponse.PayTypeForResult.PaymentAmountTotal;
+        if (paymentDataResponse && paymentDataResponse.PayTypeForResult && paymentDataResponse.PayTypeForResult.PaymentAmountTotal) {
+          this.PaymentAmount = paymentDataResponse.PayTypeForResult.PaymentAmountTotal;
+        }
         this.CCApprovalCode = paymentDataResponse.PayTypeForResult.CCApprovalCode;
         if ( paymentDataResponse.intErrorCode === 1 ) {
           if (this.navigateToReserve) {
