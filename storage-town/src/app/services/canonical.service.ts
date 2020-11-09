58440cc2d02c092c6a9ad63ff9f398553f1cf2f4
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2020-11-09 19:11:01 +0530

    canonical tag for unit-sizer, about, direction

--- storage-town/src/app/services/canonical.service.ts
+++ storage-town/src/app/services/canonical.service.ts
@@ -19,28 +19,35 @@ export class CanonicalService {
      let newStr1 = str.replace('storage-units', 'unit-sizer');
      let newStr = str.replace('storage-units', 'about');
      let newStr2 = str.replace('storage-units', 'directions');
+
      console.log('newStr, newStr1, newStr2', newStr, newStr1, newStr2);
      console.log('pathname', pathname);
      if (environment.production) {
       const link: HTMLLinkElement = this.dom.createElement('link');
-      
       link.setAttribute('rel', 'canonical');
       this.dom.head.appendChild(link);
       link.setAttribute('href', `${environment.websiteUrl}${pathname}`);
+
+      if (str.includes("storage-units")) {
+        console.log('storage-units  included');
+        
+        const link1: HTMLLinkElement = this.dom.createElement('link');
+        link1.setAttribute('rel', 'canonical');
+        this.dom.head.appendChild(link1);
+        link1.setAttribute('href', `${newStr}`);
+      
   
-      link.setAttribute('rel', 'canonical');
-      this.dom.head.appendChild(link);
-      link.setAttribute('href', `${newStr}`);
-    
-      link.setAttribute('rel', 'canonical');
-      this.dom.head.appendChild(link);
-      link.setAttribute('href', `${newStr1}`);
-
-      link.setAttribute('rel', 'canonical');
-      this.dom.head.appendChild(link);
-      link.setAttribute('href', `${newStr2}`);
-
-     }
+        const link2: HTMLLinkElement = this.dom.createElement('link');
+        link2.setAttribute('rel', 'canonical');
+        this.dom.head.appendChild(link2);
+        link2.setAttribute('href', `${newStr1}`);
+  
+  
+        const link3: HTMLLinkElement = this.dom.createElement('link');
+        link3.setAttribute('rel', 'canonical');
+        this.dom.head.appendChild(link3);
+        link3.setAttribute('href', `${newStr2}`);
+       }
+      }
    }
-   
 }
