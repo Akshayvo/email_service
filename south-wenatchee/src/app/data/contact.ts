740efc32df0b0ec0b774c94934fe191ec1b739be
Sagar <sagar.g@codeparva.in>
2020-11-23 19:45:00 +0530

    initial update

---
+++ south-wenatchee/src/app/data/contact.ts
@@ -0,0 +1,50 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Address:',
+        data: '1536 S Wenatchee Ave, Wenatchee, WA 98801',
+        class: 'fas fa-map-marker-alt mx-2 iconColor',
+    },
+    {
+        id: 1,
+        label: 'Email:',
+        redirect: 'mailto:selfstorageinc@hotmail.com',
+        data: 'selfstorageinc@hotmail.com',
+        class: 'fas fa-envelope mx-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'tel:5096622600',
+        label: 'Phone:',
+        data: '(509) 662-2600',
+        class: 'fas fa-phone mx-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday - Friday: ',
+        data: '12:00PM to 5:00PM'
+    },
+    {
+        label: 'Saturday - Sunday: ',
+        data: 'CLOSED'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '7 Days a Week 6:30AM to 9:00PM'
+    }
+];
+
+export const socialLinks = [
+    {
+        label: 'twitter',
+        path: 'https://twitter.com/CatskillSS',
+        class: 'fab fa-twitter-square',
+    },
+    {
+        label: 'facebook',
+        path: 'https://www.facebook.com/catskillselfstorage',
+        class: 'fab fa-facebook-square',
+    },
+];
