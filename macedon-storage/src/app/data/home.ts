7d8b35610716441a2438738a2a8994aa4c7b1600
Sagar <sagar.g@codeparva.in>
2020-12-01 18:42:55 +0530

    home page updates

--- macedon-storage/src/app/data/home.ts
+++ macedon-storage/src/app/data/home.ts
@@ -1,69 +1,92 @@
 
 export const featuresList = [
     {
-        td1: 'Convenient Location ',
-        td2: 'Pin-Code Activated Gate',
+        td1: 'Key-Pad Operated Gate with 24-7 Access',
+        td2: 'Security Cameras with Replay',
     },
-    // {
-    //     td1: 'Online Reservations ',
-    //     td2: 'Awesome Customer Service',
-    // },
-    // {
-    //     td1: 'Drive-Up Access',
-    //     td2: 'Well-Lit Facility'
-    // },
-    // {
-    //     td1: 'Online Payment Portal',
-    //     td2: 'Fully-Fenced'
-    // },
-    // {
-    //     td1: 'Security Cameras',
-    // }
+    {
+        td1: 'Fully Fenced',
+        td2: 'Well-Lit, Inside & Out',
+    },
+    {
+        td1: 'Asphalt Paved',
+        td2: 'Units are Drive-Up Accessible'
+    },
+    {
+        td1: 'Clean Facility',
+        td2: 'On-Site Management'
+    },
+    {
+        td1: 'Online Reservations & Payment',
+        td2: `Easy Access Off Rt. 31`
+    }
 ];
 
 export const feature = [
     {
         // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Macedon/location.svg',
-        label: 'Conveniently Located',
-        alt: 'Icon art displaying a location pin'
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/lock.svg',
+        label: 'Secure Storage',
+        alt: 'Icon of a locked padlock'
     },
     {
         // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Macedon/team.svg',
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/team.svg',
         label: 'Locally Owned and Operated',
         alt: 'Icon of people standing together '
     },
     {
         // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Macedon/garage.svg',
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/garage.svg',
         label: 'Boat and Recreational Vehicle Storage',
         alt: 'Icon art displaying boat and motorcycle'
     },
     {
         // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Macedon/24-hours.svg',
+        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/24-hours.svg',
         label: '24 Hour Facility Access',
         alt: 'Icon art displaying clock hands and the number 24'
     },
 ];
 
 
+// export const aboutUs = [
+//     {
+//         p: `Macedon Storage has been proudly serving Wayne County with high-quality self storage and recreational vehicle parking services since 2006. We are locally owned & operated, and we strive to provide the highest level of customer service at extremely competitive rates.`,
+//     },
+//     {
+//         p: `Our facility is our second location in the Greater Rochester Area. We also own Linden Self Storage in Pittsford, NY, and are excited to announce the grand opening of our third location, 390 Self Storage on Buell Road in Rochester, NY. 
+//         `,
+//     },
+//     {
+//         p: `All three of our facilities are high-quality buildings with every modern convenience, and we make the security of your belongings our highest priority. Macedon Storage is fully-fenced, and well-lit with ample security camera covered. `,
+//     },
+//     {
+//         p: `If you have any questions about our services that are not answered on our website, please reach out to our office at (585) 381-6557 `
+//     }
+// ];
+
 export const aboutUs = [
     {
-        p: `Macedon Storage has been proudly serving Wayne County with high-quality self storage and recreational vehicle parking services since 2006. We are locally owned & operated, and we strive to provide the highest level of customer service at extremely competitive rates.`,
-    },
-    {
-        p: `Our facility is our second location in the Greater Rochester Area. We also own Linden Self Storage in Pittsford, NY, and are excited to announce the grand opening of our third location, 390 Self Storage on Buell Road in Rochester, NY. 
-        `,
-    },
-    {
-        p: `All three of our facilities are high-quality buildings with every modern convenience, and we make the security of your belongings our highest priority. Macedon Storage is fully-fenced, and well-lit with ample security camera covered. `,
-    },
-    {
-        p: `If you have any questions about our services that are not answered on our website, please reach out to our office at (585) 381-6557 `
+        mainHeading: `Affordable Storage Units and RV Parking in Macedon NY`,
+        mainParagraph: `Macedon Self Storage is proud to offer high quality self storage and RV parking since 2006.
+        When it comes to self storage services, we are your local experts with three self storage facilities in the
+        Rochester area. We work hard every day to make our facilities secure, convenient, and affordable.`,
+        subHeading2: {
+            heading: `Security for Your Possessions`,
+            p: `Macedon Self Storage goes beyond the standard padlock on a unit to offer additional protection for
+            your belongings. Our facility is fenced with an electronic security gate that is controlled by your
+            unique access code issued during move in. We have security cameras recording 24 hours a day, and the facility is well-lit.`
+        },
+        subHeading3: {
+            heading: `For Your Convenience`,
+            p: `Our facility offers 24/7 drive-up access to your units. We also have a 24-hour account management
+            portal where your rent can be conveniently paid. For personal assistance, our management team is always
+            happy to help. Call (585) 381-6557.`
+        },
+
     }
-];
+]
 
 
 export const gettingStarted = `Are you ready to start de-cluttering your home or reclaiming that space in your yard? Click below to reserve your unit or RV parking space today!`;
