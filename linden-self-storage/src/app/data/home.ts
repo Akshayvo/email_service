ac66094e2bad02f248360712d34228b2ea0e6871
Sagar <sagar.g@codeparva.in>
2020-12-01 16:55:20 +0530

    case no 23976 updates still not deployed

--- linden-self-storage/src/app/data/home.ts
+++ linden-self-storage/src/app/data/home.ts
@@ -1,23 +1,28 @@
 
 export const featuresList = [
     {
-        td1: 'Conveniently Located ',
-        td2: 'Affordable Rates',
+        td1: 'Key-Pad Operated Gate with 24-7 Access',
+        td2: 'Security Cameras with Replay',
     },
     {
-        td1: 'Online Reservations ',
-        td2: 'Awesome Customer Service',
+        td1: 'Fully Fenced',
+        td2: 'Well-Lit, Inside & Out',
     },
     {
-        td1: 'Drive-Up Access',
-        td2: 'Well-Lit Facility'
+        td1: 'Keypad Entry for Climate Controlled Units',
+        td2: 'Asphalt Paved'
     },
     {
-        td1: 'Online Payment Portal',
-        td2: 'Fully-Fenced'
+        td1: 'Climate Controlled & Standard Units',
+        td2: 'Units are Drive-Up Accessible'
     },
     {
-        td1: 'Security Cameras',
+        td1: 'Clean Facility',
+        td2: 'On-Site Management'
+    },
+    {
+        td1: 'Online Reservations & Payment',
+        td2: 'Easy Access from Rt. 441'
     }
 ];
 
@@ -49,21 +54,60 @@ export const feature = [
 ];
 
 
+// export const aboutUs = [
+//     {
+//         p: `Linden Self Storage is proud to offer Monroe County a state-of-the-art storage facility in Pittsford, New York. We cater to both 
+//             residential and commercial tenants with a wide variety of self storage unit types and sizes.`,
+//     },
+//     {
+//         p: `Need a little extra protection from the elements for your belongings? Ask about our climate controlled units! Need a place to 
+//             store that boat or RV? We've got outdoor units perfectly suited!`,
+//     },
+//     {
+//         p: `As a privately held, owner-operated company, started in 2004, we strive to bring you the safest, cleanest, and most secure 
+//             storage facility possible. All of our locations are backed by excellent customer service and easy access to your 
+//             account information 24/7 through our website!`,
+//     },
+// ];
+
 export const aboutUs = [
     {
-        p: `Linden Self Storage is proud to offer Monroe County a state-of-the-art storage facility in Pittsford, New York. We cater to both 
-            residential and commercial tenants with a wide variety of self storage unit types and sizes.`,
-    },
-    {
-        p: `Need a little extra protection from the elements for your belongings? Ask about our climate controlled units! Need a place to 
-            store that boat or RV? We've got outdoor units perfectly suited!`,
-    },
-    {
-        p: `As a privately held, owner-operated company, started in 2004, we strive to bring you the safest, cleanest, and most secure 
-            storage facility possible. All of our locations are backed by excellent customer service and easy access to your 
-            account information 24/7 through our website!`,
-    },
-];
+        mainHeading: `Affordable Self Storage Units in Monroe County, Pittsford NY`,
+        mainParagraph: `Linden Self Storage is proud to offer a state-of-the-art, complete storage facility. When it comes to self
+        storage, we are your hometown experts with three self storage facilities in the greater Rochester area. We work hard every
+        dya to provide the right mix of affordability, security and convenience for our patrons.`,
+        // subHeading1: {
+        //     heading: `Conveniently Located`,
+        //     p1: `Just a couple minutes from the Greater Rochester Airport.`,
+        //     p2: `<b>From I-390 traveling North:</b> Take the Chile Ave. Exit 19. Cross straight over Chile Ave. onto Buell Rd.
+        //          You'll see a sign on the left for 390 Self Storage.`,
+        //     p3: `<b>From I-390 traveling South:</b> Take the Chile Ave. exit 19. Turn left onto Chile Ave. and then turn right
+        //          at the traffic light onto Buell Rd. You'll see a sign on the left for 390 Self Storage.`
+        // },
+        subHeading2: {
+            heading: `Security for Your Possessions`,
+            p: `Beyond the standard padlock, Linden Self Storage offers additional security for your possessions. The entire
+            facility is fenced with a security gate controlled by you with your unique access code. Security cameras record
+            activity 24 hours a day and the facility is well-lit. Climate controlled storage requires your access code to enter.`
+        },
+        subHeading3: {
+            heading: `For Your Convenience`,
+            p: `Linden Self Storage offers a number of modern conveniences to make your say with us enjoyable. Many of our
+            units have drive-up accessibility, and all units are accessible 24-7. Our online customer management portal
+            provides 24-hour account access where your rent payment can be made. For personal assistance during office hours,
+            our on-site management team is always ready to help.`
+        },
+        subHeading4: {
+            heading: `Storage Solutions`,
+            p1: `Linden Self Storage offers a number of unit sizes that will fit your need for self storage however long you may have it.`,
+            l1: `<b>Standard,</b> Drive-Up Accessible Units`,
+            l2: `<b>Climate Controlled</b> (heated & cooled) units, ideal for furniture, pictures, and important documents. Businesses routinely
+            store their supplies, inventory, and records to save on expensive commercial office space.`,
+            l3: `<b>Outside Storage</b>for boats, RVs,campers, and cars.`
+        },
+
+    }
+]
 
 
 export const gettingStarted = `Are you ready to begin de-cluttering your home, or getting some of that elbow room back around the 
