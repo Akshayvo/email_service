875172096c91dab21cf8d6c1c6e55317dc657597
Sagar <sagar.g@codeparva.in>
2021-01-11 12:40:59 +0530

    initial update

---
+++ easy-storage/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+  type: 0,  // { API: 0, iframe: 1 }
+  locationName: 'new-york/baldwinsville',  // state/city
+  signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
+  alternateType: `navigateToMoveIn`, // { moveInAlternateType, reservationAlternateType, alternateType, noAlternateType },
+  template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
+  errorsMails: ['website.dev@codeparva.in'],
+  senderEmail: 'errors@codeparva.in',
+  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
+  appId: 'Sample Self Storage - North',
+  facilityName: 'Sample Self Storage - North',
+  VERSION: require('../../package.json').version,
+  baseUrl: `https://simapi.syrasoft.com/1/api/`,
+  websiteUrl: 'https://sample-self-storage-staging.netlify.app',
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
+};
