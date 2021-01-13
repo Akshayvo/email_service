b8b3862f67b76449ae3974783fcb347d8f12da15
Sagar <sagar.g@codeparva.in>
2021-01-14 00:54:09 +0530

    pay rent instruction update

--- affordable-storage-kingwood/src/app/data/pay-rent.ts
+++ affordable-storage-kingwood/src/app/data/pay-rent.ts
@@ -1,11 +1,11 @@
 export const loginDetail = [
     {
         userName: `First Initial + Tenant’s Last Name`,
-        password: `Tenant's Unit Number`,
+        password: `Tenant's Unit Number (Must be at least 3 digits)`,
         example: {
-            code: `John Smith's Unit Number is 1234`,
+            code: `John Smith's Unit Number is 7`,
             userName: 'jsmith',
-            password: '1234'
+            password: '007'
         }
     }
 ];
