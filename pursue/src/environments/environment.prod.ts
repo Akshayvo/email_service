fc7f0711c27a20c56ab812b7a8216038b138413a
sanghmitra13194 <72544328+sanghmitra13194@users.noreply.github.com>
2021-01-20 21:04:48 +0530

    case 24328

--- pursue/src/environments/environment.prod.ts
+++ pursue/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  locationName: 'new-york',
+  locationName: 'illinois',
   type: 0,  // { API: 0, iframe: 1 }
   paymentPageType: 0, // { withoutTab: 0, withTab: 1}
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
