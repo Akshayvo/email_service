c74c9c9a313f61f621c66072846d1c223b68837e
pragathi <pragathi.n@codeparva.in>
2021-02-05 11:58:20 +0530

    review-comment

--- railroad/src/app/data/galleryImage.ts
+++ railroad/src/app/data/galleryImage.ts
@@ -39,25 +39,35 @@ export const galleryHome = [
 
 export const galleryViewRates = [
   {
-    // tslint:disable-next-line: max-line-length
-    url: `inside-of-self-storage-unit`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `inside-of-self-storage-unit`,
-    alt: ` interior of a self storage unit with clean concrete floor and wooden walls`
-  },
-  {
-    // tslint:disable-next-line: max-line-length
-    url: `interior-of-self-storage-unit`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `interior-of-self-storage-unit`,
-    alt: ` interior of a self storage facility unit with concrete flooring and wooden walls`
-  },
-  {
-    // tslint:disable-next-line: max-line-length
     url: `self-storage-faciliy`,
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-faciliy`,
     alt: ` self storge unit buildings with large parking areas and one building with oversized doors`
+
+    
+  },
+  {
+    url: `self-storage-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `self-storage-units`,
+    alt: ` a row of self storage units with metal roll up doors`
+
+    
+  },
+  {
+    url: `storage-unit-sign`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `storage-unit-sign`,
+    alt: ` self storage facility sign with Railroad Street Self Storage printed next to the contact information`
+    // tslint:disable-next-line: max-line-length
+    
+  },
+  {
+     // tslint:disable-next-line: max-line-length
+     url: `storage-units-at-night`,
+     // tslint:disable-next-line:max-line-length
+     thumb: `storage-units-at-night`,
+     alt: ` self storage unit buildings with large parking lanes for backing up and turning around`
   },
   {
     // tslint:disable-next-line: max-line-length
@@ -65,12 +75,5 @@ export const galleryViewRates = [
     // tslint:disable-next-line:max-line-length
     thumb: `storage-units`,
     alt: ` storage units photographed at night with ample lighting`
-  },
-  {
-    // tslint:disable-next-line: max-line-length
-    url: `storage-units-at-night`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `storage-units-at-night`,
-    alt: ` self storage unit buildings with large parking lanes for backing up and turning around`
   },
 ];
