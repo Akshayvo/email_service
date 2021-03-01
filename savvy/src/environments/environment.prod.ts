e2d57235c8b5fd7e90940ac2af97331f02931b39
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2021-03-01 20:51:58 +0530

    updated robots.txt

--- savvy/src/environments/environment.prod.ts
+++ savvy/src/environments/environment.prod.ts
@@ -12,5 +12,5 @@ export const environment = {
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `UjJmOGo1fjVCNmE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28556
-  websiteUrl: 'https://www.savvy-storage.com/'
+  websiteUrl: 'https://www.savvy-storage.com'
 };
