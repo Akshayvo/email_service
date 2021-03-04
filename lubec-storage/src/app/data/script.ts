d3fd65157d62d780743fd56c07c6e066222eff5e
pragathi <pragathi.n@codeparva.in>
2021-03-04 14:29:04 +0530

    seo updates

--- lubec-storage/src/app/data/script.ts
+++ lubec-storage/src/app/data/script.ts
@@ -98,12 +98,12 @@ export const homePageScript = [ {
       "postalCode": script.postalCode,
       "addressCountry": script.addressCountry
     },
-   "aggregateRating": {
-      "@type": "AggregateRating",
-      "ratingValue": script.ratingValue,
-      "bestRating": script.bestRating,
-      "reviewCount": script.reviewCount
-    },
+  //  "aggregateRating": {
+  //     "@type": "AggregateRating",
+  //     "ratingValue": script.ratingValue,
+  //     "bestRating": script.bestRating,
+  //     "reviewCount": script.reviewCount
+  //   },
     "geo": {
       "@type": "GeoCoordinates",
       "latitude": script.latitude,
