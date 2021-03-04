68818a5e3757108d380a36d0abf0fbf10592db68
bala <balasubramanian.k@codeparva.in>
2021-03-04 19:55:52 +0530

    unit sizes change

--- on-the-move/src/app/unit-sizes/unit-sizes.component.ts
+++ on-the-move/src/app/unit-sizes/unit-sizes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { unitSizer } from '../data/unitSizer';
-import { Title, Meta } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
@@ -24,7 +23,7 @@ export class UnitSizesComponent implements OnInit {
 
   ngOnInit() {
     this.i = 0;
-    this.h = 7;
+    this.h = 3;
     this.j = this.i + 1;
     this.fetchUnitSizer();
     window.scrollTo(0, 0);
@@ -49,7 +48,7 @@ export class UnitSizesComponent implements OnInit {
     this.j = this.i;
     this.i = this.h;
     if ( this.h === 0 ) {
-      this.h = 7;
+      this.h = 3;
     } else {
       this.h = this.h - 1;
     }
@@ -58,7 +57,7 @@ export class UnitSizesComponent implements OnInit {
   public moveRight() {
     this.h = this.i;
     this.i = this.j;
-    if ( this.j === 7 ) {
+    if ( this.j === 3 ) {
       this.j = 0;
     } else {
     this.j = this.j + 1;
@@ -68,9 +67,9 @@ export class UnitSizesComponent implements OnInit {
   public activeUnit(unitId: number) {
     this.i = unitId;
     if ( this.i === 0 ) {
-      this.h = 7;
-      this.j = this.i + 7;
-    } else if ( this.i === 7 ) {
+      this.h = 3;
+      this.j = this.i + 3;
+    } else if ( this.i === 3 ) {
       this.j = 0;
       this.h = this.i - 1;
     } else {
