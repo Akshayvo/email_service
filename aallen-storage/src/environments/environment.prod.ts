754f5572e6e5b67c8ef07e5dc211066a3a05b5a9
bala <balasubramanian.k@codeparva.in>
2021-03-16 17:56:07 +0530

    Seo Related changes

--- aallen-storage/src/environments/environment.prod.ts
+++ aallen-storage/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'northdakota/fargo', // state/city  **Remeber to update locationName in sitemap.xml as well
+  locationName: 'north-dakota/fargo', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: false, reserve: false }, // { moveIn: true, reserve: true },
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
