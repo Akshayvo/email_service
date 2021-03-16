a39bfe6755bb655dba60b1542aac61bfbcbfc526
pragathi <pragathi.n@codeparva.in>
2021-03-16 13:17:26 +0530

    deactivate reservation

--- hwy-77-self-storage/src/app/data/view-rates.ts
+++ hwy-77-self-storage/src/app/data/view-rates.ts
@@ -21,9 +21,9 @@ export const th = [
     {
       data: `Deposit`
     },
-    {
-      data: `Reserve a Unit`
-    },
+    // {
+    //   data: `Reserve a Unit`
+    // },
     {
       data: `Move In`
     },
