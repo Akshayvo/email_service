094e95349536535f4979da108c9ade44c5dfd6bf
pragathi <pragathi.n@codeparva.in>
2021-03-17 19:11:24 +0530

    logo and content update

--- allender-storage/src/app/data/home.ts
+++ allender-storage/src/app/data/home.ts
@@ -22,17 +22,34 @@ export const featuresList = [
 
 export const aboutUsAlt = `a metal roll up self storage unit with luggage, a bike, shelves, and boxes inside`;
 
-export const aboutUsHeading = `Affordable Storage Units in White Marsh, Maryland`;
+export const aboutUsHeading = `Affordable and Secure Storage in White Marsh, Maryland`;
 
 export const aboutUs = [
     {
-        p: `Allender Storage is a locally owned and operated self storage facility located in White Marsh, Maryland. We are conveniently located on Allender Road across from Freestate Indoor Sports. Thanks to our decade of self storage experience, you’ll find all of the modern conveniences and security considerations required to make your next storage project a breeze. From our contactless rentals through our website to our 24-hour access policies, all you have to worry about is what to do with all that extra space around the house or office.`,
+        p: `Our fenced in and gated facility features free 24/7 access, 24-hour video surveillance,
+        physical security patrols, clean and well-lit ground level units, an integrated access gate
+        and extra wide aisles to provide you with the convenience and security you’re looking
+        for in a self-storage facility. You will find that we provide a variety of unit sizes from 5’x8’
+        units that are perfect for seasonal clothing and decorations up to our oversized 08’x40’
+        units that can hold the equipment and inventory of a growing business!`,
     },
     {
-        p: `We are well-lit, fenced, and access to the facility is controlled with a pin-code activated gate to provide a sense of security as you come and go from your self storage units. You’ll find we provide a number of unit sizes from 5-foot by 8-foot units that are perfect for seasonal clothing and decorations up to our oversized 15-foot by 20-foot units that can hold the equipment and inventory of a growing business!`,
+        p: `Take a moment to review our rates and availability, then use our easy online reservation
+        form or online move-in option to secure your unit or parking space. Whether you are a
+        local homeowner or renter in need of some extra space around your home, or a growing
+        business that values saving money on storage costs, Allender Storage has you
+        covered! Our friendly, knowledgeable staff is available to answer any questions you
+        may have, so simply give us a call, or fill out our contact form!`,
     },
     {
-        p: `Take a moment to review our rates and availability, then use our contactless rental process to fill out your lease, pay your first months’ charges, and receive your unique access pin-code! Whether you’re a local homeowner or renter in need of some extra space around your home or a growing business that values saving money on storage costs, Allender Storage has you covered! Our friendly staff are available to answer any questions you may have, simply give us a call, or fill out our contact form!`,
+        p: `With our family owned and operated business, we guarantee access to the owners and
+        managers whenever you need them. Store more for less and call us today so you can
+        have the best possible storage experience yet!`,
+    },
+    {
+        p:`-Your Allender Storage Family
+        Owners, Mike & Kathy Wilmot; Son, Chris Wilmot & Daughter in Law, Jenna Wilmot
+        (with their kids) and their Dog, Bentley (also our number one Lock Salesman).`
     }
     
 ];
@@ -70,7 +87,14 @@ export const feature = [
 
 export const jumbotron = [
     {
-        h2: `24-Hour Access Storage Units in White Marsh`,
-        blurb: `Allender Storage is the one-stop storage solution for White Marsh. We have been serving White Marsh for over two decades with clean, well-maintained self storage services. Our facility offers a variety of self storage unit sizes perfect to help you free up some elbow room around the home, office, or worksite. Whether you're a homeowner trying to clean their garage out or a local business in search of a place for that excess inventory or supplies, Allender storage has a unit for you!`,
+        h2: `24-Hour Access Self Storage Units in White Marsh, Maryland`,
+        blurb: `Allender Storage is a family owned and operated self-storage facility in White Marsh,
+        Maryland. We are conveniently located off Pulaski Highway on Allender Road (across
+        the street from Freestate Indoor Sports Arena) - and easily accessible to Baltimore
+        County residents and the surrounding areas. Our facility offers a variety of affordable
+        self-storage units and parking to meet all your residential and commercial storage
+        needs. Thanks to our extensive history of self-storage experience (established in 2004),
+        you will find all the modern self-storage conveniences and security protocols necessary
+        to make your storage experience a breeze.`,
     }
 ];
