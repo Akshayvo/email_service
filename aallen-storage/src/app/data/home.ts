12d1a502246bf5532adb2911fa83465c6c3236a6
pragathi <pragathi.n@codeparva.in>
2021-03-25 15:00:28 +0530

    text changes and deployed on production

--- aallen-storage/src/app/data/home.ts
+++ aallen-storage/src/app/data/home.ts
@@ -50,16 +50,14 @@ export const aboutUsHeading = `Affordable Self-Storage Units in the Fargo-Moorhe
 
 export const aboutUs = [
     {
-        p: `Aallen Self Storage provides Self-Storage space and 'For Sale by Owner' vehicle parking pads at competitive prices. We are your one-stop self-storage solution. We understand the key to success is building long-term relationships with our valued customers. That’s why we go the extra mile to provide you the best experience possible.`,
+        p: `Aallen Self Storage provides self-storage space and 'For Sale by Owner' vehicle parking pads at competitive prices. We are your one-stop self-storage solution. We understand the key to success is building long-term relationships with our valued customers. That’s why we go the extra mile to provide you the best experience possible.
+        Whether you utilize our full-service website or our 24/7 on-site kiosk, you can rent, reservse, or pay for your storage space at your convenience. You’ll have 24-hour access to a secure, well-lit, high-tech, self storage facility, and you will be able to pay online or through our on-site kiosk 24-hours a day.`,
     },
     {
-        p: `Whether you utilize our full-service website or our 24/7 on-site kiosk, you can rent or reserve your storage space at your convenience. You’ll have 24-hour access to a secure, well-lit, high-tech, self storage facility, and you will be able to pay online or through our kiosk 24-hours a day.`,
+        p: `We understand the need for self-storage. That is why we strive to provide all of the modern conveniences that make self-storage projects fast and easy. You can start the contactless rental process by viewing our available storage units, and renting or reserving your unit on our website. Once the rental process complete, you will have 24-hour access to a secure, well-lit, clean, and well-maintained self-storage unit. You will rest easy knowing our facility is covered by strategically placed security cameras, continuously recording every movement.`,
     },
     {
-        p: `We understand the need for self-storage. That is why we strive to provide all of the modern conveniences that make self-storage projects fast and easy. You can start the contactless rental process by viewing our available storage units, and renting or reserving your unit on our website. Once the rental process complete, you will have 24-hour access to a secure, well-lit, clean, and well-maintained self-storage unit. You will rest easy knowing our facility is covered by strategically placed security cameras.`,
-    },
-    {
-        p: `Prefer doing business on-site? Our 24-hour, on-site customer service kiosk can complete the rental of new or additional storage units in minutes. It can also accept cash, check, or credit card payments 24-hours a day. So whether you are someone preparing for a move, a college student in search of summer storage, or you are growing a business that needs a little extra elbow room, Aallen Self Storage has you covered!`,
+        p: `Prefer doing business on-site? Our 24-hour, on-site customer service kiosk can complete the rental of new or additional storage units in minutes. It accepts cash, check, or credit card payments 24-hours a day. So whether you are someone preparing for a move, a college student in search of summer storage, or you are growing a business that needs a little extra elbow room, Aallen Self Storage has you covered!`,
     },
     {
         p: `Take a few moments to review our available storage units and rent online, or stop by our facility to utilize our on-site kiosk, which is available 24-hours per day. Our friendly staff would love to make your next storage project a fantastic experience!`,
@@ -68,16 +66,17 @@ export const aboutUs = [
         p: `Have questions? The staff at Aallen Self Storage is available to answer any questions or assist you with a contactless rental or reservation. Simply give us a call, and a friendly, knowledgeable member of our staff will be glad to help. We look forward to making your next storage project easy and affordable!`,
     },
     
+    
 ];
 export const aboutUsBold = [
     {
-        b: `By the way, if you own a car, truck, trailer, or camper you'd like to turn into cash, consider utilizing one of 'For Sale by Owner' parking pads. With more than 27,000 vehicles seeing your 'For Sale by Owner' vehicle everyday, you will very likely sell it in a matter of days. Where else can you get such great exposure for such an affordable rate?`
+        b: `By the way, if you own a car, truck, trailer, or camper you'd like to turn into cash, consider utilizing one of our 'For Sale by Owner' parking pads. With more than 27,000 vehicles seeing your 'For Sale by Owner' vehicle everyday, you will very likely sell it in a matter of days. Where else can you get such great exposure for such an affordable rate?`
     }
 ];
 
 export const jumbotron = [
     {
-        h1: `Affordable 24/7 Self Storage in the Fargo-Moorhead Arena`,
+        h1: `Affordable 24/7 Self Storage in the Fargo-Moorhead Area`,
         blurb: `Aallen Self Storage is your one-stop self-storage solution. Since 2009, we have provided the Fargo-Moorhead area with clean, secure, high-quality, self-storage services, as well as,  'For Sale by Owner' vehicle parking pads, both at affordable prices. Our locally owned and operated self-storage facility offers storage units of different sizes to accommodate your extra space needs. We are conveniently located just a stone’s throw away from the intersection of I-29 and Main Avenue. The facility entrance is on Main Avenue, right at the 34th Street South stoplight, making coming and going from your unit a breeze.`,
     }
 ];
