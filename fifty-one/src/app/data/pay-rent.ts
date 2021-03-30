36c81f31a55c44588b70eb02c6652e8774dd1ff5
bala <balasubramanian.k@codeparva.in>
2021-03-30 13:39:34 +0530

    Pay rent login page changes

--- fifty-one/src/app/data/pay-rent.ts
+++ fifty-one/src/app/data/pay-rent.ts
@@ -3,7 +3,7 @@ export const loginDetail = [
         userName: `Tenant's Email Address`,
         password: `Tenant's Gate Code(Minus the # Symbol)`,
         example: {
-            code: `John Smith's Gate Code is: 0123#`,
+            code: `Gate Code is: 0123#`,
             userName: 'jsmith@xyz.com',
             password: '0123'
         }
