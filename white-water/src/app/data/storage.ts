8700302c34241fa4c680532b29bf23b740b63b6a
pragathi <pragathi.n@codeparva.in>
2021-04-01 14:14:21 +0530

    whitewater setup

--- white-water/src/app/data/storage.ts
+++ white-water/src/app/data/storage.ts
@@ -1,10 +1,8 @@
 export const storageTip = [
     {
         data: `Try to use boxes that are a uniform size, they’re easier to stack and
-        try to keep the heavy ones on the bottom and the light ones on top. `
-    },
-    {
-        data: `Leave small walkways between the boxes and furniture in your
+        try to keep the heavy ones on the bottom and the light ones on top. 
+        Leave small walkways between the boxes and furniture in your
         storage unit so you can easily get to the items you want without
         having to move anything around and place the items that you plan on
         using toward the front if possible.`
@@ -25,98 +23,25 @@ export const storageTip = [
 
 export const storageTipAccordion = [
     {
-        id: 0,
-        label: `Furniture & Appliances`,
-        status: true,
-        data : [
-            {
-                p: `Rent, or purchase an appliance dolly for furniture, major appliances, and stacks of boxes.`
-            },
-            {
-                p: `For major appliances, consult your user\`s manuals for specific moving and safety instructions.`
-            },
-            {
-                p: `Wipe and dry the interiors of all appliances.`
-            },
-            {
-                p: `Tape and or tie down all movable parts.`
-            },
-            {
-                p: `Remember, loading heavy furniture and appliances is not a one-person job. Get help.`
-            },
-        ]
+        data: `The humidity in your self-storage unit can cause your furniture to
+        warp and your appliances to mildew. Leaving a space between your
+        stuff and the unit’s wall allows for air to circulate within the unit.
+        Laying plastic sheeting on the floor and stacking boxes on top of
+        wooden pallets can prevent condensation damage. So can using old
+        linens or other fabrics, instead of plastic, to protect your stuff from
+        dust. The other option available to you is the climate-controlled units.
+        If you’re storing a refrigerator in your unit leave the door ajar. This will
+        prevent mold from growing inside.`
     },
     {
-        id: '1',
-        label: `Beds`,
-        status: false,
-        data : [
-            {
-                p: `Disassemble the bed frame and mark each piece so you will have an easy time
-                    putting it back together. (Use packing tape to tie rails together.)`
-            },
-            {
-                p: `Wrap mattresses in plastic mattress covers.`
-            },
-        ]
+        data: `You should make sure there are no food products in any of the boxes
+        that you are storing.`
     },
     {
-        id: '2',
-        label: `Books`,
-        status: false,
-        data : [
-            {
-                p: `Never pack more than 30 pounds of books in a box.`
-            },
-            {
-                p: `Alternate bindings, and cushion with packing paper.`
-            },
-        ]
+        data: `Under no circumstances should you keep anything flammable or
+        combustible in your storage building. This means no gasoline, oil,
+        cleaning fluids or paint thinner. If you’re storing any machinery that
+        runs on gas, drain the tank before you store it.`
     },
-    {
-        id: '3',
-        label: `Clothing`,
-        status: false,
-        data : [
-            {
-                p: `Hang clothing from closets in wardrobe boxes or plastic garment bags.`
-            },
-            {
-                p: `Store clothing from dresser drawers in sturdy cardboard boxes.
-                    If the boxes still have some space, consider filling them with bubble wrap.`
-            },
-        ]
-    },
-    {
-        id: '4',
-        label: `Artwork, Mirrors & Lamps`,
-        status: false,
-        data : [
-            {
-                p: `Wrap in bubble wrap and stack in a large box with bubble wrap or large peanuts.`
-            },
-            {
-                p: `Remove light bulbs, harps, and lamp shades off lamp.`
-            },
-            {
-                p: `Artwork & mirrors should be wrapped in paper, covered with cardboard.`
-            },
-        ]
-    },
-    {
-        id: '5',
-        label: `Collectibles`,
-        status: false,
-        data : [
-            {
-                p: `Wrap each fragile item separately with bubble wrap and packing tape`
-            },
-            {
-                p: `Layer wrapped items in sturdy cardboard box using large peanuts or wadded paper between.`
-            },
-            {
-                p: `Place a final layer of peanuts or wadded paper on top, seal and mark.`
-            },
-        ]
-    }
+   
 ];
