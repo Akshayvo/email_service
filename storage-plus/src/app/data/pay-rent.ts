3330548b3cea3e5e8dd4623f5c0b53f06a27a2f2
pragathi <pragathi.n@codeparva.in>
2021-04-08 12:36:36 +0530

    disable payrent for locations,responsiveness and map changes

--- storage-plus/src/app/data/pay-rent.ts
+++ storage-plus/src/app/data/pay-rent.ts
@@ -12,31 +12,31 @@ export const tableHeader = [
 ];
 
 export const tableData = [
-  {
-    facility: ' Foley',
-    address: '17860 US-98, Foley, AL 36535',
-    url: 'foley/rent-sub/login'
-  },
-  {
-    facility: 'Silverhill',
-    address: '15410 AL-104, Silverhill, AL 36576',
-    url: 'silverhill/rent-sub/login'
-  },
-  {
-    facility: 'Barnwell',
-    address: '8089A US-98, Fairhope, AL 36532',
-    url: 'barnwell/rent-sub/login'
-  },
-  {
-    facility: 'Belforest',
-    address: '11455 Co Rd 64, Daphne, AL 36526',
-    url: 'belforest/rent-sub/login'
-  },
-  {
-    facility: 'Fairhope',
-    address: '21193 AL-181, Fairhope, AL 36532',
-    url: 'fairhope/rent-sub/login'
-  },
+  // {
+  //   facility: ' Foley',
+  //   address: '17860 US-98, Foley, AL 36535',
+  //   url: 'foley/rent-sub/login'
+  // },
+  // {
+  //   facility: 'Silverhill',
+  //   address: '15410 AL-104, Silverhill, AL 36576',
+  //   url: 'silverhill/rent-sub/login'
+  // },
+  // {
+  //   facility: 'Barnwell',
+  //   address: '8089A US-98, Fairhope, AL 36532',
+  //   url: 'barnwell/rent-sub/login'
+  // },
+  // {
+  //   facility: 'Belforest',
+  //   address: '11455 Co Rd 64, Daphne, AL 36526',
+  //   url: 'belforest/rent-sub/login'
+  // },
+  // {
+  //   facility: 'Fairhope',
+  //   address: '21193 AL-181, Fairhope, AL 36532',
+  //   url: 'fairhope/rent-sub/login'
+  // },
   {
     facility: 'Robertsdale',
     address: '20218 US-90, Robertsdale, AL 36567',
