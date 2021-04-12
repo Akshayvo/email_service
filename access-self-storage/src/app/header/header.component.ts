14f15aaf680f14bc627916c12479e6e493579fba
bala <balasubramanian.k@codeparva.in>
2021-04-12 17:42:55 +0530

    navbar changes

--- access-self-storage/src/app/header/header.component.ts
+++ access-self-storage/src/app/header/header.component.ts
@@ -31,6 +31,64 @@ export class HeaderComponent implements OnInit {
     this.fetchNavigationLinks();
     this.fetchSocialLinks();
     this.fetchContactDetails();
+
+    (function(){
+
+      var doc = document.documentElement;
+      var w = window;
+    
+      var prevScroll = w.scrollY || doc.scrollTop;
+      var curScroll;
+      var direction = 0;
+      var prevDirection = 0;
+    
+      // var header = document.getElementById('site-header');
+
+      var navHeader  = document.getElementById('nav-header');
+    
+      var checkScroll = function() {
+    
+        /*
+        ** Find the direction of scroll
+        ** 0 - initial, 1 - up, 2 - down
+        */
+    
+        curScroll = w.scrollY || doc.scrollTop;
+        if (curScroll > prevScroll) { 
+          //scrolled up
+          direction = 2;
+        }
+        else if (curScroll < prevScroll) { 
+          //scrolled down
+          direction = 1;
+        }
+    
+        if (direction !== prevDirection) {
+          toggleHeader(direction, curScroll);
+        }
+        
+        prevScroll = curScroll;
+      };
+    
+      var toggleHeader = function(direction, curScroll) {
+        if (direction === 2 && curScroll > 52) { 
+          
+          //replace 52 with the height of your header in px
+    
+          // header.classList.add('hide');
+          navHeader.classList.add('customClass');
+          prevDirection = direction;
+        }
+        else if (direction === 1) {
+          // header.classList.remove('hide');
+          navHeader.classList.remove('customClass');
+          prevDirection = direction;
+        }
+      };
+      
+      window.addEventListener('scroll', checkScroll);
+    
+    })();
   }
 
   public fetchContactDetails() {
