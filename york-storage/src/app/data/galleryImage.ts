20624cc8da6ce552e5018aab04a3fdaabe34c35b
pragathi <pragathi.n@codeparva.in>
2021-04-14 18:15:04 +0530

    setup

---
+++ york-storage/src/app/data/galleryImage.ts
@@ -0,0 +1,37 @@
+export const galleryHome = [
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `row-of-self-storage-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `row-of-self-storage-units`,
+    alt: `a single story self storage building with the name Self Storage of York and a phone number painted on the roof`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `outdoor-parking`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `outdoor-parking`,
+    alt: `parking spaces filled with recreational vehicles and campers`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `self-storage-doors`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `self-storage-doors`,
+    alt: `row of self storage unit doors under a blue sky with gravel parking spaces in the foreground`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `storage-facility-gate`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `storage-facility-gate`,
+    alt: `closed self storage unit facility security gate as viewed from outside of the facility grounds`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `self-storage-unit-row`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `self-storage-unit-row`,
+    alt: `row of self storage units with red metal roll-up doors`
+  },
+];
