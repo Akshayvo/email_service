f94f75d88927cc500f4cc36caf0e33e6ea70155d
bala <balasubramanian.k@codeparva.in>
2021-04-23 10:53:04 +0530

    initial commit

---
+++ william-penn/src/app/data/home.ts
@@ -0,0 +1,95 @@
+export const featuresList = [
+  {
+    td1: "Convenient Location",
+    td2: "Online Bill Pay",
+  },
+  {
+    td1: "Security Cameras",
+    td2: "Well-Lit Facility",
+  },
+  {
+    td1: "Brand New Units",
+    td2: "Single-Level Facility",
+  },
+  {
+    td1: "Drive-Up Units",
+    td2: "Auto-Pay Available",
+  },
+  {
+    td1: "Climate-Controlled Units Available",
+    td2: "Fully-Fenced Facility",
+  },
+  {
+    td1: "Family-Owned and Operated",
+    td2: "Online Reservations",
+  },
+];
+
+export const aboutUsAlt = `an open self storage unit revealing household belongings packed neatly on shelves`;
+
+export const aboutUsHeading = `Self Storage Units in New Alexandria, PA`;
+
+export const aboutUs = [
+  {
+    p: `William Penn Self Storage is New Alexandria’s newest self storage facility!
+        Our facility offers a large number of affordable storage units in both outdoor 
+        and climate controlled varieties. We provide a variety of unit sizes from our 
+        walk-in closet-sized 5-foot-by-5-foot units through our 10-foot-by-25 foot units
+        that are an ideal solution for multi-bedroom homes during a move or renovation. 
+        We are conveniently located on Hannastown Road a tenth of a mile south of the William Penn Highway. 
+        We serve New Alexandria, Greensburg, Murrysville, Latrobe and the surrounding communities.`,
+  },
+  {
+    p: `Our brand new, family-owned, and operated self storage facility is built with all of the modern 
+        self storage conveniences in mind. We offer 24/7 access to all of our units, and our contactless 
+        rental process can be performed over the phone or online. Once you’re a tenant at William Penn Self Storage, 
+        you’ll have access to your account and payment options through our easy online tenant portal. 
+        Unlike some facilities, we also fully-paved to reduce dust build-up in your units.`,
+  },
+  {
+    p: `Selecting a self storage is about more than just conveniences and amenities, which is why at William Penn Self Storage 
+        we go above and beyond to make your stay with us feel safe and secure. Our entire facility is well-lit and fully fenced 
+        with a pin-code accessible electronic gate. Security cameras are placed strategically around the facility, 
+        and our management team performs regular lot checks.`,
+  },
+  {
+    p: `Take a moment to check out our unit rates and availability, then reserve your unit or perform a contactless 
+        rental right through our website! You can sign your lease electronically, and receive the access code to your new storage unit in minutes.`,
+  },
+];
+
+export const gettingStarted = ``;
+
+export const feature = [
+  {
+    // tslint:disable-next-line:max-line-length
+    label: "Convenient Location",
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
+    label: "Online Reservation & Bill Pay",
+    alt: "Icon art displaying a screen with cursor",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
+    label: "24 Hour Facility Access",
+    alt: "Icon art displaying clock hands and the number 24",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/cctv.svg`,
+    label: "Security Cameras on Premises",
+    alt: "Icon of a security camera",
+  },
+];
+
+export const jumbotron = [
+  {
+    h2: ``,
+    blurb: ``,
+  },
+];
