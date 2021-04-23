7538e9bea2cec8dbb54ee69baf8b62fadfaf2690
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2021-04-23 22:45:19 +0530

    case 24995 deployed site on production

--- heated/src/environments/environment.ts
+++ heated/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'ontario/canada', // state/city  **Remeber to update locationName in sitemap.xml as well
+  locationName: 'ontario', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: false }, // { moveIn: true, reserve: true },
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
@@ -17,7 +17,7 @@ export const environment = {
   facilityName: 'Heated Self Storage Inc',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'https://heated-self-storage-staging.netlify.app',
+  websiteUrl: 'https://heatedselfstorage.ca',
   APIKey: `bTJUOW40TDJbN1s6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // APIKey:`Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, //demo north   
 };
