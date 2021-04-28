bbeee491413acd51b30790ac368ff665743d55aa
Sagar <sagar.g@codeparva.in>
2021-04-28 21:48:44 +0530

    email link and floating contact form update

--- aallen-storage/src/app/data/contact.ts
+++ aallen-storage/src/app/data/contact.ts
@@ -11,7 +11,7 @@ export const contact = [
         label: 'Email:',
         data: 'Aallenselfstorage@gmail.com',
         class: 'fas fa-envelope mx-2 iconColor',
-        redirect: 'mailto:`Aallenselfstorage@gmail.com`'
+        redirect: 'mailto:Aallenselfstorage@gmail.com'
     },
     {
         id: 2,
