b9c155b7db78da09a18fd5cc45c8f9eb4b3ed585
bala <balasubramanian.k@codeparva.in>
2021-05-12 19:05:12 +0530

    reserve unit page removal and home page changes

--- white-lake-storage/src/app/data/home.ts
+++ white-lake-storage/src/app/data/home.ts
@@ -1,21 +1,20 @@
 export const featuresList = [
-    {
-        td1: 'Convenient Location',
-        td2: 'Online Bill Pay',
-    },
-    {
-        td1: 'Well-Lit Facility',
-        td2: 'Brand New Units',
-    },
-    {
-        td1: 'Single-Level Facility',
-        td2: 'Drive-Up Units',
-    },
-    {
-        td1: 'Affordable Rates',
-        td2: 'Security Cameras Coming Soon!',
-    },
-    
+  {
+    td1: "Convenient Location",
+    td2: "Online Bill Pay",
+  },
+  {
+    td1: "Well-Lit Facility",
+    td2: "Brand New Units",
+  },
+  {
+    td1: "Single-Level Facility",
+    td2: "Drive-Up Units",
+  },
+  {
+    td1: "Affordable Rates",
+    td2: "Security Cameras Coming Soon!",
+  },
 ];
 
 export const aboutUsAlt = `indoor climate controlled storage units with roll-up doors inside of a clean, well-lit building`;
@@ -23,61 +22,59 @@ export const aboutUsAlt = `indoor climate controlled storage units with roll-up 
 export const aboutUsHeading = `Affordable Self Storage in Whitehall, MI`;
 
 export const aboutUs = [
-    {
-        p: `White Lake Storage is Whitehall’s newest self storage facility! We are conveniently located on Whitehall Rd
-        between Silver Creek Rd and Lorenson Rd just east of Whitehall. We are proud to be a locally owned and operated
-        business serving Whitehall, Holton, Montague, and the surrounding communities. Our facility offers a number of
+  {
+    p: `White Lake Storage is Whitehall’s newest self storage facility! We are conveniently located on Holton Whitehall Rd between Silver Creek Rd and Lorenson Rd just east of Whitehall.
+        We are proud to be a locally owned and operated business serving Whitehall, Holton, Montague, and the surrounding communities. Our facility offers a number of
         self storage unit sizes and all of the modern conveniences necessary for a hassle-free rental experience.`,
-    },
-    {
-        p: `We understand that the need for a storage unit can come at a stressful time. Whether you’re preparing for a
+  },
+  {
+    p: `We understand that the need for a storage unit can come at a stressful time. Whether you’re preparing for a
         move, a renovation, or are just looking for a place to store the contents of your garage, we’ve got a unit for
         you. You can reserve, rent, and pay online. No need to call or drive to the facility. Our well-lit grounds are
         secured by an electronic pin-code activated gate, and security cameras will be installed soon to further enhance
         the sense of safety and security around the facility.`,
-    },
-    {
-        p: `If you have any questions, please feel free to contact our friendly management team. We look forward
+  },
+  {
+    p: `If you have any questions, please feel free to contact our friendly management team. We look forward
         to meeting you and making your next storage project fast, affordable and easy!`,
-    },    
+  },
 ];
 
 export const gettingStarted = ``;
 
 export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        label: 'Convenient Location',
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
-        alt: 'Icon art displaying a location pin'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
-        label: '24 Hour Facility Access',
-        alt: 'Icon art displaying clock hands and the number 24'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/hand.svg`,
-        label: 'Affordable Self Storage Rates',
-        alt: 'Icon of a hand holding money'
-
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/light-bulb.svg`,
-        label: 'Well-Lit Facility',
-        alt: 'Icon of an illuminated light'
-    },
+  {
+    // tslint:disable-next-line:max-line-length
+    label: "Convenient Location",
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
+    label: "24 Hour Facility Access",
+    alt: "Icon art displaying clock hands and the number 24",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/hand.svg`,
+    label: "Affordable Self Storage Rates",
+    alt: "Icon of a hand holding money",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/light-bulb.svg`,
+    label: "Well-Lit Facility",
+    alt: "Icon of an illuminated light",
+  },
 ];
 
 export const jumbotron = [
-    {
-        h2: `Affordable Self Storage Units in Whitehall`,
-        blurb: `White Lake Storage is Whitehall's newest self storage facility. We provide high-quality, well-maintained
+  {
+    h2: `Affordable Self Storage Units in Whitehall`,
+    blurb: `White Lake Storage is Whitehall's newest self storage facility. We provide high-quality, well-maintained
         self storage services in Whitehall, Holton, Montague, and the neighboring communities. Our locally owned and
         operated facility offers several self storage unit sizes to fit a variety of storage needs for our residential
         and commercial customers. Check out our inventory and reserve or rent your unit today!`,
-    }
+  },
 ];
