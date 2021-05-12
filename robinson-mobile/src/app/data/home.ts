c3851ff780347e1e6f0381bef8d9cc55fad8386f
pragathi <pragathi.n@codeparva.in>
2021-05-12 19:16:04 +0530

    setup

---
+++ robinson-mobile/src/app/data/home.ts
@@ -0,0 +1,80 @@
+export const featuresList = [
+    {
+        td1: 'Convenient Location',
+        td2: 'On-Site Manager',
+    },
+    {
+        td1: 'Online Bill Pay',
+        td2: 'Brand New Units',
+    },
+    {
+        td1: 'Auto-Pay Available',
+        td2: 'Friendly Customer Service',
+    },
+    {
+        td1: 'Affordable Rates',
+    },
+];
+
+export const aboutUsAlt = `indoor climate controlled storage units with roll-up doors inside of a clean, well-lit building`;
+
+export const aboutUsHeading = `Affordable Portable Storage Units in Ennismore`;
+
+export const aboutUs = [
+    {
+        p: `Robinson Mobile Storage serves Peterborough, Ennismore, Bridgenorth, and the surrounding communities with quality portable storage units delivered right to your door, office, or worksite! We understand that some storage projects are best handled with an on-site storage solution. That’s where Robinson Mobile Storage comes into play. Our mobile storage units are 20’x8’x8’ and offer 160 square feet of storage space anywhere you want it.`,
+    },
+    {
+        p: `The Robinson Mobile Storage units are the ideal solution for renovations where furniture, tools, or supplies require storage and easy access throughout the duration of the project. We also serve contractors, auctioneers, and hospitals with temporary on-site storage.`,
+    },
+    {
+        p: `Our no-tilt delivery and pickup service means all you have to do is pack your belongings for temporary on-site storage or for long term storage at our self storage facility on Robinson Road in Ennismore. Simply give us a call with the details of when and where you need your unit delivered, and our professional crew will provide the same level of service you’ve come to expect from our Robinson Storage self storage facility.`,
+    },
+    {
+        p: `If you have any questions, give our friendly team a call, and we’ll be happy to help! Ready to start making your next project easier with on-site storage? Take a moment to check out our availability and pricing information, then reserve your portable storage solution in seconds through our website!`,
+    }
+    
+];
+
+export const gettingStarted = `Are you ready to stop putting off cleaning out your attic, garage, or shed?
+Clear up some room around the house with Sample Self Storage - North and one of our affordable storage units,
+accessible 24 hours a day!`;
+
+export const feature = [
+    {
+        // tslint:disable-next-line:max-line-length
+        label: 'Online Reservation & Bill Pay',
+        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
+        alt: 'Icon art displaying a screen with cursor'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
+        label: '24 Hour Facility Access',
+        alt: 'Icon art displaying clock hands and the number 24'
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/hand.svg`,
+        label: 'Affordable Self Storage Rates',
+        alt: 'Icon of a hand holding money'
+
+    },
+    {
+        // tslint:disable-next-line:max-line-length
+        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/team.svg`,
+        label: 'Locally Owned and Operated',
+        alt: 'Icon of people standing together'
+    },
+];
+
+export const jumbotron = [
+    {
+        h2: `Brand New, Affordable Portable Storage Units`,
+        blurb: `Sample Self Storage - North was the first self storage facility we built nearly 20 years ago.
+        We recently renovated our entire operation to make sure our valued customers have another 20 years of
+        clean, affordable regular and climate controlled self storage options in Baldwinsville! Let Sample Self
+        Storage - North help you today. Our well-lit and personalized pin-code secured facility awaits your next
+        storage project!`,
+    }
+];
---
+++ robinson-mobile/src/app/data/pay-rent.ts
@@ -0,0 +1,11 @@
+export const loginDetail = [
+    {
+        userName: `Email Address`,
+        password: `Unit Number`,
+        example: {
+            code: 'Phone Number is 555-555-5555',
+            userName: 'jsmith',
+            password: '5555555555'
+        }
+    }
+];
