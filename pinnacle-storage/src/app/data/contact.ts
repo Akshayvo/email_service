178d450d0dfd4fdbd0741511ba230059f89ff9df
bala <balasubramanian.k@codeparva.in>
2021-05-12 17:56:30 +0530

    hours changes

--- pinnacle-storage/src/app/data/contact.ts
+++ pinnacle-storage/src/app/data/contact.ts
@@ -1,42 +1,41 @@
 export const contact = [
-    {
-        id: 0,
-        label: 'Address: ',
-        data: '1944 New Scotland Rd, Slingerlands, NY 12159',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-    },
-    {
-        id: 1,
-        redirect: 'mailto:pinnacle1944@gmail.com',
-        label: 'Email: ',
-        data: 'pinnacle1944@gmail.com',
-        class: 'fas fa-envelope mr-2 iconColor',
-    },
-    {
-        id: 2,
-        redirect: 'tel:5188989295',
-        label: 'Phone: ',
-        data: '(518) 898-9295',
-        class: 'fas fa-phone mr-2 iconColor',
-    }
+  {
+    id: 0,
+    label: "Address: ",
+    data: "1944 New Scotland Rd, Slingerlands, NY 12159",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+  },
+  {
+    id: 1,
+    redirect: "mailto:pinnacle1944@gmail.com",
+    label: "Email: ",
+    data: "pinnacle1944@gmail.com",
+    class: "fas fa-envelope mr-2 iconColor",
+  },
+  {
+    id: 2,
+    redirect: "tel:5188989295",
+    label: "Phone: ",
+    data: "(518) 898-9295",
+    class: "fas fa-phone mr-2 iconColor",
+  },
 ];
 
 export const hours = [
-    {
-        label: 'Monday to Friday: ',
-        data: '9:00AM to 5:00PM'
-    },
-    {
-        label: 'Saturday: ',
-        data: '9:00AM to 1:00PM'
-    },
-    {
-        label: 'Sunday: ',
-        data: 'By Appointment Only'
-    },
-    {
-        label: 'Access Hours: ',
-        data: '7 Days a Week 6:00AM to 9:00PM'
-    }
+  {
+    label: "Monday to Friday: ",
+    data: "10:00AM to 5:00PM",
+  },
+  {
+    label: "Saturday: ",
+    data: "9:00AM to 1:00PM",
+  },
+  {
+    label: "Sunday: ",
+    data: "By Appointment Only",
+  },
+  {
+    label: "Access Hours: ",
+    data: "7 Days a Week 6:00AM to 9:00PM",
+  },
 ];
-
