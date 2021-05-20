5e0915b344f3f0cafb52130bcef8d032c0da6669
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2021-05-21 00:36:03 +0530

    updated env file

--- sample/src/environments/environment.prod.ts
+++ sample/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   type: 0,  // { API: 0, iframe: 1 }
   locationName: 'new-york/baldwinsville',  // state/city   **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
-  alternateType: { moveIn: false, reserve: false }, // { moveIn: true, reserve: true },
+  alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
   military: true, // set true for Military and Driver's Details 
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
   cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
