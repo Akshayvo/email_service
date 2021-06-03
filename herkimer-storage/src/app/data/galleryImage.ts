11d733d666d30afcd2e281376f71975c794d341d
pragathi <pragathi.n@codeparva.in>
2021-06-03 17:30:21 +0530

    case 26349

--- herkimer-storage/src/app/data/galleryImage.ts
+++ herkimer-storage/src/app/data/galleryImage.ts
@@ -1,9 +1,37 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: ``,
+    url: `herkimer-storage-outlet-11`,
     // tslint:disable-next-line:max-line-length
-    thumb: ``,
-    alt: ``
+    thumb: `herkimer-storage-outlet-11-thumb`,
+    alt: `two rows of indoor self storage units on either side of a clean aisle`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `herkimer-storage-outlet-indoor-parking`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `herkimer-storage-outlet-indoor-parking-thumb`,
+    alt: `vehicles under dust covers parked in a large room with a concrete floor`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `herkimer-storage-outlet-outdoor-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `herkimer-storage-outlet-outdoor-units-thumb`,
+    alt: ` a row of red metal self storage unit door with a paved parking lane in the foreground`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `herkimer-storage-outlet-sign`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `herkimer-storage-outlet-sign-thumb`,
+    alt: `a tall road sign that reads Herkimer’s Storage Outlet`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `herkimer-storage-outlet-front-fence`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `herkimer-storage-outlet-front-fence-thumb`,
+    alt: `a sign behind a wooden fence that reads Herkimer Storage Outlet`
   },
 ];
