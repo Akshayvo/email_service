c01ef6278b10cb2f7428e00bf13d8294c1a757a5
pragathi <pragathi.n@codeparva.in>
2021-06-04 21:11:11 +0530

    case 26360

--- safe-self-storage/src/environments/environment.ts
+++ safe-self-storage/src/environments/environment.ts
@@ -17,7 +17,7 @@ export const environment = {
   facilityName: 'SAFE Self Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'http://safeself-storage.com',
+  websiteUrl: 'http://safeselfstoragellc.com',
   // APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
   // APIKey: `dzJ0OVc0SjMqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29435 Safe self Storage
   APIKey: `dzJ0OVc0SjMqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // Website Demo Data 4-29462
