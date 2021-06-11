75ebfb7653c32dd14644cd001b8bf20849ce4258
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-06-11 18:57:51 +0530

    Added images to the gallery home and view rates page

--- madison-verona/src/app/data/galleryImage.ts
+++ madison-verona/src/app/data/galleryImage.ts
@@ -4,36 +4,54 @@ export const galleryHome = [
     url: `self-storage-office`,
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-office`,
-    alt: `a self storage facility front office building`
+    alt: `a self storage facility front office building`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `climate-controlled-self-storage`,
     // tslint:disable-next-line:max-line-length
     thumb: `climate-controlled-self-storage`,
-    alt: `a climate controlled self storage building with signage that reads "Climate Control and RV Storage"`
+    alt: `a climate controlled self storage building with signage that reads "Climate Control and RV Storage"`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `open-storage-unit`,
     // tslint:disable-next-line:max-line-length
     thumb: `open-storage-unit`,
-    alt: `an open self storage unit revealing brand new, clean interior`
+    alt: `an open self storage unit revealing brand new, clean interior`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `row-of-self-storage-units`,
     // tslint:disable-next-line:max-line-length
     thumb: `row-of-self-storage-units`,
-    alt: `a long row of brand new self storage units with green roll-up doors`
+    alt: `a long row of brand new self storage units with green roll-up doors`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `electrical-outlet-equipped-self-storage-unit`,
     // tslint:disable-next-line:max-line-length
     thumb: `electrical-outlet-equipped-self-storage-unit`,
-    alt: `electrical outlet on the inside of a brand new clean self storage unit`
+    alt: `electrical outlet on the inside of a brand new clean self storage unit`,
   },
- 
-  
+  {
+    url: `madison-verona-main-office`,
+    thumb: `madison-verona-main-office`,
+    alt: `a self storage front office with a gate and units behind a security`,
+  },
+  {
+    url: `self-storage-office-and-gate`,
+    thumb: `self-storage-office-and-gate`,
+    alt: `a self storage facility with large front office, electronic security gate, and units in the background`,
+  },
+  {
+    url: `self-storage-facility-buildings`,
+    thumb: `self-storage-facility-buildings`,
+    alt: `rows self storage unit buildings behind a row of small pine trees`,
+  },
+  {
+    url: `self-storage-units-rows`,
+    thumb: `self-storage-units-rows`,
+    alt: `two rows of self storage units on either side of a paved walkway`,
+  },
 ];
