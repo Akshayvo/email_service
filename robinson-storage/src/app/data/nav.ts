07593d47baeb3b580e379a54d4c88380920c723a
pragathi <pragathi.n@codeparva.in>
2021-06-15 13:07:32 +0530

    case 26427

--- robinson-storage/src/app/data/nav.ts
+++ robinson-storage/src/app/data/nav.ts
@@ -1,26 +1,39 @@
 export const navLinks = [
     {
+        id: 1,
         label: 'HOME',
         path: '/',
     },
     {
+        id: 1,
         label: 'VIEW RATES',
         path: '/view-rates',
     },
     {
+        id: 1,
         label: 'PAY RENT',
         path: '/pay-rent',
     },
     {
+        id: 1,
         label: 'UNIT SIZER',
         path: '/unit-sizer',
     },
     {
+        id: 1,
         label: 'STORAGE TIPS',
         path: '/storage-tips'
     },
     {
+        id: 0,
+        label: 'BOXES',
+        // path: 'https://www.robinsonstorage.ca/'
+        
+    },
+    {
+        id: 1,
         label: 'CONTACT US',
         path: '/contact-us'
     },
+    
 ];
