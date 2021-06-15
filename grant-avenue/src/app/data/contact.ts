e1bb3a52ef2ac14abeaf7b1c91b1f34cc9103b6e
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-06-15 10:36:14 +0530

    Changed Email address sitewide

--- grant-avenue/src/app/data/contact.ts
+++ grant-avenue/src/app/data/contact.ts
@@ -1,45 +1,45 @@
 export const contact = [
-    {
-        id: 0,
-        label: 'Facility Address: ',
-        data: '819 Grant Ave, Junction City, KS 66441',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-    },
-    {
-        id: 1,
-        redirect: 'mailto:gillproperties@mail.com',
-        label: 'Email: ',
-        data: 'gillproperties@mail.com',
-        class: 'fas fa-envelope mr-2 iconColor',
-    },
-    {
-        id: 2,
-        redirect: 'tel:7857612400',
-        label: 'Phone: ',
-        data: '(785) 761-2400',
-        class: 'fas fa-phone mr-2 iconColor',
-    }
+  {
+    id: 0,
+    label: "Facility Address: ",
+    data: "819 Grant Ave, Junction City, KS 66441",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+  },
+  {
+    id: 1,
+    redirect: "mailto:spmcacorp@aol.com",
+    label: "Email: ",
+    data: "spmcacorp@aol.com",
+    class: "fas fa-envelope mr-2 iconColor",
+  },
+  {
+    id: 2,
+    redirect: "tel:7857612400",
+    label: "Phone: ",
+    data: "(785) 761-2400",
+    class: "fas fa-phone mr-2 iconColor",
+  },
 ];
 
 export const hours = [
-    {
-        label: 'Monday - Saturday: ',
-        data: '10:00AM - 6:00pm'
-    },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'Closed'
-    // },
-    {
-        label: 'Access hours: ',
-        data: 'Monday-Saturday 10:00AM - 6:00pm'
-    }
+  {
+    label: "Monday - Saturday: ",
+    data: "10:00AM - 6:00pm",
+  },
+  // {
+  //     label: 'Sunday: ',
+  //     data: 'Closed'
+  // },
+  {
+    label: "Access hours: ",
+    data: "Monday-Saturday 10:00AM - 6:00pm",
+  },
 ];
 
 export const socialLinks = [
-    // {
-    //     label: 'facebook',
-    //     path: '',
-    //     class: 'fab fa-facebook-square',
-    // }
+  // {
+  //     label: 'facebook',
+  //     path: '',
+  //     class: 'fab fa-facebook-square',
+  // }
 ];
