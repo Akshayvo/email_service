b9f667037354dd3e0bc5472602ee947d4caf9629
pragathi <pragathi.n@codeparva.in>
2021-06-16 15:34:57 +0530

    case 26441

--- madison-verona/src/app/data/nav.ts
+++ madison-verona/src/app/data/nav.ts
@@ -2,34 +2,41 @@ import { environment } from '../../environments/environment';
 
 export const navLinks = [
     {
+        id:'home',
         label: 'HOME',
         path: '/',
     },
     {
+        id:'view-rates',
         label: 'VIEW RATES',
         path: `${environment.locationName}/view-rates`,
     },
-    // {
-    //     label: 'RENT NOW',
-    //     path: `${environment.locationName}/rent-now`,
-    // },
+    {
+        id:'rent-now',
+        label: 'RENT NOW',
+        path: `${environment.locationName}/rent-now`,
+    },
     // {
     //     label: 'RESERVE UNIT',
     //     path: `${environment.locationName}/reserve-unit`,
     // },
     {
+        id:'pay-rent',
         label: 'PAY RENT',
         path: '/pay-rent'
     },
     {
+        id:'storage-tips',
         label: 'STORAGE TIPS',
         path: `${environment.locationName}/storage-tips`
     },
     {
+        id:'unit-sizer',
         label: 'UNIT SIZER',
         path: '/unit-sizer'
     },
     {
+        id:'contact-us',
         label: 'CONTACT US',
         path: '/contact-us'
     },
