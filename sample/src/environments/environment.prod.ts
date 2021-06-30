ba818f7bf9545c65d9be4bdd644fa9a2cd10fa20
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2021-06-30 20:15:55 +0530

    updated api key with 29462

--- sample/src/environments/environment.prod.ts
+++ sample/src/environments/environment.prod.ts
@@ -15,5 +15,7 @@ export const environment = {
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://sample-self-storage-staging.netlify.app',
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
+  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
+  APIKey: `dzJ0OVc0SjYqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
+
 };
--- sample/src/environments/environment.ts
+++ sample/src/environments/environment.ts
@@ -19,7 +19,9 @@ export const environment = {
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://sample-self-storage-staging.netlify.app',
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
+  APIKey: `dzJ0OVc0SjYqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
+  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
+  
 };
 
 /*
