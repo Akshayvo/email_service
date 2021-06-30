4e981a966cb41056f90147bcb5b582388049eb0a
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-06-30 13:10:56 +0530

    Updates images in homepage and view rates page

--- safe-self-storage/src/app/data/home.ts
+++ safe-self-storage/src/app/data/home.ts
@@ -1,84 +1,81 @@
 export const featuresList = [
-    {
-        td1: 'Convenient Location',
-        td2: 'Online Bill Pay',
-    },
-    {
-        td1: 'Well-Lit Facility',
-        td2: 'Auto-Pay Available',
-    },
-    {
-        td1: 'Friendly Customer Service',
-        td2: 'Affordable Rates',
-    },
-    {
-        td1: 'Climate-Controlled Units Available',
-        td2: 'Concrete Construction'
-    },
+  {
+    td1: "Convenient Location",
+    td2: "Online Bill Pay",
+  },
+  {
+    td1: "Well-Lit Facility",
+    td2: "Auto-Pay Available",
+  },
+  {
+    td1: "Friendly Customer Service",
+    td2: "Affordable Rates",
+  },
+  {
+    td1: "Climate-Controlled Units Available",
+    td2: "Concrete Construction",
+  },
 ];
 
-export const aboutUsAlt = `row of self storage units under brightly lit indoor lights`;
+export const aboutUsAlt = `a row of self storage unit doors with personal belongings inside a unit`;
 
 export const aboutUsHeading = `Affordable Regular & Climate Controlled Self Storage Units in Rochester, N.Y.`;
 
 export const aboutUs = [
-    {
-        p: `SAFE Self-Storage provides high-quality, well-maintained regular and climate controlled self storage space
+  {
+    p: `SAFE Self-Storage provides high-quality, well-maintained regular and climate controlled self storage space
         to East Rochester, Penfield, Panorama, Pittsford, and the surrounding communities. Our facility is locally
         owned and operated, and we pride ourselves on our friendly service and affordable prices. Our facility is
         conveniently located on West Commercial Street next to the Elite Performance Baseball & Softball Training
         Center. That gives us easy access to the expressway, right off of Interstate 490. We are also a stone’s
         throw away from St. John Fisher College!`,
-    },
-    {
-        p: `We offer all of the modern amenities to make your stay with SAFE Self-Storage an easy one. Once you
+  },
+  {
+    p: `We offer all of the modern amenities to make your stay with SAFE Self-Storage an easy one. Once you
         select a unit type from our live online inventory one of our friendly staff will reach out to help you
         complete your contactless rental. After you’ve moved into your new unit, you’ll have 24-hour access to
         your belongings in our well-lit self storage facility. As part of our continuing effort to improve your
         convenience, we now offer an online tenant portal to check your balance and pay your rent online around-the-clock!`,
-    },
-    {
-        p: `SAFE Self-Storage is here to make your next move, renovation, or other storage project fast and easy!
+  },
+  {
+    p: `SAFE Self-Storage is here to make your next move, renovation, or other storage project fast and easy!
         So take a few minutes to review our current availability, and reserve your unit through our website, or give
         our friendly self storage staff a call today! We look forward to meeting you!`,
-    },
-    
-    
+  },
 ];
 
 export const gettingStarted = ``;
 
 export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        label: 'Convenient Location',
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
-        alt: 'Icon art displaying a location pin'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
-        label: 'Online Reservation & Bill Pay',
-        alt: 'Icon art displaying a screen with cursor'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/thermometer.svg`,
-        label: 'Climate Controlled',
-        alt: 'Icon of thermometer indicating climate-controlled units'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/hand.svg`,
-        label: 'Affordable Self Storage Rates',
-        alt: 'Icon of a hand holding money'
-
-    },
+  {
+    // tslint:disable-next-line:max-line-length
+    label: "Convenient Location",
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
+    label: "Online Reservation & Bill Pay",
+    alt: "Icon art displaying a screen with cursor",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/thermometer.svg`,
+    label: "Climate Controlled",
+    alt: "Icon of thermometer indicating climate-controlled units",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/hand.svg`,
+    label: "Affordable Self Storage Rates",
+    alt: "Icon of a hand holding money",
+  },
 ];
 
 export const jumbotron = [
-    {
-        h2: `Affordable Self Storage Units in East Rochester`,
-        blurb: `SAFE Self-Storage is your one-stop storage stop for East Rochester and the surrounding communities. We offer a number of self storage unit sizes at very affordable rates! Our facility provides the modern conveniences that make self storage a fast and easy experience such as online rental, and payments. Take a moment to review our rates, then reserve online. Still have questions? Reach out today, and our friendly managers will be happy to help!`,
-    }
+  {
+    h2: `Affordable Self Storage Units in East Rochester`,
+    blurb: `SAFE Self-Storage is your one-stop storage stop for East Rochester and the surrounding communities. We offer a number of self storage unit sizes at very affordable rates! Our facility provides the modern conveniences that make self storage a fast and easy experience such as online rental, and payments. Take a moment to review our rates, then reserve online. Still have questions? Reach out today, and our friendly managers will be happy to help!`,
+  },
 ];
--- safe-self-storage/src/environments/environment.ts
+++ safe-self-storage/src/environments/environment.ts
@@ -4,24 +4,23 @@
 
 export const environment = {
   production: true,
-  type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'new-york/east-rochester', // state/city  **Remeber to update locationName in sitemap.xml as well
+  type: 0, // { API: 0, iframe: 1 }
+  locationName: "new-york/east-rochester", // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
-  template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
-  cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
-  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
-  senderEmail: 'errors@codeparva.in',
-  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'SAFE Self Storage - Testing',
-  facilityName: 'SAFE Self Storage',
-  VERSION: require('../../package.json').version,
+  template: "darkTheme", // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ["Visa", "Master Card", "Amex", "Discover", "test"],
+  errorsMails: ["sanghmitra.v@codeparva.in"],
+  senderEmail: "errors@codeparva.in",
+  emailBaseUrl: "https://emailsvc-ext.syrasoft.com/v1/email",
+  appId: "SAFE Self Storage - Testing",
+  facilityName: "SAFE Self Storage",
+  VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'http://safeselfstoragellc.com',
-  // APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
-  // APIKey: `dzJ0OVc0SjMqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29435 Safe self Storage
-  APIKey: `dzJ0OVc0SjMqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // Website Demo Data 4-29462
-
+  websiteUrl: "http://safeselfstoragellc.com",
+  //APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
+  APIKey: `dzJ0OVc0SjMqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29435 Safe self Storage
+  //APIKey: `dzJ0OVc0SjMqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // Website Demo Data 4-29462
 };
 
 /*
