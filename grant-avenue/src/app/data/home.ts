893294966a3a4ef7052337d115e4dcf92a273d2b
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-01 18:41:08 +0530

    case 26456

--- grant-avenue/src/app/data/home.ts
+++ grant-avenue/src/app/data/home.ts
@@ -1,82 +1,76 @@
-
 export const featuresList = [
-    {
-        td1: 'Convenient Location ',
-        td2: 'Online Bill Pay',
-    },
-    {
-        td1: 'Security Cameras',
-        td2: 'Well-Lit Facility',
-    },
-    {
-        td1: 'Drive-Up Units',
-        td2: 'Auto-Pay Available'
-    },
-    {
-        td1: 'Pin-Pad Activated Security Gate',
-        td2: 'Affordable Rates'
-    },
+  {
+    td1: "Convenient Location ",
+    td2: "Online Bill Pay",
+  },
+  {
+    td1: "Security Cameras",
+    td2: "Well-Lit Facility",
+  },
+  {
+    td1: "Drive-Up Units",
+    td2: "Auto-Pay Available",
+  },
+  {
+    td1: "Pin-Pad Activated Security Gate",
+    td2: "Affordable Rates",
+  },
 ];
 
 export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/location.svg',
-        label: 'Conveniently Located',
-        alt: 'Icon art displaying a location pin'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/online.svg',
-        label: 'Online Reservation & Bill Pay',
-        alt: 'Icon art displaying a screen with cursor'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/support.svg',
-        label: 'Stellar Customer Service',
-        alt: 'Customer Service Representative Icon'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/light-bulb.svg',
-        label: 'Well-Lit Facility',
-        alt: ' Icon of an illuminated light'
-    },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/location.svg",
+    label: "Conveniently Located",
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/online.svg",
+    label: "Online Reservation & Bill Pay",
+    alt: "Icon art displaying a screen with cursor",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/support.svg",
+    label: "Stellar Customer Service",
+    alt: "Customer Service Representative Icon",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Syrasoft_Poweredby/icons/light-bulb.svg",
+    label: "Well-Lit Facility",
+    alt: " Icon of an illuminated light",
+  },
 ];
 
-
 export const aboutUs = [
-    {
-        p: `Grant Avenue Storage has a wide variety of climate-controlled, and unconditioned self storage units in
-            a well-lit, keypad secured self storage facility just north of the Junction City center. We serve residents
-            and businesses in Junction City, the surrounding communities, and our service people stationed
-            at Fort Riley.`,
-    },
-    {
-        p: `We pride ourselves in our quality customer service, and we strive to go above and beyond for our tenants
+  {
+    p: `Grant Avenue Storage has a wide variety of self storage unit sizes in a well-lit, keypad secured self storage facility just north of the Junction City center. We serve residents and businesses in Junction City, the surrounding communities, and our service people stationed at Fort Riley.`,
+  },
+  {
+    p: `We pride ourselves in our quality customer service, and we strive to go above and beyond for our tenants
             each and every day. Our service starts with our online reservation form. Simply fill out the form, and one
             of our friendly management staff will be in touch to confirm your reservation. Once you've moved into your
             new unit, you can pay your rent online 24/7. We're with you every step of the way, making your self storage
             experience a convenient, and affordable one!`,
-    },
-    {
-        p: `From local businesses in need of a little extra room for supplies or inventory to homeowners and renters
+  },
+  {
+    p: `From local businesses in need of a little extra room for supplies or inventory to homeowners and renters
             de-cluttering, renovating, or moving, Grant Avenue Storage is your ideal self storage solution. Take a
             moment to fill out our reservation form, or give our friendly managers a call!`,
-    },
+  },
 ];
-
 
 export const gettingStarted = `Are you ready to start de-cluttering now? Click below to fill out our reservation
                                form, and start reclaiming that elbow room today!`;
 
 export const blurb = [
-    {
-        h1: ` Affordable Self Storage in Archdale`,
-        p: `We are a locally owned & operated self storage facility serving Archdale, Thomasville, High
+  {
+    h1: ` Affordable Self Storage in Archdale`,
+    p: `We are a locally owned & operated self storage facility serving Archdale, Thomasville, High
             Point and the surrounding communities! We offer affordable self storage units in a variety of
             sizes and service with a smile. Take a moment to review our pricing and availability, then call
             or reserve online to start your de-cluttering journey!`,
-    }
+  },
 ];
--- grant-avenue/src/app/data/view-rates.ts
+++ grant-avenue/src/app/data/view-rates.ts
@@ -1,15 +1,12 @@
 export const dataViewRates = [
-    {
-        p: `Grant Avenue Storage has a wide variety of climate-controlled unit and locker sizes, perfect
-            for all of your storage needs! Our selection offers everything from small lockers to 15x15 foot
-            spaces ideal for inventory or equipment storage. All of our units are competitively priced for both
-            residential and commercial use.
+  {
+    p: `Grant Avenue Storage has a wide variety of unit and locker sizes, perfect for all of your storage needs! Our selection offers everything from small lockers to 15x15 foot spaces ideal for inventory or equipment storage. All of our units are competitively priced for both residential and commercial use.
         `,
-    },
-    {
-        p: `Whether you're a homeowner or renter preparing for a remodel, or a move; or if you're a growing
+  },
+  {
+    p: `Whether you're a homeowner or renter preparing for a remodel, or a move; or if you're a growing
             business searching for additional space without paying commercial real estate prices, Grant Storage
             has you covered! Fill out our quick form, or call the office today, and one of our friendly
             managers will help get you started!`,
-    },
+  },
 ];
