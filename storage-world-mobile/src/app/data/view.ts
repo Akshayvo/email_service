f9dcae7d027cc2092c91597841047a6966c2dd38
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-05 19:29:56 +0530

    case 26501

--- storage-world-mobile/src/app/data/view.ts
+++ storage-world-mobile/src/app/data/view.ts
@@ -1,23 +1,23 @@
 export const viewRate = [
-    {
-        p: `Storage World Mobile has a large service area which includes Ham Lake, Blaine, East Bethel, Coon Rapids,
+  {
+    p: `Storage World Mobile has a large service area which includes Ham Lake, Blaine, East Bethel, Coon Rapids,
         Brooklyn Park, Maple Grove, Elk River, and most of Minneapolis and surrounding areas.`,
-    },
-    {
-        p: `We make portable storage easy for you! Simply make a reservation with our handy online form, or call our
+  },
+  {
+    p: `We make portable storage easy for you! Simply make a reservation with our handy online form, or call our
         office today and schedule the delivery of your well-maintained, portable storage unit! We take care of the rest!
         Pickup and delivery are handled by our trained storage professionals, leaving you to enjoy the convenience of
         24/7 access to your unit!`,
-    },
+  },
 ];
 
 export const rate = [
-    {
-        td1: 'SIZE',
-        td2: 'RATE ',
-    },
-    {
-        td1: '8\' X 16\' Portable Storage Unit',
-        td2: ' $170 a Month',
-    }
+  {
+    td1: "SIZE",
+    td2: "RATE ",
+  },
+  {
+    td1: "8' X 16' Portable Storage Unit",
+    td2: " $165 a Month",
+  },
 ];
