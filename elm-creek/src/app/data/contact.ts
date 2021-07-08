2f46a6904f816e449d826e61a9530544077b0dc7
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2021-07-08 19:14:25 +0530

    26556

--- elm-creek/src/app/data/contact.ts
+++ elm-creek/src/app/data/contact.ts
@@ -1,57 +1,55 @@
 export const contact = [
-    {
-        id: 0,
-        label: 'Address:',
-        data: '8502 128th E Ave, Owasso, OK 74055',
-        class: 'fas fa-map-marker-alt mx-2 iconColor',
-        redirect: 'https://goo.gl/maps/YdxJxvSvpPTJXuzU6'
-    },
-    {
-        id: 1,
-        label: 'Email:',
-        data: 'weirapp@familynet.net',
-        class: 'fas fa-envelope mx-2 iconColor',
-        redirect: 'mailto:`weirapp@familynet.net`'
-    },
-    {
-        id: 2,
-        label: 'Phone:',
-        data: '(918) 272-8894',
-        class: 'fas fa-phone mx-2 iconColor',
-        redirect: 'tel:9182728894',
-    }
+  {
+    id: 0,
+    label: "Address:",
+    data: "8500 N 128th E Ave, Owasso, OK 74055",
+    class: "fas fa-map-marker-alt mx-2 iconColor",
+    redirect: "https://goo.gl/maps/YdxJxvSvpPTJXuzU6",
+  },
+  {
+    id: 1,
+    label: "Email:",
+    data: "elmcreekstorage@gmail.com",
+    class: "fas fa-envelope mx-2 iconColor",
+    redirect: "mailto:`elmcreekstorage@gmail.com`",
+  },
+  {
+    id: 2,
+    label: "Phone:",
+    data: "(918) 272-8894",
+    class: "fas fa-phone mx-2 iconColor",
+    redirect: "tel:9182728894",
+  },
 ];
 
 export const hours = [
-    {
-        label: 'Monday - Friday: ',
-        data: '8:30AM to 5:00PM'
-    },
-    {
-        label: 'Saturday: ',
-        data: '8:30AM to 12:00PM'
-    },
-    {
-        label: 'Sunday: ',
-        data: 'CLOSED'
-    },
-    {
-        label: 'Storage Access Hours: ',
-        data: '24 Hours a Day 7 Days a Week'
-    }
+  {
+    label: "Monday - Friday: ",
+    data: "8:30AM to 5:00PM",
+  },
+  {
+    label: "Saturday: ",
+    data: "8:30AM to 12:00PM",
+  },
+  {
+    label: "Sunday: ",
+    data: "CLOSED",
+  },
+  {
+    label: "Storage Access Hours: ",
+    data: "24 Hours a Day 7 Days a Week",
+  },
 ];
 
 export const socialLinks = [
-    // {
-    //     label: 'twitter',
-    //     path: '',
-    //     class: 'fab fa-twitter-square',
-    // },
-    // {
-    //     label: 'facebook',
-    //     path: '',
-    //     class: 'fab fa-facebook-square',
-    // },
+  // {
+  //     label: 'twitter',
+  //     path: '',
+  //     class: 'fab fa-twitter-square',
+  // },
+  // {
+  //     label: 'facebook',
+  //     path: '',
+  //     class: 'fab fa-facebook-square',
+  // },
 ];
-
-
