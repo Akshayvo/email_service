f6986ea1ecbbcd5c39f19ffe4512f2cc26a143ee
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-08 16:17:54 +0530

    case 26550

--- glenmont/src/app/data/contact.ts
+++ glenmont/src/app/data/contact.ts
@@ -1,55 +1,55 @@
 export const contact = [
-    {
-        id: 0,
-        label: 'Address:',
-        data: '560 Rte 9W, Glenmont, NY 12077',
-        class: 'fas fa-map-marker-alt mx-2 iconColor',
-        redirect: 'https://goo.gl/maps/mEPDzqvkE7rXcjgQ9'
-    },
-    {
-        id: 1,
-        label: 'Email:',
-        data: 'info@glenmontselfstorage.com',
-        class: 'fas fa-envelope mx-2 iconColor',
-        redirect: 'mailto:`info@glenmontselfstorage.com`'
-    },
-    {
-        id: 2,
-        label: 'Phone:',
-        data: '(518) 470-1000',
-        class: 'fas fa-phone mx-2 iconColor',
-        redirect: 'tel:5184701000',
-    }
+  {
+    id: 0,
+    label: "Address:",
+    data: "560 Rte 9W, Glenmont, NY 12077",
+    class: "fas fa-map-marker-alt mx-2 iconColor",
+    redirect: "https://goo.gl/maps/mEPDzqvkE7rXcjgQ9",
+  },
+  {
+    id: 1,
+    label: "Email:",
+    data: "info@glenmontselfstorage.com",
+    class: "fas fa-envelope mx-2 iconColor",
+    redirect: "mailto: info@glenmontselfstorage.com",
+  },
+  {
+    id: 2,
+    label: "Phone:",
+    data: "(518) 470-1000",
+    class: "fas fa-phone mx-2 iconColor",
+    redirect: "tel:5184701000",
+  },
 ];
 
 export const hours = [
-    {
-        label: 'Monday - Friday: ',
-        data: '9:00AM to 5:00PM'
-    },
-    {
-        label: 'Saturday: ',
-        data: '9:00AM to 1:00PM'
-    },
-    {
-        label: 'Sunday: ',
-        data: 'CLOSED'
-    },
-    {
-        label: 'Storage Access Hours: ',
-        data: '7 Days a Week 7:00AM to 9:00PM'
-    }
+  {
+    label: "Monday - Friday: ",
+    data: "9:00AM to 5:00PM",
+  },
+  {
+    label: "Saturday: ",
+    data: "9:00AM to 1:00PM",
+  },
+  {
+    label: "Sunday: ",
+    data: "CLOSED",
+  },
+  {
+    label: "Storage Access Hours: ",
+    data: "7 Days a Week 7:00AM to 9:00PM",
+  },
 ];
 
 export const socialLinks = [
-    // {
-    //     label: 'twitter',
-    //     path: '',
-    //     class: 'fab fa-twitter-square',
-    // },
-    // {
-    //     label: 'facebook',
-    //     path: '',
-    //     class: 'fab fa-facebook-square',
-    // },
+  // {
+  //     label: 'twitter',
+  //     path: '',
+  //     class: 'fab fa-twitter-square',
+  // },
+  // {
+  //     label: 'facebook',
+  //     path: '',
+  //     class: 'fab fa-facebook-square',
+  // },
 ];
--- glenmont/src/app/data/galleryImage.ts
+++ glenmont/src/app/data/galleryImage.ts
@@ -4,37 +4,36 @@ export const galleryHome = [
     url: `glenmont-self-storage-retail-store`,
     // tslint:disable-next-line:max-line-length
     thumb: `glenmont-self-storage-retail-store`,
-    alt: `interior of a self storage retail store with boxes, tape, and packing supplies`
+    alt: `interior of a self storage retail store with boxes, tape, and packing supplies`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `glenmont-self-storage-sign`,
     // tslint:disable-next-line:max-line-length
     thumb: `glenmont-self-storage-sign`,
-    alt: `a self storage facility sign surrounded by landscaping outside of a self storage facility`
+    alt: `a self storage facility sign surrounded by landscaping outside of a self storage facility`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `glenmont-sign`,
     // tslint:disable-next-line:max-line-length
     thumb: `glenmont-sign`,
-    alt: `a time and temperature sign in the foreground with a self storage gate in the background`
+    alt: `a time and temperature sign in the foreground with a self storage gate in the background`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `interior-self-storage-unit`,
     // tslint:disable-next-line:max-line-length
     thumb: `interior-self-storage-unit`,
-    alt: `interior of a well-maintained self storage unit with a roll up metal door and clean concrete floor`
+    alt: `interior of a well-maintained self storage unit with a roll up metal door and clean concrete floor`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `self-storage-facility`,
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-facility`,
-    alt: ` A welcome sign in white letters placed in brown landscaping gravel and a moving truck with self storage facility signage printed on the truck`
+    alt: ` A welcome sign in white letters placed in brown landscaping gravel and a moving truck with self storage facility signage printed on the truck`,
   },
-
 ];
 
 export const galleryViewRates = [
@@ -43,14 +42,14 @@ export const galleryViewRates = [
     url: `self-storage-gate`,
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-gate`,
-    alt: `self storage facility automated gate with personalized pin-code pad`
+    alt: `self storage facility automated gate with personalized pin-code pad`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `self-storage-unit-buildings`,
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-unit-buildings`,
-    alt: `rows of self storage unit buildings with wide paved parking lanes between the buildings`
+    alt: `rows of self storage unit buildings with wide paved parking lanes between the buildings`,
   },
   {
     // tslint:disable-next-line: max-line-length
@@ -58,21 +57,69 @@ export const galleryViewRates = [
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-units`,
     alt: `several self storage unit buildings with paved parking spaces between buildings
-    `
+    `,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `self-storage`,
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage`,
-    alt: `self storage facility exit sign thanking customers for storing with Glenmont Self Storage`
+    alt: `self storage facility exit sign thanking customers for storing with Glenmont Self Storage`,
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `welcome-to-glenmont`,
     // tslint:disable-next-line:max-line-length
     thumb: `welcome-to-glenmont`,
-    alt: ` welcome sign posted outside of a self storage facility`
+    alt: ` welcome sign posted outside of a self storage facility`,
   },
-
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-electronic-gate`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-electronic-gate`,
+    alt: `self storage units behind a pin-code activated gate under a clear blue sky`,
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-exterior`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-exterior`,
+    alt: `the front office and facade of a self storage facility`,
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-indoor-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-indoor-units`,
+    alt: `indoor temperature controlled self storage units inside a brightly lit building`,
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-office`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-office`,
+    alt: `an exterior photo of a self storage office under a bright blue sky`,
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-outdoor-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-outdoor-units`,
+    alt: `outdoor self storage units on either side of a paved parkway`,
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-temperature-controlled-units`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-temperature-controlled-units`,
+    alt: `two rows of temperature controlled self storage units on either side of a brightly lit interior hallway`,
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `glenmont-self-storage-units-behind-gate`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `glenmont-self-storage-units-behind-gate`,
+    alt: `self storage units behind a pin-pad controlled security gate`,
+  },
 ];
