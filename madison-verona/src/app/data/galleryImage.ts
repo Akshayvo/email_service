06d21c305bb2562089c4e7f74d59031dd9f39101
pragathi <pragathi.n@codeparva.in>
2021-07-14 15:01:07 +0530

    case 26590

--- madison-verona/src/app/data/galleryImage.ts
+++ madison-verona/src/app/data/galleryImage.ts
@@ -5,6 +5,7 @@ export const galleryHome = [
     // tslint:disable-next-line:max-line-length
     thumb: `self-storage-office`,
     alt: `a self storage facility front office building`,
+    caption: '',
   },
   {
     // tslint:disable-next-line: max-line-length
@@ -12,6 +13,7 @@ export const galleryHome = [
     // tslint:disable-next-line:max-line-length
     thumb: `climate-controlled-self-storage`,
     alt: `a climate controlled self storage building with signage that reads "Climate Control and RV Storage"`,
+    caption: '',
   },
   {
     // tslint:disable-next-line: max-line-length
@@ -19,6 +21,7 @@ export const galleryHome = [
     // tslint:disable-next-line:max-line-length
     thumb: `open-storage-unit`,
     alt: `an open self storage unit revealing brand new, clean interior`,
+    caption: '',
   },
   {
     // tslint:disable-next-line: max-line-length
@@ -26,6 +29,7 @@ export const galleryHome = [
     // tslint:disable-next-line:max-line-length
     thumb: `row-of-self-storage-units`,
     alt: `a long row of brand new self storage units with green roll-up doors`,
+    caption: '',
   },
   {
     // tslint:disable-next-line: max-line-length
@@ -33,45 +37,91 @@ export const galleryHome = [
     // tslint:disable-next-line:max-line-length
     thumb: `electrical-outlet-equipped-self-storage-unit`,
     alt: `electrical outlet on the inside of a brand new clean self storage unit`,
+    caption: '',
   },
   {
     url: `madison-verona-main-office`,
     thumb: `madison-verona-main-office`,
     alt: `a self storage front office with a gate and units behind a security`,
+    caption: '',
   },
   {
     url: `self-storage-office-and-gate`,
     thumb: `self-storage-office-and-gate`,
     alt: `a self storage facility with large front office, electronic security gate, and units in the background`,
+    caption: '',
   },
   {
     url: `self-storage-facility-buildings`,
     thumb: `self-storage-facility-buildings`,
     alt: `rows self storage unit buildings behind a row of small pine trees`,
+    caption: '',
   },
   {
     url: `self-storage-units-rows`,
     thumb: `self-storage-units-rows`,
     alt: `two rows of self storage units on either side of a paved walkway`,
+    caption: '',
   },
   {
     url: `madison-verona-self-storage-parked-car`,
     thumb: `madison-verona-self-storage-parked-car`,
     alt: `a vehicle parked inside of a self storage unit with a roll-up door`,
+    caption: '',
   },
   {
     url: `madison-verona-self-storage-23`,
     thumb: `madison-verona-self-storage-23`,
     alt: `a row of self storage unit doors under a grey sky`,
+    caption: '',
   },
   {
     url: `madison-verona-self-storage-24`,
     thumb: `madison-verona-self-storage-24`,
     alt: `aerial photograph of a single story self storage facility with multiple metal buildings inside a fence`,
+    caption: '',
   },
   {
     url: `madison-verona-self-storage-26`,
     thumb: `madison-verona-self-storage-26`,
     alt: `an electronic, pin-code activated security gate in the foreground and self storage units in the background`,
+    caption: '',
   },
+  {
+    url: `madison-verona-self-storage-kiosk`,
+    thumb: `madison-verona-self-storage-kiosk-thumb`,
+    alt: `automated kiosk system built into a self storage facility's foyer wall`,
+    caption: 'Rental station open 24-hours a day',
+  },
+  {
+    url: `madison-verona-self-storage-aerial`,
+    thumb: `madison-verona-self-storage-aerial-thumb`,
+    alt: `aerial photography of a single story self storage facility`,
+    caption: '',
+  },
+  {
+    url: `madison-verona-self-storage-37`,
+    thumb: `madison-verona-self-storage-37-thumb`,
+    alt: `an electronic pin-pad that controls a self storage access gate`,
+    caption: 'Enter and exit pin-pads',
+  },
+  {
+    url: `madison-verona-self-storage-35`,
+    thumb: `madison-verona-self-storage-35-thumb`,
+    alt: `rows of single story metal self storage buildings with roll up doors`,
+    caption: '',
+  },
+  {
+    url: `madison-verona-self-storage-34`,
+    thumb: `madison-verona-self-storage-34-thumb`,
+    alt: `row of self storage units and a door leading to interior climate controlled storage units`,
+    caption: '',
+  },
+  {
+    url: `madison-verona-self-storage-33`,
+    thumb: `madison-verona-self-storage-33-thumb`,
+    alt: `corner of a single story self storage building`,
+    caption: '',
+  },
+
 ];
