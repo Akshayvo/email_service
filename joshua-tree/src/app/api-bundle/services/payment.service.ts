b41f2678f8c6096387369887a27459c20a6ed8d6
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-23 17:16:53 +0530

    Added move in payment charges

--- joshua-tree/src/app/api-bundle/services/payment.service.ts
+++ joshua-tree/src/app/api-bundle/services/payment.service.ts
@@ -1,18 +1,15 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { LstPayTypes } from '../models/payment';
-
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { LstPayTypes } from "../models/payment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PaymentService {
-
   private paymentUrl = `payment`;
 
-  constructor(private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
   makePayment(lstPayTypes: LstPayTypes[]): Observable<any> {
     return this.http.post<any>(this.paymentUrl, lstPayTypes);
