d0daa54faf9b604c7e3462e42398b37ed68cc3a5
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-26 17:52:37 +0530

    case 26625

--- starlite/src/app/data/pay-rent.ts
+++ starlite/src/app/data/pay-rent.ts
@@ -1,11 +1,14 @@
 export const loginDetail = [
-    {
-        userName: `Use Primary Tenants info; example: Al Smith = asmith`,
-        password: `A-units start with: A001 / all other units start with: B, C, D01 / Parking: RV01 or CAR01.`,
-        example: {
-            code: `John Smith's Unit is A01 and His Access Code is 1234`,
-            userName: 'jsmith',
-            password: 'A011234'
-        }
-    }
+  {
+    userName: ` Use Primary Tenants First Initial and last name; example: John Smith = JSmith`,
+    password: `Use Unit Number and Gate code; EXAMPLE:  Unit A001, Gate Code 12345 = A00112345`,
+    example: {
+      code: `John Smith's Unit is A001 and His Access Code is 12345`,
+      userName: "JSmith",
+      password: "A00112345",
+    },
+    note: {
+      info: "A-units start with: A001 / all other units start with: B, C, D01 / Parking: RV01 or CAR01. Unit Letters must be “CAPITALIZED” for password to work.",
+    },
+  },
 ];
