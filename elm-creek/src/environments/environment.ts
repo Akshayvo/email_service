8e17437e76ebb1502a477457651aff9244b68d5f
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-28 11:54:17 +0530

    case 26659 - production deployment

--- elm-creek/src/environments/environment.ts
+++ elm-creek/src/environments/environment.ts
@@ -4,24 +4,23 @@
 
 export const environment = {
   production: true,
-  type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'oklahoma/owasso', // state/city  **Remeber to update locationName in sitemap.xml as well
+  type: 0, // { API: 0, iframe: 1 }
+  locationName: "oklahoma/owasso", // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
-  military: false, // set true for Military and Driver's Details 
-  template: 'withoutJumbotron', // { darkTheme, lightTheme, withoutJumbotron}
-  cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
-  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
-  senderEmail: 'errors@codeparva.in',
-  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Elm Creek Mini Storage',
-  facilityName: 'Elm Creek Mini Storage',
-  VERSION: require('../../package.json').version,
+  military: false, // set true for Military and Driver's Details
+  template: "withoutJumbotron", // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ["Visa", "Master Card", "Amex", "Discover", "test"],
+  errorsMails: ["sanghmitra.v@codeparva.in"],
+  senderEmail: "errors@codeparva.in",
+  emailBaseUrl: "https://emailsvc-ext.syrasoft.com/v1/email",
+  appId: "Elm Creek Mini Storage",
+  facilityName: "Elm Creek Mini Storage",
+  VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'https://elm-creek-staging.netlify.app',
-  // APIKey: `dzF0OVc2SjkqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 
-  APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 
-  
+  websiteUrl: "https://elmcreekstorage.com",
+  // APIKey: `dzF0OVc2SjkqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, //
+  APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, //
 };
 
 /*
