dfe2169a865ad843eb5ec2f28407c7de4bf81c4b
Sanghmitra Vishwakarma <sanghmitra.v@codeparva.in>
2021-07-30 11:12:38 +0530

    case 26680 Updated temporary map

--- sibley/src/app/data/home.ts
+++ sibley/src/app/data/home.ts
@@ -1,25 +1,24 @@
 export const featuresList = [
-    {
-        td1: 'Convenient Location',
-        td2: 'Online Bill Pay',
-    },
-    {
-        td1: 'Security Cameras',
-        td2: 'Well-Lit Facility',
-    },
-    {
-        td1: 'Brand New Units',
-        td2: 'Single-Level Facility',
-    },
-    {
-        td1: 'Drive-Up Units',
-        td2: 'Auto-Pay Available',
-    },
-    {
-        td1: 'Friendly Customer Service',
-        td2: 'Affordable Rates',
-    },
-    
+  {
+    td1: "Convenient Location",
+    td2: "Online Bill Pay",
+  },
+  {
+    td1: "Security Cameras",
+    td2: "Well-Lit Facility",
+  },
+  {
+    td1: "Brand New Units",
+    td2: "Single-Level Facility",
+  },
+  {
+    td1: "Drive-Up Units",
+    td2: "Auto-Pay Available",
+  },
+  {
+    td1: "Friendly Customer Service",
+    td2: "Affordable Rates",
+  },
 ];
 
 export const aboutUsAlt = `indoor climate controlled storage units with roll-up doors inside of a clean, well-lit building`;
@@ -27,21 +26,20 @@ export const aboutUsAlt = `indoor climate controlled storage units with roll-up 
 export const aboutUsHeading = `Affordable Storage Units in Green Isle, MN`;
 
 export const aboutUs = [
-    {
-        p: `Sibley Storage is Green Isle’s newest self storage facility. Our brand new self storage units are conveniently located just north of Green Isle on 180th and offer all of the modern self storage conveniences you’d expect from a locally owned and operated business. From our contactless rental process to our 24-hour access policy and online tenant management portal, you’ll find storing at Sibley Storage to be a convenient and affordable experience.`,
-    },
-    {
-        p: `Start by taking a moment to review our available units and pricing, then rent your storage unit through our website in a matter of minutes. Once your agreement is signed, you’ll receive a lock code that gives you access to your new self storage unit. Inside, a complementary lock and key will be waiting for you. Just drop the combination lock in lock and payment dropbox on your way out, and you’re all set! Come and go as you please in our well-lit self storage facility with security cameras placed strategically around the grounds.`,
-    },
-    {
-        p: `Whether you’re a residential user in search of a home for the contents of your garage, attic, or basement, or if you’re a growing business who understands the savings to be had storing documents and equipment in a storage unit, Sibley Storage has you covered. If you have any questions or concerns, please feel free to call our office and inquire today!`,
-    },
-    // {
-    //     p: `Whether you're a growing business in need of a place for equipment, documents, and supplies,
-    //     or if you're a residential storage user in the process of a move, or a renovation, we've got your
-    //     storage needs covered. Let Sibley Storage help de-clutter your life today!`,
-    // }
-    
+  {
+    p: `Sibley Storage is Green Isle’s newest self storage facility. Our brand new self storage units are conveniently located just north of Green Isle on 180th and offer all of the modern self storage conveniences you’d expect from a locally owned and operated business. From our contactless rental process to our 24-hour access policy and online tenant management portal, you’ll find storing at Sibley Storage to be a convenient and affordable experience.`,
+  },
+  {
+    p: `Start by taking a moment to review our available units and pricing, then rent your storage unit through our website in a matter of minutes. Once your agreement is signed, you’ll receive a lock code that gives you access to your new self storage unit. Inside, a complementary lock and key will be waiting for you. Just drop the combination lock in lock and payment dropbox on your way out, and you’re all set! Come and go as you please in our well-lit self storage facility with security cameras placed strategically around the grounds.`,
+  },
+  {
+    p: `Whether you’re a residential user in search of a home for the contents of your garage, attic, or basement, or if you’re a growing business who understands the savings to be had storing documents and equipment in a storage unit, Sibley Storage has you covered. If you have any questions or concerns, please feel free to call our office and inquire today!`,
+  },
+  // {
+  //     p: `Whether you're a growing business in need of a place for equipment, documents, and supplies,
+  //     or if you're a residential storage user in the process of a move, or a renovation, we've got your
+  //     storage needs covered. Let Sibley Storage help de-clutter your life today!`,
+  // }
 ];
 
 export const gettingStarted = `Are you ready to stop putting off cleaning out your attic, garage, or shed?
@@ -49,36 +47,35 @@ Clear up some room around the house with Sibley Storage and one of our affordabl
 accessible 24 hours a day!`;
 
 export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        label: 'Convenient Location',
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
-        alt: 'Icon art displaying a location pin'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
-        label: 'Online Reservation & Bill Pay',
-        alt: 'Icon art displaying a screen with cursor'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
-        label: '24 Hour Facility Access',
-        alt: 'Icon art displaying clock hands and the number 24'
-
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/cctv.svg`,
-        label: 'Security Cameras on Premises',
-        alt: 'Icon of a security camera'
-    },
+  {
+    // tslint:disable-next-line:max-line-length
+    label: "Convenient Location",
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
+    label: "Online Bill Pay",
+    alt: "Icon art displaying a screen with cursor",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
+    label: "24 Hour Facility Access",
+    alt: "Icon art displaying clock hands and the number 24",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/cctv.svg`,
+    label: "Security Cameras on Premises",
+    alt: "Icon of a security camera",
+  },
 ];
 
 export const jumbotron = [
-    {
-        h2: `Self Storage Units in Green Isle, MN`,
-        blurb: `Sibley Storage is Green Isle's newest self storage facility. Our well-lit, affordable self storage units are the ideal solution for your home or business storage space needs! Rent online today, or call our friendly team for more information!`,
-    }
+  {
+    h2: `Self Storage Units in Green Isle, MN`,
+    blurb: `Sibley Storage is Green Isle's newest self storage facility. Our well-lit, affordable self storage units are the ideal solution for your home or business storage space needs! Rent online today, or call our friendly team for more information!`,
+  },
 ];
