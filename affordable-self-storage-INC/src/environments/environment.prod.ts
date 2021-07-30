57b33d25ca424e42a86602f66d933dd4376c96c7
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-07-30 12:15:49 +0530

    case 26660 deployment to prod

--- affordable-self-storage-INC/src/environments/environment.prod.ts
+++ affordable-self-storage-INC/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  type: 0,  // { API: 0, iframe: 1 }
-  template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
-  cards: ['Visa', 'Master Card', 'Amex', 'Discover'],
-  errorsMails: ['website.dev@codeparva.in'],
-  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  senderEmail: 'errors@codeparva.in',
-  appId: 'Affordable Self Storage INC - Production',
-  VERSION: require('../../package.json').version,
+  type: 0, // { API: 0, iframe: 1 }
+  template: "darkTheme", // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ["Visa", "Master Card", "Amex", "Discover"],
+  errorsMails: ["website.dev@codeparva.in"],
+  emailBaseUrl: "https://emailsvc-ext.syrasoft.com/v1/email",
+  senderEmail: "errors@codeparva.in",
+  appId: "Affordable Self Storage INC - Production",
+  VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   APIKey: `dzF0NVcxSjJOOjIyfCU4Nzg3NyQoOD1iVzBzaGRkOWtlb2NeTg==`, // Affordable self storage INC 1512
-  websiteUrl: ''
+  websiteUrl: "https://affordableselfstoragenj.com",
 };
--- affordable-self-storage-INC/src/environments/environment.ts
+++ affordable-self-storage-INC/src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-  type: 0,  // { API: 0, iframe: 1 }
-  template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
-  cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
-  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
-  senderEmail: 'errors@codeparva.in',
-  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Affordable Self Storage INC - Production',
-  VERSION: require('../../package.json').version,
+  type: 0, // { API: 0, iframe: 1 }
+  template: "darkTheme", // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ["Visa", "Master Card", "Amex", "Discover", "test"],
+  errorsMails: ["sanghmitra.v@codeparva.in"],
+  senderEmail: "errors@codeparva.in",
+  emailBaseUrl: "https://emailsvc-ext.syrasoft.com/v1/email",
+  appId: "Affordable Self Storage INC - Production",
+  VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: '',
+  websiteUrl: "https://affordableselfstoragenj.com",
   // baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // AnotherLocationAPIKey: `MTEyOTE6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`,
   // DemoNorthAPIKey: 'Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O',
