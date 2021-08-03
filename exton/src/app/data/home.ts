295254269ea2c7dcc159cede3208103076015105
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-03 20:19:05 +0530

    case 26699

--- exton/src/app/data/home.ts
+++ exton/src/app/data/home.ts
@@ -1,97 +1,93 @@
-
 export const featuresList = [
-    {
-        td1: 'Family-Owned',
-        td2: 'Daily Office Hours',
-    },
-    {
-        td1: 'Convenient Location',
-        td2: 'Self-Service Paper Shredding',
-    },
-    {
-        td1: '24 Hour Access Available',
-        td2: 'Online Reservations',
-    },
-    {
-        td1: 'Conference Room',
-        td2: 'Private Mailboxes',
-    },
-    {
-        td1: 'Pin-Code Activated Gates',
-        td2: 'Online Payments',
-    },
-    {
-        td1: 'Automatic Payments Available',
-        td2: 'Climate-Controlled Units Available'
-    },
+  {
+    td1: "Family-Owned",
+    td2: "Daily Office Hours",
+  },
+  {
+    td1: "Convenient Location",
+    td2: "Self-Service Paper Shredding",
+  },
+  {
+    td1: "24 Hour Access Available",
+    td2: "Online Reservations",
+  },
+  {
+    td1: "Conference Room",
+    td2: "Private Mailboxes",
+  },
+  {
+    td1: "Pin-Code Activated Gates",
+    td2: "Online Payments",
+  },
+  {
+    td1: "Automatic Payments Available",
+    td2: "Climate-Controlled Units Available",
+  },
 ];
 
 export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/location.svg',
-        label: 'Conveniently Located',
-        alt: 'Icon art displaying a location pin'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/garage.svg',
-        label: 'Boat and Vehicle Storage',
-        alt: 'Icon art displaying boat and motorcycle'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/light-bulb.svg',
-        label: 'Well-Lit Facility',
-        alt: 'Icon of an illuminated light'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/fence.svg',
-        label: 'Fully-Fenced Facility',
-        alt: 'Icon of a section of fencing'
-    },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/location.svg",
+    label: "Conveniently Located",
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/garage.svg",
+    label: "Boat and Vehicle Storage",
+    alt: "Icon art displaying boat and motorcycle",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/light-bulb.svg",
+    label: "Well-Lit Facility",
+    alt: "Icon of an illuminated light",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: "https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Exton_Store/fence.svg",
+    label: "Fully-Fenced Facility",
+    alt: "Icon of a section of fencing",
+  },
 ];
 
-
 export const aboutUs = [
-    {
-        p: `Exton Store-N-More is the premiere self storage facility serving Exton, Downingtown, West Chester,
+  {
+    p: `Exton Store-N-More is the premiere self storage facility serving Exton, Downingtown, West Chester,
         and the surrounding areas. We are conveniently located on East Clover Mill Road at the Whitford Road
         intersection.  We have a large variety of unit sizes and types to cover every storage need under the sun! `,
-    },
-    {
-        p: `We're more than just storage units! Do you have a boat, R/V, or classic car that requires seasonal
+  },
+  {
+    p: `We're more than just storage units! Do you have a boat, R/V, or classic car that requires seasonal
         storage? We've got plenty of parking and reasonable rates! Exton Store-N-More also offers a conference
         room, self-service shredding, private mailboxes, and a fax service for your convenience. We can also
         help you unload your gently-used belongings on e-Bay!`,
-    },
-    {
-        p: `On top of all of our extra amenities, we've also gone the extra mile to ensure a sense of safety
+  },
+  {
+    p: `On top of all of our extra amenities, we've also gone the extra mile to ensure a sense of safety
         in our facility. We are a well-lit, fully-fenced facility with pin-code activated front gate access, and
         have security cameras on-site. Our management team also performs routine lot-checks to ensure
         units are secured.`,
-    }
+  },
 ];
-
 
 export const gettingStarted = `Are you ready to start de-cluttering your life and reclaiming some of that
 elbow room around home or the office?
                                Click here to check out our affordable rates and make your reservation!`;
 
 export const blurb = [
-    {
-        h1: `Family Owned & Operated Self Storage`,
-        p: `We offer a wide variety of self storage unit sizes and types! Whether you're looking for
+  {
+    h1: `Family Owned & Operated Self Storage`,
+    p: `We offer a wide variety of self storage unit sizes and types! Whether you're looking for
         some extra space while relocating or remodeling
             a room, or need seasonal parking for you R/V, boat or motorcycle, we've got a unit for you! Call today!`,
-    }
+  },
 ];
 
-
 export const specialRow = [
-    {
-        h2: `Special Offer! One Month Free!`,
-        text: `Pay one month's rental, get one month free!`
-    }
+  // {
+  //     h2: `Special Offer! One Month Free!`,
+  //     text: `Pay one month's rental, get one month free!`
+  // }
 ];
