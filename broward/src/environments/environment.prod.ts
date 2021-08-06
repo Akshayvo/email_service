c532ba74bae860cd91ec1632a77c4cb0ee34215c
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-06 17:37:15 +0530

    case 26733

--- broward/src/environments/environment.prod.ts
+++ broward/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   facilityName: "Broward County Self Storage",
   VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: "https://broward-county-self-storage-staging.netlify.app",
+  websiteUrl: "https://browardcountyselfstorage.com",
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
   APIKey: `dzJ0OVc0SjgqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
 };
