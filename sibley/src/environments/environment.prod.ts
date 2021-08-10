764d116fa2ee450be323bb8122c2f0e9bd90d91f
pragathi <pragathi.n@codeparva.in>
2021-08-10 12:07:14 +0530

    domain change

--- sibley/src/environments/environment.prod.ts
+++ sibley/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   facilityName: 'Sibley Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'https://sibleystorage.com',
+  websiteUrl: 'https://www.sibleystorage.com',
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
   APIKey: `dzJ0OVc0SjkqOE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
 
