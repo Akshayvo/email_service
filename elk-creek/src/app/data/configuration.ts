e5822336334b5a96fcc8b7af357c7c2a128a533e
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-11 10:57:52 +0530

    case 26760 - turned off deposit

--- elk-creek/src/app/data/configuration.ts
+++ elk-creek/src/app/data/configuration.ts
@@ -1,23 +1,23 @@
 export const objSIMSetting = {
-    objActionSetting: {
-        blnAllowMoveIn: false,
-        blnAllowReservation: true
-    },
-    objPaymentSetting: {
-        blnAllowACHPayments: false,
-        blnAllowAutoPay: false,
-        blnAllowCCPayments: true,
-        blnAllowCancelAutoPay: true,
-        blnAllowPartialPayments: true
-    },
-    objReservationSetting: {
-        blnHideGateAccessCode: true,
-    },
-    objUnitSizesSetting: {
-        blnClimateControl: false,
-        blnShowDeposit: true,
-        blnShowRate: true,
-        blnShowUnavailableUnitTypes: true,
-        blnShowUnitAvailability: false
-    },
+  objActionSetting: {
+    blnAllowMoveIn: false,
+    blnAllowReservation: true,
+  },
+  objPaymentSetting: {
+    blnAllowACHPayments: false,
+    blnAllowAutoPay: false,
+    blnAllowCCPayments: true,
+    blnAllowCancelAutoPay: true,
+    blnAllowPartialPayments: true,
+  },
+  objReservationSetting: {
+    blnHideGateAccessCode: true,
+  },
+  objUnitSizesSetting: {
+    blnClimateControl: false,
+    blnShowDeposit: false,
+    blnShowRate: true,
+    blnShowUnavailableUnitTypes: true,
+    blnShowUnitAvailability: false,
+  },
 };
