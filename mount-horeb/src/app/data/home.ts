7bf0069b39a0e9b4dd9e58bbec778255b0f6fea2
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-12 14:07:24 +0530

    case 26762

--- mount-horeb/src/app/data/home.ts
+++ mount-horeb/src/app/data/home.ts
@@ -1,60 +1,59 @@
 export const featuresList = [
-    {
-        td1: 'Convenient Location',
-        td2: 'On-Site Manager',
-    },
-    {
-        td1: 'Online Bill Pay',
-        td2: 'Security Cameras',
-    },
-    {
-        td1: 'Well-Lit Facility',
-        td2: 'Brand New Units',
-    },
-    {
-        td1: 'Single-Level Facility',
-        td2: 'Drive-Up Units',
-    },
-    {
-        td1: 'Security System',
-        td2: 'Auto-Pay Available',
-    },
-    {
-        td1: 'Friendly Customer Service',
-        td2: 'Fully-Paved Facility',
-    },
-    {
-        td1: 'Pin-Pad Activated Security Gate',
-        td2: 'Affordable Rates',
-    },
-    {
-        td1: 'Recreational Vehicle Storage',
-        td2: 'Boat Storage',
-    },
-    {
-        td1: 'Climate-Controlled Units Available',
-        td2: 'Fully-Fenced Facility',
-    }
+  {
+    td1: "Convenient Location",
+    td2: "On-Site Manager",
+  },
+  {
+    td1: "Online Bill Pay",
+    td2: "Security Cameras",
+  },
+  {
+    td1: "Well-Lit Facility",
+    td2: "Brand New Units",
+  },
+  {
+    td1: "Single-Level Facility",
+    td2: "Drive-Up Units",
+  },
+  {
+    td1: "Security System",
+    td2: "Auto-Pay Available",
+  },
+  {
+    td1: "Friendly Customer Service",
+    td2: "Fully-Paved Facility",
+  },
+  {
+    td1: "Pin-Pad Activated Security Gate",
+    td2: "Affordable Rates",
+  },
+  {
+    td1: "Recreational Vehicle Storage",
+    td2: "Boat Storage",
+  },
+  {
+    td1: "Climate-Controlled Units Available",
+    td2: "Fully-Fenced Facility",
+  },
 ];
 
-export const aboutUsAlt = `indoor climate controlled storage units with roll-up doors inside of a clean, well-lit building`;
+export const aboutUsAlt = `the corner of a single story self storage building`;
 
 export const aboutUsHeading = `Brand New Self Storage Units & Vehicle Parking in Mount Horeb`;
 
 export const aboutUs = [
-    {
-        p: `Mount Horeb Self Storage is Mount Horeb Wisconsin’s newest self storage facility. Our state-of-the-art facility is conveniently located on Red Fox Drive and provides a number of self storage or vehicle parking solutions to the surrounding communities.`,
-    },
-    {
-        p: `Mount Horeb Self Storage was designed with all of the modern conveniences and security options available. Make your stay easy by finding the perfect unit or parking space through our website or the 24-hour kiosk located at the facility. Our well-lit, fully-fenced self storage facility offers a full on-site office, pin-code controlled electronic security gate, and security cameras placed strategically around the facility to ensure a sense of security at all times.`,
-    },
-    {
-        p: `From our climate-controlled 5-foot-by-5-foot units perfect for seasonal belongings that need a little extra protection from the elements to our oversized 12-foot-by-45-foot indoor parking units, we’ve got a storage solution for just about everything!`,
-    },
-    {
-        p: `Take a moment to check out our current sizes and monthly rates, then give our friendly management team a call, and we’ll be happy to take your reservation! We are open during the final phases of construction!`,
-    }
-    
+  {
+    p: `Mount Horeb Self Storage is Mount Horeb Wisconsin’s newest self storage facility. Our state-of-the-art facility is conveniently located on Red Fox Drive and provides a number of self storage or vehicle parking solutions to the surrounding communities.`,
+  },
+  {
+    p: `Mount Horeb Self Storage was designed with all of the modern conveniences and security options available. Make your stay easy by finding the perfect unit or parking space through our website or the 24-hour kiosk located at the facility. Our well-lit, fully-fenced self storage facility offers a full on-site office, pin-code controlled electronic security gate, and security cameras placed strategically around the facility to ensure a sense of security at all times.`,
+  },
+  {
+    p: `From our climate-controlled 5-foot-by-5-foot units perfect for seasonal belongings that need a little extra protection from the elements to our oversized 12-foot-by-45-foot indoor parking units, we’ve got a storage solution for just about everything!`,
+  },
+  {
+    p: `Take a moment to check out our current sizes and monthly rates, then give our friendly management team a call, and we’ll be happy to take your reservation! We are open during the final phases of construction!`,
+  },
 ];
 
 export const gettingStarted = `Are you ready to stop putting off cleaning out your attic, garage, or shed?
@@ -62,36 +61,36 @@ Clear up some room around the house with Sample Self Storage - North and one of 
 accessible 24 hours a day!`;
 
 export const feature = [
-    {
-        // tslint:disable-next-line:max-line-length
-        label: 'Boat and Vehicle Storage',
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/garage.svg`,
-        alt: 'Icon art displaying boat and motorcycle'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
-        label: 'Online Reservation & Bill Pay',
-        alt: 'Icon art displaying a location pin'
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
-        label: 'Online Reservation & Bill Pay',
-        alt: 'Icon art displaying a screen with cursor'
-
-    },
-    {
-        // tslint:disable-next-line:max-line-length
-        path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
-        label: '24 Hour Facility Access',
-        alt: 'Icon art displaying clock hands and the number 24'
-    },
+  {
+    // tslint:disable-next-line:max-line-length
+    label: "Boat and Vehicle Storage",
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/garage.svg`,
+    alt: "Icon art displaying boat and motorcycle",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/location.svg`,
+    label: "Online Reservation & Bill Pay",
+    alt: "Icon art displaying a location pin",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/online.svg`,
+    label: "Online Reservation & Bill Pay",
+    alt: "Icon art displaying a screen with cursor",
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    path: `https://s3.amazonaws.com/syrasoft-tenant-facing-websites/Syrasoft_Poweredby/icons/24-hours.svg`,
+    label: "24 Hour Facility Access",
+    alt: "Icon art displaying clock hands and the number 24",
+  },
 ];
 
 export const jumbotron = [
-    {
-        h2: `24-Hour Access Self Storage Units & Vehicle Parking`,
-        blurb: `Mount Horeb Self Storage is Mount Horeb's newest self storage and vehicle parking facility. Our self storage facility uses top-of-the-line technology and self storage management experience to make your next storage or parking project fast, easy, and affordable. Check out our current pricing, then give our team a call to make a reservation!`,
-    }
+  {
+    h1: `OPEN DURING CONSTRUCTION`,
+    h2: `24-Hour Access Self Storage Units & Vehicle Parking`,
+    blurb: `Mount Horeb Self Storage is Mount Horeb's newest self storage and vehicle parking facility. Our self storage facility uses top-of-the-line technology and self storage management experience to make your next storage or parking project fast, easy, and affordable. Check out our current pricing, then give our team a call to make a reservation!`,
+  },
 ];
--- mount-horeb/src/app/data/view-rates.ts
+++ mount-horeb/src/app/data/view-rates.ts
@@ -1,159 +1,135 @@
-import { objSIMSetting } from './configuration';
+import { objSIMSetting } from "./configuration";
 
 export const dataViewRates = [
-    {
-      p: `Review our sizes and monthly rates in both regular self storage units and our climate-controlled variety, then give our friendly management team a call, and we’ll be more than happy to take your reservation! We are open during the final stage of construction!`,
-    },
-    {
-      p: `Still not sure what sized unit will suit your home, business, or parking needs best? Check out our unit sizer page to find charts that show all of the sizes with descriptions of what contents are best suited for each type.`
-    },
-    // {
-    //   p: `Our residential storage users love our convenient location and affordable rates that take the stress
-    //   out of their move, renovation, or seasonal de-cluttering projects. An extra closet to an extra garage's
-    //   worth of space makes the most out of your home's space!
-    //   `
-    // },
-    // {
-    //   p: `Many local businesses in use our climate controlled storage units to save money by storing in a climate
-    //   controlled storage unit instead of expensive commercial real estate. We are the ideal space for your extra
-    // inventory, or documents. Our 24-hour access policy is the ideal solution for your local business.`
-    // }
+  {
+    p: `Review our sizes and monthly rates in both regular self storage units and our climate-controlled variety, then give our friendly management team a call, and we’ll be more than happy to take your reservation! We are open during the final stage of construction!`,
+  },
+  {
+    p: `Still not sure what sized unit will suit your home, business, or parking needs best? Check out our unit sizer page to find charts that show all of the sizes with descriptions of what contents are best suited for each type.`,
+  },
+  // {
+  //   p: `Our residential storage users love our convenient location and affordable rates that take the stress
+  //   out of their move, renovation, or seasonal de-cluttering projects. An extra closet to an extra garage's
+  //   worth of space makes the most out of your home's space!
+  //   `
+  // },
+  // {
+  //   p: `Many local businesses in use our climate controlled storage units to save money by storing in a climate
+  //   controlled storage unit instead of expensive commercial real estate. We are the ideal space for your extra
+  // inventory, or documents. Our 24-hour access policy is the ideal solution for your local business.`
+  // }
 ];
 
-export const viewRatesAltText = `two rows of self storage units on either side of a wide parking area`;
+export const viewRatesAltText = `the corner of a single story self storage building`;
 
 export const MilitaryTypes = [
   {
     Description: `NA`,
-    Value: 0 
+    Value: 0,
   },
   {
     Description: `Active`,
-    Value: 1
+    Value: 1,
   },
   {
     Description: `Reserve`,
-    Value: 2 
+    Value: 2,
   },
   {
     Description: `Veteran`,
-    Value: 3 
+    Value: 3,
   },
   {
     Description: `Retired`,
-    Value: 4
+    Value: 4,
   },
   {
     Description: `Deployed`,
-    Value: 5 
+    Value: 5,
   },
-]
+];
 
 export const newUnitTypes = [
-  { type: "5x5",
-  rate:"$50"
- },
-  { type: "5x10",
-  rate:"$80" },
-  { type: "10x10",
-  rate:"$110" },
-  { type: "10x15",
-  rate:"$125" },
-  { type: "10x20",
-  rate:"$170" },
-  { type: "10x25",
-  rate:"$190" },
-  { type: "10x30",
-  rate:"$220" },
-  { type: "11x25",
-  rate:"$205" },
-  { type: "12x30",
-  rate:"$240" },
+  { type: "5x5", rate: "$50" },
+  { type: "5x10", rate: "$80" },
+  { type: "10x10", rate: "$110" },
+  { type: "10x15", rate: "$125" },
+  { type: "10x20", rate: "$170" },
+  { type: "10x25", rate: "$190" },
+  { type: "10x30", rate: "$220" },
+  { type: "11x25", rate: "$205" },
+  { type: "12x30", rate: "$240" },
 ];
 
 export const newUnitTypes1 = [
-  { type: "5x10",
-  rate:"$50"
- },
-  { type: "10x10",
-  rate:"$85" },
-  { type: "10x15",
-  rate:"$95" },
-  { type: "10x20",
-  rate:"$115" },
-  { type: "10x25",
-  rate:"$125" },
-  { type: "10x30",
-  rate:"$135" },
-  { type: "11x10",
-  rate:"$95" },
-  { type: "11x30",
-  rate:"$165" },
-  { type: "12x20",
-  rate:"$130" },
-  { type: "12x25",
-  rate:"$165" },
-  { type: "12x30",
-  rate:"$200" },
-  { type: "12x35",
-  rate:"$230" },
-  { type: "12x40",
-  rate:"$265" },
-  { type: "12x45",
-  rate:"$295" },
+  { type: "5x10", rate: "$50" },
+  { type: "10x10", rate: "$85" },
+  { type: "10x15", rate: "$95" },
+  { type: "10x20", rate: "$115" },
+  { type: "10x25", rate: "$125" },
+  { type: "10x30", rate: "$135" },
+  { type: "11x10", rate: "$95" },
+  { type: "11x30", rate: "$165" },
+  { type: "12x20", rate: "$130" },
+  { type: "12x25", rate: "$165" },
+  { type: "12x30", rate: "$200" },
+  { type: "12x35", rate: "$230" },
+  { type: "12x40", rate: "$265" },
+  { type: "12x45", rate: "$295" },
 ];
 
 export const MilitaryBranch = [
   {
     Description: `NA`,
-    Value: 0 
+    Value: 0,
   },
   {
     Description: `Army`,
-    Value: 1
+    Value: 1,
   },
   {
     Description: `Navy`,
-    Value: 2 
+    Value: 2,
   },
   {
     Description: `AirForce`,
-    Value: 3 
+    Value: 3,
   },
   {
     Description: `Marines`,
-    Value: 4
+    Value: 4,
   },
   {
     Description: `CoastGuard`,
-    Value: 5 
+    Value: 5,
   },
-]
+];
 
 export const th = [
   {
-    id: 'size',
+    id: "size",
     data: `Size`,
     state: true,
   },
   {
-    id: 'rate',
+    id: "rate",
     data: `Rate`,
-    state: objSIMSetting.objUnitSizesSetting.blnShowRate
+    state: objSIMSetting.objUnitSizesSetting.blnShowRate,
   },
   {
-    id: 'deposit',
-    data: 'Deposit',
-    state: objSIMSetting.objUnitSizesSetting.blnShowDeposit
+    id: "deposit",
+    data: "Deposit",
+    state: objSIMSetting.objUnitSizesSetting.blnShowDeposit,
   },
-  {  id: 'reserve',
+  {
+    id: "reserve",
     data: `Reserve a Unit`,
-    state: objSIMSetting.objActionSetting.blnAllowReservation
+    state: objSIMSetting.objActionSetting.blnAllowReservation,
   },
   {
-    id: 'move-in',
+    id: "move-in",
     data: `Move In`,
-    state: objSIMSetting.objActionSetting.blnAllowMoveIn
+    state: objSIMSetting.objActionSetting.blnAllowMoveIn,
   },
   // {
   //   id: 'climate-control',
@@ -168,7 +144,7 @@ export const th1 = [
   },
   {
     data: `Rate`,
-    state: objSIMSetting.objUnitSizesSetting.blnShowRate
+    state: objSIMSetting.objUnitSizesSetting.blnShowRate,
   },
   // {
   //   data: 'deposit',
@@ -201,349 +177,348 @@ export const state = {
   climateControl: objSIMSetting.objUnitSizesSetting.blnClimateControl,
 };
 
-export const option =  [
-    {
-      description: 'Alabama',
-      id: 'AL'
-    },
-    {
-      description: 'Alaska',
-      id: 'AK'
-    },
-    {
-      description: 'Alberta',
-      id: 'AB'
-    },
-    {
-      description: 'American Samoa',
-      id: 'AS'
-    },
-    {
-      description: 'Arizona',
-      id: 'AZ'
-    },
-    {
-      description: 'Arkansas',
-      id: 'AR'
-    },
-    {
-      description: 'British Columbia',
-      id: 'BC'
-    },
-    {
-      description: 'California',
-      id: 'CA'
-    },
-    {
-      description: 'Colorado',
-      id: 'CO'
-    },
-    {
-      description: 'Connecticut',
-      id: 'CT'
-    },
-    {
-      description: 'Delaware',
-      id: 'DE'
-    },
-    {
-      description: 'District Of Columbia',
-      id: 'DC'
-    },
-    {
-      description: 'Federated States Of Micronesia',
-      id: 'FM'
-    },
-    {
-      description: 'Florida',
-      id: 'FL'
-    },
-    {
-      description: 'Georgia',
-      id: 'GA'
-    },
-    {
-      description: 'Guam',
-      id: 'GU'
-    },
-    {
-      description: 'Hawaii',
-      id: 'HI'
-    },
-    {
-      description: 'Idaho',
-      id: 'ID'
-    },
-    {
-      description: 'Illinois',
-      id: 'IL'
-    },
-    {
-      description: 'Indiana',
-      id: 'IN'
-    },
-    {
-      description: 'Iowa',
-      id: 'IA'
-    },
-    {
-      description: 'Kansas',
-      id: 'KS'
-    },
-    {
-      description: 'Kentucky',
-      id: 'KY'
-    },
-    {
-      description: 'Louisiana',
-      id: 'LA'
-    },
-    {
-      description: 'Maine',
-      id: 'ME'
-    },
-    {
-      description: 'Manitoba',
-      id: 'MB'
-    },
-    {
-      description: 'Marshall Islands',
-      id: 'MH'
-    },
-    {
-      description: 'Maryland',
-      id: 'MD'
-    },
-    {
-      description: 'Massachusetts',
-      id: 'MA'
-    },
-    {
-      description: 'Michigan',
-      id: 'MI'
-    },
-    {
-      description: 'Minnesota',
-      id: 'MN'
-    },
-    {
-      description: 'Mississippi',
-      id: 'MS'
-    },
-    {
-      description: 'Missouri',
-      id: 'MO'
-    },
-    {
-      description: 'Montana',
-      id: 'MT'
-    },
-    {
-      description: 'Nebraska',
-      id: 'NE'
-    },
-    {
-      description: 'Nevada',
-      id: 'NV'
-    },
-    {
-      description: 'Newfoundland and Labrador',
-      id: 'NL'
-    },
-    {
-      description: 'New Brunswick',
-      id: 'NB'
-    },
-    {
-      description: 'New Hampshire',
-      id: 'NH'
-    },
-    {
-      description: 'New Jersey',
-      id: 'NJ'
-    },
-    {
-      description: 'New Mexico',
-      id: 'NM'
-    },
-    {
-      description: 'New York',
-      id: 'NY'
-    },
-    {
-      description: 'Northwest Territories',
-      id: 'NT'
-    },
-    {
-      description: 'North Carolina',
-      id: 'NC'
-    },
-    {
-      description: 'North Dakota',
-      id: 'ND'
-    },
-    {
-      description: 'Northern Mariana Islands',
-      id: 'MP'
-    },
-    {
-      description: 'Nova Scotia',
-      id: 'NS'
-    },
-    {
-      description: 'Nunavut',
-      id: 'NU'
-    },
-    {
-      description: 'Ohio',
-      id: 'OH'
-    },
-    {
-      description: 'Oklahoma',
-      id: 'OK'
-    },
-    {
-      description: 'Ontario',
-      id: 'ON'
-    },
-    {
-      description: 'Oregon',
-      id: 'OR'
-    },
-    {
-      description: 'Palau',
-      id: 'PW'
-    },
-    {
-      description: 'Pennsylvania',
-      id: 'PA'
-    },
-    {
-      description: 'Prince Edward Island',
-      id: 'PE'
-    },
-    {
-      description: 'Puerto Rico',
-      id: 'PR'
-    },
-    {
-      description: 'Quebec',
-      id: 'QC'
-    },
-    {
-      description: 'Rhode Island',
-      id: 'RI'
-    },
-    {
-      description: 'Saskatchewan',
-      id: 'SK'
-    },
-    {
-      description: 'South Carolina',
-      id: 'SC'
-    },
-    {
-      description: 'South Dakota',
-      id: 'SD'
-    },
-    {
-      description: 'Tennessee',
-      id: 'TN'
-    },
-    {
-      description: 'Texas',
-      id: 'TX'
-    },
-    {
-      description: 'Utah',
-      id: 'UT'
-    },
-    {
-      description: 'Vermont',
-      id: 'VT'
-    },
-    {
-      description: 'Virgin Islands',
-      id: 'VI'
-    },
-    {
-      description: 'Virginia',
-      id: 'VA'
-    },
-    {
-      description: 'Washington',
-      id: 'WA'
-    },
-    {
-      description: 'West Virginia',
-      id: 'WV'
-    },
-    {
-      description: 'Wisconsin',
-      id: 'WI'
-    },
-    {
-      description: 'Wyoming',
-      id: 'WY'
-    },
-    {
-      description: 'Yukon',
-      id: 'YT'
-    },
+export const option = [
+  {
+    description: "Alabama",
+    id: "AL",
+  },
+  {
+    description: "Alaska",
+    id: "AK",
+  },
+  {
+    description: "Alberta",
+    id: "AB",
+  },
+  {
+    description: "American Samoa",
+    id: "AS",
+  },
+  {
+    description: "Arizona",
+    id: "AZ",
+  },
+  {
+    description: "Arkansas",
+    id: "AR",
+  },
+  {
+    description: "British Columbia",
+    id: "BC",
+  },
+  {
+    description: "California",
+    id: "CA",
+  },
+  {
+    description: "Colorado",
+    id: "CO",
+  },
+  {
+    description: "Connecticut",
+    id: "CT",
+  },
+  {
+    description: "Delaware",
+    id: "DE",
+  },
+  {
+    description: "District Of Columbia",
+    id: "DC",
+  },
+  {
+    description: "Federated States Of Micronesia",
+    id: "FM",
+  },
+  {
+    description: "Florida",
+    id: "FL",
+  },
+  {
+    description: "Georgia",
+    id: "GA",
+  },
+  {
+    description: "Guam",
+    id: "GU",
+  },
+  {
+    description: "Hawaii",
+    id: "HI",
+  },
+  {
+    description: "Idaho",
+    id: "ID",
+  },
+  {
+    description: "Illinois",
+    id: "IL",
+  },
+  {
+    description: "Indiana",
+    id: "IN",
+  },
+  {
+    description: "Iowa",
+    id: "IA",
+  },
+  {
+    description: "Kansas",
+    id: "KS",
+  },
+  {
+    description: "Kentucky",
+    id: "KY",
+  },
+  {
+    description: "Louisiana",
+    id: "LA",
+  },
+  {
+    description: "Maine",
+    id: "ME",
+  },
+  {
+    description: "Manitoba",
+    id: "MB",
+  },
+  {
+    description: "Marshall Islands",
+    id: "MH",
+  },
+  {
+    description: "Maryland",
+    id: "MD",
+  },
+  {
+    description: "Massachusetts",
+    id: "MA",
+  },
+  {
+    description: "Michigan",
+    id: "MI",
+  },
+  {
+    description: "Minnesota",
+    id: "MN",
+  },
+  {
+    description: "Mississippi",
+    id: "MS",
+  },
+  {
+    description: "Missouri",
+    id: "MO",
+  },
+  {
+    description: "Montana",
+    id: "MT",
+  },
+  {
+    description: "Nebraska",
+    id: "NE",
+  },
+  {
+    description: "Nevada",
+    id: "NV",
+  },
+  {
+    description: "Newfoundland and Labrador",
+    id: "NL",
+  },
+  {
+    description: "New Brunswick",
+    id: "NB",
+  },
+  {
+    description: "New Hampshire",
+    id: "NH",
+  },
+  {
+    description: "New Jersey",
+    id: "NJ",
+  },
+  {
+    description: "New Mexico",
+    id: "NM",
+  },
+  {
+    description: "New York",
+    id: "NY",
+  },
+  {
+    description: "Northwest Territories",
+    id: "NT",
+  },
+  {
+    description: "North Carolina",
+    id: "NC",
+  },
+  {
+    description: "North Dakota",
+    id: "ND",
+  },
+  {
+    description: "Northern Mariana Islands",
+    id: "MP",
+  },
+  {
+    description: "Nova Scotia",
+    id: "NS",
+  },
+  {
+    description: "Nunavut",
+    id: "NU",
+  },
+  {
+    description: "Ohio",
+    id: "OH",
+  },
+  {
+    description: "Oklahoma",
+    id: "OK",
+  },
+  {
+    description: "Ontario",
+    id: "ON",
+  },
+  {
+    description: "Oregon",
+    id: "OR",
+  },
+  {
+    description: "Palau",
+    id: "PW",
+  },
+  {
+    description: "Pennsylvania",
+    id: "PA",
+  },
+  {
+    description: "Prince Edward Island",
+    id: "PE",
+  },
+  {
+    description: "Puerto Rico",
+    id: "PR",
+  },
+  {
+    description: "Quebec",
+    id: "QC",
+  },
+  {
+    description: "Rhode Island",
+    id: "RI",
+  },
+  {
+    description: "Saskatchewan",
+    id: "SK",
+  },
+  {
+    description: "South Carolina",
+    id: "SC",
+  },
+  {
+    description: "South Dakota",
+    id: "SD",
+  },
+  {
+    description: "Tennessee",
+    id: "TN",
+  },
+  {
+    description: "Texas",
+    id: "TX",
+  },
+  {
+    description: "Utah",
+    id: "UT",
+  },
+  {
+    description: "Vermont",
+    id: "VT",
+  },
+  {
+    description: "Virgin Islands",
+    id: "VI",
+  },
+  {
+    description: "Virginia",
+    id: "VA",
+  },
+  {
+    description: "Washington",
+    id: "WA",
+  },
+  {
+    description: "West Virginia",
+    id: "WV",
+  },
+  {
+    description: "Wisconsin",
+    id: "WI",
+  },
+  {
+    description: "Wyoming",
+    id: "WY",
+  },
+  {
+    description: "Yukon",
+    id: "YT",
+  },
 ];
 
-export const option1 =  [
+export const option1 = [
   {
-    description: 'Alberta',
-    id: 'AB'
-  },
-  {
-    description: 'British Columbia',
-    id: 'BC'
+    description: "Alberta",
+    id: "AB",
   },
   {
-    description: 'Manitoba',
-    id: 'MB'
-  },
-  { 
-    description: 'New Brunswick',
-    id: 'NB'
+    description: "British Columbia",
+    id: "BC",
   },
   {
-    description: 'Newfoundland and Labrador',
-    id: 'NL'
+    description: "Manitoba",
+    id: "MB",
   },
   {
-    description: 'Northwest Territories',
-    id: 'NT'
+    description: "New Brunswick",
+    id: "NB",
   },
   {
-    description: 'Nova Scotia',
-    id: 'NS'
+    description: "Newfoundland and Labrador",
+    id: "NL",
   },
   {
-    description: 'Nunavut',
-    id: 'NU'
+    description: "Northwest Territories",
+    id: "NT",
   },
   {
-    description: 'Ontario',
-    id: 'ON'
+    description: "Nova Scotia",
+    id: "NS",
   },
   {
-    description: 'Prince Edward Island',
-    id: 'PE'
+    description: "Nunavut",
+    id: "NU",
   },
   {
-    description: 'Quebec',
-    id: 'QC'
+    description: "Ontario",
+    id: "ON",
   },
   {
-    description: 'Saskatchewan',
-    id: 'SK'
+    description: "Prince Edward Island",
+    id: "PE",
   },
   {
-    description: 'Yukon',
-    id: 'YT'
+    description: "Quebec",
+    id: "QC",
   },
-  ];
-  
+  {
+    description: "Saskatchewan",
+    id: "SK",
+  },
+  {
+    description: "Yukon",
+    id: "YT",
+  },
+];
