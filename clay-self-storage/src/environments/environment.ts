b7cfba4be91515b381b5205ac897082348502074
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-20 12:12:47 +0530

    case 26818

--- clay-self-storage/src/environments/environment.ts
+++ clay-self-storage/src/environments/environment.ts
@@ -4,24 +4,23 @@
 
 export const environment = {
   production: true,
-  type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'new-york/clay', // state/city  **Remeber to update locationName in sitemap.xml as well
+  type: 0, // { API: 0, iframe: 1 }
+  locationName: "new-york/clay", // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
-  military: true, // set true for Military and Driver's Details 
-  template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
-  cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
-  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
-  senderEmail: 'errors@codeparva.in',
-  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Clay Self Storage - Testing',
-  facilityName: 'Clay Self Storage',
-  VERSION: require('../../package.json').version,
+  military: true, // set true for Military and Driver's Details
+  template: "darkTheme", // { darkTheme, lightTheme, withoutJumbotron}
+  cards: ["Visa", "Master Card", "Amex", "Discover", "test"],
+  errorsMails: ["sanghmitra.v@codeparva.in"],
+  senderEmail: "errors@codeparva.in",
+  emailBaseUrl: "https://emailsvc-ext.syrasoft.com/v1/email",
+  appId: "Clay Self Storage - Testing",
+  facilityName: "Clay Self Storage",
+  VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'https://clay-self-storage-staging.netlify.app',
-  // APIKey: `bTJUOG43TDVbNls6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28756
-  APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
-  
+  websiteUrl: "https://clay-self-storage-staging.netlify.app",
+  APIKey: `bTJUOG43TDVbNls6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28756
+  //APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
 };
 
 /*
