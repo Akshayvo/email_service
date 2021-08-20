f24f5d97134fe603d6bceaf32e8649b1aca6a915
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-20 12:46:26 +0530

    case 26817 - phone number change

--- in-n-out/src/app/data/contact.ts
+++ in-n-out/src/app/data/contact.ts
@@ -1,286 +1,285 @@
 export const contactsHomePage = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: 'N7124 Willow Rd, Sheboygan Falls, WI 53085',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/n2LkowFuwMuEZCtt5'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        email: 'info@in-n-out-storage.com',
-        data: `info@in-n-out-storage.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:info@in-n-out-storage.com'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(920) 332-5899',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:9203325899'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "N7124 Willow Rd, Sheboygan Falls, WI 53085",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://goo.gl/maps/n2LkowFuwMuEZCtt5",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    email: "info@in-n-out-storage.com",
+    data: `info@in-n-out-storage.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:info@in-n-out-storage.com",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(920) 644-4423",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:9206444423",
+  },
 ];
 
 export const hoursHomePage = [
-    {
-        label: 'Monday - Friday: ',
-        data: '8:00AM to 5:00PM',
-    },
-    {
-        label: 'Saturday - Sunday: ',
-        data: 'Self Check-In Available',
-    },
+  {
+    label: "Monday - Friday: ",
+    data: "8:00AM to 5:00PM",
+  },
+  {
+    label: "Saturday - Sunday: ",
+    data: "Self Check-In Available",
+  },
 ];
 
 export const contactsLocation1 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '405 Commerce St Plymouth, WI 53073',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/r5Q8eeg2bTZyA7YFA'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        email: 'info@in-n-out-storage.com',
-        data: `info@in-n-out-storage.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`info@in-n-out-storage.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(920) 332-5899',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:9203325899'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "405 Commerce St Plymouth, WI 53073",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://goo.gl/maps/r5Q8eeg2bTZyA7YFA",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    email: "info@in-n-out-storage.com",
+    data: `info@in-n-out-storage.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:`info@in-n-out-storage.com`",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(920) 644-4423",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:9206444423",
+  },
 ];
 
 export const hoursLocation1 = [
-    {
-        label: 'Monday - Sunday: ',
-        data: 'No On-Site Office',
-    },
-    // {
-    //     label: 'Saturday - Sunday: ',
-    //     data: 'CLOSED',
-    // },
+  {
+    label: "Monday - Sunday: ",
+    data: "No On-Site Office",
+  },
+  // {
+  //     label: 'Saturday - Sunday: ',
+  //     data: 'CLOSED',
+  // },
 ];
 
 export const hoursLocation1AcessHours = [
-    {
-        data: '24 Hours a Day 7 Days a Week'
-    },
+  {
+    data: "24 Hours a Day 7 Days a Week",
+  },
 ];
 
 export const socialLinks = [
-    // {
-    //     label: 'twitter',
-    //     path: '',
-    //     class: 'fab fa-twitter-square',
-    // },
-    {
-        label: 'facebook',
-        path: 'https://www.facebook.com/In.N.Out.Storage/',
-        class: 'fab fa-facebook-square',
-    },
+  // {
+  //     label: 'twitter',
+  //     path: '',
+  //     class: 'fab fa-twitter-square',
+  // },
+  {
+    label: "facebook",
+    path: "https://www.facebook.com/In.N.Out.Storage/",
+    class: "fab fa-facebook-square",
+  },
 ];
 
-
 export const contactsLocation2 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: 'N6112 N Pleasant View Rd, Plymouth, WI 53073',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://g.page/r/CT1A0QzwMwCHEBA'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'info@in-n-out-storage.com',
-        email: `info@in-n-out-storage.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:info@in-n-out-storage.com'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(920) 332-5899',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:9203325899'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "N6112 N Pleasant View Rd, Plymouth, WI 53073",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://g.page/r/CT1A0QzwMwCHEBA",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    data: "info@in-n-out-storage.com",
+    email: `info@in-n-out-storage.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:info@in-n-out-storage.com",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(920) 644-4423",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:9206444423",
+  },
 ];
 
 export const hoursLocation2 = [
-    {
-        label: 'Monday - Sunday: ',
-        data: 'No On-Site Office',
-    },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    // {
-    //     data: '24-Hours a Day 7 Days a Week',
-    // },
+  {
+    label: "Monday - Sunday: ",
+    data: "No On-Site Office",
+  },
+  // {
+  //     label: 'Sunday: ',
+  //     data: 'CLOSED',
+  // },
+  // {
+  //     data: '24-Hours a Day 7 Days a Week',
+  // },
 ];
 
 export const contactsLocation3 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '424 E Clifford St, Plymouth, WI 53073',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/j7GCwA4RkQYLnSLt5'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'info@in-n-out-storage.com',
-        email: `info@in-n-out-storage.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:info@in-n-out-storage.com'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(920) 332-5899',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:9203325899'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "424 E Clifford St, Plymouth, WI 53073",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://goo.gl/maps/j7GCwA4RkQYLnSLt5",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    data: "info@in-n-out-storage.com",
+    email: `info@in-n-out-storage.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:info@in-n-out-storage.com",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(920) 644-4423",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:9206444423",
+  },
 ];
 
 export const hoursLocation3 = [
-    {
-        label: 'Monday - Sunday: ',
-        data: 'No On-Site Office',
-    },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    // {
-    //     data: '24-Hours a Day 7 Days a Week',
-    // },
+  {
+    label: "Monday - Sunday: ",
+    data: "No On-Site Office",
+  },
+  // {
+  //     label: 'Sunday: ',
+  //     data: 'CLOSED',
+  // },
+  // {
+  //     data: '24-Hours a Day 7 Days a Week',
+  // },
 ];
 
 export const contactsLocation4 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '423 E Clifford St Plymouth, WI 53073',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://g.page/r/CYgxbl0IyGw9EBA'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'info@in-n-out-storage.com',
-        email: `info@in-n-out-storage.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:info@in-n-out-storage.com'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(920) 332-5899',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:9203325899'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "423 E Clifford St Plymouth, WI 53073",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://g.page/r/CYgxbl0IyGw9EBA",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    data: "info@in-n-out-storage.com",
+    email: `info@in-n-out-storage.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:info@in-n-out-storage.com",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(920) 644-4423",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:9206444423",
+  },
 ];
 
 export const hoursLocation4 = [
-    {
-        label: 'Monday - Sunday: ',
-        data: 'No On-Site Office',
-    },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    // {
-    //     data: '24-Hours a Day 7 Days a Week',
-    // },
+  {
+    label: "Monday - Sunday: ",
+    data: "No On-Site Office",
+  },
+  // {
+  //     label: 'Sunday: ',
+  //     data: 'CLOSED',
+  // },
+  // {
+  //     data: '24-Hours a Day 7 Days a Week',
+  // },
 ];
 
 export const contactsLocation5 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '21193 AL-181, Location5, AL 36532',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/FANSU6w7QY9vCpiDA'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "21193 AL-181, Location5, AL 36532",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://goo.gl/maps/FANSU6w7QY9vCpiDA",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    data: "accounts@storageplusbaldwin.com",
+    email: `accounts@storageplusbaldwin.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:`accounts@storageplusbaldwin.com`",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(251) 970-3168",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:2519703168",
+  },
 ];
 
 export const hoursLocation5 = [
-    // {
-    //     label: 'Monday - Saturday: ',
-    //     data: '9:00AM - 5:00PM',
-    // },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    {
-        data: '24-Hours a Day 7 Days a Week',
-    },
+  // {
+  //     label: 'Monday - Saturday: ',
+  //     data: '9:00AM - 5:00PM',
+  // },
+  // {
+  //     label: 'Sunday: ',
+  //     data: 'CLOSED',
+  // },
+  {
+    data: "24-Hours a Day 7 Days a Week",
+  },
 ];
 
 export const contactsLocation6 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '20218 US-90, Robertsdale, AL 36567',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/kt1bhA6aHeRmjXsN7'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
-    },
+  {
+    id: 1,
+    label: "Address: ",
+    data: "20218 US-90, Robertsdale, AL 36567",
+    class: "fas fa-map-marker-alt mr-2 iconColor",
+    redirect: "https://goo.gl/maps/kt1bhA6aHeRmjXsN7",
+  },
+  {
+    id: 2,
+    label: "Email: ",
+    data: "accounts@storageplusbaldwin.com",
+    email: `accounts@storageplusbaldwin.com`,
+    class: "fas fa-envelope-open mr-2 iconColor",
+    redirect: "mailto:`accounts@storageplusbaldwin.com`",
+  },
+  {
+    id: 3,
+    label: "Phone: ",
+    data: "(251) 970-3168",
+    class: "fas fa-phone mr-2 iconColor",
+    redirect: "tel:2519703168",
+  },
 ];
 
 export const hoursLocation6 = [
-    // {
-    //     label: 'Monday - Saturday: ',
-    //     data: '9:00AM - 5:00PM',
-    // },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    {
-        data: '24-Hours a Day 7 Days a Week',
-    },
+  // {
+  //     label: 'Monday - Saturday: ',
+  //     data: '9:00AM - 5:00PM',
+  // },
+  // {
+  //     label: 'Sunday: ',
+  //     data: 'CLOSED',
+  // },
+  {
+    data: "24-Hours a Day 7 Days a Week",
+  },
 ];
