dad1c89ada0d1bdf4bbd5976b9acb1e9473a0820
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-20 10:44:15 +0530

    case 26808 - removed reservation for 29295 locid

--- self-storage-wis-dells-munroe-ave/src/app/data/view-rates.ts
+++ self-storage-wis-dells-munroe-ave/src/app/data/view-rates.ts
@@ -1,336 +1,336 @@
 export const viewRates = [
-    {
-      p: `Do you need some extra elbow-room around the house or office on a budget? Self Storage of Wis Dells - Munroe
+  {
+    p: `Do you need some extra elbow-room around the house or office on a budget? Self Storage of Wis Dells - Munroe
       Ave is the ideal storage solution for you! Our facility offers many self storage conveniences at extremely affordable rates!`,
-    },
-    {
-      p: `You'll have 24-hour access to your clean storage unit in our conveniently located facility 7 days a week at
+  },
+  {
+    p: `You'll have 24-hour access to your clean storage unit in our conveniently located facility 7 days a week at
       Self Storage of Wis Dells - Munroe Ave. On top of that, our online rental and bill-pay portal means you can move
       in, or access your account on your schedule! Do you have questions about the services we offer? Call today, and
       our staff will be happy to assist in any way possible!`,
-    },
+  },
 ];
 
 export const altText = ``;
 
 export const th = [
   {
-    data: `Size`
+    data: `Size`,
   },
   {
-    data: `Rate`
+    data: `Rate`,
   },
   // {
   //   data: `Annual Rate`,
   // },
+  // {
+  //   data: "Reservation Fee",
+  // },
+  // {
+  //   data: `Reserve a Unit`,
+  // },
   {
-    data: 'Reservation Fee'
-  },
-  {
-    data: `Reserve a Unit`
-  },
-  {
-    data: `Move In`
+    data: `Move In`,
   },
   // {
   //   data: `Climate Control`
   // },
 ];
 
-export const option =  [
-    {
-      description: 'Alaska',
-      id: 'AK'
-    },
-    {
-      description: 'Alabama',
-      id: 'AL'
-    },
-    {
-      description: 'Arkansas',
-      id: 'AR'
-    },
-    {
-      description: 'Arizona',
-      id: 'AZ'
-    },
-    {
-      description: 'Alberta',
-      id: 'AB'
-    },
-    {
-      description: 'American Samoa',
-      id: 'AS'
-    },
-    {
-      description: 'California',
-      id: 'CA'
-    },
-    {
-      description: 'Colorado',
-      id: 'CO'
-    },
-    {
-      description: 'Connecticut',
-      id: 'CT'
-    },
-    {
-      description: 'Delaware',
-      id: 'DE'
-    },
-    {
-      description: 'District Of Columbia',
-      id: 'DC'
-    },
-    {
-      description: 'Federated States Of Micronesia',
-      id: 'FM'
-    },
-    {
-      description: 'Florida',
-      id: 'FL'
-    },
-    {
-      description: 'Georgia',
-      id: 'GA'
-    },
-    {
-      description: 'Guam',
-      id: 'GU'
-    },
-    {
-      description: 'Hawaii',
-      id: 'HI'
-    },
-    {
-      description: 'Idaho',
-      id: 'ID'
-    },
-    {
-      description: 'Illinois',
-      id: 'IL'
-    },
-    {
-      description: 'Indiana',
-      id: 'IN'
-    },
-    {
-      description: 'Iowa',
-      id: 'IA'
-    },
-    {
-      description: 'Kansas',
-      id: 'KS'
-    },
-    {
-      description: 'Kentucky',
-      id: 'KY'
-    },
-    {
-      description: 'Louisiana',
-      id: 'LA'
-    },
-    {
-      description: 'Maine',
-      id: 'ME'
-    },
-    {
-      description: 'Marshall Islands',
-      id: 'MH'
-    },
-    {
-      description: 'Maryland',
-      id: 'MD'
-    },
-    {
-      description: 'Massachusetts',
-      id: 'MA'
-    },
-    {
-      description: 'Michigan',
-      id: 'MI'
-    },
-    {
-      description: 'Minnesota',
-      id: 'MN'
-    },
-    {
-      description: 'Mississippi',
-      id: 'MS'
-    },
-    {
-      description: 'Missouri',
-      id: 'MO'
-    },
-    {
-      description: 'Montana',
-      id: 'MT'
-    },
-    {
-      description: 'Nebraska',
-      id: 'NE'
-    },
-    {
-      description: 'Nevada',
-      id: 'NV'
-    },
-    {
-      description: 'New Hampshire',
-      id: 'NH'
-    },
-    {
-      description: 'New Jersey',
-      id: 'NJ'
-    },
-    {
-      description: 'New Mexico',
-      id: 'NM'
-    },
-    {
-      description: 'New York',
-      id: 'NY'
-    },
-    {
-      description: 'North Carolina',
-      id: 'NC'
-    },
-    {
-      description: 'North Dakota',
-      id: 'ND'
-    },
-    {
-      description: 'Northern Mariana Islands',
-      id: 'MP'
-    },
-    {
-      description: 'Ohio',
-      id: 'OH'
-    },
-    {
-      description: 'Oklahoma',
-      id: 'OK'
-    },
-    {
-      description: 'Oregon',
-      id: 'OR'
-    },
-    {
-      description: 'Palau',
-      id: 'PW'
-    },
-    {
-      description: 'Pennsylvania',
-      id: 'PA'
-    },
-    {
-      description: 'Puerto Rico',
-      id: 'PR'
-    },
-    {
-      description: 'Rhode Island',
-      id: 'RI'
-    },
-    {
-      description: 'South Carolina',
-      id: 'SC'
-    },
-    {
-      description: 'South Dakota',
-      id: 'SD'
-    },
-    {
-      description: 'Tennessee',
-      id: 'TN'
-    },
-    {
-      description: 'Texas',
-      id: 'TX'
-    },
-    {
-      description: 'Utah',
-      id: 'UT'
-    },
-    {
-      description: 'Vermont',
-      id: 'VT'
-    },
-    {
-      description: 'Virgin Islands',
-      id: 'VI'
-    },
-    {
-      description: 'Virginia',
-      id: 'VA'
-    },
-    {
-      description: 'Washington',
-      id: 'WA'
-    },
-    {
-      description: 'West Virginia',
-      id: 'WV'
-    },
-    {
-      description: 'Wisconsin',
-      id: 'WI'
-    },
-    {
-      description: 'Wyoming',
-      id: 'WY'
-    },
+export const option = [
+  {
+    description: "Alaska",
+    id: "AK",
+  },
+  {
+    description: "Alabama",
+    id: "AL",
+  },
+  {
+    description: "Arkansas",
+    id: "AR",
+  },
+  {
+    description: "Arizona",
+    id: "AZ",
+  },
+  {
+    description: "Alberta",
+    id: "AB",
+  },
+  {
+    description: "American Samoa",
+    id: "AS",
+  },
+  {
+    description: "California",
+    id: "CA",
+  },
+  {
+    description: "Colorado",
+    id: "CO",
+  },
+  {
+    description: "Connecticut",
+    id: "CT",
+  },
+  {
+    description: "Delaware",
+    id: "DE",
+  },
+  {
+    description: "District Of Columbia",
+    id: "DC",
+  },
+  {
+    description: "Federated States Of Micronesia",
+    id: "FM",
+  },
+  {
+    description: "Florida",
+    id: "FL",
+  },
+  {
+    description: "Georgia",
+    id: "GA",
+  },
+  {
+    description: "Guam",
+    id: "GU",
+  },
+  {
+    description: "Hawaii",
+    id: "HI",
+  },
+  {
+    description: "Idaho",
+    id: "ID",
+  },
+  {
+    description: "Illinois",
+    id: "IL",
+  },
+  {
+    description: "Indiana",
+    id: "IN",
+  },
+  {
+    description: "Iowa",
+    id: "IA",
+  },
+  {
+    description: "Kansas",
+    id: "KS",
+  },
+  {
+    description: "Kentucky",
+    id: "KY",
+  },
+  {
+    description: "Louisiana",
+    id: "LA",
+  },
+  {
+    description: "Maine",
+    id: "ME",
+  },
+  {
+    description: "Marshall Islands",
+    id: "MH",
+  },
+  {
+    description: "Maryland",
+    id: "MD",
+  },
+  {
+    description: "Massachusetts",
+    id: "MA",
+  },
+  {
+    description: "Michigan",
+    id: "MI",
+  },
+  {
+    description: "Minnesota",
+    id: "MN",
+  },
+  {
+    description: "Mississippi",
+    id: "MS",
+  },
+  {
+    description: "Missouri",
+    id: "MO",
+  },
+  {
+    description: "Montana",
+    id: "MT",
+  },
+  {
+    description: "Nebraska",
+    id: "NE",
+  },
+  {
+    description: "Nevada",
+    id: "NV",
+  },
+  {
+    description: "New Hampshire",
+    id: "NH",
+  },
+  {
+    description: "New Jersey",
+    id: "NJ",
+  },
+  {
+    description: "New Mexico",
+    id: "NM",
+  },
+  {
+    description: "New York",
+    id: "NY",
+  },
+  {
+    description: "North Carolina",
+    id: "NC",
+  },
+  {
+    description: "North Dakota",
+    id: "ND",
+  },
+  {
+    description: "Northern Mariana Islands",
+    id: "MP",
+  },
+  {
+    description: "Ohio",
+    id: "OH",
+  },
+  {
+    description: "Oklahoma",
+    id: "OK",
+  },
+  {
+    description: "Oregon",
+    id: "OR",
+  },
+  {
+    description: "Palau",
+    id: "PW",
+  },
+  {
+    description: "Pennsylvania",
+    id: "PA",
+  },
+  {
+    description: "Puerto Rico",
+    id: "PR",
+  },
+  {
+    description: "Rhode Island",
+    id: "RI",
+  },
+  {
+    description: "South Carolina",
+    id: "SC",
+  },
+  {
+    description: "South Dakota",
+    id: "SD",
+  },
+  {
+    description: "Tennessee",
+    id: "TN",
+  },
+  {
+    description: "Texas",
+    id: "TX",
+  },
+  {
+    description: "Utah",
+    id: "UT",
+  },
+  {
+    description: "Vermont",
+    id: "VT",
+  },
+  {
+    description: "Virgin Islands",
+    id: "VI",
+  },
+  {
+    description: "Virginia",
+    id: "VA",
+  },
+  {
+    description: "Washington",
+    id: "WA",
+  },
+  {
+    description: "West Virginia",
+    id: "WV",
+  },
+  {
+    description: "Wisconsin",
+    id: "WI",
+  },
+  {
+    description: "Wyoming",
+    id: "WY",
+  },
 ];
 
-export const option1 =  [
+export const option1 = [
   {
-    description: 'Alberta',
-    id: 'AB'
+    description: "Alberta",
+    id: "AB",
   },
   {
-    description: 'British Columbia',
-    id: 'BC'
+    description: "British Columbia",
+    id: "BC",
   },
   {
-    description: 'Manitoba',
-    id: 'MB'
-  },
-  { 
-    description: 'New Brunswick',
-    id: 'NB'
+    description: "Manitoba",
+    id: "MB",
   },
   {
-    description: 'Newfoundland and Labrador',
-    id: 'NL'
+    description: "New Brunswick",
+    id: "NB",
   },
   {
-    description: 'Northwest Territories',
-    id: 'NT'
+    description: "Newfoundland and Labrador",
+    id: "NL",
   },
   {
-    description: 'Nova Scotia',
-    id: 'NS'
+    description: "Northwest Territories",
+    id: "NT",
   },
   {
-    description: 'Nunavut',
-    id: 'NU'
+    description: "Nova Scotia",
+    id: "NS",
   },
   {
-    description: 'Ontario',
-    id: 'ON'
+    description: "Nunavut",
+    id: "NU",
   },
   {
-    description: 'Prince Edward Island',
-    id: 'PE'
+    description: "Ontario",
+    id: "ON",
   },
   {
-    description: 'Quebec',
-    id: 'QC'
+    description: "Prince Edward Island",
+    id: "PE",
   },
   {
-    description: 'Saskatchewan',
-    id: 'SK'
+    description: "Quebec",
+    id: "QC",
   },
   {
-    description: 'Yukon',
-    id: 'YT'
+    description: "Saskatchewan",
+    id: "SK",
+  },
+  {
+    description: "Yukon",
+    id: "YT",
   },
 ];
--- self-storage-wis-dells-munroe-ave/src/environments/environment.ts
+++ self-storage-wis-dells-munroe-ave/src/environments/environment.ts
@@ -1,14 +1,13 @@
-
 export const environment = {
   production: false,
-  type: 0,  // { API: 0, iframe: 1 }
-  errorsMails: [ 'sanghmitra.v@codeparva.in' ],
-  senderEmail: 'errors@codeparva.in',
-  emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
-  appId: 'Self Storage of Wisconsin Dells-Munroe Ave - Testing',
-  VERSION: require('../../package.json').version,
+  type: 0, // { API: 0, iframe: 1 }
+  errorsMails: ["sanghmitra.v@codeparva.in"],
+  senderEmail: "errors@codeparva.in",
+  emailBaseUrl: "https://emailsvc-ext.syrasoft.com/v1/email",
+  appId: "Self Storage of Wisconsin Dells-Munroe Ave - Testing",
+  VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
   // APIKey: `dzJ0OFc5SjgqNk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` // 28986 self storage of wis dells
-  APIKey: `dzJ0OVcySjkqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` // 29295  self storage wis dells munroe ave
+  APIKey: `dzJ0OVcySjkqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29295  self storage wis dells munroe ave
 };
