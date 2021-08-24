4eaa7b9fea7d702af7463b8591bb90ace4bf859e
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-08-25 00:28:22 +0530

    case 26840 - disabled reservations

--- white-water/src/app/data/nav.ts
+++ white-water/src/app/data/nav.ts
@@ -1,44 +1,44 @@
-import { environment } from '../../environments/environment';
+import { environment } from "../../environments/environment";
 
 export const navLinks = [
-    {
-        label: 'HOME',
-        path: '/',
-    },
-    {
-        label: 'VIEW RATES',
-        path: `${environment.locationName}/view-rates`,
-    },
-    // {
-    //     label: 'RENT NOW',
-    //     path: `${environment.locationName}/rent-now`,
-    // },
-    {
-        label: 'RESERVE UNIT',
-        path: `${environment.locationName}/reserve-unit`,
-    },
-    {
-        label: 'PAY RENT',
-        path: '/pay-rent'
-    },
-    {
-        label: 'STORAGE TIPS',
-        path: `${environment.locationName}/storage-tips`
-    },
-    {
-        label: 'UNIT SIZER',
-        path: '/unit-sizer'
-    },
-    {
-        label: 'CONTACT US',
-        path: '/contact-us'
-    },
-    {
-        label: 'FAQ',
-        path: '/frequently-asked-questions'
-    },
-    {
-        label: 'RENTAL PROCEDURE',
-        path: '/rental-procedure'
-    },
+  {
+    label: "HOME",
+    path: "/",
+  },
+  {
+    label: "VIEW RATES",
+    path: `${environment.locationName}/view-rates`,
+  },
+  // {
+  //     label: 'RENT NOW',
+  //     path: `${environment.locationName}/rent-now`,
+  // },
+  // {
+  //     label: 'RESERVE UNIT',
+  //     path: `${environment.locationName}/reserve-unit`,
+  // },
+  {
+    label: "PAY RENT",
+    path: "/pay-rent",
+  },
+  {
+    label: "STORAGE TIPS",
+    path: `${environment.locationName}/storage-tips`,
+  },
+  {
+    label: "UNIT SIZER",
+    path: "/unit-sizer",
+  },
+  {
+    label: "CONTACT US",
+    path: "/contact-us",
+  },
+  {
+    label: "FAQ",
+    path: "/frequently-asked-questions",
+  },
+  {
+    label: "RENTAL PROCEDURE",
+    path: "/rental-procedure",
+  },
 ];
