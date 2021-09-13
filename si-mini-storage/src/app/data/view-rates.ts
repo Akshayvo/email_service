d81a31b674e6c722f827908b006ad573e23aae9b
bala-0 <79130180+bala-0@users.noreply.github.com>
2021-09-13 20:20:57 +0530

    case 27013 -  disabled movein

--- si-mini-storage/src/app/data/view-rates.ts
+++ si-mini-storage/src/app/data/view-rates.ts
@@ -1,324 +1,324 @@
 export const dataViewRates = [
-    {
-        p: `We have been proudly serving our neighbors in Barberton, Akron, Norton and the surrounding
+  {
+    p: `We have been proudly serving our neighbors in Barberton, Akron, Norton and the surrounding
         communities with high quality self storage services for nearly three decades! `,
-    },
-    {
-        p: `SI Mini Storage is well-lit and well-maintained with both convenience and security in mind! You have access
+  },
+  {
+    p: `SI Mini Storage is well-lit and well-maintained with both convenience and security in mind! You have access
         to your belongings 24 hours a day, 7 days a week, and you can pay your bill online at your convenience!`,
-    },
-    {
-        p: `On top of the convenience factor, SI Mini Storage has an on-site manager who performs regular
+  },
+  {
+    p: `On top of the convenience factor, SI Mini Storage has an on-site manager who performs regular
         walk arounds, we maintain a well-lit facility, and are covered by security cameras.
         We want you to feel safe at all times!`,
-    },
+  },
 ];
 
 export const th = [
-    {
-      data: `Size`
-    },
-    {
-      data: `Rate`
-    },
-    {
-      data: `Reserve a Unit`
-    },
-    {
-      data: `Move In`
-    },
-    // {
-    //   data: `Climate Control`
-    // },
-  ];
+  {
+    data: `Size`,
+  },
+  {
+    data: `Rate`,
+  },
+  {
+    data: `Reserve a Unit`,
+  },
+  // {
+  //   data: `Move In`
+  // },
+  // {
+  //   data: `Climate Control`
+  // },
+];
 
-  export const option =  [
-    {
-      description: 'Alabama',
-      id: 'AL'
-    },
-    {
-      description: 'Alaska',
-      id: 'AK'
-    },
-    {
-      description: 'Alberta',
-      id: 'AB'
-    },
-    {
-      description: 'American Samoa',
-      id: 'AS'
-    },
-    {
-      description: 'Arizona',
-      id: 'AZ'
-    },
-    {
-      description: 'Arkansas',
-      id: 'AR'
-    },
-    {
-      description: 'British Columbia',
-      id: 'BC'
-    },
-    {
-      description: 'California',
-      id: 'CA'
-    },
-    {
-      description: 'Colorado',
-      id: 'CO'
-    },
-    {
-      description: 'Connecticut',
-      id: 'CT'
-    },
-    {
-      description: 'Delaware',
-      id: 'DE'
-    },
-    {
-      description: 'District Of Columbia',
-      id: 'DC'
-    },
-    {
-      description: 'Federated States Of Micronesia',
-      id: 'FM'
-    },
-    {
-      description: 'Florida',
-      id: 'FL'
-    },
-    {
-      description: 'Georgia',
-      id: 'GA'
-    },
-    {
-      description: 'Guam',
-      id: 'GU'
-    },
-    {
-      description: 'Hawaii',
-      id: 'HI'
-    },
-    {
-      description: 'Idaho',
-      id: 'ID'
-    },
-    {
-      description: 'Illinois',
-      id: 'IL'
-    },
-    {
-      description: 'Indiana',
-      id: 'IN'
-    },
-    {
-      description: 'Iowa',
-      id: 'IA'
-    },
-    {
-      description: 'Kansas',
-      id: 'KS'
-    },
-    {
-      description: 'Kentucky',
-      id: 'KY'
-    },
-    {
-      description: 'Louisiana',
-      id: 'LA'
-    },
-    {
-      description: 'Maine',
-      id: 'ME'
-    },
-    {
-      description: 'Manitoba',
-      id: 'MB'
-    },
-    {
-      description: 'Marshall Islands',
-      id: 'MH'
-    },
-    {
-      description: 'Maryland',
-      id: 'MD'
-    },
-    {
-      description: 'Massachusetts',
-      id: 'MA'
-    },
-    {
-      description: 'Michigan',
-      id: 'MI'
-    },
-    {
-      description: 'Minnesota',
-      id: 'MN'
-    },
-    {
-      description: 'Mississippi',
-      id: 'MS'
-    },
-    {
-      description: 'Missouri',
-      id: 'MO'
-    },
-    {
-      description: 'Montana',
-      id: 'MT'
-    },
-    {
-      description: 'Nebraska',
-      id: 'NE'
-    },
-    {
-      description: 'Nevada',
-      id: 'NV'
-    },
-    {
-      description: 'Newfoundland and Labrador',
-      id: 'NL'
-    },
-    {
-      description: 'New Brunswick',
-      id: 'NB'
-    },
-    {
-      description: 'New Hampshire',
-      id: 'NH'
-    },
-    {
-      description: 'New Jersey',
-      id: 'NJ'
-    },
-    {
-      description: 'New Mexico',
-      id: 'NM'
-    },
-    {
-      description: 'New York',
-      id: 'NY'
-    },
-    {
-      description: 'Northwest Territories',
-      id: 'NT'
-    },
-    {
-      description: 'North Carolina',
-      id: 'NC'
-    },
-    {
-      description: 'North Dakota',
-      id: 'ND'
-    },
-    {
-      description: 'Northern Mariana Islands',
-      id: 'MP'
-    },
-    {
-      description: 'Nova Scotia',
-      id: 'NS'
-    },
-    {
-      description: 'Nunavut',
-      id: 'NU'
-    },
-    {
-      description: 'Ohio',
-      id: 'OH'
-    },
-    {
-      description: 'Oklahoma',
-      id: 'OK'
-    },
-    {
-      description: 'Ontario',
-      id: 'ON'
-    },
-    {
-      description: 'Oregon',
-      id: 'OR'
-    },
-    {
-      description: 'Palau',
-      id: 'PW'
-    },
-    {
-      description: 'Pennsylvania',
-      id: 'PA'
-    },
-    {
-      description: 'Prince Edward Island',
-      id: 'PE'
-    },
-    {
-      description: 'Puerto Rico',
-      id: 'PR'
-    },
-    {
-      description: 'Quebec',
-      id: 'QC'
-    },
-    {
-      description: 'Rhode Island',
-      id: 'RI'
-    },
-    {
-      description: 'Saskatchewan',
-      id: 'SK'
-    },
-    {
-      description: 'South Carolina',
-      id: 'SC'
-    },
-    {
-      description: 'South Dakota',
-      id: 'SD'
-    },
-    {
-      description: 'Tennessee',
-      id: 'TN'
-    },
-    {
-      description: 'Texas',
-      id: 'TX'
-    },
-    {
-      description: 'Utah',
-      id: 'UT'
-    },
-    {
-      description: 'Vermont',
-      id: 'VT'
-    },
-    {
-      description: 'Virgin Islands',
-      id: 'VI'
-    },
-    {
-      description: 'Virginia',
-      id: 'VA'
-    },
-    {
-      description: 'Washington',
-      id: 'WA'
-    },
-    {
-      description: 'West Virginia',
-      id: 'WV'
-    },
-    {
-      description: 'Wisconsin',
-      id: 'WI'
-    },
-    {
-      description: 'Wyoming',
-      id: 'WY'
-    },
-    {
-      description: 'Yukon',
-      id: 'YT'
-    },
+export const option = [
+  {
+    description: "Alabama",
+    id: "AL",
+  },
+  {
+    description: "Alaska",
+    id: "AK",
+  },
+  {
+    description: "Alberta",
+    id: "AB",
+  },
+  {
+    description: "American Samoa",
+    id: "AS",
+  },
+  {
+    description: "Arizona",
+    id: "AZ",
+  },
+  {
+    description: "Arkansas",
+    id: "AR",
+  },
+  {
+    description: "British Columbia",
+    id: "BC",
+  },
+  {
+    description: "California",
+    id: "CA",
+  },
+  {
+    description: "Colorado",
+    id: "CO",
+  },
+  {
+    description: "Connecticut",
+    id: "CT",
+  },
+  {
+    description: "Delaware",
+    id: "DE",
+  },
+  {
+    description: "District Of Columbia",
+    id: "DC",
+  },
+  {
+    description: "Federated States Of Micronesia",
+    id: "FM",
+  },
+  {
+    description: "Florida",
+    id: "FL",
+  },
+  {
+    description: "Georgia",
+    id: "GA",
+  },
+  {
+    description: "Guam",
+    id: "GU",
+  },
+  {
+    description: "Hawaii",
+    id: "HI",
+  },
+  {
+    description: "Idaho",
+    id: "ID",
+  },
+  {
+    description: "Illinois",
+    id: "IL",
+  },
+  {
+    description: "Indiana",
+    id: "IN",
+  },
+  {
+    description: "Iowa",
+    id: "IA",
+  },
+  {
+    description: "Kansas",
+    id: "KS",
+  },
+  {
+    description: "Kentucky",
+    id: "KY",
+  },
+  {
+    description: "Louisiana",
+    id: "LA",
+  },
+  {
+    description: "Maine",
+    id: "ME",
+  },
+  {
+    description: "Manitoba",
+    id: "MB",
+  },
+  {
+    description: "Marshall Islands",
+    id: "MH",
+  },
+  {
+    description: "Maryland",
+    id: "MD",
+  },
+  {
+    description: "Massachusetts",
+    id: "MA",
+  },
+  {
+    description: "Michigan",
+    id: "MI",
+  },
+  {
+    description: "Minnesota",
+    id: "MN",
+  },
+  {
+    description: "Mississippi",
+    id: "MS",
+  },
+  {
+    description: "Missouri",
+    id: "MO",
+  },
+  {
+    description: "Montana",
+    id: "MT",
+  },
+  {
+    description: "Nebraska",
+    id: "NE",
+  },
+  {
+    description: "Nevada",
+    id: "NV",
+  },
+  {
+    description: "Newfoundland and Labrador",
+    id: "NL",
+  },
+  {
+    description: "New Brunswick",
+    id: "NB",
+  },
+  {
+    description: "New Hampshire",
+    id: "NH",
+  },
+  {
+    description: "New Jersey",
+    id: "NJ",
+  },
+  {
+    description: "New Mexico",
+    id: "NM",
+  },
+  {
+    description: "New York",
+    id: "NY",
+  },
+  {
+    description: "Northwest Territories",
+    id: "NT",
+  },
+  {
+    description: "North Carolina",
+    id: "NC",
+  },
+  {
+    description: "North Dakota",
+    id: "ND",
+  },
+  {
+    description: "Northern Mariana Islands",
+    id: "MP",
+  },
+  {
+    description: "Nova Scotia",
+    id: "NS",
+  },
+  {
+    description: "Nunavut",
+    id: "NU",
+  },
+  {
+    description: "Ohio",
+    id: "OH",
+  },
+  {
+    description: "Oklahoma",
+    id: "OK",
+  },
+  {
+    description: "Ontario",
+    id: "ON",
+  },
+  {
+    description: "Oregon",
+    id: "OR",
+  },
+  {
+    description: "Palau",
+    id: "PW",
+  },
+  {
+    description: "Pennsylvania",
+    id: "PA",
+  },
+  {
+    description: "Prince Edward Island",
+    id: "PE",
+  },
+  {
+    description: "Puerto Rico",
+    id: "PR",
+  },
+  {
+    description: "Quebec",
+    id: "QC",
+  },
+  {
+    description: "Rhode Island",
+    id: "RI",
+  },
+  {
+    description: "Saskatchewan",
+    id: "SK",
+  },
+  {
+    description: "South Carolina",
+    id: "SC",
+  },
+  {
+    description: "South Dakota",
+    id: "SD",
+  },
+  {
+    description: "Tennessee",
+    id: "TN",
+  },
+  {
+    description: "Texas",
+    id: "TX",
+  },
+  {
+    description: "Utah",
+    id: "UT",
+  },
+  {
+    description: "Vermont",
+    id: "VT",
+  },
+  {
+    description: "Virgin Islands",
+    id: "VI",
+  },
+  {
+    description: "Virginia",
+    id: "VA",
+  },
+  {
+    description: "Washington",
+    id: "WA",
+  },
+  {
+    description: "West Virginia",
+    id: "WV",
+  },
+  {
+    description: "Wisconsin",
+    id: "WI",
+  },
+  {
+    description: "Wyoming",
+    id: "WY",
+  },
+  {
+    description: "Yukon",
+    id: "YT",
+  },
 ];
