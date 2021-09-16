56e6181be8e1ac7e5bab4ea3e97c536f04afbee7
DarshanPH <darshanhegde5@gmail.com>
2021-09-16 13:31:32 +0530

    case 26930

--- deforest/src/environments/environment.ts
+++ deforest/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   type: 0, // { API: 0, iframe: 1 }
   locationName: "deforest/wisconsin", // state/city  **Remeber to update locationName in sitemap.xml as well
-  signUpForAuotoPay: 0, // { Yes: 1, No: 0 }
+  signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: false, reserve: false }, // { moveIn: true, reserve: true },
   template: "darkTheme", // { darkTheme, lightTheme, withoutJumbotron}
   cards: ["Visa", "Master Card", "Amex", "Discover", "test"],
@@ -18,8 +18,8 @@ export const environment = {
   VERSION: require("../../package.json").version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: "https://www.deforestselfstorage.com",
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo 28463
-  APIKey: `dTJXN0Q3UDhIMU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // deforest 27781
+  APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo 28464
+  // APIKey: `dTJXN0Q3UDhIMU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // deforest 27781
 };
 
 /*
