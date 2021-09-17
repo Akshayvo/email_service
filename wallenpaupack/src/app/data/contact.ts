9b2f6978426234254f8e7e89bcfe512edd987201
pragathi <pragathi.n@codeparva.in>
2021-09-17 11:23:05 +0530

    removed extra character in email

--- wallenpaupack/src/app/data/contact.ts
+++ wallenpaupack/src/app/data/contact.ts
@@ -12,25 +12,18 @@ export const contactsHomePage = [
         email: 'andy@wallenpaupackselfstorage.com',
         data: `andy@wallenpaupackselfstorage.com`,
         class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`andy@wallenpaupackselfstorage.com`'
+        redirect: 'mailto:andy@wallenpaupackselfstorage.com'
     },
     {
         id: 3,
         label: 'Phone: ',
-<<<<<<< HEAD
         data: '(251) 970-3168',
         class: 'fas fa-phone mr-2 iconColor',
         redirect: 'tel:2519703168'
-=======
-        data: '',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
 export const hoursHomePage = [
-<<<<<<< HEAD
     // {
     //     label: 'Monday - Friday: ',
     //     data: '8:30AM to 5:00PM',
@@ -48,15 +41,6 @@ export const hoursHomePage = [
 export const acessHoursHomepage = [
     {
         data: '9:00AM to 5:00PM 7 Days a Week'
-=======
-    {
-        label: 'Monday - Friday: ',
-        data: '8:30AM to 5:00PM',
-    },
-    {
-        label: 'Saturday - Sunday: ',
-        data: 'Self Check-In Available',
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
@@ -64,17 +48,12 @@ export const contactsLocation1 = [
     {
         id: 1,
         label: 'Address: ',
-<<<<<<< HEAD
         data: '116 Ehrgood Rd, Greentown, PA 18426',
-=======
-        data: '17860 US-98, Foley, AL 36535',
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
         class: 'fas fa-map-marker-alt mr-2 iconColor',
         redirect: 'https://g.page/r/CeBIy_cEP-szEBA'
     },
     {
         id: 2,
-<<<<<<< HEAD
         label: 'Office Address: ',
         data: '204 Purdytown Turnpike, Lakeville, PA 18438',
         class: 'fas fa-map-marker-alt mr-2 iconColor',
@@ -86,7 +65,7 @@ export const contactsLocation1 = [
         email: 'andy@wallenpaupackselfstorage.com',
         data: `andy@wallenpaupackselfstorage.com`,
         class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`andy@wallenpaupackselfstorage.com`'
+        redirect: 'mailto:andy@wallenpaupackselfstorage.com'
     },
     {
         id: 4,
@@ -94,26 +73,11 @@ export const contactsLocation1 = [
         data: '(570) 676-0851',
         class: 'fas fa-phone mr-2 iconColor',
         redirect: 'tel:5706760851'
-=======
-        label: 'Email: ',
-        email: 'accounts@storageplusbaldwin.com',
-        data: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
 export const hoursLocation1 = [
     {
-<<<<<<< HEAD
         label: 'Monday - Sunday: ',
         data: '9:00AM to 5:00PM'
     }
@@ -138,20 +102,6 @@ export const hoursLocation3AcessHours = [
 export const hoursLocation4AcessHours = [
     {
         data: '6:00AM to 8:00PM 7 Days a Week'
-=======
-        label: 'Monday - Friday: ',
-        data: '8:30AM to 5:00PM',
-    },
-    {
-        label: 'Saturday - Sunday: ',
-        data: 'CLOSED',
-    },
-];
-
-export const hoursLocation1AcessHours = [
-    {
-        data: '24 Hours a Day 7 Days a Week'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
@@ -173,17 +123,12 @@ export const contactsLocation2 = [
     {
         id: 1,
         label: 'Address: ',
-<<<<<<< HEAD
         data: '80 Miller Rd, Lake Ariel, PA 18436',
-=======
-        data: '15410 AL-104, Silverhill, AL 36576',
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
         class: 'fas fa-map-marker-alt mr-2 iconColor',
         redirect: 'https://g.page/r/CT1A0QzwMwCHEBA'
     },
     {
         id: 2,
-<<<<<<< HEAD
         label: 'Office Address: ',
         data: '204 Purdytown Turnpike, Lakeville, PA 18438',
         class: 'fas fa-map-marker-alt mr-2 iconColor',
@@ -195,7 +140,7 @@ export const contactsLocation2 = [
         data: 'andy@wallenpaupackselfstorage.com',
         email: `andy@wallenpaupackselfstorage.com`,
         class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`andy@wallenpaupackselfstorage.com`'
+        redirect: 'mailto:andy@wallenpaupackselfstorage.com'
     },
     {
         id: 4,
@@ -203,20 +148,6 @@ export const contactsLocation2 = [
         data: '(570) 676-0851',
         class: 'fas fa-phone mr-2 iconColor',
         redirect: 'tel:5706760851'
-=======
-        label: 'Email: ',
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
@@ -230,31 +161,21 @@ export const hoursLocation2 = [
     //     data: 'CLOSED',
     // },
     {
-<<<<<<< HEAD
         label: 'Monday - Sunday: ',
         data: '6:00AM to 8:00PM'
     }
-=======
-        data: '24-Hours a Day 7 Days a Week',
-    },
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
 ];
 
 export const contactsLocation3 = [
     {
         id: 1,
         label: 'Address: ',
-<<<<<<< HEAD
         data: '54 Texas Palmyra Hwy, Hawley, PA 18428',
-=======
-        data: '8089A US-98, Fairhope, AL 36532',
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
         class: 'fas fa-map-marker-alt mr-2 iconColor',
         redirect: 'https://g.page/r/CeskY5sIbmGZEAE'
     },
     {
         id: 2,
-<<<<<<< HEAD
         label: 'Office Address: ',
         data: '204 Purdytown Turnpike, Lakeville, PA 18438',
         class: 'fas fa-map-marker-alt mr-2 iconColor',
@@ -266,7 +187,7 @@ export const contactsLocation3 = [
         data: 'andy@wallenpaupackselfstorage.com',
         email: `andy@wallenpaupackselfstorage.com`,
         class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`andy@wallenpaupackselfstorage.com`'
+        redirect: 'mailto:andy@wallenpaupackselfstorage.com'
     },
     {
         id: 4,
@@ -274,20 +195,6 @@ export const contactsLocation3 = [
         data: '(570) 676-0851',
         class: 'fas fa-phone mr-2 iconColor',
         redirect: 'tel:5706760851'
-=======
-        label: 'Email: ',
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
@@ -301,55 +208,33 @@ export const hoursLocation3 = [
     //     data: 'CLOSED',
     // },
     {
-<<<<<<< HEAD
         label: 'Monday - Sunday: ',
         data: '6:00AM to 8:00PM'
     }
-=======
-        data: '24-Hours a Day 7 Days a Week',
-    },
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
 ];
 
 export const contactsLocation4 = [
     {
         id: 1,
         label: 'Address: ',
-<<<<<<< HEAD
         data: '204 Purdytown Turnpike, Lakeville, PA 18438',
-=======
-        data: '11455 Co Rd 64, Daphne, AL 36526',
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
         class: 'fas fa-map-marker-alt mr-2 iconColor',
         redirect: 'https://g.page/r/CYgxbl0IyGw9EBA'
     },
     {
         id: 2,
         label: 'Email: ',
-<<<<<<< HEAD
         data: 'andy@wallenpaupackselfstorage.com',
         email: `andy@wallenpaupackselfstorage.com`,
         class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`andy@wallenpaupackselfstorage.com`'
-=======
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
+        redirect: 'mailto:andy@wallenpaupackselfstorage.com'
     },
     {
         id: 3,
         label: 'Phone: ',
-<<<<<<< HEAD
         data: '(570) 676-0851',
         class: 'fas fa-phone mr-2 iconColor',
         redirect: 'tel:5706760851'
-=======
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
     },
 ];
 
@@ -363,7 +248,6 @@ export const hoursLocation4 = [
     //     data: 'CLOSED',
     // },
     {
-<<<<<<< HEAD
         label: 'Monday - Sunday: ',
         data: '6:00AM to 8:00PM'
     }
@@ -371,86 +255,3 @@ export const hoursLocation4 = [
 
 
 
-=======
-        data: '24-Hours a Day 7 Days a Week',
-    },
-];
-
-export const contactsLocation5 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '21193 AL-181, Fairhope, AL 36532',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/FANSU6w7QY9vCpiDA'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
-    },
-];
-
-export const hoursLocation5 = [
-    // {
-    //     label: 'Monday - Saturday: ',
-    //     data: '9:00AM - 5:00PM',
-    // },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    {
-        data: '24-Hours a Day 7 Days a Week',
-    },
-];
-
-export const contactsLocation6 = [
-    {
-        id: 1,
-        label: 'Address: ',
-        data: '20218 US-90, Robertsdale, AL 36567',
-        class: 'fas fa-map-marker-alt mr-2 iconColor',
-        redirect: 'https://goo.gl/maps/kt1bhA6aHeRmjXsN7'
-    },
-    {
-        id: 2,
-        label: 'Email: ',
-        data: 'accounts@storageplusbaldwin.com',
-        email: `accounts@storageplusbaldwin.com`,
-        class: 'fas fa-envelope-open mr-2 iconColor',
-        redirect: 'mailto:`accounts@storageplusbaldwin.com`'
-    },
-    {
-        id: 3,
-        label: 'Phone: ',
-        data: '(251) 970-3168',
-        class: 'fas fa-phone mr-2 iconColor',
-        redirect: 'tel:2519703168'
-    },
-];
-
-export const hoursLocation6 = [
-    // {
-    //     label: 'Monday - Saturday: ',
-    //     data: '9:00AM - 5:00PM',
-    // },
-    // {
-    //     label: 'Sunday: ',
-    //     data: 'CLOSED',
-    // },
-    {
-        data: '24-Hours a Day 7 Days a Week',
-    },
-];
->>>>>>> 2fb4710631fd66ed61fa300b3f9b4f3d08827039
