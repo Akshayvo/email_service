5c94d5a047718cd4153a2b2a019e01b66e29691a
Akshayone8 <62467248+Akshayone8@users.noreply.github.com>
2021-09-17 16:05:32 +0530

    updated as per reuirment

--- quitman-mini/src/environments/environment.ts
+++ quitman-mini/src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
   facilityName: 'Quitman Mini Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: `https://boxerstorage.com`  
+  websiteUrl: `https://quitman-mini-staging.netlify.app`  
 };
 
 /*
