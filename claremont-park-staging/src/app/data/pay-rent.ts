f3ffbd954f9008cc05e09c08dea09d443dbfab6a
DarshanPH <darshanhegde5@gmail.com>
2021-10-13 19:31:56 +0530

    case 27240 setup folder renamed

---
+++ claremont-park-staging/src/app/data/pay-rent.ts
@@ -0,0 +1,11 @@
+export const loginDetail = [
+    {
+        userName: `First Initial + Tenant's Last Name`,
+        password: `Tenant's Full Phone Numbers`,
+        example: {
+            code: `First Unit is 0123`,
+            userName: 'jsmith',
+            password: '0123'
+        }
+    }
+];
---
+++ claremont-park-staging/src/app/data/title.ts
@@ -0,0 +1,26 @@
+export const homePageTitle = `Affordable Self Storage Units in Baldwinsville, New York, 13027`;
+export const homePageContent = `Our regular and climate controlled self storage units are clean, well-lit, accessible 24 hours a day, and affordable! Reserve today!`;
+
+export const viewRatesPageTitle = `Storage Units in Baldwinsville | Claremont Park Self Storage - North`;
+export const viewRatesPageContent = `Claremont Park Self Storage - North serves Baldwinsville and the surrounding communities with a variety of well-maintained and affordable self storage units!`;
+
+export const reservePageTitle = `Reserve a Unit | Claremont Park Self Storage - North`;
+export const reservePageContent = `Simply fill out our easy reservation form and one of our friendly self storage associates will be ready and waiting for your arrival!`;
+
+export const payRentPageTitle = `Pay Rent  | Claremont Park Self Storage - North`;
+export const payRentPageContent = `Simply follow the on-page instructions, or call one of our friendly management professionals to learn how to access your account, today!`;
+
+export const storageTipsTitle = `Storage Tips  | Claremont Park Self Storage - North`;
+export const storageTipsContent = `Use our helpful self storage tips list to make your stay at Claremont Park Self Storage - North an easy and enjoyable one!`;
+
+export const unitSizerPageTitle = `Unit Sizer  | Claremont Park Self Storage - North`;
+export const unitSizerPageContent = `Are you not sure what size unit you require? Use this guide to help you figure out what fits inside the units offered at Claremont Park Self Storage - North!`;
+
+export const contactPageTitle = `Contact Us  | Claremont Park Self Storage - North`;
+export const contactPageContent = `Use the contact information or the contact form on this page to speak to one of our friendly self storage managers!`;
+
+export const thankYouPageTitle = `Thank You For Reserving Your Unit at Claremont Park Self Storage!`;
+export const thankYouPageContent = `We've received your reservation! One of our friendly staff will be in touch!`;
+
+export const privacyPolicyPageTitle = `Privacy Policy | Claremont Park Self Storage - North`;
+export const privacyPolicyPageContent = `This page contains this website's privacy policy. For more information, please contact the business directly.`;
