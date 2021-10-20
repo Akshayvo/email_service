97b3d5c70d3107090a75b82f2dd79df1380ff30d
pragathi <pragathi.n@codeparva.in>
2021-10-20 14:05:48 +0530

    case 27268

--- multi-location-site/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
+++ multi-location-site/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
@@ -595,7 +595,7 @@ public navigateToPrevious() {
           }
         } else {
           this.makePaymentForUnit = false;
-
+          this.showloaderForPayment = false;
           this.invalidPayment = 'Unable to make the payment. Please check your card detail.';
         }
 
