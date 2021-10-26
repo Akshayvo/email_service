65099ec355c958c94e0fbefa9e922f514414c1b0
pragathi <pragathi.n@codeparva.in>
2021-10-26 12:34:35 +0530

    case 27311

--- dakota-self-storage/src/app/data/home.ts
+++ dakota-self-storage/src/app/data/home.ts
@@ -34,13 +34,13 @@ export const aboutUsHeading = `Storage Units and Vehicle Parking in Rapid City`;
 
 export const aboutUs = [
     {
-        p: `Dakota Self Storage is Rapid City’s newest self storage facility. Our new facility is conveniently located on Valley Drive just north of SD-44, and was built from the ground up with convenience and security in mind. We offer a variety of self storage unit sizes at affordable prices that are ideal for any residential or commercial storage need. `,
+        p: `Dakota Self Storage is Rapid City’s newest self storage facility. Our new facility is conveniently located on Valley Drive just north of Menards, and was built from the ground up with convenience and security in mind. We offer a variety of self storage unit sizes at affordable prices that are ideal for any residential or commercial storage need.`,
     },
     {
         p: `Whether you’re preparing for a move, or just looking to clear some space around the office or work-site, Dakota Self Storage has a unit for you! We also have outdoor parking spaces for boats, recreational vehicles, and trailers! `,
     },
     {
-        p: `We go the extra mile to make sure your stay with us is easy and enjoyable. Our storage facility is well-lit, fully-fenced, and uses a pin-code activated gate along with strategically placed security cameras to ensure a sense of safety at all times. You can check out the live unit availability and pricingon our website, then rent your unit in minutes when you’ve found the right unit for your renovation, or seasonal decorations storage. An electronic lease will be emailed to you, and you’ll receive your unique access code upon signing the document`,
+        p: `We go the extra mile to make sure your stay with us is easy and enjoyable. Our storage facility is well-lit, fully-fenced, and uses a pin-code activated gate along with strategically placed security cameras to ensure a sense of safety at all times. You can check out the live unit availability and pricing on our website, then rent your unit in minutes when you’ve found the right unit for your renovation, or seasonal decorations storage. An electronic lease will be emailed to you, and you’ll receive your unique access code upon signing the document`,
     },
     {
         p: `Still have questions about the services offered at Dakota Self Storage? Give us a call today, or fill out our online contact form, and one of our friendly staff will be happy to help!`,
