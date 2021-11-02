8dd73eb231a26c9b68cd4de9238f465e900dc280
DarshanPH <darshanhegde5@gmail.com>
2021-11-02 11:54:15 +0530

    case 27352

--- safe-self-storage/src/app/data/pay-rent.ts
+++ safe-self-storage/src/app/data/pay-rent.ts
@@ -2,10 +2,11 @@ export const loginDetail = [
     {
         userName: `Tenant's First Initial + Last Name`,
         password: `Tenant's First  Unit`,
+        note: `<b>Note: </b> Please put a <b>Capital C in front of your unit number</b> if it is an outside unit, and please place a <b>Capital H in front of your unit number</b> if it is a heated unit.`,
         example: {
             code: `John Smith's Unit Number: 123`,
             userName: 'jsmith',
-            password: '123'
+            password: 'C123'
         }
     }
 ];
