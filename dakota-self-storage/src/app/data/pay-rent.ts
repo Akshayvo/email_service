66f377db3d4fca135854017471c8987f7b6ec573
DarshanPH <darshanhegde5@gmail.com>
2021-11-04 11:31:10 +0530

    case 27377

--- dakota-self-storage/src/app/data/pay-rent.ts
+++ dakota-self-storage/src/app/data/pay-rent.ts
@@ -3,7 +3,7 @@ export const loginDetail = [
         userName: `Tenant's First Initial + Last Name`,
         password: `Tenant's Unit Number + Gate Code`,
         example: {
-            code: "John Smith's first unit was 045 and his gate code is: 0107",
+            code: "first unit was 045 and his gate code is: 0107",
             userName: 'jsmith',
             password: '0460107'
         }
