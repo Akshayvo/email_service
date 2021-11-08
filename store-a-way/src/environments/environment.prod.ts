f636ab32992339761f8798e44d8a07bfc1e9345f
DarshanPH <darshanhegde5@gmail.com>
2021-11-08 12:04:51 +0530

    case 27383

--- store-a-way/src/environments/environment.prod.ts
+++ store-a-way/src/environments/environment.prod.ts
@@ -16,6 +16,6 @@ export const environment = {
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://storeawayllc.com',
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
-  APIKey: `dzJ0OVc0SjYqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
+  APIKey: `dzJ0OVc1SjUqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29552
 
 };
