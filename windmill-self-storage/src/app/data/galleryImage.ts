5a6a7dce9da2cdbf6c66d16b8f77d8e6d80f6d77
DarshanPH <darshanhegde5@gmail.com>
2021-11-16 19:00:47 +0530

    case 27357

--- windmill-self-storage/src/app/data/galleryImage.ts
+++ windmill-self-storage/src/app/data/galleryImage.ts
@@ -4,48 +4,111 @@ export const galleryHome = [
     url: `windmill-self-storage-1`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-1-thumb`,
-    alt: ``
+    alt: `entire view of self storage units`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `windmill-self-storage-2`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-2-thumb`,
-    alt: ``
+    alt: `door of self storage unit`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `windmill-self-storage-3`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-3-thumb`,
-    alt: ``
+    alt: `door of self storage unit`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `windmill-self-storage-4`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-4-thumb`,
-    alt: ``
+    alt: `two side row of self storage units`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `windmill-self-storage-5`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-5-thumb`,
-    alt: ``
+    alt: `two side row of self storage units`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `windmill-self-storage-7`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-7-thumb`,
-    alt: ``
+    alt: `two side row of self storage units`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `windmill-self-storage-8`,
     // tslint:disable-next-line:max-line-length
     thumb: `windmill-self-storage-8-thumb`,
-    alt: ``
+    alt: `two side row of self storage units`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-9`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-9`,
+    alt: `outside view of a storage unit`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-10`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-10`,
+    alt: `entire one row of storage units`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-11`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-11`,
+    alt: `view of self storage units`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-12`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-12`,
+    alt: `a row of self storage units`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-13`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-13`,
+    alt: `one storage unit`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-14`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-14`,
+    alt: `a row of self storage units`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-15`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-15`,
+    alt: `insider view of entire row`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-16`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-16`,
+    alt: `climate control self storage units`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `windmill-self-storage-17`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `windmill-self-storage-17`,
+    alt: `truck for transfering various stuffs`
   },
 ];
