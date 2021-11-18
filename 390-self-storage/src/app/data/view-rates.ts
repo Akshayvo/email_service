c47a1aaa08142d10abf8db76949670b080d1adae
pragathi <pragathi.n@codeparva.in>
2021-11-18 13:19:40 +0530

    redesign

--- 390-self-storage/src/app/data/view-rates.ts
+++ 390-self-storage/src/app/data/view-rates.ts
@@ -6,7 +6,7 @@ export const dataViewRates = [
     {
         p: `Our friendly management staff are always ready to help answer your questions, fill out a lease, and get you settled 
             in with our regular storage units or the climate-controlled variety. You can call or email anytime with questions 
-            about our services or your account. We can't wait to meet you!`,
+            about our services  or your account. We can't wait to meet you!`,
     },
     
 ];
