55d6aa0cb930d8eba1bd450e2861b87b14e38f38
Akshayone8 <62467248+Akshayone8@users.noreply.github.com>
2021-11-23 20:47:59 +0530

    case 27464

---
+++ fleming-storage/src/app/data/title.ts
@@ -0,0 +1,26 @@
+export const homePageTitle = `Affordable Self Storage Units in Cavan-Monaghan, ON`;
+export const homePageContent = `Are you in search of affordable self storage units near Cavan-Monaghan? Fleming Storage Solutions has a wide variety of convenient sizes and excellent customer service!`;
+
+export const viewRatesPageTitle = `View Rates  | Fleming Storage Solutions`;
+export const viewRatesPageContent = `Review our current rates and availability, then reserve or rent your unit in minutes with our easy online reservation form!`;
+
+export const reservePageTitle = ``;
+export const reservePageContent = ``;
+
+export const payRentPageTitle = `Pay Rent  | Fleming Storage Solutions`;
+export const payRentPageContent = `Take a moment to review our easy login instructions, or call our friendly management team and start paying your rent online 24 hours a day!`;
+
+export const storageTipsTitle = `Storage Tips  | Fleming Storage Solutions`;
+export const storageTipsContent = `Our quick storage tips page will help you figure out how to best use our services and avoid common pitfalls in self storage!`;
+
+export const unitSizerPageTitle = `Unit Sizer  | Fleming Storage Solutions`;
+export const unitSizerPageContent = `Not sure what sized unit you need? Use our handy chart to determine what unit will fit your belongings!`;
+
+export const contactPageTitle = `Contact Us  | Fleming Storage Solutions`;
+export const contactPageContent = `If you have questions about your account or the services we offer, take a moment to fill our out form, or use the contact information to speak with us!`;
+
+export const thankYouPageTitle = `Thank You For Reserving Your Unit at Fleming Storage Solutions`;
+export const thankYouPageContent = `We've received your reservation! One of our friendly staff will be in touch!`;
+
+export const privacyPolicyPageTitle = ``;
+export const privacyPolicyPageContent = ``;
