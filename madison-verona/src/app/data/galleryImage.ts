256f6d5abd7ac8be04e3edb5113be33f18ccee2d
Darshan-ph <darshan.h@codeparva.in>
2021-12-07 18:27:06 +0530

    case 27552

--- madison-verona/src/app/data/galleryImage.ts
+++ madison-verona/src/app/data/galleryImage.ts
@@ -1,122 +1,66 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: `self-storage-office`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `self-storage-office`,
-    alt: `a self storage facility front office building`,
-    caption: '',
-  },
- 
-  {
-    // tslint:disable-next-line: max-line-length
-    url: `open-storage-unit`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `open-storage-unit`,
-    alt: `an open self storage unit revealing brand new, clean interior`,
-    caption: '',
-  },
-
-  {
-    // tslint:disable-next-line: max-line-length
-    url: `electrical-outlet-equipped-self-storage-unit`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `electrical-outlet-equipped-self-storage-unit`,
-    alt: `electrical outlet on the inside of a brand new clean self storage unit`,
-    caption: '',
-  },
- 
-  {
-    url: `self-storage-office-and-gate`,
-    thumb: `self-storage-office-and-gate`,
-    alt: `a self storage facility with large front office, electronic security gate, and units in the background`,
-    caption: '',
-  },
-  {
-    url: `self-storage-facility-buildings`,
-    thumb: `self-storage-facility-buildings`,
-    alt: `rows self storage unit buildings behind a row of small pine trees`,
-    caption: '',
-  },
-  {
-    url: `self-storage-units-rows`,
-    thumb: `self-storage-units-rows`,
-    alt: `two rows of self storage units on either side of a paved walkway`,
-    caption: '',
-  },
-  {
-    url: `madison-verona-self-storage-parked-car`,
-    thumb: `madison-verona-self-storage-parked-car`,
-    alt: `a vehicle parked inside of a self storage unit with a roll-up door`,
-    caption: '',
-  },
-  {
-    url: `madison-verona-self-storage-23`,
-    thumb: `madison-verona-self-storage-23`,
-    alt: `a row of self storage unit doors under a grey sky`,
-    caption: '',
-  },
-
-  {
-    url: `madison-verona-self-storage-26`,
-    thumb: `madison-verona-self-storage-26`,
-    alt: `an electronic, pin-code activated security gate in the foreground and self storage units in the background`,
-    caption: '',
-  },
-  {
     url: `madison-verona-self-storage-kiosk`,
-    thumb: `madison-verona-self-storage-kiosk-thumb`,
-    alt: `automated kiosk system built into a self storage facility's foyer wall`,
-    caption: 'Rental station open 24-hours a day',
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-kiosk`,
+    alt: `Move In machine`,
+    caption: '',
   },
-  
   {
+    // tslint:disable-next-line: max-line-length
+    url: `madison-verona-self-storage-100`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-100`,
+    alt: `Entire view from the top`,
+    caption: '',
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `madison-verona-self-storage-104`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-104`,
+    alt: `Entire row view from the top`,
+    caption: '',
+  },
+  {
+    // tslint:disable-next-line: max-line-length
     url: `madison-verona-self-storage-37`,
-    thumb: `madison-verona-self-storage-37-thumb`,
-    alt: `an electronic pin-pad that controls a self storage access gate`,
-    caption: 'Enter and exit pin-pads',
-  },
-  {
-    url: `madison-verona-self-storage-35`,
-    thumb: `madison-verona-self-storage-35-thumb`,
-    alt: `rows of single story metal self storage buildings with roll up doors`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-37`,
+    alt: `Checking`,
     caption: '',
   },
   {
-    url: `madison-verona-self-storage-34`,
-    thumb: `madison-verona-self-storage-34-thumb`,
-    alt: `row of self storage units and a door leading to interior climate controlled storage units`,
+    // tslint:disable-next-line: max-line-length
+    url: `madison-verona-self-storage-105`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-105`,
+    alt: `View of self storage unit`,
     caption: '',
   },
   {
-    url: `madison-verona-self-storage-33`,
-    thumb: `madison-verona-self-storage-33-thumb`,
-    alt: `corner of a single story self storage building`,
+    // tslint:disable-next-line: max-line-length
+    url: `madison-verona-self-storage-107`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-107`,
+    alt: `Beautiful view of madison self storage`,
     caption: '',
   },
   {
-    url: `madison-verona-self-storage-30`,
-    thumb: `madison-verona-self-storage-30-thumb`,
-    alt: `rows of self storage units`,
+    // tslint:disable-next-line: max-line-length
+    url: `madison-verona-self-storage-109`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-109`,
+    alt: `a single unit of self storage`,
     caption: '',
   },
   {
-    url: `madison-verona-self-storage-31`,
-    thumb: `madison-verona-self-storage-31-thumb`,
-    alt: `top view of self storage building`,
+    // tslint:disable-next-line: max-line-length
+    url: `madison-verona-self-storage-parked-car`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `madison-verona-self-storage-parked-car`,
+    alt: `Parked car inside self storage unit`,
     caption: '',
   },
-  {
-    url: `madison-verona-self-storage-32`,
-    thumb: `madison-verona-self-storage-32-thumb`,
-    alt: `vehicle filling the goods`,
-    caption: '',
-  },
-  {
-    url: `madison-verona-self-storage-38`,
-    thumb: `madison-verona-self-storage-38-thumb`,
-    alt: `side top entrance view of self storage building`,
-    caption: '',
-  },
-
 ];
