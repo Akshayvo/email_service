76f5127932d203d58a7aafd38ec55bbb45fbca6a
pragathi <pragathi.n@codeparva.in>
2021-12-10 11:04:51 +0530

    pay rent page update

--- premont-self-storage/src/app/data/pay-rent.ts
+++ premont-self-storage/src/app/data/pay-rent.ts
@@ -3,7 +3,7 @@ export const loginDetail = [
         userName: `Tenant's Email Address`,
         password: `Tenant's Full Phone Number`,
         example: {
-            code: "John Smith's Full Phone Number is 555-555-5555",
+            code: "Full Phone Number is 555-555-5555",
             userName: 'jsmith@email.com',
             password: '5555555555'
         }
