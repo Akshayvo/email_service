bbbc82a3e454eaa1d6a6bebc4ea7a006bed08baa
Darshan-ph <darshan.h@codeparva.in>
2021-12-10 12:47:47 +0530

    case 27507 404 fixed

--- handy-dandy-storage/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
+++ handy-dandy-storage/src/app/api-bundle/pay-rent-form/pay-rent-form.component.ts
@@ -662,11 +662,11 @@ public navigateToPrevious() {
       this.reservationInProgress = false;
       this.showloaderForPayment = false;
 
-      if (!!localStorage.getItem('paymentTab')) {
-        this.router.navigate([`location/${this.facilityLocation}/reserve-unit/${localStorage.getItem('paymentTab')}/thank-you`]);
-      } else {
+      // if (!!localStorage.getItem('paymentTab')) {
+      //   this.router.navigate([`location/${this.facilityLocation}/reserve-unit/${localStorage.getItem('paymentTab')}/thank-you`]);
+      // } else {
         this.router.navigate([`${environment.locationName}/${this.facilityLocation}/reserve-unit/thank-you`]);
-      }
+      // }
     }
     }, (err: any) => {
       if (err.status === 403) {
@@ -696,11 +696,13 @@ public navigateToPrevious() {
         this.dataSharingService.strAccessCode = strConfirmationResponse.strAccessCode;
         if (strConfirmationResponse.intErrorCode === 1  ) {
           this.makePaymentForUnit = false;
-          if (!!localStorage.getItem('paymentTab')) {
-            this.router.navigate([`${environment.locationName}/${this.facilityLocation}/move-in/${localStorage.getItem('paymentTab')}/thank-you`]);
-          } else {
+
+          // if (!!localStorage.getItem('paymentTab')) {
+          //   this.router.navigate([`${environment.locationName}/${this.facilityLocation}/move-in/${localStorage.getItem('paymentTab')}/thank-you`]);
+          // } else {
+
             this.router.navigate([`${environment.locationName}/${this.facilityLocation}/move-in/thank-you`]);
-          }
+          // }
           this.showloaderForPayment = false;
 
         } else {
