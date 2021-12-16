6cd6108e5e14554ed027bd26db0ab64a775a25ec
pragathi <pragathi.n@codeparva.in>
2021-12-17 01:43:39 +0530

    case 27519

--- stor-it-associates/src/environments/environment.ts
+++ stor-it-associates/src/environments/environment.ts
@@ -13,8 +13,8 @@ export const environment = {
   emailBaseUrl: 'https://emailsvc-ext.syrasoft.com/v1/email',
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://storeitwexford.com',
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
-  APIKey: `dzF0MVc0SjgqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` // 11483
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
+  // APIKey: `dzF0MVc0SjgqM046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O` // 11483
 };
 
 /*
