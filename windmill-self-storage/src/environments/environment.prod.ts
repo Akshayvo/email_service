11dec8700ee1c773baa29ec34252a09b506b9ae7
akshayone8 <akshay.o@codeparva.in>
2022-01-04 17:35:03 +0530

    case 27600

--- windmill-self-storage/src/environments/environment.prod.ts
+++ windmill-self-storage/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   facilityName: 'Windmill Self Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'http://windmillselfstorageinc.com',
+  websiteUrl: 'https://windmillselfstorageinc.com',
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
   APIKey: `bTh+OW4weTQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 8904
 
--- windmill-self-storage/src/environments/environment.ts
+++ windmill-self-storage/src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
   facilityName: 'Windmill Self Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'http://windmillselfstorageinc.com',
+  websiteUrl: 'https://windmillselfstorageinc.com',
   // APIKey: `dzJ0OFc0SjYqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29464
   APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
 };
