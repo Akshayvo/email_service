166cb3d298e275313dde36b08ce48f2601a968f0
Darshan-ph <darshan.h@codeparva.in>
2022-01-04 19:35:21 +0530

    case 27602

--- sodus-self-storage-plus/src/app/data/script.ts
+++ sodus-self-storage-plus/src/app/data/script.ts
@@ -41,9 +41,9 @@ export const script = {               // Please fill this script according to fa
      id: environment.websiteUrl || "",
      paymentAccepted:  "Cash, Check, Credit, Debit",
      currenciesAccepted: "USD",
-     ratingValue: "",
+     ratingValue: " ",
      bestRating: "5",
-     reviewCount: "",
+     reviewCount: " ",
      latitude: 43.2377799,
      longitude:-77.2273771,     
      openingHours: [],
@@ -51,89 +51,143 @@ export const script = {               // Please fill this script according to fa
 };
 
 export const homePageScript = [ {
-    "@context": "https://schema.org",
-    "@type": ["Organization","SelfStorage","Place","LocalBusiness"],
-    "image": script.imagesHomePage,
-    "@id": script.id,
-    "name": environment.facilityName,
-     "description": script.description || homePageContent,
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": script.streetAddress,
-      "addressLocality": script.addressLocality,
-      "addressRegion": script.addressRegion,
-      "postalCode": script.postalCode,
-      "addressCountry": script.addressCountry
-    },
-   "aggregateRating": {
-      "@type": "AggregateRating",
-      "ratingValue": script.ratingValue,
-      "bestRating": script.bestRating,
-      "reviewCount": script.reviewCount
-    },
-    "geo": {
-      "@type": "GeoCoordinates",
-      "latitude": script.latitude,
-      "longitude": script.longitude
-    },
-    "url": environment.websiteUrl,
-    "telephone": script.telephone,
-    
-    "priceRange": script.priceRange,
-    "openingHours": script.openingHours,
-    "currenciesAccepted": script.currenciesAccepted,
-    "paymentAccepted": script.paymentAccepted,
-     "areaServed": {
-    "@type": "State",
-    "name": script.state
-   },
-   
-  "hasMap": script.map,
-  "amenityFeature": amenityFeature,
-  "acceptsReservations": "True"
-  }];
+  "@context": "https://schema.org",
+  "@type": ["Organization","SelfStorage","Place","LocalBusiness"],
+  "image": script.imagesHomePage,
+  "@id": script.id,
+  "name": environment.facilityName,
+   "description": script.description || homePageContent,
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": script.streetAddress,
+    "addressLocality": script.addressLocality,
+    "addressRegion": script.addressRegion,
+    "postalCode": script.postalCode,
+    "addressCountry": script.addressCountry
+  },
+ "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": script.ratingValue,
+    "bestRating": script.bestRating,
+    "reviewCount": script.reviewCount
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": script.latitude,
+    "longitude": script.longitude
+  },
+  "url": environment.websiteUrl,
+  "telephone": script.telephone,
+  
+  "priceRange": script.priceRange,
+  "openingHours": script.openingHours,
+  "currenciesAccepted": script.currenciesAccepted,
+  "paymentAccepted": script.paymentAccepted,
+   "areaServed": {
+  "@type": "State",
+  "name": script.state
+ },
+ 
+"hasMap": script.map,
+"amenityFeature": amenityFeature,
+"acceptsReservations": "True"
+}];
 
 
-  export const contactPageScript = [
-    {
-        "@context": "http://schema.org",
-        "@type": "Selfstorage",
-        "name": environment.facilityName,
-        "address": {
-          "@type": "PostalAddress",
-          "addressLocality": script.addressLocality,
-          "postalCode": script.postalCode,
-          "streetAddress": script.streetAddress,
-        },
-         "url": `${environment.websiteUrl}/contact-us`,
-         "image": script.imagesHomePage[0],
-        "contactPoint": [{
-          "@type": "ContactPoint",
-          "telephone": script.telephone,
-          "contactType": "reservations"
-        },{
-          "@type": "ContactPoint",
-          "telephone": script.telephone,
-          "contactType": "customer service"
-        }],
-        "email": contact[emailIndex].data,
-        "sameAs": sameAs
-      }
-  ];
+export const contactPageScript = [
+  {
+      "@context": "http://schema.org",
+      "@type": "Selfstorage",
+      "name": environment.facilityName,
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": script.addressLocality,
+        "postalCode": script.postalCode,
+        "streetAddress": script.streetAddress,
+      },
+       "url": `${environment.websiteUrl}/contact-us`,
+       "image": script.imagesHomePage[0],
+      "contactPoint": [{
+        "@type": "ContactPoint",
+        "telephone": script.telephone,
+        "contactType": "reservations"
+      },{
+        "@type": "ContactPoint",
+        "telephone": script.telephone,
+        "contactType": "customer service"
+      }],
+      "email": contact[emailIndex].data,
+      "sameAs": sameAs
+    }
+];
 
 
 export const ogHomePage = [
+{
+  property: `og:description`,
+  content: `${homePageContent}`
+},
+{
+  property: `og:type`,
+  content: `website`
+},
+{
+  property: `og:title`,
+  content: `${homePageTitle}`
+},
+{
+  property: `og:site_name`,
+  content: `${environment.facilityName}`
+},
+{
+  property: `og:url`,
+  content: `${environment.websiteUrl}`
+},
+{
+  property: `og:image`,
+  content: script.imagesHomePage[1]
+},
+];
+
+export const twitterHomePage = [
+{
+  name: `twitter:site`,
+  content: `@${environment.facilityName}`
+},
+{
+  name: `twitter:card`,
+  content: `summary`
+},
+{
+  name: `twitter:creator`,
+  content: `@syrasoft_connect`
+},
+{
+  name: `twitter:title`,
+  content: `@${homePageTitle}`
+},
+{
+  name: `twitter:description`,
+  content: `${homePageContent}`
+},
+{
+  name: `twitter:image`,
+  content: script.imagesHomePage[0]
+},
+];
+
+export const ogPayRentPage = [
+  {
+    property: `og:title`,
+    content: `${payRentPageTitle}`
+  },
   {
     property: `og:description`,
-    content: `${homePageContent}`
+    content: `${payRentPageContent}`
   },
   {
     property: `og:type`,
     content: `website`
-  },
-  {
-    property: `og:title`,
-    content: `${homePageTitle}`
   },
   {
     property: `og:site_name`,
@@ -141,7 +195,7 @@ export const ogHomePage = [
   },
   {
     property: `og:url`,
-    content: `${environment.websiteUrl}`
+    content: `${environment.websiteUrl}/pay-rent/rent-sub/login`
   },
   {
     property: `og:image`,
@@ -149,7 +203,15 @@ export const ogHomePage = [
   },
 ];
 
-export const twitterHomePage = [
+export const twitterPayRentPage = [
+  {
+    name: `twitter:title`,
+    content: `@${payRentPageTitle}`
+  },
+  {
+    name: `twitter:description`,
+    content: `${payRentPageContent}`
+  },
   {
     name: `twitter:site`,
     content: `@${environment.facilityName}`
@@ -163,12 +225,58 @@ export const twitterHomePage = [
     content: `@syrasoft_connect`
   },
   {
+    name: `twitter:image`,
+    content: script.imagesHomePage[0]
+  },
+];
+
+export const ogStorageTipsPage = [
+  {
+    property: `og:title`,
+    content: `${storageTipsTitle}`
+  },
+  {
+    property: `og:description`,
+    content: `${storageTipsContent}`
+  },
+  {
+    property: `og:type`,
+    content: `website`
+  },
+  {
+    property: `og:site_name`,
+    content: `${environment.facilityName}`
+  },
+  {
+    property: `og:url`,
+    content: `${environment.websiteUrl}/${environment.locationName}/storage-tips`
+  },
+  {
+    property: `og:image`,
+    content: script.imagesHomePage[1]
+  },
+];
+
+export const twitterStorageTipsPage = [
+  {
     name: `twitter:title`,
-    content: `@${homePageTitle}`
+    content: `@${storageTipsTitle}`
   },
   {
     name: `twitter:description`,
-    content: `${homePageContent}`
+    content: `${storageTipsContent}`
+  },
+  {
+    name: `twitter:site`,
+    content: `@${environment.facilityName}`
+  },
+  {
+    name: `twitter:card`,
+    content: `summary`
+  },
+  {
+    name: `twitter:creator`,
+    content: `@syrasoft_connect`
   },
   {
     name: `twitter:image`,
@@ -176,166 +284,58 @@ export const twitterHomePage = [
   },
 ];
 
-export const ogPayRentPage = [
-    {
-      property: `og:title`,
-      content: `${payRentPageTitle}`
-    },
-    {
-      property: `og:description`,
-      content: `${payRentPageContent}`
-    },
-    {
-      property: `og:type`,
-      content: `website`
-    },
-    {
-      property: `og:site_name`,
-      content: `${environment.facilityName}`
-    },
-    {
-      property: `og:url`,
-      content: `${environment.websiteUrl}/pay-rent/rent-sub/login`
-    },
-    {
-      property: `og:image`,
-      content: script.imagesHomePage[1]
-    },
-  ];
-  
-  export const twitterPayRentPage = [
-    {
-      name: `twitter:title`,
-      content: `@${payRentPageTitle}`
-    },
-    {
-      name: `twitter:description`,
-      content: `${payRentPageContent}`
-    },
-    {
-      name: `twitter:site`,
-      content: `@${environment.facilityName}`
-    },
-    {
-      name: `twitter:card`,
-      content: `summary`
-    },
-    {
-      name: `twitter:creator`,
-      content: `@syrasoft_connect`
-    },
-    {
-      name: `twitter:image`,
-      content: script.imagesHomePage[0]
-    },
-  ];
+export const ogContactPage = [
+  {
+    property: `og:title`,
+    content: `${contactPageTitle}`
+  },
+  {
+    property: `og:description`,
+    content: `${contactPageContent}`
+  },
+  {
+    property: `og:type`,
+    content: `website`
+  },
+  {
+    property: `og:site_name`,
+    content: `${environment.facilityName}`
+  },
+  {
+    property: `og:url`,
+    content: `${environment.websiteUrl}/contact-us`
+  },
+  {
+    property: `og:image`,
+    content: script.imagesHomePage[1]
+  },
+];
 
-  export const ogStorageTipsPage = [
-    {
-      property: `og:title`,
-      content: `${storageTipsTitle}`
-    },
-    {
-      property: `og:description`,
-      content: `${storageTipsContent}`
-    },
-    {
-      property: `og:type`,
-      content: `website`
-    },
-    {
-      property: `og:site_name`,
-      content: `${environment.facilityName}`
-    },
-    {
-      property: `og:url`,
-      content: `${environment.websiteUrl}/${environment.locationName}/storage-tips`
-    },
-    {
-      property: `og:image`,
-      content: script.imagesHomePage[1]
-    },
-  ];
-  
-  export const twitterStorageTipsPage = [
-    {
-      name: `twitter:title`,
-      content: `@${storageTipsTitle}`
-    },
-    {
-      name: `twitter:description`,
-      content: `${storageTipsContent}`
-    },
-    {
-      name: `twitter:site`,
-      content: `@${environment.facilityName}`
-    },
-    {
-      name: `twitter:card`,
-      content: `summary`
-    },
-    {
-      name: `twitter:creator`,
-      content: `@syrasoft_connect`
-    },
-    {
-      name: `twitter:image`,
-      content: script.imagesHomePage[0]
-    },
-  ];
-  
-  export const ogContactPage = [
-    {
-      property: `og:title`,
-      content: `${contactPageTitle}`
-    },
-    {
-      property: `og:description`,
-      content: `${contactPageContent}`
-    },
-    {
-      property: `og:type`,
-      content: `website`
-    },
-    {
-      property: `og:site_name`,
-      content: `${environment.facilityName}`
-    },
-    {
-      property: `og:url`,
-      content: `${environment.websiteUrl}/contact-us`
-    },
-    {
-      property: `og:image`,
-      content: script.imagesHomePage[1]
-    },
-  ];
-  
-  export const twitterContactPage = [
-    {
-      name: `twitter:title`,
-      content: `@${contactPageTitle}`
-    },
-    {
-      name: `twitter:description`,
-      content: `${contactPageContent}`
-    },
-    {
-      name: `twitter:site`,
-      content: `@${environment.facilityName}`
-    },
-    {
-      name: `twitter:card`,
-      content: `summary`
-    },
-    {
-      name: `twitter:creator`,
-      content: `@syrasoft_connect`
-    },
-    {
-      name: `twitter:image`,
-      content: script.imagesHomePage[0]
-    },
-  ];
+export const twitterContactPage = [
+  {
+    name: `twitter:title`,
+    content: `@${contactPageTitle}`
+  },
+  {
+    name: `twitter:description`,
+    content: `${contactPageContent}`
+  },
+  {
+    name: `twitter:site`,
+    content: `@${environment.facilityName}`
+  },
+  {
+    name: `twitter:card`,
+    content: `summary`
+  },
+  {
+    name: `twitter:creator`,
+    content: `@syrasoft_connect`
+  },
+  {
+    name: `twitter:image`,
+    content: script.imagesHomePage[0]
+  },
+];
 
   
