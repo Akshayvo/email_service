be6078a7e6085de0a34405c63329b534b1600ac5
akshayone8 <akshay.o@codeparva.in>
2022-01-11 15:15:05 +0530

    case 28837

--- mount-horeb/src/app/data/galleryImage.ts
+++ mount-horeb/src/app/data/galleryImage.ts
@@ -1,9 +1,23 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: ``,
+    url: `mount-horeb-self-storage-111`,
     // tslint:disable-next-line:max-line-length
-    thumb: ``,
-    alt: ``
+    thumb: `mount-horeb-self-storage-111-thumb`,
+    alt: `a single story self storage facility front office with electronic access control gate`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `mount-horeb-self-storage-112`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `mount-horeb-self-storage-112-thumb`,
+    alt: `the interior of a climate controlled self storage building with doors lining both sides of the aisle`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `mount-horeb-self-storage-113`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `mount-horeb-self-storage-113-thumb`,
+    alt: `a self storage facility front office building made of stone and glass`
   },
 ];
