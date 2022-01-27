34b35879fe8d2df31fadce9270b4a65616ad9ae1
Darshan-ph <darshan.h@codeparva.in>
2022-01-27 15:17:25 +0530

    case 28939

--- innovative-storage/src/app/data/galleryImage.ts
+++ innovative-storage/src/app/data/galleryImage.ts
@@ -1,23 +1,23 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: `innovative-storage-11`,
+    url: `innovative-self-storage-1`,
     // tslint:disable-next-line:max-line-length
-    thumb: ``,
-    alt: ``
+    thumb: `innovative-self-storage-1-thumb`,
+    alt: `a wrought iron security gate outside of a self storage facility with a facility manager standing in the foreground`
   },
-  // {
-  //   // tslint:disable-next-line: max-line-length
-  //   url: `IMG_0273`,
-  //   // tslint:disable-next-line:max-line-length
-  //   thumb: `IMG_0273-thumb`,
-  //   alt: ``
-  // },
-  // {
-  //   // tslint:disable-next-line: max-line-length
-  //   url: `IMG_0268`,
-  //   // tslint:disable-next-line:max-line-length
-  //   thumb: `IMG_0268-thumb`,
-  //   alt: ``
-  // }
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `innovative-self-storage-2`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `innovative-self-storage-2-thumb`,
+    alt: `a row of self storage units running the length of the image frame`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `innovative-self-storage-3`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `innovative-self-storage-3-thumb`,
+    alt: `a self storage facility made of single story metal buildings behind an electronic security gate`
+  }
 ];
