2c4f2ff94e15382487c46a9856dd1723b7694031
akshayone8 <akshay.o@codeparva.in>
2022-01-31 19:31:19 +0530

    update case 28967

--- fleming-storage/src/app/data/pay-rent.ts
+++ fleming-storage/src/app/data/pay-rent.ts
@@ -1,10 +1,10 @@
 export const loginDetail = [
     {
-        userName: `Tenant's First Initial + Last Name`,
+        userName: `Tenant's Email Address`,
         password: `Tenant's First Unit Number + Access Code`,
         example: {
             code: "John Smith's First Unit Number is 0123 and Access Code is 1234",
-            userName: 'jsmith',
+            userName: ' email@email.com',
             password: '01231234'
         }
     }
