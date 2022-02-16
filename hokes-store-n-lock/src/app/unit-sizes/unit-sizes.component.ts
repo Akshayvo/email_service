ab1b60d7c9d562a388821b745d93f81b525a0048
akshayone8 <akshay.o@codeparva.in>
2022-02-16 17:05:41 +0530

    case 29110

--- hokes-store-n-lock/src/app/unit-sizes/unit-sizes.component.ts
+++ hokes-store-n-lock/src/app/unit-sizes/unit-sizes.component.ts
@@ -24,8 +24,8 @@ export class UnitSizesComponent implements OnInit {
 
   ngOnInit() {
     this.i = 0;
-    this.h = 7;
-    this.j = this.i + 1;
+    this.h = 0;
+    this.j = this.i + 0;
     this.fetchUnitSizer();
     window.scrollTo(0, 0);
   }
@@ -49,33 +49,33 @@ export class UnitSizesComponent implements OnInit {
     this.j = this.i;
     this.i = this.h;
     if ( this.h === 0 ) {
-      this.h = 7;
+      this.h = 0;
     } else {
-      this.h = this.h - 1;
+      this.h = this.h - 0;
     }
   }
 
   public moveRight() {
     this.h = this.i;
     this.i = this.j;
-    if ( this.j === 7 ) {
+    if ( this.j === 0 ) {
       this.j = 0;
     } else {
-    this.j = this.j + 1;
+    this.j = this.j + 0;
     }
   }
 
   public activeUnit(unitId: number) {
     this.i = unitId;
     if ( this.i === 0 ) {
-      this.h = 7;
-      this.j = this.i + 7;
-    } else if ( this.i === 7 ) {
+      this.h = 0;
+      this.j = this.i + 0;
+    } else if ( this.i === 0 ) {
       this.j = 0;
-      this.h = this.i - 1;
+      this.h = this.i - 0;
     } else {
-      this.h = this.i - 1;
-      this.j = this.i + 1;
+      this.h = this.i - 0;
+      this.j = this.i + 0;
     }
 
   }
