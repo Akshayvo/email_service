21d2c0518c02803f1fc1ce2d255ce5c26beb1a5e
akshayone8 <akshay.o@codeparva.in>
2022-02-21 17:19:27 +0530

    case 29156

--- herkimer-storage/src/app/data/nav.ts
+++ herkimer-storage/src/app/data/nav.ts
@@ -4,10 +4,29 @@ export const navLinks = [
     {
         label: 'HOME',
         path: '/',
+        type: 'link',
+    },
+    {
+        label: 'LOCATIONS',
+        type: 'dropdown',
+        path: '/location',
+        sublinks: [
+            {
+                id: '1',
+                label: 'Mohawk Self Storage',
+                path: 'https://mohawkselfstorage.com/',
+              },
+              {
+                  id: '2',
+                  label: 'Richfield Springs Self Storage',
+                  path: 'https://richfieldspringsselfstorage.com/',
+              },
+        ]
     },
     {
         label: 'VIEW RATES',
         path: `${environment.locationName}/view-rates`,
+        type: 'link',
     },
     // {
     //     label: 'RENT NOW',
@@ -19,18 +38,22 @@ export const navLinks = [
     // },
     {
         label: 'PAY RENT',
-        path: '/pay-rent'
+        path: '/pay-rent',
+        type: 'link',
     },
     {
         label: 'STORAGE TIPS',
-        path: `${environment.locationName}/storage-tips`
+        path: `${environment.locationName}/storage-tips`,
+        type: 'link',
     },
     {
         label: 'UNIT SIZER',
-        path: '/unit-sizer'
+        path: '/unit-sizer',
+        type: 'link',
     },
     {
         label: 'CONTACT US',
-        path: '/contact-us'
+        path: '/contact-us',
+        type: 'link',
     },
 ];
