1066bcef2d1c344e933e456fac7838fe7b289296
Darshan-ph <darshan.h@codeparva.in>
2022-02-22 12:18:11 +0530

    case 29164

--- innovative-storage/src/environments/environment.prod.ts
+++ innovative-storage/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'Innovative/south-carolina', // state/city  **Remeber to update locationName in sitemap.xml as well
+  locationName: 'ohio/marion', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: false }, // { moveIn: true, reserve: true },
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
