7a1995e8127e33c595db83d915263fb63be3fcff
Darshan-ph <darshan.h@codeparva.in>
2022-03-01 17:33:02 +0530

    case 29111

---
+++ mathis-mini/src/app/data/contact.ts
@@ -0,0 +1,53 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Facility Address:',
+        data: '9932 loop, 459, Mathis, TX 78368',
+        class: 'fas fa-map-marker-alt mx-2 iconColor',
+        redirect: 'https://g.page/Mathis-Mini-Storage?share',
+    },
+    {
+        id: 1,
+        label: 'Mailing Address:',
+        data: 'P.O. 260194, Corpus Christi, TX 78426',
+        // class: 'fas fa-map-marker-alt mx-2 iconColor',
+    },
+    {
+        id: 2,
+        redirect: 'mailto:Mathis.ministorage@gmail.com',
+        data: 'Mathis.ministorage@gmail.com',
+        label: 'Email:',
+        class: 'fas fa-envelope mx-2 iconColor',
+    },
+    {
+        id: 3,
+        redirect: 'tel:3615471218',
+        label: 'Phone:',
+        data: '(361) 547-1218',
+        class: 'fas fa-phone mx-2 iconColor',
+    }
+];
+
+export const hours = [
+    {
+        label: 'Monday - Sunday: ',
+        data: '8AM - 7PM'
+    },
+    {
+        label: 'Access Hours: ',
+        data: '24 Hours a Day 7 Days a Week'
+    }
+];
+
+export const socialLinks = [
+    // {
+    //     label: 'Monday - Sunday: ',
+    //     data: '8AM - 7PM'
+    // },
+    // {
+    //     label: 'Access Hours: ',
+    //     data: '24 Hours a Day 7 Days a Week'
+    // },
+];
+
+
---
+++ mathis-mini/src/app/data/title.ts
@@ -0,0 +1,26 @@
+export const homePageTitle = `Self Storage Units in Mathis | Mathis Mini Storage`;
+export const homePageContent = `Simply follow the on-page instructions, or call one of our friendly management professionals to learn how to access your account, today!`;
+
+export const viewRatesPageTitle = `View Rates  | Mathis Mini Storage`;
+export const viewRatesPageContent = `Simply follow the on-page instructions, or call one of our friendly management professionals to learn how to access your account, today!`;
+
+export const reservePageTitle = `Reserve a Unit | Mathis Mini Storage`;
+export const reservePageContent = `Simply fill out our easy reservation form and one of our friendly self storage associates will be ready and waiting for your arrival!`;
+
+export const payRentPageTitle = `Pay Rent  | Mathis Mini Storage`;
+export const payRentPageContent = `Simply follow the on-page instructions, or call one of our friendly management professionals to learn how to access your account, today!`;
+
+export const storageTipsTitle = `Storage Tips  | Mathis Mini Storage`;
+export const storageTipsContent = `Use our helpful self storage tips list to make your stay at Mathis Mini Storage an easy and enjoyable one!`;
+
+export const unitSizerPageTitle = `Unit Sizer  | Mathis Mini Storage`;
+export const unitSizerPageContent = `Are you not sure what size unit you require? Use this guide to help you figure out what fits inside the units offered at Mathis Mini Storage!`;
+
+export const contactPageTitle = `Contact Us  | Mathis Mini Storage`;
+export const contactPageContent = `Use the contact information or the contact form on this page to speak to one of our friendly self storage managers!`;
+
+export const thankYouPageTitle = `Thank You For Reserving Your Unit at Mathis Mini Storage!`;
+export const thankYouPageContent = `We've received your reservation! One of our friendly staff will be in touch!`;
+
+export const privacyPolicyPageTitle = `Privacy Policy | Mathis Mini Storage`;
+export const privacyPolicyPageContent = `This page contains this website's privacy policy. For more information, please contact the business directly.`;
