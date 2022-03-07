d302077bc4e44fb5850e7299f94490c161b4a8e2
akshayone8 <akshay.o@codeparva.in>
2022-03-07 12:51:18 +0530

    case 29237

--- frazer-township-self-storage/src/app/data/galleryImage.ts
+++ frazer-township-self-storage/src/app/data/galleryImage.ts
@@ -1,23 +1,37 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: `frazer-township-self-storage-2`,
+    url: `frazer-township-self-storage-13`,
     // tslint:disable-next-line:max-line-length
-    thumb: `frazer-township-self-storage-2-thumb`,
-    alt: `metal single story self storage unit buildings with green roll up doors on each unit`
+    thumb: `frazer-township-self-storage-13-thumb`,
+    alt: `a single story metal self storage building with green doors under a blue sky`
   },
   {
     // tslint:disable-next-line: max-line-length
-    url: `frazer-township-self-storage-3`,
+    url: `frazer-township-self-storage-14`,
     // tslint:disable-next-line:max-line-length
-    thumb: `frazer-township-self-storage-3-thumb`,
-    alt: `several self storage units in a row under a blue sky`
+    thumb: `frazer-township-self-storage-14-thumb`,
+    alt: `several green metal storage units side by side under a blue sky`
   },
   {
     // tslint:disable-next-line: max-line-length
-    url: `frazer-township-self-storage-4`,
+    url: `frazer-township-self-storage-12`,
     // tslint:disable-next-line:max-line-length
-    thumb: `frazer-township-self-storage-4-thumb`,
-    alt: `a single self storage unit in a single story metal building with a roll up door`
+    thumb: `frazer-township-self-storage-12-thumb`,
+    alt: `several self storage units with green metal roll up doors`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `frazer-township-self-storage-15`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `frazer-township-self-storage-15-thumb`,
+    alt: `storage units in a row with brand new green metal doors`
+  },
+  {
+    // tslint:disable-next-line: max-line-length
+    url: `frazer-township-self-storage-17`,
+    // tslint:disable-next-line:max-line-length
+    thumb: `frazer-township-self-storage-17-thumb`,
+    alt: `the interior of a metal self storage unit`
   },
 ];
