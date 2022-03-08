576e0c483b83da28924c763dc3821d67c031db5d
akshayone8 <akshay.o@codeparva.in>
2022-03-08 12:09:17 +0530

    case 29250

--- added-space-storage/src/app/data/galleryImage.ts
+++ added-space-storage/src/app/data/galleryImage.ts
@@ -1,38 +1,38 @@
 export const galleryHome = [
   {
     // tslint:disable-next-line: max-line-length
-    url: `added-space-storage-10`,
+    url: `added-space-self-storage-20`,
     // tslint:disable-next-line:max-line-length
-    thumb: `added-space-storage-10-thumb`,
+    thumb: `added-space-self-storage-20-thumb`,
     alt: `a row of self storage units stretching off into the distance`
   },
   {
     // tslint:disable-next-line: max-line-length
-    url: `added-space-storage-1`,
+    url: `added-space-self-storage-21`,
     // tslint:disable-next-line:max-line-length
-    thumb: `added-space-storage-1-thumb`,
+    thumb: `added-space-self-storage-21-thumb`,
     alt: `the inside of a large self storage unit with the door rolled up`
   },
   {
     // tslint:disable-next-line: max-line-length
-    url: `added-space-storage-3`,
+    url: `added-space-self-storage-22`,
     // tslint:disable-next-line:max-line-length
-    thumb: `added-space-storage-3-thumb`,
+    thumb: `added-space-self-storage-22-thumb`,
     alt: `a covered recreational vehicle, boat, and trailer parking area`
   },
   
   {
     // tslint:disable-next-line: max-line-length
-    url: `added-space-storage-4`,
+    url: `added-space-self-storage-23`,
     // tslint:disable-next-line:max-line-length
-    thumb: `added-space-storage-4-thumb`,
+    thumb: `added-space-self-storage-23-thumb`,
     alt: `vehicles parked under a covered parking area`
   },
   {
     // tslint:disable-next-line: max-line-length
-    url: `added-space-storage-13`,
+    url: `added-space-self-storage-24`,
     // tslint:disable-next-line:max-line-length
-    thumb: `added-space-storage-13-thumb`,
+    thumb: `added-space-self-storage-24-thumb`,
     alt: `the interior of a self storage facility front office with a friendly manager sitting behind a counter`
   },
 
