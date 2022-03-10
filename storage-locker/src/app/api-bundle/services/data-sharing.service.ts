d235d794a1a336ecc3300e52063f31f373b82265
akshayone8 <akshay.o@codeparva.in>
2022-03-10 19:22:01 +0530

    case 29165

--- storage-locker/src/app/api-bundle/services/data-sharing.service.ts
+++ storage-locker/src/app/api-bundle/services/data-sharing.service.ts
@@ -11,7 +11,7 @@ export class DataSharingService {
   navigateToMoveIn: boolean;
   isValueUpdated: boolean;
   changePassword: boolean;
-
+  insuranceChoiceId: number;
 
   MoveInData = {
     Description: '',
--- storage-locker/src/environments/environment.ts
+++ storage-locker/src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   appId: 'Storage Locker Ltd',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  APIKey: `dzJ0OVcySjkqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29292
+  APIKey: `Mjg0NjU6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29292
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // north demo
 };
