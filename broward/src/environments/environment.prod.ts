e02a20c6f0dc96aa96dcd72179b398eadc8511f5
akshayone8 <akshay.o@codeparva.in>
2022-03-30 13:37:56 +0530

    case 29366

--- broward/src/environments/environment.prod.ts
+++ broward/src/environments/environment.prod.ts
@@ -16,5 +16,5 @@ export const environment = {
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: "https://browardcountyselfstorage.com",
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
-  APIKey: `dzJ0OVc0SjgqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
+  APIKey: `dzJ0OVc0SjgqOU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29489
 };
