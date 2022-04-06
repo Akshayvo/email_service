423fe0700b354e51ab393597bb1825cc93c0497b
Darshan-ph <darshan.h@codeparva.in>
2022-04-06 16:01:56 +0530

    case 29465

--- okc-storage/src/app/data/pay-rent.ts
+++ okc-storage/src/app/data/pay-rent.ts
@@ -35,7 +35,7 @@ export const loginDetail = [
       userName: `Tenant's First Initial + Last Name`,
       password: `Tenant's Full Phone Number`,
       example: {
-          code: `John Smith's Full Phone Number is 555-555-5555`,
+          code: `Full Phone Number is 555-555-5555`,
           userName: 'jsmith',
           password: '5555555555'
       },
