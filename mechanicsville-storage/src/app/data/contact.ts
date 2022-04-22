a42f16307ae019355e7f7d9ca8d141b3baa9e2ef
akshayone8 <akshay.o@codeparva.in>
2022-04-22 16:19:22 +0530

    setup-case-

---
+++ mechanicsville-storage/src/app/data/contact.ts
@@ -0,0 +1,50 @@
+export const contact = [
+    {
+        id: 0,
+        label: 'Address:',
+        data: '1 Industrial Park Dr, Mechanicville, NY 12118',
+        class: 'fas fa-map-marker-alt mx-2 iconColor',
+        redirect: 'https://goo.gl/maps/RTWESYcjDh4959LW7'
+    },
+    {
+        id: 1,
+        label: 'Email:',
+        data: 'syrasoftsoftware@gmail.com',
+        class: 'fas fa-envelope mx-2 iconColor',
+        redirect: 'mailto:syrasoftsoftware@gmail.com'
+    },
+    {
+        id: 2,
+        label: 'Phone:',
+        data: '(518) 466-0700',
+        class: 'fas fa-phone mx-2 iconColor',
+        redirect: 'tel:5184660700',
+    }
+];
+
+export const hours = [
+    {
+        label: 'No Office On Site',
+        data: ''
+    },
+    
+    {
+        label: 'Storage Access Hours: ',
+        data: '24 Hours a Day, 7 Days a Week'
+    }
+];
+
+export const socialLinks = [
+    {
+        label: 'twitter',
+        path: '',
+        class: 'fab fa-twitter-square',
+    },
+    {
+        label: 'facebook',
+        path: '',
+        class: 'fab fa-facebook-square',
+    },
+];
+
+
---
+++ mechanicsville-storage/src/app/data/pay-rent.ts
@@ -0,0 +1,11 @@
+export const loginDetail = [
+    {
+        userName: `Tenant's Email Address`,
+        password: `Tenant's First Unit Number`,
+        example: {
+            code: "John Smith's First Unit Was 101",
+            userName: 'jsmith@email.com',
+            password: '101'
+        }
+    }
+];
