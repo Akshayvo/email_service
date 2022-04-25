0fedf0972ae04453987791ca8b49da4863d7ebd4
akshayone8 <akshay.o@codeparva.in>
2022-04-25 10:42:58 +0530

    case-29639

--- mechanicsville-storage/src/environments/environment.ts
+++ mechanicsville-storage/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'new-york/baldwinsville', // state/city  **Remeber to update locationName in sitemap.xml as well
+  locationName: 'new-york/mechanicville', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 0, // { Yes: 1, No: 0 }
   alternateType: { moveIn: false, reserve: false }, // { moveIn: true, reserve: true },
   military: false, // set true for Military and Driver's Details 
