b47f1e99ffd6d8642534815ff70b6c8a245de69c
akshayone8 <akshay.o@codeparva.in>
2022-04-26 10:59:44 +0530

    case-29651

--- premont-self-storage/src/environments/environment.ts
+++ premont-self-storage/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   type: 0,  // { API: 0, iframe: 1 }
   locationName: 'texas/premont', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
-  alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
+  alternateType: { moveIn: false, reserve: true }, // { moveIn: true, reserve: true },
   military: true, // set true for Military and Driver's Details 
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
   cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
@@ -22,7 +22,7 @@ export const environment = {
 
   // APIKey: `dzJ0OFc0SjYqNE46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 28464
   
-  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
+  APIKey: `Mjg0NjU6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28465
   
 };
 
