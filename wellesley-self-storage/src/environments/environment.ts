3fe3cbecc3dc06a74bcb1ce24fc860b7d0f04f25
Darshan-ph <darshan.h@codeparva.in>
2022-04-28 16:49:42 +0530

    case 29669

--- wellesley-self-storage/src/environments/environment.ts
+++ wellesley-self-storage/src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
   facilityName: 'Wellesley Self Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'https://wellesley-self-storage-staging.netlify.app',
+  websiteUrl: 'https://wellesleystorage.ca',
   APIKey: `dzJ0OVc0SjYqMk46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
   // APIKey: `Mjg0NjU6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
   
