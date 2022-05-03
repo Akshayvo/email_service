4bffe6e65040c0d8fc10fe2ddaa9e86668af22d6
Darshan-ph <darshan.h@codeparva.in>
2022-05-03 15:34:16 +0530

    404 error email code added

--- sample/src/app/error/error.component.ts
+++ sample/src/app/error/error.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { EmailService } from '../services/email.service';
 
 @Component({
   selector: 'app-error',
@@ -7,18 +9,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./error.component.scss']
 })
 export class ErrorComponent implements OnInit {
+  mailSent: boolean;
+  completeMessage: any;
+  currentTimeInSeconds: any;
 
   constructor(
     public router: Router,
+    private emailService: EmailService,
   ) { 
     this.fetchMail()
   }
 
   fetchMail(){
-      console.log("404 error triggered")
+
+    this.currentTimeInSeconds= new Date();
+
+    this.completeMessage = `Domain Name: ${environment.websiteUrl}, <br/>
+     Timestamp: ${this.currentTimeInSeconds}, <br />
+     URL:  ${environment.websiteUrl}${this.router.url}`;
+
+      const body = {
+        name: "404 Error Occured",
+        email: 'Marketing@syrasoft.com',
+        receiveremail: 'darshanhegde5@gmail.com',
+        message: this.completeMessage,
+        subject: "Error - 404",
+      };
+      this.emailService.sendEmail(body).subscribe(
+        (response: any) => {
+          if (response.result != null) {
+            this.mailSent = true;
+          } else {
+            console.log("email not sent")
+          }
+        },
+        (err) => {}
+      );
   }
 
   ngOnInit() {
   }
-
 }
