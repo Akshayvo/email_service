66d1eefd41e86fa6605f5a3710d5db373a4eabc6
sandeepcodeparva <sandeep.a@codeparva.in>
2022-05-10 09:31:58 +0530

    case-29733

--- sandusky/src/app/data/pay-rent.ts
+++ sandusky/src/app/data/pay-rent.ts
@@ -1,17 +1,17 @@
 export const loginDetail = [
     {
         userName: `First initial + Last Name`,
-        password: `5 Digit Unit Number`,
-        instruction: `All unit numbers are stored in our system using 5 digits. If your unit number does not have 5 digits, you will need to use preceding zeroes.`,
+        password: `4 Digit Unit Number`,
+        instruction: `All unit numbers are stored in our system using 4 digits. If your unit number has 3 digits, you will need to use a preceding zero. If your unit number has a letter in the beginning it needs to be capitalized.`,
         example: {
-            code: '1450',
+            code: '450',
             userName: 'jsmith',
-            password: '01450'
+            password: '0450'
         },
         otherExample: {
-            code: '123',
+            code: 'A123',
             userName: 'jsmith',
-            password: '00123'
+            password: 'A123'
         }
     }
 ];
