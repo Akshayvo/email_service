86517108df0e6244e19d214161a648b94b33e9f6
akshayone8 <akshay.o@codeparva.in>
2022-05-17 10:49:15 +0530

    setup case-29764

--- redi-storage/src/environments/environment.prod.ts
+++ redi-storage/src/environments/environment.prod.ts
@@ -19,8 +19,8 @@ export const environment = {
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://www.390selfstorage.com',
-  APIKey: `dzJ0OVcxSjYqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
+  // APIKey: `dzJ0OVcxSjYqNU46MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463
   
 };
 
