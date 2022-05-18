860ee392023cf8ae437e3ac87e98d9b358e6bc32
sandeepcodeparva <sandeep.a@codeparva.in>
2022-05-18 15:33:43 +0530

    case-28872

--- southern-maine/src/app/data/contact.ts
+++ southern-maine/src/app/data/contact.ts
@@ -4,7 +4,7 @@ export const contact = [
         label: 'Address:',
         data: '1030 Portland Rd, Saco, ME 04072',
         class: 'fas fa-map-marker-alt mx-2 iconColor',
-        redirect: 'https://www.google.com/maps/place/1030+Portland+Rd,+Saco,+ME+04072,+USA/data=!4m2!3m1!1s0x4cb2a25685c94037:0x156715ee510b8820?sa=X&ved=2ahUKEwib_YGO2bLvAhVAyjgGHVViCc0Q8gEwAHoECAYQAQ'
+        redirect: 'https://www.google.com/maps?ll=43.558072,-70.402578&z=15&t=m&hl=en&gl=US&mapclient=embed&cid=4204498317009612037'
     },
     {
         id: 1,
