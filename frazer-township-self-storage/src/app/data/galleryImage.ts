7f9bcc1bd7ef848e5d9cc50eb541c3bd207febf9
akshayone8 <akshay.o@codeparva.in>
2022-05-18 11:04:51 +0530

    case-29772

--- frazer-township-self-storage/src/app/data/galleryImage.ts
+++ frazer-township-self-storage/src/app/data/galleryImage.ts
@@ -4,14 +4,14 @@ export const galleryHome = [
     url: `IMG_1768`,
     // tslint:disable-next-line:max-line-length
     thumb: `IMG_1768-thumb`,
-    alt: ``
+    alt: `Frontal View of exterior green units`
   },
   {
     // tslint:disable-next-line: max-line-length
     url: `IMG_2098`,
     // tslint:disable-next-line:max-line-length
     thumb: `IMG_2098-thumb`,
-    alt: ``
+    alt: `Front view of the business sign reading "Frazer Township Self Storage`
   },
  
   {
