bbbd5cfa78a9749489c3cf9ebc2eb529b7f7fb7f
akshayone8 <akshay.o@codeparva.in>
2022-05-27 12:28:56 +0530

    case-29857

--- dakota-self-storage/src/app/data/galleryImage.ts
+++ dakota-self-storage/src/app/data/galleryImage.ts
@@ -13,11 +13,11 @@ export const galleryHome = [
     thumb: `dakota-self-storage-2`,
     alt: `Complete view with surrounding of Dakota Self Storage units`
   },
-  {
-    // tslint:disable-next-line: max-line-length
-    url: `dakota-self-storage-3`,
-    // tslint:disable-next-line:max-line-length
-    thumb: `dakota-self-storage-3`,
-    alt: `a single unit view of dacota self storage`
-  },
+  // {
+  //   // tslint:disable-next-line: max-line-length
+  //   url: `dakota-self-storage-3`,
+  //   // tslint:disable-next-line:max-line-length
+  //   thumb: `dakota-self-storage-3`,
+  //   alt: `a single unit view of dacota self storage`
+  // },
 ];
