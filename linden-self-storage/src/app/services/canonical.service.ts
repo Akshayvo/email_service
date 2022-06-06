7d8b4bfd09c08c5e4f4d83ed08f85d15b8369a58
akshayone8 <akshay.o@codeparva.in>
2022-06-06 16:23:30 +0530

    seo-case

--- linden-self-storage/src/app/services/canonical.service.ts
+++ linden-self-storage/src/app/services/canonical.service.ts
@@ -13,7 +13,7 @@ export class CanonicalService {
 
 create() {
   const { location: {pathname} } = window;
-  if (environment || environment.production) {
+  if (environment) {
 
    const link: HTMLLinkElement= this.dom.querySelector(`link[rel='canonical']`) || null
 
