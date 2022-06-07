af246b54887874e55274f68bbfc377daab19a224
Darshan-ph <darshan.h@codeparva.in>
2022-06-07 16:01:07 +0530

    case 29841

--- jimmis-mini/src/app/data/galleryImage.ts
+++ jimmis-mini/src/app/data/galleryImage.ts
@@ -1,32 +1,47 @@
 export const galleryHome = [
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/climate-controlled-storage.jpg`,
-        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/climate-controlled-storage-thumb1.jpg`,
-        alt: `Climate Controlled Storage Rochester NY`
-    },
-    // {
-    //     url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/self-storage.jpg`,
-    //     thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/self-storage-thumb.jpg`,
-    //     alt: `Storage Units Rochester NY`
-    // },
-    {
-        url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/storage-units.jpg`,
-        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/storage-units-thumb1.jpg`,
-        alt: `Self Storage Rochester NY`
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/Jimmys1.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/Jimmys1-thumb.jpg`,
+        alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/self-storage-unit-corner.jpg`,
-        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/self-storage-unit-corner-thumb.jpg`,
-        alt: `Storage Facilities`
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys2.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys2-thumb.jpg`,
+        alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/woman-behind-counter.jpg`,
-        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/woman-behind-counter-thumb.jpg`,
-        alt: `Secure Storage`
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys3.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/Jimmys3-thumb.jpg`,
+        alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-security-camera.jpg`,
-        thumb: `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/390_Self_Storage/390-self-storage-security-camera-thumb.jpg`,
-        alt: `a security camera mounted on the side of a metal self storage building`
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys4.jpg.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys4-thumb.jpg`,
+        alt: ``
+    },
+    {
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys5.jpg.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys5-thumb.jpg`,
+        alt: ``
+    },
+    {
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys6.jpg.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys6-thumb.jpg`,
+        alt: ``
+    },
+    {
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys7.jpg.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys7-thumb.jpg`,
+        alt: ``
+    },
+    {
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys43.jpg.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys43-thumb.jpg`,
+        alt: ``
+    },
+    {
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys169.jpg.jpg`,
+        thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys169-thumb.jpg`,
+        alt: ``
     },
 ];
--- jimmis-mini/src/app/services/ua-parser.service.ts
+++ jimmis-mini/src/app/services/ua-parser.service.ts
@@ -31,9 +31,9 @@ export class UaParserService {
       }
     }
     if (this.typeOfImages === 'WEBP') {
-      this.baseUrl = `https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Sample_Self_Storage/webp`;
+      this.baseUrl = `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Webp`;
     } else {
-      this.baseUrl = 'https://syrasoft-tenant-facing-websites.s3.amazonaws.com/Sample_Self_Storage/jpg';
+      this.baseUrl = 'https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg';
     }
   }
 }
