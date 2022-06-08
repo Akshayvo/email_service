064620adf7a2dd73de887c1072f66c01b3950155
Darshan-ph <darshan.h@codeparva.in>
2022-06-08 16:01:02 +0530

    case 29305

--- jimmis-mini/src/app/data/galleryImage.ts
+++ jimmis-mini/src/app/data/galleryImage.ts
@@ -10,37 +10,37 @@ export const galleryHome = [
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys3.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/Jimmys3.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/Jimmys3-thumb.jpg`,
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys4.jpg.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys4.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys4-thumb.jpg`,
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys5.jpg.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys5.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys5-thumb.jpg`,
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys6.jpg.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys6.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys6-thumb.jpg`,
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys7.jpg.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys7.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys7-thumb.jpg`,
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys43.jpg.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys43.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys43-thumb.jpg`,
         alt: ``
     },
     {
-        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys169.jpg.jpg`,
+        url: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys169.jpg`,
         thumb: `https://syrasoft-tenant-facing-websites.s3.us-east-1.amazonaws.com/Jimmis-Mini/Jpg/jimmys169-thumb.jpg`,
         alt: ``
     },
