733be65387b4c283ab58e343b1e0ed1fb4d26cf3
Darshan-ph <darshan.h@codeparva.in>
2022-06-15 13:03:56 +0530

    redirection added

--- richmond/src/environments/environment.ts
+++ richmond/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'new-york/baldwinsville', // state/city  **Remeber to update locationName in sitemap.xml as well
+  locationName: 'vermont/richmond', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
   military: true, // set true for Military and Driver's Details 
