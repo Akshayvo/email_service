84b78044c27708dd0793ce4ae3bffc09c71727ea
Darshan-ph <darshan.h@codeparva.in>
2022-06-16 13:49:52 +0530

    redirection added

--- route-9-boat/src/environments/environment.ts
+++ route-9-boat/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: true,
   type: 0,  // { API: 0, iframe: 1 }
-  locationName: 'new-york/baldwinsville', // state/city  **Remeber to update locationName in sitemap.xml as well
-  signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
+  locationName: 'new-york/clifton-park', // state/city  **Remeber to update locationName in sitemap.xml as well
+  signUpForAuotoPay: 0, // { Yes: 1, No: 0 }
   alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
   military: true, // set true for Military and Driver's Details 
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
