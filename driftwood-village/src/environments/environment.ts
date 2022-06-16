857c0447cae94f0cf67cdd9be5d114b76296eac9
sandeepcodeparva <sandeep.a@codeparva.in>
2022-06-16 16:35:54 +0530

    case-29177

--- driftwood-village/src/environments/environment.ts
+++ driftwood-village/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   type: 0,  // { API: 0, iframe: 1 }
   locationName: 'california/auburn', // state/city  **Remeber to update locationName in sitemap.xml as well
   signUpForAuotoPay: 1, // { Yes: 1, No: 0 }
-  alternateType: { moveIn: true, reserve: true }, // { moveIn: true, reserve: true },
-  military: true, // set true for Military and Driver's Details 
+  alternateType: { moveIn: false, reserve: true }, // { moveIn: true, reserve: true },
+  military: false, // set true for Military and Driver's Details 
   template: 'darkTheme', // { darkTheme, lightTheme, withoutJumbotron}
   cards: ['Visa', 'Master Card', 'Amex', 'Discover', 'test'],
   errorsMails: [ 'sanghmitra.v@codeparva.in' ],
