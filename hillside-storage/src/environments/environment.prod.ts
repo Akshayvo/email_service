cf0071086432b5c2f9c02e05eba7e160f773e9fc
akshayone8 <akshay.o@codeparva.in>
2022-06-20 14:48:51 +0530

    seo-case-20584

--- hillside-storage/src/environments/environment.prod.ts
+++ hillside-storage/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   facilityName: 'Hillside Self Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'http://www.hillsideselfstoragellc.com',
+  websiteUrl: 'https://hillsideselfstoragellc.com',
   // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north
   APIKey: `dzJ0OVcySjMqN046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
 
--- hillside-storage/src/environments/environment.ts
+++ hillside-storage/src/environments/environment.ts
@@ -18,9 +18,9 @@ export const environment = {
   facilityName: 'Hillside Self Storage',
   VERSION: require('../../package.json').version,
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
-  websiteUrl: 'http://www.hillsideselfstoragellc.com',
-  APIKey: `dzJ0OVcySjMqN046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462
-  // APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463  
+  websiteUrl: 'https://hillsideselfstoragellc.com',
+  // APIKey: `dzJ0OVcySjMqN046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29462 
+  APIKey: `Mjg0NjM6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28463   
   
 };
 
