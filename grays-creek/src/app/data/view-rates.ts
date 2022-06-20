106b7f70a51dd9dffe62e2c7bb3c9762cbd2af43
akshayone8 <akshay.o@codeparva.in>
2022-06-20 16:12:24 +0530

    case-29619

--- grays-creek/src/app/data/view-rates.ts
+++ grays-creek/src/app/data/view-rates.ts
@@ -89,15 +89,15 @@ export const th = [
     data: 'Reservation Deposit',
     state: objSIMSetting.objUnitSizesSetting.blnShowDeposit
   },
-  // {  id: 'reserve',
-  //   data: `Reserve a Unit`,
-  //   state: objSIMSetting.objActionSetting.blnAllowReservation
-  // },
-  // {
-  //   id: 'move-in',
-  //   data: `Move In`,
-  //   state: objSIMSetting.objActionSetting.blnAllowMoveIn
-  // },
+  {  id: 'reserve',
+    data: `Reserve a Unit`,
+    state: objSIMSetting.objActionSetting.blnAllowReservation
+  },
+  {
+    id: 'move-in',
+    data: `Move In`,
+    state: objSIMSetting.objActionSetting.blnAllowMoveIn
+  },
   {
     id: 'climate-control',
     data: `Climate Control`,
