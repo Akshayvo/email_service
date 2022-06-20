c98766acec80cfa2bcb843f6afcd1b08ebd2c122
akshayone8 <akshay.o@codeparva.in>
2022-06-20 16:57:41 +0530

    update-case

--- mechanicville-storage/src/environments/environment.ts
+++ mechanicville-storage/src/environments/environment.ts
@@ -20,7 +20,7 @@ export const environment = {
   baseUrl: `https://simapi.syrasoft.com/1/api/`,
   websiteUrl: 'https://selfstorageofmechanicville.com',
   APIKey: `dzJ0OVc2SjAqN046MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // 29607  
-  // APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464
+  // APIKey: `Mjg0NjQ6MjJ8JTg3ODc3JCg4PWJXMHNoZGQ5a2VvY15O`, // demo north 28464 
   
 };
 
