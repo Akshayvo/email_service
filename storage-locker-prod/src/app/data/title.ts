308f4bfd9e96c4bd7d32e424a2bbc6544aabe2e7
sandeepcodeparva <sandeep.a@codeparva.in>
2022-06-23 13:21:05 +0530

    case redesign - 28955

--- storage-locker-prod/src/app/data/title.ts
+++ storage-locker-prod/src/app/data/title.ts
@@ -1,21 +1,21 @@
-export const  homePageTitle = `Climate-Controlled Storage Units Hendersonville | Storage Locker Ltd`;
+export const  homePageTitle = `Climate-Controlled Storage Units | Storage Locker Ltd Baraboo, WI`;
 export const homePageContent = `Are you in search of affordable climate-controlled self storage units near Hendersonville,
-NC? Storage Locker Ltd has a wide variety of sizes and excellent customer service!`;
+NC? Storage Locker Ltd Baraboo has a wide variety of sizes and excellent customer service!`;
 
 
-export const viewRatesPageTitle = `Affordable Climate Controlled Units Hendersonville | Storage Locker Ltd`;
+export const viewRatesPageTitle = `Affordable Climate Controlled Units Hendersonville | Storage Locker Ltd Baraboo, WI`;
 export const viewRatesPageContent = `Take a moment to view our affordable self storage unit rates, then make your
 reservation by filling out our form or calling our office today!`;
 
 
-export const reservePageTitle = `Reserve Your Unit  | Storage Locker Ltd`;
+export const reservePageTitle = `Reserve Your Unit  | Storage Locker Ltd Baraboo, WI`;
 export const reservePageContent = `Fill out our reservation form and we'll be in touch with all of the information you need to move into your new self storage unit!`;
 
-export const payRentPageTitle = `Pay Rent  | Storage Locker Ltd`;
+export const payRentPageTitle = `Pay Rent  | Storage Locker Ltd Baraboo, WI`;
 export const payRentPageContent = `Pay your rent online in seconds, or sign up for autopay! Simply follow the instructions to access your account with us!`;
 
 
-export const storageTipsTitle = `Storage Tips  | Storage Locker Ltd`;
+export const storageTipsTitle = `Storage Tips  | Storage Locker Ltd Baraboo, WI`;
 export const storageTipsContent = `Follow these easy self storage tips to make your stay with Storage Locker Ltd a breeze! Have questions? Call (828) 273-8879, today!`;
 
 
@@ -23,11 +23,11 @@ export const unitSizerPageTitle = ``;
 export const unitSizerPageContent = ``;
 
 
-export const contactPageTitle = `Contact Us  | Storage Locker Ltd`;
+export const contactPageTitle = `Contact Us  | Storage Locker Ltd Baraboo, WI`;
 export const contactPageContent = `Use the contact information or the contact form on this page to speak to one of our friendly self storage managers!`;
 
-export const thankYouPageTitle = `Thank You For Reserving Your Unit at Storage Locker Ltd`;
+export const thankYouPageTitle = `Thank You For Reserving Your Unit at Storage Locker Ltd Baraboo, WI`;
 export const thankYouPageContent = `We've received your reservation! One of our friendly staff will be in touch!`;
 
-export const privacyPolicyPageTitle = `Privacy Policy | Storage Locker Ltd`;
+export const privacyPolicyPageTitle = `Privacy Policy | Storage Locker Ltd Baraboo, WI`;
 export const privacyPolicyPageContent = `This page contains this website's privacy policy. For more information, please contact the business directly.`;
